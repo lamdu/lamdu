f70b129394d57acdba17f7c213e1ffd4a276bd47
Eyal Lotem <eyal.lotem@gmail.com>
2011-11-10 00:35:52 +0200

    Clean up GUIWidgets

--- src/GUIWidgets.hs
+++ src/GUIWidgets.hs
@@ -8,10 +8,13 @@ import Data.IORef
 import Data.List.Utils (enumerate2d, nth)
 import Data.Maybe
 import Data.Vector.Vector2(Vector2(..))
+import SizeRange (Size)
 import EventMap
 import Graphics.DrawingCombinators((%%))
+import Graphics.DrawingCombinators.Utils (Image)
 import Graphics.UI.GLFW
-import SizeRange (Size)
+import KeyHandlers(modifiersEventHandlerWrap)
+import Typematic(typematicKeyHandlerWrap)
 import Widget(Widget(..))
 import qualified FocusDelegator
 import qualified Graphics.DrawingCombinators as Draw
@@ -19,8 +22,6 @@ import qualified GridEdit
 import qualified System.Info
 import qualified TextEdit
 import qualified Widget
-import Typematic(typematicKeyHandlerWrap)
-import KeyHandlers(modifiersEventHandlerWrap)
 
 type Model = ([[(FocusDelegator.Model, TextEdit.Model)]],
               GridEdit.Model)
@@ -29,16 +30,17 @@ defaultFont :: String -> FilePath
 defaultFont "darwin" = "/Library/Fonts/Arial.ttf"
 defaultFont _ = "/usr/share/fonts/truetype/freefont/FreeSerifBold.ttf"
 
-main :: IO ()
-main = GLFWWrap.withGLFW $ do
-  font <- Draw.openFont (defaultFont System.Info.os)
+mainLoop :: (Size -> Event -> IO ()) -> (Size -> IO Image) -> IO a
+mainLoop eventHandler makeImage = GLFWWrap.withGLFW $ do
   GLFWWrap.openWindow defaultDisplayOptions
 
-  modelVar <- newIORef (replicate 3 . replicate 3 $ (False, TextEdit.Model 4 "Text"),
-                        Vector2 0 0)
-                        --TextEdit.Model 0 "Text"]
+  let
+    windowSize = do
+      (x, y) <- getWindowDimensions
+      return $ Vector2 (fromIntegral x) (fromIntegral y)
 
-  modifiersHandler <- modifiersEventHandlerWrap (modifyIORef modelVar . updateModel font)
+    eventHandlerWithSize event = (`eventHandler` event) =<< windowSize
+  modifiersHandler <- modifiersEventHandlerWrap eventHandlerWithSize
 
   let
     keyHandler key isPress = modifiersHandler $ GLFWWrap.KeyEvent key isPress
@@ -48,18 +50,29 @@ main = GLFWWrap.withGLFW $ do
 
   let
     handleEvent (GLFWWrap.KeyEvent key isPress) = typematicKeyHandler key isPress
-    handleEvent GLFWWrap.WindowClose = error "Quit"
+    handleEvent GLFWWrap.WindowClose = error "Quit" -- TODO: Make close event
     handleEvent x = modifiersHandler x
 
   GLFWWrap.eventLoop $ \events -> do
+    winSize@(Vector2 winSizeX winSizeY) <- windowSize
     mapM_ handleEvent events
     Draw.clearRender .
-      (Draw.scale (20/800) (-20/600) %%) .
-      ($ fullSize) . ($ True) . Widget.image . widget font =<<
-      readIORef modelVar
+      (Draw.scale (20/winSizeX) (-20/winSizeY) %%) =<<
+      makeImage winSize
 
-fullSize :: Size
-fullSize = Vector2 800 600
+main :: IO ()
+main = do
+  font <- Draw.openFont (defaultFont System.Info.os)
+  modelVar <- newIORef (replicate 3 . replicate 3 $ (False, TextEdit.Model 4 "Text"),
+                        Vector2 0 0)
+  let
+    draw size = do
+      model <- readIORef modelVar
+      return $ Widget.image (widget font model) True size
+
+    eventHandler size = modifyIORef modelVar . updateModel font size
+
+  mainLoop eventHandler draw
 
 widget :: Draw.Font -> Model -> Widget Model
 widget font origModel@(rowModels, gModel) =
@@ -76,7 +89,7 @@ widget font origModel@(rowModels, gModel) =
     liftRowModel (rowIndex, colIndex) editCell =
       (first . nth rowIndex . nth colIndex) editCell origModel
 
-updateModel :: Draw.Font -> Event -> Model -> Model
-updateModel font event model =
+updateModel :: Draw.Font -> Size -> Event -> Model -> Model
+updateModel font size event model =
   fromMaybe model $
-  lookup event =<< Widget.eventMap (widget font model) True fullSize
+  lookup event =<< Widget.eventMap (widget font model) True size
