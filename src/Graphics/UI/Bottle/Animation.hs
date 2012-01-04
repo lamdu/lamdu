80dd1a16f85335fb470facb08bed6235613f4b99
Eyal Lotem <eyal.lotem@gmail.com>
2012-01-04 20:12:42 +0200

    Towards Animation 2

---
+++ src/Graphics/UI/Bottle/Animation.hs
@@ -0,0 +1,81 @@
+module Graphics.UI.Bottle.Animation(
+  AnimId, Rect(..), PositionedImage(..), Frame(..),
+  draw, nextFrame, augmentId, simpleFrame)
+where
+
+import Data.ByteString.Char8() -- IsString instance
+import Data.Map(Map)
+import Data.Monoid(Monoid(..))
+import Data.Vector.Vector2 (Vector2(..))
+import Graphics.DrawingCombinators((%%))
+import qualified Data.ByteString as SBS
+import qualified Data.Map as Map
+import qualified Data.Vector.Vector2 as Vector2
+import qualified Graphics.DrawingCombinators as Draw
+
+type AnimId = [SBS.ByteString]
+
+data Rect = Rect {
+  rectTopLeft :: Vector2 Draw.R,
+  rectSize :: Vector2 Draw.R
+  }
+
+data PositionedImage = PositionedImage {
+  piImage :: Draw.Image (), -- Image always occupies (0,0)..(1,1), the translation/scaling occurs when drawing
+  piRect :: Rect
+  }
+
+data Frame = Frame {
+  iSubImages :: Map AnimId PositionedImage
+  }
+
+simpleFrame :: Vector2 Draw.R -> Draw.Image () -> Frame
+simpleFrame size image =
+  Frame $ Map.singleton mempty $ PositionedImage image (Rect 0 size)
+
+instance Monoid Frame where
+  mempty = Frame mempty
+  mappend (Frame x) (Frame y) =
+    Frame $
+    Map.unionWith (error "Attempt to unify same-id sub-images!") x y
+
+draw :: Frame -> Draw.Image ()
+draw = mconcat . map posImage . Map.elems . iSubImages
+  where
+    posImage (PositionedImage img (Rect { rectTopLeft = Vector2 t l, rectSize = Vector2 w h })) =
+      Draw.translate (t, l) %% Draw.scale w h %% img
+
+center :: Rect -> Vector2 Draw.R
+center (Rect tl size) = tl + size / 2
+
+sqrNorm :: Num a => Vector2 a -> a
+sqrNorm = Vector2.vector2 (+) . (^ (2::Int))
+
+animSpeed :: Fractional a => a
+animSpeed = 0.2
+
+nextFrame :: Frame -> Frame -> Frame
+nextFrame (Frame dest) (Frame cur) =
+  Frame . Map.mapMaybe id $
+  mconcat [
+    fmap add $ Map.difference dest cur,
+    fmap del $ Map.difference cur dest,
+    Map.intersectionWith modify dest cur
+  ]
+  where
+    add (PositionedImage img r) =
+      Just . PositionedImage img $ Rect (center r) 0
+    del (PositionedImage img (Rect pos size))
+      | sqrNorm size < 1 = Nothing
+      | otherwise = Just $ PositionedImage img (Rect (pos + size/2 * animSpeed) (size * (1 - animSpeed)))
+    modify
+      (PositionedImage destImg (Rect destTopLeft destSize))
+      (PositionedImage _ (Rect curTopLeft curSize)) =
+      Just $
+        PositionedImage destImg
+        (Rect
+          (animSpeed * destTopLeft + (1 - animSpeed) * curTopLeft)
+          (animSpeed * destSize + (1 - animSpeed) * curSize))
+
+augmentId :: AnimId -> Frame -> Frame
+augmentId prefix (Frame frame) = Frame $ Map.mapKeys (mappend prefix) frame
