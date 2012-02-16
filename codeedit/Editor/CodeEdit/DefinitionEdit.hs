a0ba0aca3fa6b99559c2cfa9d2e67c7d0f91a703
Eyal Lotem <eyal.lotem@gmail.com>
2012-02-16 19:36:15 +0200

    Del on params moves to sane position

--- codeedit/Editor/CodeEdit/DefinitionEdit.hs
+++ codeedit/Editor/CodeEdit/DefinitionEdit.hs
@@ -28,28 +28,34 @@ make definitionI = do
     BWidgets.makeNameEdit Config.unnamedStr definitionI nameEditAnimId
   equals <- BWidgets.makeTextView "=" $ Widget.joinId myId ["equals"]
   expressionEdit <- ExpressionEdit.make [] definitionI bodyRef
-  paramsEdits <- mapM makeParamEdit $ enumerate params
 
   let
     replaceEventMap =
       Widget.actionEventMapMovesCursor Config.delKeys "Replace" .
       ETypes.diveIn $ DataOps.replace bodyRef
-  return .
-    Widget.weakerEvents eventMap . BWidgets.hboxSpaced $
-    [nameEdit] ++ paramsEdits ++ [equals, Widget.weakerEvents replaceEventMap expressionEdit]
-  where
+    paramEventMap i paramI =
+      Widget.actionEventMapMovesCursor Config.delKeys
+      "Delete parameter" $ do
+        DataOps.delParameter definitionRef paramI
+        Data.Definition newParams _ <- Property.get definitionRef
+        return $
+          (myId : map WidgetIds.fromIRef newParams)
+          !! min (1+i) (length newParams)
     makeParamEdit (i, paramI) =
-      (liftM . Widget.weakerEvents) (paramEventMap paramI) .
+      (liftM . Widget.weakerEvents) (paramEventMap i paramI) .
       BWidgets.wrapDelegated FocusDelegator.NotDelegating
       (BWidgets.setTextColor Config.parameterColor .
        BWidgets.makeNameEdit ("<unnamed param " ++ show i ++ ">") paramI) $
       WidgetIds.fromIRef paramI
+
+  paramsEdits <- mapM makeParamEdit $ enumerate params
+
+  return .
+    Widget.weakerEvents eventMap . BWidgets.hboxSpaced $
+    [nameEdit] ++ paramsEdits ++ [equals, Widget.weakerEvents replaceEventMap expressionEdit]
+  where
     bodyRef = Property.composeLabel Data.defBody Data.atDefBody definitionRef
     definitionRef = Transaction.fromIRef definitionI
-    paramEventMap paramI =
-      Widget.actionEventMapMovesCursor Config.delKeys "Delete parameter" .
-      (liftM . const) myId $
-      DataOps.delParameter definitionRef paramI
     eventMap =
       Widget.actionEventMapMovesCursor Config.addParamKeys "Add parameter" .
       liftM (WidgetIds.delegating . WidgetIds.fromIRef) $
