4ad87ef864b7a8b56c81b9ff30143a31a1f6489a
Eyal Lotem <eyal.lotem@gmail.com>
2012-03-08 00:39:12 +0200

    Allow removal of lambda wrapper (del of arg)

--- codeedit/Editor/CodeEdit/ExpressionEdit/LambdaEdit.hs
+++ codeedit/Editor/CodeEdit/ExpressionEdit/LambdaEdit.hs
@@ -1,17 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Editor.CodeEdit.ExpressionEdit.LambdaEdit(make) where
 
-import Editor.CodeEdit.Types(AncestryItem(..), LambdaParent(..))
+import Control.Monad(liftM)
 import Data.Store.Property (Property(Property))
 import Editor.Anchors (ViewTag)
 import Editor.CTransaction (TWidget, getP, assignCursor)
+import Editor.CodeEdit.Types(AncestryItem(..), LambdaParent(..))
 import Editor.MonadF (MonadF)
 import qualified Data.Store.Property as Property
 import qualified Data.Store.Transaction as Transaction
 import qualified Editor.BottleWidgets as BWidgets
 import qualified Editor.CodeEdit.ParamEdit as ParamEdit
 import qualified Editor.CodeEdit.Types as ETypes
+import qualified Editor.Config as Config
 import qualified Editor.Data as Data
+import qualified Editor.DataOps as DataOps
 import qualified Editor.WidgetIds as WidgetIds
 import qualified Graphics.UI.Bottle.Widget as Widget
 
@@ -35,13 +38,17 @@ make makeExpressionEdit ancestry expressionPtr lambda myId =
       bodyIPtr =
         Property (return bodyI) $ Property.set expressionRef . Data.ExpressionLambda . (paramI `Data.Lambda`)
       ancestryItem = AncestryItemLambda $ LambdaParent lambda expressionPtr
+      paramEditEventMap =
+        Widget.actionEventMapMovesCursor Config.delKeys "Remove lambda wrapper" $
+        setExpr bodyI
     bodyEdit <- makeExpressionEdit (ancestryItem : ancestry) bodyIPtr
     return $ BWidgets.hbox [
       lambdaLabel,
-      paramEdit,
+      Widget.weakerEvents paramEditEventMap paramEdit,
       rightArrowLabel,
       bodyEdit
       ]
   where
+    setExpr = liftM WidgetIds.fromIRef . DataOps.replace expressionPtr
     paramI = Data.lambdaParam lambda
     bodyI = Data.lambdaBody lambda
