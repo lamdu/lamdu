f5780c767d3af63594b639b5594db78332cf9b57
Eyal Lotem <eyal.lotem@gmail.com>
2012-04-17 15:34:15 +0300

    Remove controversial folding of where items

--- codeedit/Editor/CodeEdit/ExpressionEdit/WhereEdit.hs
+++ codeedit/Editor/CodeEdit/ExpressionEdit/WhereEdit.hs
@@ -2,15 +2,10 @@
 module Editor.CodeEdit.ExpressionEdit.WhereEdit(make) where
 
 import Control.Monad (liftM)
-import Data.Store.IRef (IRef)
-import Data.Store.Property(Property)
-import Data.Store.Transaction (Transaction)
 import Editor.Anchors (ViewTag)
-import Editor.CTransaction (CTransaction, TWidget, getP, atTextSizeColor, assignCursor)
+import Editor.CTransaction (TWidget, getP, atTextSizeColor, assignCursor)
 import Editor.MonadF (MonadF)
-import Graphics.UI.Bottle.Widget(Widget)
 import qualified Data.Store.Property as Property
-import qualified Editor.Anchors as Anchors
 import qualified Editor.BottleWidgets as BWidgets
 import qualified Editor.CodeEdit.DefinitionEdit as DefinitionEdit
 import qualified Editor.CodeEdit.Sugar as Sugar
@@ -20,37 +15,6 @@ import qualified Editor.WidgetIds as WidgetIds
 import qualified Graphics.UI.Bottle.Widget as Widget
 import qualified Graphics.UI.Bottle.Widgets.Grid as Grid
 
-handleFold
-  :: MonadF m
-  => Bool
-  -> Property (Transaction ViewTag m) Bool
-  -> TWidget ViewTag m
-  -> IRef a
-  -> Widget.Id
-  -> CTransaction ViewTag m
-     (Widget (Transaction ViewTag m),
-      Widget.EventHandlers (Transaction ViewTag m),
-      Widget.EventHandlers (Transaction ViewTag m))
-handleFold False foldedRef makeWheres bodyI _ = do
-  wheres <- makeWheres
-  let
-    eventMapOnBody =
-      Widget.actionEventMap Config.foldKeys "Fold where" $
-      Property.set foldedRef True
-    eventMapOnItems =
-      Widget.actionEventMapMovesCursor Config.foldKeys "Fold where" $ do
-        Property.set foldedRef True
-        return $ WidgetIds.fromIRef bodyI
-  return (wheres, eventMapOnBody, eventMapOnItems)
-
-handleFold True foldedRef _ _ myId = do
-  whereEdits <- BWidgets.makeLabel "[...]" myId
-  let
-    eventMap =
-      Widget.actionEventMap Config.unfoldKeys "Unfold where" $
-      Property.set foldedRef False
-  return (whereEdits, eventMap, eventMap)
-
 make
   :: MonadF m
   => ETypes.ExpressionEditMaker m
@@ -59,21 +23,17 @@ make
   -> Widget.Id -> TWidget ViewTag m
 make makeExpressionEdit ancestry w@(Sugar.Where items bodyPtr) myId = do
   bodyI <- getP bodyPtr
-  let foldedRef = Anchors.aDataRef "folded" False bodyI -- TODO: bodyI is a bad place to attach this!
   assignCursor myId (WidgetIds.fromIRef bodyI) $ do
     whereLabel <-
       atTextSizeColor Config.whereTextSize Config.whereColor $
       BWidgets.makeLabel "where" myId
     bodyEdit <- makeExpressionEdit bodyAncestry bodyPtr
-    folded <- getP foldedRef
-    (whereEdits, eventMapOnBody, eventMapOnItems) <-
-      handleFold folded foldedRef makeWhereItemsGrid bodyI myId
-    return .
-      BWidgets.vbox $ Widget.weakerEvents eventMapOnBody bodyEdit :
-        (map . Widget.weakerEvents) eventMapOnItems
-        [ whereLabel
-        , Widget.scale Config.whereScaleFactor whereEdits
-        ]
+    whereEdits <- makeWhereItemsGrid
+    return . BWidgets.vbox $
+      [ bodyEdit
+      , whereLabel
+      , Widget.scale Config.whereScaleFactor whereEdits
+      ]
   where
     makeWhereItemsGrid = liftM (Grid.toWidget . Grid.make) $ mapM makeWhereItemEdits items
     makeAncestry role = ETypes.AncestryItemWhere (ETypes.WhereParent w role) : ancestry
