e7bb2ebf933c54e5dc56856ce6e177b2cdb1a014
Eyal Lotem <eyal.lotem@gmail.com>
2012-05-08 15:53:53 +0300

    ApplyEdit doesn't do infix shenanigans

--- codeedit/Editor/CodeEdit/ExpressionEdit/ApplyEdit.hs
+++ codeedit/Editor/CodeEdit/ExpressionEdit/ApplyEdit.hs
@@ -2,11 +2,10 @@
 module Editor.CodeEdit.ExpressionEdit.ApplyEdit(make) where
 
 import Editor.Anchors (ViewTag)
-import Editor.CTransaction (TWidget, getP, assignCursor, transaction)
+import Editor.CTransaction (TWidget, getP, assignCursor)
 import Editor.CodeEdit.ExpressionEdit.ExpressionMaker (ExpressionEditMaker)
 import Editor.MonadF (MonadF)
 import qualified Editor.BottleWidgets as BWidgets
-import qualified Editor.CodeEdit.Infix as Infix
 import qualified Editor.CodeEdit.Sugar as Sugar
 import qualified Editor.WidgetIds as WidgetIds
 import qualified Graphics.UI.Bottle.Widget as Widget
@@ -20,11 +19,6 @@ make
 make makeExpressionEdit (Sugar.Apply func arg) myId = do
   argI <- getP $ Sugar.rExpressionPtr arg
   assignCursor myId (WidgetIds.fromIRef argI) $ do
-    funcI <- getP $ Sugar.rExpressionPtr func
-    -- TODO: This will come from sugar
-    isInfix <- transaction $ Infix.isInfixFunc funcI
     funcEdit <- makeExpressionEdit func
     argEdit <- makeExpressionEdit arg
-    return . BWidgets.hbox $
-      (if isInfix then reverse else id)
-      [funcEdit, BWidgets.spaceWidget, argEdit]
+    return $ BWidgets.hboxSpaced [funcEdit, argEdit]
