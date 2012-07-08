559d6ab970e8d5a68ff0c3d11c25daac0ecd44df
Eyal Lotem <eyal.lotem@gmail.com>
2012-07-08 19:51:34 +0300

    Underlines of correct width

--- codeedit/Editor/CodeEdit/InferredTypes.hs
+++ codeedit/Editor/CodeEdit/InferredTypes.hs
@@ -3,6 +3,7 @@ module Editor.CodeEdit.InferredTypes(addType) where
 
 import Data.Vector.Vector2 (Vector2(..))
 import Graphics.UI.Bottle.Widget (Widget)
+import qualified Data.Vector.Vector2 as Vector2
 import qualified Editor.BottleWidgets as BWidgets
 import qualified Editor.Config as Config
 import qualified Editor.WidgetIds as WidgetIds
@@ -20,10 +21,12 @@ addType _ [] widget = widget
 addType exprId typeEdits widget =
   BWidgets.vboxCentered $
   [ widget
-  , Spacer.makeHorizLine underlineId (Vector2 50 1) -- TODO: HACK HACK
+  , Spacer.makeHorizLine underlineId (Vector2 underLineWidth 1)
   , typeEdit
   ]
   where
+    width = Vector2.fst . Widget.wSize
+    underLineWidth = max (width widget) (width typeEdit)
     typeEdit =
       addTint . addBackground .
       Widget.scale Config.typeScaleFactor $
