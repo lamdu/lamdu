37b54336ff68e69e2e4a9cb980baf9a85d2b72d9
Eyal Lotem <eyal.lotem@gmail.com>
2012-11-13 20:35:55 +0200

    WidgetId: Remove redundant use of AtFieldTH

--- bottlelib/Graphics/UI/Bottle/WidgetId.hs
+++ bottlelib/Graphics/UI/Bottle/WidgetId.hs
@@ -1,19 +1,15 @@
-{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving #-}
-module Graphics.UI.Bottle.WidgetId (Id(..), joinId, subId, atId) where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Graphics.UI.Bottle.WidgetId (Id(..), joinId, subId) where
 
 import Data.Binary (Binary)
 import Data.List (isPrefixOf, intercalate)
 import Data.Monoid (Monoid(..))
 import Graphics.UI.Bottle.Animation (AnimId)
 import Numeric.Utils (encodeHex)
-import qualified Data.AtFieldTH as AtFieldTH
 
 newtype Id = Id {
   toAnimId :: AnimId
-  }
-  deriving (Eq, Ord, Read, Binary, Monoid)
-
-AtFieldTH.make ''Id
+  } deriving (Eq, Ord, Read, Binary, Monoid)
 
 instance Show Id where
   show = ('W' :) . intercalate ":" . map encodeHex . toAnimId
