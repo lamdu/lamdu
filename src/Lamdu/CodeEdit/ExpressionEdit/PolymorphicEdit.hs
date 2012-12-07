564e67aa5df66e915a67b8d004ceb8447ee4fc2f
Eyal Lotem <eyal.lotem@gmail.com>
2012-12-07 15:54:57 +0200

    TODOs

--- src/Lamdu/CodeEdit/ExpressionEdit/PolymorphicEdit.hs
+++ src/Lamdu/CodeEdit/ExpressionEdit/PolymorphicEdit.hs
@@ -23,11 +23,12 @@ makeInner ::
   MonadA m => Sugar.Polymorphic (m ()) (Sugar.Expression m) -> Widget.Id ->
   ExprGuiM m (ExpressionGui m)
 makeInner poly myId = do
-  -- TODO: This is just to detect whether cursor is in the full expression.
-  -- Even when it's not displayed, which is wasteful.
+  -- TODO: This is just to detect whether cursor is in the full
+  -- expression.  Even when it's not displayed, which may be wasteful
+  -- (even with laziness, at least the names are going to be read).
   fullExprEdit <- ExprGuiM.makeSubexpresion $ Sugar.pFullExpression poly
-  -- We are inside a non-delegating focus delegator made by makeSubexpresion,
-  -- so if the cursor is on us it means user enterred our widget.
+  -- We are inside a focus delegator made by 'make' below, so if the
+  -- cursor is on us it means user entered our widget.
   if fullExprEdit ^. ExpressionGui.egWidget . Widget.wIsFocused
     then
       return $ bg Layers.expandedPolymorphicBG Config.polymorphicFullBGColor fullExprEdit
