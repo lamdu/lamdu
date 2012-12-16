e5a0126529411d9a8974100bc4b8620fcac3d507
Eyal Lotem <eyal.lotem@gmail.com>
2012-12-16 02:59:36 +0200

    Vector2 exports Monoid instance

--- bottlelib/Data/Vector/Vector2.hs
+++ bottlelib/Data/Vector/Vector2.hs
@@ -7,11 +7,10 @@ module Data.Vector.Vector2
     )
 where
 
--- import Test.QuickCheck.Arbitrary(Arbitrary(..))
---import Control.Lens (Lens)
 import Control.Applicative (Applicative(..), liftA2)
 import Control.Monad (join)
 import Data.Binary (Binary(..))
+import Data.Monoid
 import Prelude hiding (curry, uncurry, zip)
 import qualified Control.Lens.TH as LensTH
 
@@ -77,6 +76,11 @@ instance Functor Vector2 where
 instance Applicative Vector2 where
   pure x = Vector2 x x
   Vector2 f g <*> Vector2 x y = Vector2 (f x) (g y)
+
+instance Monoid a => Monoid (Vector2 a) where
+  mempty = Vector2 mempty mempty
+  mappend (Vector2 x0 y0) (Vector2 x1 y1) =
+    Vector2 (mappend x0 x1) (mappend y0 y1)
 
 -- An improper Num instance, for convenience
 instance Num a => Num (Vector2 a) where
