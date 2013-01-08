b3ae4bfafd7c9b09bfafa70b6f6711d338169286
Eyal Lotem <eyal.lotem@gmail.com>
2013-01-08 17:39:28 +0200

    Add TODO

--- src/Lamdu/CodeEdit/Sugar/Infer.hs
+++ src/Lamdu/CodeEdit/Sugar/Infer.hs
@@ -126,6 +126,7 @@ loader =
   (fmap void . DataIRef.readExpression . Lens.view Definition.defType <=<
    Transaction.readIRef)
 
+-- TODO: Do these need to be cached?
 inferMaybe ::
   MonadA m =>
   Maybe (DefI (Tag m)) ->
