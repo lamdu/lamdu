e47ddbcee0218db0463b8ee8cd1871a7f5fe71b6
Eyal Lotem <eyal.lotem@gmail.com>
2013-05-07 13:26:52 +0300

    Builds with 7.6 (Had use of RankNTypes without extension enabled)

--- bottlelib/Control/Lens/Utils.hs
+++ bottlelib/Control/Lens/Utils.hs
@@ -5,6 +5,6 @@ module Control.Lens.Utils
 import Data.Maybe (fromMaybe)
 import qualified Control.Lens as Lens
 
-_fromJust :: String -> Lens.Iso (Maybe a) (Maybe b) a b
+_fromJust :: (Lens.Profunctor p, Functor f) => String -> Lens.Overloaded p f (Maybe a) (Maybe b) a b
 _fromJust msg = Lens.iso (fromMaybe (error msg)) Just
 {-# INLINE _fromJust #-}
