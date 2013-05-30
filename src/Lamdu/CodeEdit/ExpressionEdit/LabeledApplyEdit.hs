6b6b4751030ee1541fad716042d3f11965f1577a
Eyal Lotem <eyal.lotem@gmail.com>
2013-05-30 13:04:10 +0300

    Refactor LabeledApplyEdit

--- src/Lamdu/CodeEdit/ExpressionEdit/LabeledApplyEdit.hs
+++ src/Lamdu/CodeEdit/ExpressionEdit/LabeledApplyEdit.hs
@@ -3,7 +3,7 @@ module Lamdu.CodeEdit.ExpressionEdit.LabeledApplyEdit
   ( make
   ) where
 
-import Control.Applicative (pure)
+import Control.Applicative (Applicative(..), (<$>))
 import Control.Lens.Operators
 import Control.MonadA (MonadA)
 import Data.Monoid (mappend)
@@ -46,44 +46,51 @@ make ::
   MonadA m =>
   Sugar.LabeledApply Sugar.Name (Sugar.ExpressionN m) ->
   Widget.Id -> ExprGuiM m (ExpressionGui m)
-make (Sugar.LabeledApply func args) =
-  ExpressionGui.wrapExpression $ \myId -> do
-    presentationMode <- getPresentationMode func
-    ExprGuiM.assignCursor myId (WidgetIds.fromGuid (func ^. Sugar.rGuid)) $ do
-      funcEdit <- ExprGuiM.makeSubexpresion 10 func
-      let
-        makeArg (tagG, namedArgExpr) = do
-          let tagGuid = tagG ^. Sugar.tagGuid
-          argTagEdit <-
-            TagEdit.makeView tagG .
-            Widget.toAnimId . mappend myId $
-            WidgetIds.fromGuid tagGuid
-          argValEdit <- ExprGuiM.makeSubexpresion 0 namedArgExpr
-          pure $ ExpressionGui.makeRow
-            [ (0, scaleTag argTagEdit)
-            , (0.5, space)
-            , (0, argValEdit)
-            ]
-      argEdits <-
-        traverse makeArg $
-        case presentationMode of
-        Verbose -> args
-        OO -> tail args
-      let
-        argEditsGrid = Grid.toWidget $ Grid.make argEdits
-      topEdit <-
-        case presentationMode of
-        Verbose -> return funcEdit
-        OO ->
-          case args of
-          (_, firstArg) : _ -> do
-            firstArgEdit <- ExprGuiM.makeSubexpresion 11 firstArg
-            return $ ExpressionGui.hboxSpaced [funcEdit, firstArgEdit]
-          _ -> error "LabeledApplyEdit must take arg list of at least 2 elements"
-      pure .
-        ExpressionGui.withBgColor Layers.labeledApplyBG
-        Config.labeledApplyBGColor (Widget.toAnimId myId ++ ["bg"]) $
-        ExpressionGui.addBelow 0 [(0, argEditsGrid)] topEdit
+make (Sugar.LabeledApply func args) myId = do
+  presentationMode <- getPresentationMode func
+  case presentationMode of
+    Verbose -> mkBoxed args []
+    OO ->
+      case args of
+        ((_firstArgTag, firstArg):rest) -> mkBoxed rest . (: []) =<< ExprGuiM.makeSubexpresion 11 firstArg
+        _ -> mkBoxed args []
   where
-    space = ExpressionGui.fromValueWidget BWidgets.stdSpaceWidget
+    mkBoxed as adjacant = boxedApply func as adjacant myId
+
+makeTagView :: MonadA m => Widget.Id -> Sugar.TagG Sugar.Name -> ExprGuiM m (ExpressionGui m)
+makeTagView myId tagG =
+  TagEdit.makeView tagG . Widget.toAnimId . mappend myId .
+  WidgetIds.fromGuid $ tagG ^. Sugar.tagGuid
+
+makeArgRow ::
+  MonadA m => Widget.Id ->
+  (Sugar.TagG Sugar.Name, Sugar.ExpressionN m) ->
+  ExprGuiM m [(Grid.Alignment, ExprGuiM.WidgetT m)]
+makeArgRow myId (tagG, namedArgExpr) = do
+  argTagEdit <- makeTagView myId tagG
+  argValEdit <- ExprGuiM.makeSubexpresion 0 namedArgExpr
+  pure $ ExpressionGui.makeRow
+    [ (0, scaleTag argTagEdit)
+    , (0.5, space)
+    , (0, argValEdit)
+    ]
+  where
     scaleTag = ExpressionGui.egWidget %~ Widget.scale Config.fieldTagScale
+    space = ExpressionGui.fromValueWidget BWidgets.stdSpaceWidget
+
+boxedApply ::
+  MonadA m => Sugar.ExpressionN m ->
+  [(Sugar.TagG Sugar.Name, Sugar.ExpressionN m)] ->
+  [ExpressionGui m] ->
+  Widget.Id -> ExprGuiM m (ExpressionGui m)
+boxedApply func args funcAdjacants =
+  ExpressionGui.wrapExpression $ \myId ->
+  ExprGuiM.assignCursor myId (WidgetIds.fromGuid (func ^. Sugar.rGuid)) $ do
+    funcEdit <- ExprGuiM.makeSubexpresion 10 func
+    argEdits <-
+      Grid.toWidget . Grid.make <$> traverse (makeArgRow myId) args
+    pure .
+      ExpressionGui.withBgColor Layers.labeledApplyBG
+      Config.labeledApplyBGColor (Widget.toAnimId myId ++ ["bg"]) .
+      ExpressionGui.addBelow 0 [(0, argEdits)] .
+      ExpressionGui.hboxSpaced $ funcEdit : funcAdjacants
