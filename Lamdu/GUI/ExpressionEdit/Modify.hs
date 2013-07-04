dc3f2b5d3174af93f706e5b22312e2dcfa1c7da3
Eyal Lotem <eyal.lotem@gmail.com>
2013-07-04 03:47:02 +0300

    ExpressionEdit.Modify: Use T alias for Transaction

--- Lamdu/GUI/ExpressionEdit/Modify.hs
+++ Lamdu/GUI/ExpressionEdit/Modify.hs
@@ -8,11 +8,11 @@ import Control.MonadA (MonadA)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (Monoid(..))
 import Data.Store.Guid (Guid)
-import Data.Store.Transaction (Transaction)
 import Graphics.UI.Bottle.Widget (EventHandlers)
 import Lamdu.CharClassification (operatorChars)
 import Lamdu.Config (Config)
 import Lamdu.GUI.ExpressionEdit.HoleEdit.Info (HoleState(..))
+import qualified Data.Store.Transaction as Transaction
 import qualified Graphics.UI.Bottle.EventMap as E
 import qualified Graphics.UI.Bottle.Widget as Widget
 import qualified Graphics.UI.Bottle.Widgets.FocusDelegator as FocusDelegator
@@ -21,8 +21,10 @@ import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.Info as HoleInfo
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import qualified Lamdu.Sugar.Types as Sugar
 
+type T = Transaction.Transaction
+
 applyOperatorEventMap ::
-  MonadA m => Transaction m Guid -> EventHandlers (Transaction m)
+  MonadA m => T m Guid -> EventHandlers (T m)
 applyOperatorEventMap wrap =
   E.charGroup "Operator" (E.Doc ["Edit", "Apply operator"]) operatorChars action
   where
@@ -30,7 +32,7 @@ applyOperatorEventMap wrap =
       Widget.eventResultFromCursor <$>
       (HoleInfo.setHoleStateAndJump (HoleState [c]) =<< wrap)
 
-wrapEventMap :: MonadA m => Config -> Sugar.Actions m -> EventHandlers (Transaction m)
+wrapEventMap :: MonadA m => Config -> Sugar.Actions m -> EventHandlers (T m)
 wrapEventMap config actions =
   fromMaybe mempty $ do
     wrap <- actions ^. Sugar.wrap
@@ -41,7 +43,7 @@ wrapEventMap config actions =
         (FocusDelegator.delegatingId . WidgetIds.fromGuid <$> wrap)
     Just $ mappend (applyOperatorEventMap wrap) wrapEv
 
-replaceEventMap :: MonadA m => Config -> Sugar.Actions m -> EventHandlers (Transaction m)
+replaceEventMap :: MonadA m => Config -> Sugar.Actions m -> EventHandlers (T m)
 replaceEventMap config actions =
   mconcat
   [ actionEventMap Sugar.mSetToInnerExpr
@@ -57,7 +59,7 @@ replaceEventMap config actions =
        fmap WidgetIds.fromGuid) $ actions ^. lens
     delKeys = Config.replaceKeys config ++ Config.delKeys config
 
-eventMap :: MonadA m => Config -> Sugar.Actions m -> EventHandlers (Transaction m)
+eventMap :: MonadA m => Config -> Sugar.Actions m -> EventHandlers (T m)
 eventMap config actions =
   mconcat
   [ wrapEventMap config actions
