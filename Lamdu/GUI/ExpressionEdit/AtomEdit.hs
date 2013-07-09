81eb0b7af9adb57aa69cdd3462c1d473c12fd0f4
Eyal Lotem <eyal.lotem@gmail.com>
2013-07-09 13:45:39 +0300

    AtomEdit: Had tabs

--- Lamdu/GUI/ExpressionEdit/AtomEdit.hs
+++ Lamdu/GUI/ExpressionEdit/AtomEdit.hs
@@ -13,11 +13,11 @@ import qualified Lamdu.GUI.WidgetEnvT as WE
 import qualified Lamdu.Sugar.Types as Sugar
 
 make ::
-	MonadA m =>
-	String -> Sugar.Payload Sugar.Name m ExprGuiM.Payload ->
-	Widget.Id -> ExprGuiM m (ExpressionGui m)
+  MonadA m =>
+  String -> Sugar.Payload Sugar.Name m ExprGuiM.Payload ->
+  Widget.Id -> ExprGuiM m (ExpressionGui m)
 make name pl myId = do
   config <- ExprGuiM.widgetEnv WE.readConfig
   ExpressionGui.stdWrap pl .
-  	ExprGuiM.withFgColor (Config.atomColor config) . ExprGuiM.widgetEnv $
+    ExprGuiM.withFgColor (Config.atomColor config) . ExprGuiM.widgetEnv $
     ExpressionGui.fromValueWidget <$> BWidgets.makeFocusableTextView name myId
