8521b0d7cb747d348b16f761d3cb298d99dcaf61
Yair Chuchem <yairchu@gmail.com>
2013-07-16 17:54:42 +0300

    unqualified import of Infer.Internal

--- Lamdu/Data/Infer/Monad.hs
+++ Lamdu/Data/Infer/Monad.hs
@@ -7,19 +7,19 @@ import Control.Applicative (Applicative)
 import Control.Monad.Trans.Class (MonadTrans(..))
 import Control.Monad.Trans.Either (EitherT)
 import Control.Monad.Trans.State (StateT(..))
+import Lamdu.Data.Infer.Internal
 import qualified Control.Monad.Trans.Either as Either
-import qualified Lamdu.Data.Infer.Internal as I
 
-newtype InferT def m a = InferT { runInferT :: StateT (I.Context def) (EitherT (I.Error def) m) a }
+newtype InferT def m a = InferT { runInferT :: StateT (Context def) (EitherT (Error def) m) a }
   deriving (Functor, Applicative, Monad)
 
-liftContext :: StateT (I.Context def) (EitherT (I.Error def) m) a -> InferT def m a
+liftContext :: StateT (Context def) (EitherT (Error def) m) a -> InferT def m a
 liftContext = InferT
 
-liftError :: Monad m => EitherT (I.Error def) m a -> InferT def m a
+liftError :: Monad m => EitherT (Error def) m a -> InferT def m a
 liftError = liftContext . lift
 
-inferError :: Monad m => I.Error def -> InferT def m a
+inferError :: Monad m => Error def -> InferT def m a
 inferError = liftError . Either.left
 
 instance MonadTrans (InferT def) where
