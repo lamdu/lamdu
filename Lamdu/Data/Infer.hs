f268d6ed93ab79e6c5f568d7f05dee331411179f
Yair Chuchem <yairchu@gmail.com>
2013-07-17 13:09:18 +0300

    Infer: Towards TypeRef logic

--- Lamdu/Data/Infer.hs
+++ Lamdu/Data/Infer.hs
@@ -14,9 +14,9 @@ import Control.Lens.Operators
 import Control.Monad (void)
 import Control.Monad.Trans.Class (MonadTrans(..))
 import Control.Monad.Trans.State (StateT)
+import Control.MonadA (MonadA)
 import Data.Map (Map)
 import Data.Maybe (fromMaybe)
-import Data.Maybe.Utils (unsafeUnjust)
 import Data.Monoid (Monoid(..))
 import Data.Set (Set)
 import Data.Store.Guid (Guid)
@@ -52,7 +52,10 @@ renameRefVars renames (RefVars scope getVars) =
   where
     mapping = rename renames
 
-data Error def = VarEscapesScope | Mismatch (Expr.Body def Ref) (Expr.Body def Ref)
+data Error def
+  = VarEscapesScope
+  | VarNotInScope
+  | Mismatch (Expr.Body def Ref) (Expr.Body def Ref)
   deriving (Show)
 
 type Infer def = StateT (Context def) (Either (Error def))
@@ -113,13 +116,61 @@ infer ::
   Infer def (Expr.Expression (LoadedDef def) (ScopedTypedValue, a))
 infer scope = exprIntoSTV scope . ExprUtil.annotateUsedVars
 
+-- TODO: move to appropriate module
+emptyRefVars :: RefVars
+emptyRefVars = RefVars Map.empty Set.empty
+
+makeHoleRef :: MonadA m => Map Guid Ref -> StateT (Context def) m Ref
+makeHoleRef scope =
+  ExprRefs.fresh . RefData (RefVars scope Set.empty) $ ExprLens.bodyHole # ()
+
+makeTypeRef ::
+  Map Guid Ref ->
+  Expr.Body (LoadedDef def) (Expr.Expression (LoadedDef def) (ScopedTypedValue, a)) ->
+  Infer def Ref
+makeTypeRef scope (Expr.BodyLeaf leaf) =
+  case leaf of
+  Expr.GetVariable (Expr.DefinitionRef (LoadedDef _ ref)) -> pure ref
+  Expr.GetVariable (Expr.ParameterRef guid) ->
+    case scope ^. Lens.at guid of
+    Nothing -> lift $ Left VarNotInScope
+    Just ref -> pure ref
+  Expr.LiteralInteger _ -> resNoScope $ ExprLens.bodyIntegerType # ()
+  Expr.Type -> resType
+  Expr.IntegerType -> resType
+  Expr.Hole -> makeHoleRef scope
+  Expr.TagType -> resType
+  Expr.Tag _ -> resNoScope $ ExprLens.bodyTagType # ()
+  where
+    resType = resNoScope $ ExprLens.bodyType # ()
+    resNoScope = ExprRefs.fresh . RefData emptyRefVars
+makeTypeRef _ (Expr.BodyLam (Expr.Lam Expr.KType _ _ _)) =
+  ExprRefs.fresh . RefData emptyRefVars $ ExprLens.bodyType # ()
+makeTypeRef scope (Expr.BodyLam (Expr.Lam Expr.KVal paramGuid paramType body)) =
+  ExprRefs.fresh . RefData (RefVars scope Set.empty) . Expr.BodyLam .
+  Expr.Lam Expr.KType paramGuid (paramType ^. Expr.ePayload . Lens._1 . stvTV . tvVal) =<<
+  makeHoleRef (body ^. Expr.ePayload . Lens._1 . stvScope)
+makeTypeRef _ (Expr.BodyRecord (Expr.Record Expr.KType _)) =
+  ExprRefs.fresh . RefData emptyRefVars $ ExprLens.bodyType # ()
+makeTypeRef scope (Expr.BodyRecord (Expr.Record Expr.KVal fields)) =
+  fields
+  & Lens.traverse %%~ onField
+  >>= ExprRefs.fresh . RefData (RefVars scope Set.empty) .
+      Expr.BodyRecord . Expr.Record Expr.KType
+  where
+    onField (tag, _) =
+      (,) (tag ^. Expr.ePayload . Lens._1 . stvTV . tvVal)
+      <$> makeHoleRef scope
+makeTypeRef scope (Expr.BodyApply _) = makeHoleRef scope
+makeTypeRef scope (Expr.BodyGetField _) = makeHoleRef scope
+
 -- With hole apply vals and hole types
 exprIntoSTV ::
   Map Guid Ref ->
   Expr.Expression (LoadedDef def) (Set Guid, a) ->
   Infer def (Expr.Expression (LoadedDef def) (ScopedTypedValue, a))
 exprIntoSTV scope (Expr.Expression body (usedVars, pl)) = do
-  (newBody, typeRef) <-
+  newBody <-
     case body of
     Expr.BodyLam (Expr.Lam k paramGuid paramType result) -> do
       paramTypeS <- exprIntoSTV scope paramType
@@ -129,22 +180,9 @@ exprIntoSTV scope (Expr.Expression body (usedVars, pl)) = do
           & Lens.at paramGuid .~
             Just (paramTypeS ^. Expr.ePayload . Lens._1 . stvTV . tvVal)
       resultS <- exprIntoSTV newScope result
-      typeRef <- mkHoleRef
-      return
-        ( Expr.BodyLam $ Expr.Lam k paramGuid paramTypeS resultS
-        , typeRef
-        )
-    Expr.BodyLeaf leaf@(Expr.GetVariable (Expr.ParameterRef guid)) ->
-      return
-      ( Expr.BodyLeaf leaf
-      , unsafeUnjust "GetVar out of scope!" $ scope ^. Lens.at guid
-      )
-    Expr.BodyLeaf leaf@(Expr.GetVariable (Expr.DefinitionRef (LoadedDef _ ref))) ->
-      return (Expr.BodyLeaf leaf, ref)
+      pure . Expr.BodyLam $ Expr.Lam k paramGuid paramTypeS resultS
     _ ->
-      (,)
-      <$> (body & Lens.traverse %%~ exprIntoSTV scope)
-      <*> mkHoleRef
+      body & Lens.traverse %%~ exprIntoSTV scope
   valRef <-
     newBody
     <&> (^. Expr.ePayload . Lens._1 . stvTV . tvVal)
@@ -152,11 +190,11 @@ exprIntoSTV scope (Expr.Expression body (usedVars, pl)) = do
     & RefData (RefVars scope usedVars)
     & circumcizeApply
     & ExprRefs.fresh
+  typeRef <- makeTypeRef scope newBody
   pure $
     Expr.Expression newBody
     ((ScopedTypedValue (TypedValue valRef typeRef) scope), pl)
   where
-    mkHoleRef = ExprRefs.fresh . RefData (RefVars scope Set.empty) $ ExprLens.bodyHole # ()
     circumcizeApply (RefData (RefVars s _) Expr.BodyApply{}) =
       RefData (RefVars s Set.empty) $ ExprLens.bodyHole # ()
     circumcizeApply x = x
