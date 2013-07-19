58e3c690ee54accf88a93da88dfe3ffeb1ae3bc4
Eyal Lotem <eyal.lotem@gmail.com>
2013-07-19 19:03:54 +0300

    Infer benchmark is runnable

--- test/InferExamples.hs
+++ test/InferExamples.hs
@@ -1,40 +1,40 @@
 module InferExamples where
 
-import Control.Lens.Operators
-import Lamdu.Data.Expression.Utils (pureHole)
-import Utils
 import InferWrappers
+import InferCombinators
 
-factorialExpr :: PureExprDefI t
+factorialExpr :: InferResults t
 factorialExpr =
-  pureLambda "x" pureHole $
-  pureApplyPoly1 "if"
-  [ pureApplyPoly1 "==" [pureGetParam "x", pureLiteralInt # 0]
-  , pureLiteralInt # 1
-  , pureApplyPoly1 "*"
-    [ pureGetParam "x"
-    , pureApply
-      [ pureGetRecursiveDefI
-      , pureApplyPoly1 "-" [pureGetParam "x", pureLiteralInt # 1]
-      ]
+  lambda "x" iInt $ \x ->
+  getDef "if" $$ iInt $$:
+  [ getDef "==" $$ iInt $$:
+    [x, literalInteger 0]
+  , literalInteger 1
+  , getDef "*" $$ iInt $$:
+    [ x
+    , recurse (integerType ~> integerType) $$
+      (getDef "-" $$ iInt $$: [x, literalInteger 1])
     ]
   ]
+  where
+    iInt = asHole integerType
 
-euler1Expr :: PureExprDefI t
+euler1Expr :: InferResults t
 euler1Expr =
-  pureApplyPoly1 "sum"
-  [ pureApplyPoly1 "filter"
-    [ pureLambda "x" pureHole $
-      pureApply
-      [ pureGetDef "||"
-      , pureApplyPoly1 "=="
-        [ pureLiteralInt # 0, pureApplyPoly1 "%" [pureGetParam "x", pureLiteralInt # 3] ]
-      , pureApplyPoly1 "=="
-        [ pureLiteralInt # 0, pureApplyPoly1 "%" [pureGetParam "x", pureLiteralInt # 5] ]
+  getDef "sum" $$ iInt $$
+  ( getDef "filter" $$ iInt $$:
+    [ getDef ".." $$: [literalInteger 1, literalInteger 1000]
+    , lambda "x" iInt $ \x ->
+      getDef "||" $$:
+      [ getDef "==" $$ iInt $$:
+        [ literalInteger 0, getDef "%" $$ iInt $$: [x, literalInteger 3] ]
+      , getDef "==" $$ iInt $$:
+        [ literalInteger 0, getDef "%" $$ iInt $$: [x, literalInteger 5] ]
       ]
-    , pureApply [pureGetDef "..", pureLiteralInt # 1, pureLiteralInt # 1000]
     ]
-  ]
+  )
+  where
+    iInt = asHole integerType
 
 factorial :: Int -> Int
 factorial = inferAndEncode factorialExpr
