212d8c7c71d1d51df6a5654b72bb42e09d5b4a80
Eyal Lotem <eyal.lotem@gmail.com>
2013-07-19 18:46:04 +0300

    Deref: Share deref work

--- Lamdu/Data/Infer/Deref.hs
+++ Lamdu/Data/Infer/Deref.hs
@@ -6,12 +6,14 @@ module Lamdu.Data.Infer.Deref
 
 import Control.Applicative (Applicative(..), (<$>))
 import Control.Lens.Operators
-import Control.Monad.Trans.State (StateT)
+import Control.Monad.Trans.Class (MonadTrans(..))
+import Control.Monad.Trans.State (StateT, evalStateT)
 import Control.MonadA (MonadA)
 import Data.Function.Decycle (decycle)
 import Data.UnionFind (Ref)
 import Lamdu.Data.Infer.Internal
 import qualified Control.Lens as Lens
+import qualified Data.Map as Map
 import qualified Lamdu.Data.Expression as Expr
 import qualified Lamdu.Data.Expression.Utils as ExprUtil
 import qualified Lamdu.Data.Infer.ExprRefs as ExprRefs
@@ -24,14 +26,23 @@ Lens.makeLenses ''Derefed
 
 deref :: MonadA m => Ref -> StateT (Context def) m (Expr.Expression def ())
 deref =
-  decycle loop
+  (`evalStateT` Map.empty) . decycle loop
   where
     loop Nothing _ = error "Cycle at deref?!"
-    loop (Just recurse) ref =
-      ExprRefs.read ref
-      <&> (^. rdBody)
-      >>= Lens.traverse %%~ recurse
-      <&> ExprUtil.pureExpression
+    loop (Just recurse) ref = do
+      rep <- lift $ ExprRefs.find "deref lookup" ref
+      mFound <- Lens.use (Lens.at rep)
+      case mFound of
+        Just found -> return found
+        Nothing -> do
+          derefed <-
+            ExprRefs.readRep rep
+            & lift
+            <&> (^. rdBody)
+            >>= Lens.traverse %%~ recurse
+            <&> ExprUtil.pureExpression
+          Lens.at rep .= Just derefed
+          return derefed
 
 expr ::
   MonadA m =>
