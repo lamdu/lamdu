4131c7ba70fe8c9bbce45006ef765a6be6b2030a
Eyal Lotem <eyal.lotem@gmail.com>
2013-07-19 18:46:19 +0300

    Towards benchmark

--- test/benchmark_Infer.hs
+++ test/benchmark_Infer.hs
@@ -1,16 +1,29 @@
+import Control.Applicative
 import Control.Monad
 import Criterion.Main
-import InferExamples
+import Data.Binary.Utils (encodeS)
+import InferWrappers
+import Utils
+import qualified Data.ByteString as SBS
+import qualified Lamdu.Data.Expression as Expr
+
+inferAndEncode :: Expr.Expression Def a -> Int -> Int
+inferAndEncode expr par =
+  SBS.length . encodeS $ fst result
+  where
+    result = assertSuccess . loadInferRun . void $ par <$ expr
 
 main :: IO ()
 main = do
   putStrLn "============================================="
   putStrLn "Binary encode size:"
-  forM_ [("factorial", factorial 0), ("euler1", euler1 0)] $ \(name, size) ->
+  forM_ [ ("factorial", inferAndEncode factorialExpr 0)
+        , ("euler1", inferAndEncode euler1Expr 0)
+        ] $ \(name, size) ->
     putStrLn $ unwords ["==", name, "inferred:", show size, "bytes"]
   putStrLn "============================================="
 
   defaultMain
-    [ bench "factorial" $ whnf factorial 0
-    , bench "euler1" $ whnf euler1 0
+    [ bench "factorial" $ whnf (inferAndEncode factorialExpr) 0
+    , bench "euler1" $ whnf (inferAndEncode euler1Expr) 0
     ]
