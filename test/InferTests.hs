a8c63c95f401965b2a071675399daa574d95813b
Yair Chuchem <yairchu@gmail.com>
2013-07-20 15:38:07 +0300

    simpler failing test

--- test/InferTests.hs
+++ test/InferTests.hs
@@ -69,7 +69,15 @@ monomorphRedex =
 idTest = testInfer "id test" $ getDef "id" $$ integerType
 
 inferFromOneArgToOther =
-  testInfer "f = \\ a b (c:Map _ _) (d:Map a b) -> if {_ c d}" $
+  testInfer "f = \\ a (b:_) (c:a) -> if {_ b c}" $
+  lambda "a" (asHole set) $ \a ->
+  lambda "b" (asHole a) $ \b ->
+  lambda "c" a $ \c ->
+  getDef "if" $$ asHole a $$:
+  [holeWithInferredType (getDef "Bool"), b, c]
+
+inferFromOneArgToOtherMap =
+  testInfer "f = \\ a b (c:List _ _) (d:Map a b) -> if {_ c d}" $
   lambda "a" (asHole set) $ \a ->
   lambda "b" (asHole set) $ \b ->
   let mkMapType f = getDef "Map" $$: [f a, f b] in
@@ -363,6 +371,7 @@ hunitTests =
   , idOnARecord
   , idOnHole
   , inferFromOneArgToOther
+  , inferFromOneArgToOtherMap
   , depApply
   , forceMono
   , fOfXIsFOf5
