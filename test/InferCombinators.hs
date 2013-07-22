da2ac719e2691592a0d4ee1f6b272a3764d3baf6
Eyal Lotem <eyal.lotem@gmail.com>
2013-07-23 02:16:42 +0300

    Add failing GetField tests

--- test/InferCombinators.hs
+++ test/InferCombinators.hs
@@ -324,3 +324,6 @@ record k fields =
 
 asHole :: InputExpr -> InputExpr
 asHole = Expr.eBody .~ (ExprLens.bodyHole # ())
+
+typeAnnotate :: InputExpr -> InputExpr -> InputExpr
+typeAnnotate t v = getDef "id" $$ t $$ v `setInferredType` t
