1933e51bed84dc627c5589241efee01ab1343001
Eyal Lotem <eyal.lotem@gmail.com>
2013-07-23 02:31:54 +0300

    Split handleGetField to its own module

--- Lamdu/Data/Infer.hs
+++ Lamdu/Data/Infer.hs
@@ -18,6 +18,7 @@ import Control.Monad.Trans.State (StateT)
 import Data.Foldable (traverse_)
 import Data.UnionFind (Ref)
 import Lamdu.Data.Infer.AppliedPiResult (handleAppliedPiResult)
+import Lamdu.Data.Infer.GetField (handleGetField)
 import Lamdu.Data.Infer.Internal
 import Lamdu.Data.Infer.MakeTypes (makeTypeRef)
 import Lamdu.Data.Infer.Monad (Infer, Error(..))
@@ -54,43 +55,6 @@ isTag ref = do
   case refData ^. rdIsComposite of
     Monoid.Any True -> InferM.error $ CompositeTag ref
     _ -> return ()
-
-data TagBodyMatch = TagBodyMatch | TagBodyMismatch | TagBodyMaybeMatch
-  deriving (Eq)
-
-tagBodiesMayMatch :: Expr.Body def a -> Expr.Body def a -> TagBodyMatch
-tagBodiesMayMatch (Expr.BodyLeaf Expr.Hole) _ = TagBodyMaybeMatch
-tagBodiesMayMatch _ (Expr.BodyLeaf Expr.Hole) = TagBodyMaybeMatch
-tagBodiesMayMatch (Expr.BodyLeaf (Expr.Tag a)) (Expr.BodyLeaf (Expr.Tag b)) | a == b = TagBodyMatch
-tagBodiesMayMatch _ _ = TagBodyMismatch
-
-handleGetField :: Eq def => GetFieldRefs -> Infer def ()
-handleGetField (GetFieldRefs getFieldTagRef getFieldTypeRef recordTypeRef) = do
-  recordTypeBody <- (^. rdBody) <$> ExprRefs.read recordTypeRef
-  getFieldTagBody <- (^. rdBody) <$> ExprRefs.read getFieldTagRef
-  case recordTypeBody of
-    Expr.BodyLeaf Expr.Hole -> return ()
-    Expr.BodyRecord (Expr.Record Expr.KType fields) -> do
-      matchedFields <-
-        fields
-        & Lens.traverse . Lens._1 %%~ \tagRef ->
-          ExprRefs.read tagRef
-          <&> tagBodiesMayMatch getFieldTagBody . (^. rdBody)
-          <&> (,) tagRef
-      let ofMatchType t = filter ((== t) . snd . fst) matchedFields <&> Lens._1 %~ fst
-      case ofMatchType TagBodyMatch of
-        (fieldTagRef, fieldTypeRef) : _ -> void $ unionField fieldTagRef fieldTypeRef
-        [] -> -- No exact matches
-          case ofMatchType TagBodyMaybeMatch of
-          -- This is as good as an exact match:
-          [(fieldTagRef, fieldTypeRef)] -> void $ unionField fieldTagRef fieldTypeRef
-          [] -> InferM.error GetMissingField
-          _ -> return ()
-    _ -> InferM.error GetFieldRequiresRecord
-  where
-    unionField fieldTagRef fieldTypeRef = do
-      void $ Unify.unify fieldTagRef getFieldTagRef
-      void $ Unify.unify fieldTypeRef getFieldTypeRef
 
 executeRelation :: Eq def => Relation -> Ref -> Infer def ()
 executeRelation rel =
