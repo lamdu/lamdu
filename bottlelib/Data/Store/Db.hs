c68df584dbd684f75abb02b903932dadddf819e4
Eyal Lotem <eyal.lotem@gmail.com>
2013-09-16 20:06:57 +0300

    Port Lamdu to use sophia

--- bottlelib/Data/Store/Db.hs
+++ bottlelib/Data/Store/Db.hs
@@ -6,40 +6,26 @@ import Data.ByteString (ByteString)
 import Data.Foldable (traverse_)
 import Data.Store.Guid (Guid)
 import Data.Store.Transaction (Store(..))
+import Database.Sophia (Db)
 import Prelude hiding (lookup)
-import qualified Control.Exception as Exc
 import qualified Data.Store.Guid as Guid
-import qualified Database.KeyValueHash as HashDB
-
-type Db = HashDB.Database
-
--- TODO: this should be automatic in HashDB
-hashSize :: HashDB.Size
-hashSize = HashDB.mkSize $ 2 ^ (17::Int)
-
-open :: FilePath -> IO Db
-open fileName =
-  HashDB.openDatabase fileName HashDB.stdHash hashSize
-  `Exc.catch`
-  (\(Exc.SomeException _) ->
-    HashDB.createDatabase fileName HashDB.stdHash hashSize)
-
-close :: Db -> IO ()
-close _ = return ()
+import qualified Database.Sophia as Sophia
 
 withDb :: FilePath -> (Db -> IO a) -> IO a
-withDb filePath = Exc.bracket (open filePath) close
+withDb filePath act =
+  Sophia.withEnv $ \env -> do
+    Sophia.openDir env Sophia.ReadWrite Sophia.AllowCreation filePath
+    Sophia.withDb env act
 
 lookup :: Db -> Guid -> IO (Maybe ByteString)
-lookup db = HashDB.readKey db . Guid.bs
+lookup db = Sophia.getValue db . Guid.bs
 
 transaction :: Db -> [(Guid, Maybe ByteString)] -> IO ()
-transaction db changes = do
+transaction db changes =
   traverse_ applyChange changes
-  HashDB.msync db
   where
-    applyChange (key, Nothing) = HashDB.deleteKey db (Guid.bs key)
-    applyChange (key, Just value) = HashDB.writeKey db (Guid.bs key) value
+    applyChange (key, Nothing) = Sophia.delValue db (Guid.bs key)
+    applyChange (key, Just value) = Sophia.setValue db (Guid.bs key) value
 
 -- You get a Store tagged however you like...
 store :: Db -> Store IO
