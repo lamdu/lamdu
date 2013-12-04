c138e6aebb8ea0fa1cce6542111ca15fe30a1b2d
Eyal Lotem <eyal.lotem@gmail.com>
2013-12-04 17:01:37 +0200

    Oops: Control.Monad.Trans.Decycle.visit did not visit

--- bottlelib/Control/Monad/Trans/Decycle.hs
+++ bottlelib/Control/Monad/Trans/Decycle.hs
@@ -11,6 +11,7 @@ import Control.Monad.Trans.Class (MonadTrans)
 import Control.Monad.Trans.Reader (ReaderT(..))
 import Data.Set (Set)
 import qualified Control.Lens as Lens
+import qualified Control.Monad.Trans.Reader as Reader
 import qualified Data.Set as Set
 
 newtype DecycleT k m a =
@@ -21,9 +22,9 @@ runDecycleT :: DecycleT k m a -> m a
 runDecycleT (DecycleT (ReaderT action)) = action Set.empty
 
 visit :: (Ord k, Monad m) => k -> DecycleT k m a -> DecycleT k m (Maybe a)
-visit key (DecycleT (ReaderT action)) =
-  DecycleT $ ReaderT go
-  where
-    go visited
-      | visited ^. Lens.contains key = return Nothing
-      | otherwise = liftM Just $ action visited
+visit key (DecycleT action) =
+  DecycleT $ do
+    visited <- Reader.ask
+    if visited ^. Lens.contains key
+      then return Nothing
+      else Reader.local (Lens.contains key .~ True) . liftM Just $ action
