1e066d81232bfc233cc5c5454bc97cd9d9a0be29
Eyal Lotem <eyal.lotem@gmail.com>
2014-09-02 13:39:27 +0300

    Add .dir-locals.el which enables flycheck to find bottlelib

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+(
+ (nil . ((eval . (setq dir-local-curdir
+                       (file-name-directory
+                        (let ((d (dir-locals-find-file ".")))
+                          (if (stringp d) d (car d))))))))
+ (haskell-mode . ((eval .
+   (progn
+     (setq flycheck-ghc-search-path (list (concat dir-local-curdir "/bottlelib")))
+     (setq flycheck-idle-change-delay 1.0)))))
+)
