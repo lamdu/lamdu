15836b26cbc425725f34a7ec86894a122efcba1e
Eyal Lotem <eyal.lotem@gmail.com>
2014-11-19 14:58:19 +0200

    emacs dir-locals: Refer to AlgoW subdir

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,5 +6,6 @@
  (haskell-mode . ((eval .
    (progn
      (setq flycheck-ghc-search-path (list (concat dir-local-curdir "/bottlelib")))
+     (setq flycheck-ghc-search-path (list (concat dir-local-curdir "/submodules/AlgoW")))
      (setq flycheck-idle-change-delay 1.0)))))
 )
