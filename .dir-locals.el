bbc5655a9e46d05b2688eb1fa439265d4a27a207
Eyal Lotem <eyal.lotem@gmail.com>
2014-12-02 13:02:27 +0200

    Oops: dir-locals was wrong

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,7 @@
                           (if (stringp d) d (car d))))))))
  (haskell-mode . ((eval .
    (progn
-     (setq flycheck-ghc-search-path (list (concat dir-local-curdir "/bottlelib")))
-     (setq flycheck-ghc-search-path (list (concat dir-local-curdir "/submodules/AlgoW")))
+     (setq flycheck-ghc-search-path (list (concat dir-local-curdir "/submodules/AlgoW")
+                                          (concat dir-local-curdir "/bottlelib")))
      (setq flycheck-idle-change-delay 1.0)))))
 )
