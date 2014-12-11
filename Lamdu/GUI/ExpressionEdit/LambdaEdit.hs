9edf33a98a42ff3f1849a5bf12697ec39dacc14f
Yair Chuchem <yairchu@gmail.com>
2014-12-11 19:27:33 +0200

    resolve merge causing redundant import

--- Lamdu/GUI/ExpressionEdit/LambdaEdit.hs
+++ Lamdu/GUI/ExpressionEdit/LambdaEdit.hs
@@ -9,13 +9,11 @@ import Lamdu.GUI.ExpressionGui (ExpressionGui)
 import Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
 import Lamdu.Sugar.AddNames.Types (Name(..))
 import qualified Graphics.UI.Bottle.Widget as Widget
-import qualified Lamdu.Config as Config
 import qualified Lamdu.GUI.BottleWidgets as BWidgets
 import qualified Lamdu.GUI.ExpressionEdit.Parens as Parens
 import qualified Lamdu.GUI.ExpressionGui as ExpressionGui
 import qualified Lamdu.GUI.ExpressionGui.Monad as ExprGuiM
 import qualified Lamdu.GUI.ParamEdit as ParamEdit
-import qualified Lamdu.GUI.WidgetEnvT as WE
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import qualified Lamdu.Sugar.Types as Sugar
 
