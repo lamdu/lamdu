lamdu/lamdu

commit 112a5da7bdddfcb1863005ed7bc519eb170396cd

Remove redundant use of makeBridge

Lamdu/GUI/ExpressionGui.hs
@@ -227,16 +227,6 @@ parentExprFDConfig config = FocusDelegator.Config
 disallowedNameChars :: String
 disallowedNameChars = "[]\\`()"
 
--- TODO: Move to BWidgets
-makeBridge ::
-  MonadA m =>
-  (Widget.Id -> WE.WidgetEnvT m (Widget f)) ->
-  (Widget.Id -> WE.WidgetEnvT m (Widget f)) ->
-  Widget.Id -> WE.WidgetEnvT m (Widget f)
-makeBridge mkFocused mkUnfocused myId = do
-  isFocused <- WE.isSubCursor myId
-  (if isFocused then mkFocused else mkUnfocused) myId
-
 nameEditFDConfig :: FocusDelegator.Config
 nameEditFDConfig = FocusDelegator.Config
   { FocusDelegator.startDelegatingKeys = [ModKey mempty GLFW.Key'Enter]
@@ -265,18 +255,15 @@ makeNameEdit (Name nameSrc nameCollision setName name) myId = do
     ExprGuiM.widgetEnv . makeCollisionSuffixLabels nameCollision $
     Widget.toAnimId myId
   nameEdit <-
-    ExprGuiM.widgetEnv .
-    WE.localEnv (WE.envTextStyle . TextEdit.sEmptyFocusedString .~ "") $
-    makeEditor nameProp
+    makeWordEdit (Property storedName setName) myId
+    & WE.localEnv (WE.envTextStyle . TextEdit.sEmptyFocusedString .~ "")
+    & ExprGuiM.widgetEnv
   return . Box.hboxCentered $ nameEdit : collisionSuffixes
   where
-    nameProp = Property storedName setName
     storedName =
       case nameSrc of
       NameSourceAutoGenerated -> ""
       NameSourceStored -> name
-    makeEditor property =
-      makeBridge (makeWordEdit property) (BWidgets.makeFocusableTextView name) myId
     makeWordEdit =
       BWidgets.makeWordEdit <&>
       Lens.mapped . Lens.mapped . Widget.wEventMap %~

commit 5039c128b3ffc31a7ba214433b992e96b8893a32

Oops, commit 112a5da7bdddfcb1863005ed7bc519eb170396cd broke auto-generated names. Fix it

Lamdu/GUI/ExpressionGui.hs
@@ -258,10 +258,13 @@ makeNameEdit (Name nameSrc nameCollision setName name) myId = do
     Widget.toAnimId myId
   nameEdit <-
     makeWordEdit (Property storedName setName) myId
-    & WE.localEnv (WE.envTextStyle . TextEdit.sEmptyFocusedString .~ "")
+    & WE.localEnv emptyStringEnv
     & ExprGuiM.widgetEnv
   return . Box.hboxCentered $ nameEdit : collisionSuffixes
   where
+    emptyStringEnv env = env
+      & WE.envTextStyle . TextEdit.sEmptyFocusedString .~ ""
+      & WE.envTextStyle . TextEdit.sEmptyUnfocusedString .~ name
     storedName =
       case nameSrc of
       NameSourceAutoGenerated -> ""