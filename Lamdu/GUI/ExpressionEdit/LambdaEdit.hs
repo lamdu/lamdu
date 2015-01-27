e2e7cc24d9bbaae8f57bf6865bc4f2940b947a16
Eyal Lotem <eyal.lotem@gmail.com>
2015-01-28 00:15:21 +0200

    LambdaEdit: 4-space indent. Style

--- Lamdu/GUI/ExpressionEdit/LambdaEdit.hs
+++ Lamdu/GUI/ExpressionEdit/LambdaEdit.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lamdu.GUI.ExpressionEdit.LambdaEdit
-  ( make
-  ) where
+    ( make
+    ) where
 
 import           Control.Lens.Operators
 import           Control.Lens.Tuple
@@ -17,33 +17,33 @@ import           Lamdu.Sugar.AddNames.Types (Name(..))
 import qualified Lamdu.Sugar.Types as Sugar
 
 make ::
-  MonadA m =>
-  ExpressionGui.ParentPrecedence ->
-  Sugar.Binder (Name m) m (ExprGuiM.SugarExpr m) ->
-  Sugar.Payload m ExprGuiM.Payload ->
-  ExprGuiM m (ExpressionGui m)
+    MonadA m =>
+    ExpressionGui.ParentPrecedence ->
+    Sugar.Binder (Name m) m (ExprGuiM.SugarExpr m) ->
+    Sugar.Payload m ExprGuiM.Payload ->
+    ExprGuiM m (ExpressionGui m)
 make parentPrecedence binder pl =
-  ExpressionGui.stdWrapParenify plNoType parentPrecedence (ExpressionGui.MyPrecedence 0) $
-  \myId -> ExprGuiM.assignCursor myId bodyId $
+    ExpressionGui.stdWrapParenify plNoType parentPrecedence
+    (ExpressionGui.MyPrecedence 0) $ \myId ->
+    ExprGuiM.assignCursor myId bodyId $
     do
-      paramsEdit <-
-        BinderEdit.makeParamsEdit showParamType (ExprGuiM.nextHolesBefore body) bodyId params
-        <&> map (ExpressionGui.egAlignment . _1 .~ 0.5)
-        >>= ExpressionGui.vboxTopFocalSpaced
-        >>=
-          case params of
-          Sugar.FieldParams _ -> ExpressionGui.addValFrame myId
-          _ -> return
-      arrowLabel <- ExpressionGui.grammarLabel "→" $ Widget.toAnimId myId
-      bodyEdit <- BinderEdit.makeResultEdit (binder ^. Sugar.dMActions) params body
-      mWheresEdit <-
-        BinderEdit.makeWheres (binder ^. Sugar.dWhereItems) myId
-      ExpressionGui.hboxSpaced [paramsEdit, arrowLabel, bodyEdit]
-        <&> maybe id (ExpressionGui.addBelow 0 . (:[]) . (,) 0) mWheresEdit
-  where
-    params = binder ^. Sugar.dParams
-    body = binder ^. Sugar.dBody
-    -- We show the param type instead of the lambda type
-    showParamType = pl ^. Sugar.plData . ExprGuiM.plShowType
-    plNoType = pl & Sugar.plData . ExprGuiM.plShowType .~ ExprGuiM.DoNotShowType
-    bodyId = WidgetIds.fromExprPayload $ body ^. Sugar.rPayload
+        paramsEdit <-
+            BinderEdit.makeParamsEdit showParamType (ExprGuiM.nextHolesBefore body) bodyId params
+            <&> map (ExpressionGui.egAlignment . _1 .~ 0.5)
+            >>= ExpressionGui.vboxTopFocalSpaced
+            >>= case params of
+                    Sugar.FieldParams _ -> ExpressionGui.addValFrame myId
+                    _ -> return
+        arrowLabel <- ExpressionGui.grammarLabel "→" $ Widget.toAnimId myId
+        bodyEdit <- BinderEdit.makeResultEdit (binder ^. Sugar.dMActions) params body
+        mWheresEdit <-
+            BinderEdit.makeWheres (binder ^. Sugar.dWhereItems) myId
+        ExpressionGui.hboxSpaced [paramsEdit, arrowLabel, bodyEdit]
+            <&> maybe id (ExpressionGui.addBelow 0 . (:[]) . (,) 0) mWheresEdit
+    where
+        params = binder ^. Sugar.dParams
+        body = binder ^. Sugar.dBody
+        -- We show the param type instead of the lambda type
+        showParamType = pl ^. Sugar.plData . ExprGuiM.plShowType
+        plNoType = pl & Sugar.plData . ExprGuiM.plShowType .~ ExprGuiM.DoNotShowType
+        bodyId = WidgetIds.fromExprPayload $ body ^. Sugar.rPayload
