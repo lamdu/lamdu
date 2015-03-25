53c8f753e28b9f6e088e6904be468afb933c1854
Yair Chuchem <yairchu@gmail.com>
2015-03-25 16:36:01 +0200

    Eval

---
+++ Lamdu/Expr/Eval.hs
@@ -0,0 +1,201 @@
+{-# LANGUAGE DeriveFunctor, GeneralizedNewtypeDeriving, OverloadedStrings, TemplateHaskell #-}
+
+module Lamdu.Expr.Eval
+    ( EvalT(..)
+    , EvalState, EvalActions(..), initialState
+    , evalError
+    , ThunkSrc(..), ValHead(..), ThunkId, Closure, ClosureId
+    , whnf, whnfVar
+    ) where
+
+import Control.Applicative (Applicative)
+import Control.Lens (at, use, ix, traverse)
+import Control.Lens.Operators
+import Control.Monad (void)
+import Control.Monad.State (MonadState(..))
+import Control.Monad.Trans.Class (MonadTrans(..))
+import Control.Monad.Trans.Either (EitherT(..), left)
+import Control.Monad.Trans.State (StateT(..))
+import Data.Map (Map)
+import Lamdu.Data.Definition (FFIName)
+import Lamdu.Expr.Val (Val)
+import qualified Control.Lens as Lens
+import qualified Data.Map as Map
+import qualified Lamdu.Expr.Val as V
+
+type ClosureId = Int
+type AnonThunkId = Int
+
+data LocalVar = LocalVar
+    { _lClosure :: ClosureId
+    , _lVar :: V.Var
+    } deriving (Eq, Ord, Show)
+
+data ThunkId
+    = TGlobal V.GlobalId
+    | TLocalVar LocalVar
+    | TAnon AnonThunkId
+    deriving (Eq, Ord, Show)
+
+data ThunkSrc pl = ThunkSrc
+    { _srcMClosure :: Maybe ClosureId
+    , _srcExpr :: Val pl
+    } deriving (Functor, Show)
+
+data Closure pl = Closure
+    { _cOuterScope :: Maybe ClosureId
+    , _cLam :: V.Lam (Val pl)
+    } deriving (Functor, Show)
+
+data ValHead pl
+    = HAbs (Closure pl)
+    | HRecExtend (V.RecExtend ThunkId)
+    | HRecEmpty
+    | HLiteralInteger Integer
+    | HBuiltin FFIName
+    deriving (Functor, Show)
+
+data ThunkState srcPayload
+    = TSrc (ThunkSrc srcPayload)
+    | TResult (ValHead srcPayload)
+    | TEvaluating -- BlackHoled in GHC
+    deriving (Functor, Show)
+
+data EvalActions m pl = EvalActions
+    { _aLogProgress :: ThunkSrc pl -> ValHead pl -> m ()
+    , _aRunBuiltin :: FFIName -> ThunkId -> EvalT pl m (ValHead pl)
+    , _aLoadGlobal :: V.GlobalId -> m (Maybe (Either FFIName (Val pl)))
+    }
+
+data EvalState m srcPayload = EvalState
+    { _esThunks :: Map ThunkId (ThunkState srcPayload)
+    , _esThunkCounter :: AnonThunkId
+    , _esClosureParent :: Map ClosureId ClosureId
+    , _esClosureCounter :: ClosureId
+    , -- esReader inside state used instead of adding a ReaderT
+      _esReader :: EvalActions m srcPayload
+    }
+
+newtype EvalT srcPayload m a =
+    EvalT { runEvalT :: EitherT String (StateT (EvalState m srcPayload) m) a }
+    deriving (Functor, Applicative, Monad, MonadState (EvalState m srcPayload))
+
+instance MonadTrans (EvalT pl) where
+    lift = EvalT . lift . lift
+
+Lens.makeLenses ''Closure
+Lens.makeLenses ''EvalActions
+Lens.makeLenses ''EvalState
+Lens.makePrisms ''ThunkId
+
+evalError :: Monad m => String -> EvalT pl m a
+evalError = EvalT . left
+
+whnf :: Monad m => ThunkSrc pl -> EvalT pl m (ValHead pl)
+whnf src =
+    do
+        result <- calcWhnf src
+        logProgress <- use $ esReader . aLogProgress
+        lift $ logProgress src result
+        return result
+
+calcWhnf :: Monad m => ThunkSrc pl -> EvalT pl m (ValHead pl)
+calcWhnf (ThunkSrc mClosureId expr) =
+    case expr ^. V.body of
+    V.BAbs lam -> return $ HAbs $ Closure mClosureId lam
+    V.BApp (V.Apply funcExpr argExpr) ->
+        do
+            func <- whnf $ ThunkSrc mClosureId funcExpr
+            case func of
+                HAbs closure -> whnfApplyLam closure (ThunkSrc mClosureId argExpr)
+                HBuiltin ffiname ->
+                    do
+                        runBuiltin <- use $ esReader . aRunBuiltin
+                        argThunk <- makeThunk mClosureId argExpr
+                        runBuiltin ffiname argThunk
+                _ -> evalError "Apply on non function"
+    V.BGetField (V.GetField recordExpr tag) ->
+        do
+            record <- whnf $ ThunkSrc mClosureId recordExpr
+            whnfGetField $ V.GetField record tag
+    V.BRecExtend recExtend ->
+        recExtend & traverse %%~ makeThunk mClosureId <&> HRecExtend
+    V.BLeaf (V.LGlobal global) -> whnfVar $ TGlobal global
+    V.BLeaf (V.LVar var) ->
+        case mClosureId of
+        Nothing -> evalError "LVar in outer scope"
+        Just closureId -> whnfVar $ TLocalVar $ LocalVar closureId var
+    V.BLeaf V.LRecEmpty -> return HRecEmpty
+    V.BLeaf (V.LLiteralInteger i) -> HLiteralInteger i & return
+    V.BLeaf V.LHole -> evalError "Hole"
+
+whnfApplyLam :: Monad m => Closure pl -> ThunkSrc pl -> EvalT pl m (ValHead pl)
+whnfApplyLam func arg =
+    do
+        closureId <- use esClosureCounter
+        esClosureCounter += 1
+        esClosureParent . at closureId .= func ^. cOuterScope
+        let var = LocalVar closureId $ func ^. cLam . V.lamParamId
+        esThunks . at (TLocalVar var) .= Just (TSrc arg)
+        whnf $ ThunkSrc (Just closureId) (func ^. cLam . V.lamResult)
+
+makeThunk :: Monad m => Maybe ClosureId -> Val pl -> EvalT pl m ThunkId
+makeThunk mClosureId expr =
+    do
+        thunkId <- use esThunkCounter <&> TAnon
+        esThunkCounter += 1
+        esThunks . at thunkId .= Just (TSrc (ThunkSrc mClosureId expr))
+        return thunkId
+
+whnfGetField :: Monad m => V.GetField (ValHead pl) -> EvalT pl m (ValHead pl)
+whnfGetField (V.GetField (HRecExtend (V.RecExtend otherTag otherVal restRef)) tag)
+    | tag == otherTag = whnfVar otherVal
+    | otherwise =
+        whnfVar restRef
+        <&> (`V.GetField` tag)
+        >>= whnfGetField
+whnfGetField (V.GetField val _) =
+    evalError $ "GetField of value without the field " ++ show (void val)
+
+whnfVar :: Monad m => ThunkId -> EvalT pl m (ValHead pl)
+whnfVar var = do
+    thunkState <- use $ esThunks . at var
+    case thunkState of
+        Just TEvaluating -> evalError "TODO: recursive whnfVar"
+        Just (TResult r) -> return r
+        Just (TSrc thunkSrc) -> whnf thunkSrc >>= newResult
+        Nothing ->
+            case var of
+            TGlobal g -> loadGlobal g >>= newResult
+            TLocalVar (LocalVar closureId lvar) ->
+                do
+                    closureParents <- use $ esClosureParent
+                    case closureParents ^. at closureId of
+                        Just parentClosure -> whnfVar $ TLocalVar (LocalVar parentClosure lvar)
+                        Nothing -> evalError $ "Var not found " ++ show var
+            TAnon i -> evalError $ "BUG: Referenced non-existing thunk " ++ show i
+    where
+        newResult res =
+            do
+                esThunks . ix var .= TResult res
+                return res
+
+loadGlobal :: Monad m => V.GlobalId -> EvalT pl m (ValHead pl)
+loadGlobal g =
+    do
+        loader <- use $ esReader . aLoadGlobal
+        res <- lift $ loader g
+        case res of
+            Nothing -> evalError $ "Global not found " ++ show g
+            Just (Left name) -> return $ HBuiltin name
+            Just (Right expr) -> whnf $ ThunkSrc Nothing expr
+
+initialState :: EvalActions m srcPayload -> EvalState m srcPayload
+initialState actions =
+    EvalState
+    { _esThunks = Map.empty
+    , _esThunkCounter = 0
+    , _esClosureParent = Map.empty
+    , _esClosureCounter = 0
+    , _esReader = actions
+    }
---
+++ test/EvalTest.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Lamdu.Expr.Eval
+
+import Control.Lens
+import Control.Monad.Trans.Either (EitherT(..))
+import Control.Monad.Trans.State (evalStateT)
+import Control.Monad.Writer
+import Lamdu.Data.Definition
+import qualified Data.Map as Map
+import qualified Lamdu.Expr.Pure as P
+
+test :: (Either String (ValHead ()), [(ThunkSrc (), ValHead ())])
+test =
+    evalStateT (runEitherT (runEvalT (whnf (ThunkSrc Nothing expr)))) (initialState actions)
+    & runWriter
+    where
+        expr = P.app (P.global "f") (P.global "v")
+        actions =
+            EvalActions
+            { _aLogProgress = \s r -> tell [(s, r)]
+            , _aRunBuiltin = runBuiltin
+            , _aLoadGlobal = return . (`Map.lookup` globals)
+            }
+        globals =
+            Map.fromList
+            [ ("f", Right $ P.abs "r" $ P.getField (P.var "r") "y")
+            , ("g", Right $ P.app (P.global "negate") $ P.litInt 3)
+            , ("negate", Left $ FFIName ["Prelude"] "negate")
+            , ( "v"
+              , Right $
+                P.recExtend "x" (P.litInt 5) $
+                P.recExtend "y" (P.global "g") P.recEmpty
+              )
+            ]
+        runBuiltin (FFIName _ name) argThunk
+            | name == "negate" =
+                do
+                    arg <- whnfVar argThunk
+                    case arg of
+                        HLiteralInteger i -> return $ HLiteralInteger $ negate i
+                        _ -> evalError $ "negate expected integer not " ++ show arg
+            | otherwise = evalError "Builtin not implemented"
