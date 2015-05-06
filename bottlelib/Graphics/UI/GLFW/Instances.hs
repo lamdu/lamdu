10aae0dffe52286f5eb0ae32189350d33207fed4
Yair Chuchem <yairchu@gmail.com>
2015-05-06 23:35:04 +0300

    Use GLFW-b >= 1.4.7 which already has Generic instances instead of Adding them in Graphics.UI.GLFW.Instances

--- bottlelib/Graphics/UI/GLFW/Instances.hs
+++ bottlelib/Graphics/UI/GLFW/Instances.hs
@@ -5,13 +5,10 @@ module Graphics.UI.GLFW.Instances
   ) where
 
 import Data.Aeson (ToJSON(..), FromJSON(..))
-import GHC.Generics (Generic)
 import Graphics.UI.GLFW (Key(..), ModifierKeys(..))
 
-deriving instance Generic Key
 instance ToJSON Key
 instance FromJSON Key
 
-deriving instance Generic ModifierKeys
 instance ToJSON ModifierKeys
 instance FromJSON ModifierKeys
