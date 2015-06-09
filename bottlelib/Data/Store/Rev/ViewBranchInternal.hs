3d1241ace97fbae6de1dc88d89df339c139c4e2e
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-09 18:13:29 +0300

    ViewBranchInternal: 4-space indent

--- bottlelib/Data/Store/Rev/ViewBranchInternal.hs
+++ bottlelib/Data/Store/Rev/ViewBranchInternal.hs
@@ -4,12 +4,12 @@
 -- | contains the parts of both that both may depend on, to avoid the
 -- | cycle.
 module Data.Store.Rev.ViewBranchInternal
-  ( ViewData(..), vdBranch
-  , View(..)
-  , BranchData(..), brVersion, brViews
-  , Branch(..)
-  , moveView, applyChangesToView, makeViewKey
-  )
+    ( ViewData(..), vdBranch
+    , View(..)
+    , BranchData(..), brVersion, brViews
+    , Branch(..)
+    , moveView, applyChangesToView, makeViewKey
+    )
 where
 
 import Control.Monad (when)
@@ -32,19 +32,19 @@ import qualified Data.Store.Transaction as Transaction
 -- This key is XOR'd with object keys to yield the IRef to each
 -- object's current version ref:
 newtype View m = View (IRef m (ViewData m))
-  deriving (Eq, Ord, Binary, Show, Read)
+    deriving (Eq, Ord, Binary, Show, Read)
 
 data BranchData m = BranchData
-  { _brVersion :: Version m
-  , _brViews :: [View m]
-  } deriving (Eq, Ord, Read, Show, Generic)
+    { _brVersion :: Version m
+    , _brViews :: [View m]
+    } deriving (Eq, Ord, Read, Show, Generic)
 instance Binary (BranchData m)
 
 newtype Branch m = Branch { unBranch :: IRef m (BranchData m) }
-  deriving (Eq, Ord, Read, Show, Binary)
+    deriving (Eq, Ord, Read, Show, Binary)
 
 newtype ViewData m = ViewData { _vdBranch :: Branch m }
-  deriving (Eq, Ord, Show, Read, Binary)
+    deriving (Eq, Ord, Show, Read, Binary)
 
 Lens.makeLenses ''BranchData
 Lens.makeLenses ''ViewData
@@ -53,25 +53,26 @@ Lens.makeLenses ''ViewData
 -- | or it will result in undefined results!
 moveView :: MonadA m => View m -> Version m -> Version m -> Transaction m ()
 moveView vm srcVersion destVersion =
-  when (srcVersion /= destVersion) $ do
-    mraIRef <- Version.mostRecentAncestor srcVersion destVersion
-    Version.walkUp applyBackward mraIRef srcVersion
-    Version.walkDown applyForward mraIRef destVersion
-  where
-    applyForward = apply Change.newValue
-    applyBackward = apply Change.oldValue
-    apply changeDir version = applyChangesToView vm changeDir . Version.changes $ version
+    when (srcVersion /= destVersion) $ do
+        mraIRef <- Version.mostRecentAncestor srcVersion destVersion
+        Version.walkUp applyBackward mraIRef srcVersion
+        Version.walkDown applyForward mraIRef destVersion
+    where
+        applyForward = apply Change.newValue
+        applyBackward = apply Change.oldValue
+        apply changeDir version = applyChangesToView vm changeDir . Version.changes $ version
 
 makeViewKey :: View m -> Change.Key -> Guid
 makeViewKey (View iref) = Guid.combine . IRef.guid $ iref
 
 applyChangesToView ::
-  MonadA m => View m -> (Change -> Maybe Change.Value) ->
-  [Change] -> Transaction m ()
+    MonadA m => View m -> (Change -> Maybe Change.Value) ->
+    [Change] -> Transaction m ()
 applyChangesToView vm changeDir = traverse_ applyChange
-  where
-    applyChange change = setValue
-                         (makeViewKey vm $ Change.objectKey change)
-                         (changeDir change)
-    setValue key Nothing      = Transaction.delete key
-    setValue key (Just value) = Transaction.insertBS key value
+    where
+        applyChange change =
+            setValue
+            (makeViewKey vm $ Change.objectKey change)
+            (changeDir change)
+        setValue key Nothing = Transaction.delete key
+        setValue key (Just value) = Transaction.insertBS key value
