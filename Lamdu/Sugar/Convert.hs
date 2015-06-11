41f70a63098589aa19c987a00ea1c75b37cf8e90
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:34:51 +0300

    Sugar.Convert: 4-space indent

--- Lamdu/Sugar/Convert.hs
+++ Lamdu/Sugar/Convert.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts, OverloadedStrings, TypeFamilies, Rank2Types #-}
 module Lamdu.Sugar.Convert
-  ( convertDefI
-  ) where
+    ( convertDefI
+    ) where
 
 import           Control.MonadA (MonadA)
 import           Data.Store.Guid (Guid)
@@ -20,32 +20,33 @@ import qualified Lamdu.Sugar.Internal.EntityId as EntityId
 import           Lamdu.Sugar.Types
 
 convertDefIBuiltin ::
-  MonadA m => Definition.Builtin -> DefI m ->
-  DefinitionBody Guid m (ExpressionU m [EntityId])
+    MonadA m => Definition.Builtin -> DefI m ->
+    DefinitionBody Guid m (ExpressionU m [EntityId])
 convertDefIBuiltin (Definition.Builtin name scheme) defI =
-  DefinitionBodyBuiltin DefinitionBuiltin
+    DefinitionBodyBuiltin DefinitionBuiltin
     { _biName = name
     , _biSetName = setName
     , _biType = scheme
     }
-  where
-    setName =
-      Transaction.writeIRef defI .
-      Definition.BodyBuiltin . (`Definition.Builtin` scheme)
+    where
+        setName =
+            Transaction.writeIRef defI .
+            Definition.BodyBuiltin . (`Definition.Builtin` scheme)
 
 convertDefI ::
-  MonadA m => EvalResults (ExprIRef.ValI m) -> Anchors.CodeProps m ->
-  Definition.Definition (Val (ExprIRef.ValIProperty m)) (DefI m) ->
-  Transaction m (DefinitionU m [EntityId])
-convertDefI evalMap cp (Definition.Definition body defI) = do
-  bodyS <- convertDefBody body
-  return Definition
-    { _drEntityId = EntityId.ofIRef defI
-    , _drName = UniqueId.toGuid defI
-    , _drBody = bodyS
-    }
-  where
-    convertDefBody (Definition.BodyBuiltin builtin) =
-      return $ convertDefIBuiltin builtin defI
-    convertDefBody (Definition.BodyExpr expr) =
-      ConvertDefExpr.convert evalMap cp expr defI
+    MonadA m => EvalResults (ExprIRef.ValI m) -> Anchors.CodeProps m ->
+    Definition.Definition (Val (ExprIRef.ValIProperty m)) (DefI m) ->
+    Transaction m (DefinitionU m [EntityId])
+convertDefI evalMap cp (Definition.Definition body defI) =
+    do
+        bodyS <- convertDefBody body
+        return Definition
+            { _drEntityId = EntityId.ofIRef defI
+            , _drName = UniqueId.toGuid defI
+            , _drBody = bodyS
+            }
+    where
+        convertDefBody (Definition.BodyBuiltin builtin) =
+            return $ convertDefIBuiltin builtin defI
+        convertDefBody (Definition.BodyExpr expr) =
+            ConvertDefExpr.convert evalMap cp expr defI
