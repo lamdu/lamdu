bc3d2870d27b34c6ae0418b9912f9a3aeeb10349
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:35:45 +0300

    Sugar.EntityId: 4-space indent

--- Lamdu/Sugar/EntityId.hs
+++ Lamdu/Sugar/EntityId.hs
@@ -1,5 +1,5 @@
 module Lamdu.Sugar.EntityId
-  ( module Lamdu.Sugar.Internal.EntityId
-  ) where
+    ( module Lamdu.Sugar.Internal.EntityId
+    ) where
 
 import Lamdu.Sugar.Internal.EntityId (EntityId, bs)
