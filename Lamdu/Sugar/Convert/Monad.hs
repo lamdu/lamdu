32ee366eda5407248d48394a8a78f06468473eb1
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:21:08 +0300

    Sugar.Convert.Monad: 4-space indent

--- Lamdu/Sugar/Convert/Monad.hs
+++ Lamdu/Sugar/Convert/Monad.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, TemplateHaskell, PolymorphicComponents, ConstraintKinds, RecordWildCards #-}
 module Lamdu.Sugar.Convert.Monad
-  ( Context(..), TagParamInfo(..)
-  , scInferContext, scReinferCheckDefinition, scDefI
-  , scCodeAnchors, scSpecialFunctions, scTagParamInfos
-  , ConvertM(..), run
-  , readContext, liftTransaction, local
-  , codeAnchor
-  , getP
-  , convertSubexpression
-  , typeProtectTransaction, typeProtectedSetToVal, wrapOnTypeError
-  ) where
+    ( Context(..), TagParamInfo(..)
+    , scInferContext, scReinferCheckDefinition, scDefI
+    , scCodeAnchors, scSpecialFunctions, scTagParamInfos
+    , ConvertM(..), run
+    , readContext, liftTransaction, local
+    , codeAnchor
+    , getP
+    , convertSubexpression
+    , typeProtectTransaction, typeProtectedSetToVal, wrapOnTypeError
+    ) where
 
 import Control.Applicative (Applicative(..), (<$>))
 import Control.Lens ((^.))
@@ -37,69 +37,69 @@ import qualified Lamdu.Sugar.Types as Sugar
 type T = Transaction
 
 data TagParamInfo = TagParamInfo
-  { tpiFromParameters :: V.Var -- TODO: Rename "From" to something else
-  , tpiJumpTo :: Sugar.EntityId
-  }
+    { tpiFromParameters :: V.Var -- TODO: Rename "From" to something else
+    , tpiJumpTo :: Sugar.EntityId
+    }
 
 newtype ConvertM m a = ConvertM (ReaderT (Context m) (T m) a)
-  deriving (Functor, Applicative, Monad)
+    deriving (Functor, Applicative, Monad)
 
 data Context m = Context
-  { _scInferContext :: Infer.Context
-  , _scDefI :: ExprIRef.DefI m
-  , _scCodeAnchors :: Anchors.CodeProps m
-  , _scSpecialFunctions :: Anchors.SpecialFunctions m
-  , _scTagParamInfos :: Map T.Tag TagParamInfo -- tag guids
-    -- TODO: scTagParamInfos needs a reverse-lookup map too
-  , -- Check whether the definition is valid after an edit,
-    -- so that can hole-wrap bad edits.
-    _scReinferCheckDefinition :: T m Bool
-  , scConvertSubexpression ::
-       forall a. Monoid a => Val (Input.Payload m a) -> ConvertM m (ExpressionU m a)
-  }
+    { _scInferContext :: Infer.Context
+    , _scDefI :: ExprIRef.DefI m
+    , _scCodeAnchors :: Anchors.CodeProps m
+    , _scSpecialFunctions :: Anchors.SpecialFunctions m
+    , _scTagParamInfos :: Map T.Tag TagParamInfo -- tag guids
+        -- TODO: scTagParamInfos needs a reverse-lookup map too
+    , -- Check whether the definition is valid after an edit,
+        -- so that can hole-wrap bad edits.
+        _scReinferCheckDefinition :: T m Bool
+    , scConvertSubexpression ::
+              forall a. Monoid a => Val (Input.Payload m a) -> ConvertM m (ExpressionU m a)
+    }
 Lens.makeLenses ''Context
 
 typeProtectTransaction :: MonadA m => ConvertM m (T m a -> T m (Maybe a))
 typeProtectTransaction =
-  do
-    checkOk <- (^. scReinferCheckDefinition) <$> readContext
-    let
-      protect act =
-        do
-          (resume, changes) <-
-            Transaction.fork $ do
-              result <- act
-              isOk <- checkOk
-              return $
-                if isOk
-                then (>> return (Just result)) . Transaction.merge
-                else const $ return Nothing
-          resume changes
-    return protect
+    do
+        checkOk <- (^. scReinferCheckDefinition) <$> readContext
+        let protect act =
+                do
+                    (resume, changes) <-
+                        Transaction.fork $
+                        do
+                            result <- act
+                            isOk <- checkOk
+                            return $
+                                if isOk
+                                then (>> return (Just result)) . Transaction.merge
+                                else const $ return Nothing
+                    resume changes
+        return protect
 
 typeProtectedSetToVal ::
-  MonadA m =>
-  ConvertM m
-  (ExprIRef.ValIProperty m -> ExprIRef.ValI m -> T m (ExprIRef.ValI m))
-typeProtectedSetToVal = do
-  typeProtect <- typeProtectTransaction
-  let
-    setToVal dest valI =
-      do
-        mResult <- typeProtect $ DataOps.replace dest valI
-        case mResult of
-          Just result -> return result
-          Nothing -> DataOps.setToWrapper valI dest
-  return setToVal
+    MonadA m =>
+    ConvertM m
+    (ExprIRef.ValIProperty m -> ExprIRef.ValI m -> T m (ExprIRef.ValI m))
+typeProtectedSetToVal =
+    do
+        typeProtect <- typeProtectTransaction
+        let setToVal dest valI =
+                do
+                    mResult <- typeProtect $ DataOps.replace dest valI
+                    case mResult of
+                        Just result -> return result
+                        Nothing -> DataOps.setToWrapper valI dest
+        return setToVal
 
 wrapOnTypeError ::
-  MonadA m =>
-  ConvertM m (ExprIRef.ValIProperty m -> T m (ExprIRef.ValI m))
+    MonadA m =>
+    ConvertM m (ExprIRef.ValIProperty m -> T m (ExprIRef.ValI m))
 wrapOnTypeError =
-  do
-    protectedSetToVal <- typeProtectedSetToVal
-    let wrap prop = protectedSetToVal prop (Property.value prop)
-    return wrap
+    do
+        protectedSetToVal <- typeProtectedSetToVal
+        let wrap prop = protectedSetToVal prop (Property.value prop)
+        return wrap
 
 run :: MonadA m => Context m -> ConvertM m a -> T m a
 run ctx (ConvertM action) = runReaderT action ctx
@@ -120,6 +120,7 @@ getP :: MonadA m => Transaction.MkProperty m a -> ConvertM m a
 getP = liftTransaction . Transaction.getP
 
 convertSubexpression :: (MonadA m, Monoid a) => Val (Input.Payload m a) -> ConvertM m (ExpressionU m a)
-convertSubexpression exprI = do
-  convertSub <- scConvertSubexpression <$> readContext
-  convertSub exprI
+convertSubexpression exprI =
+    do
+        convertSub <- scConvertSubexpression <$> readContext
+        convertSub exprI
