2beb7d0ed6b3e5dffda134f7dd1e247a05b729e5
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 14:12:19 +0300

    Lamdu.Data.Anchors: 4-space indent

--- Lamdu/Data/Anchors.hs
+++ Lamdu/Data/Anchors.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE Rank2Types, OverloadedStrings, DeriveGeneric #-}
 module Lamdu.Data.Anchors
-  ( Code(..), onCode
-  , Revision(..), onRevision
-  , Pane, makePane
-  , CodeProps, RevisionProps
-  , assocNameRef
-  , PresentationMode(..)
-  , assocPresentationMode
-  , SpecialFunctions(..)
-  , assocTagOrder
-  ) where
+    ( Code(..), onCode
+    , Revision(..), onRevision
+    , Pane, makePane
+    , CodeProps, RevisionProps
+    , assocNameRef
+    , PresentationMode(..)
+    , assocPresentationMode
+    , SpecialFunctions(..)
+    , assocTagOrder
+    ) where
 
 import Control.MonadA (MonadA)
 import Data.Binary (Binary)
@@ -26,41 +26,41 @@ import qualified Lamdu.Expr.Type as T
 import qualified Lamdu.Expr.UniqueId as UniqueId
 
 data SpecialFunctions m = SpecialFunctions
-  { sfNil :: DefI m
-  , sfCons :: DefI m
-  , sfHeadTag :: T.Tag
-  , sfTailTag :: T.Tag
-  , sfTrue :: DefI m
-  , sfFalse :: DefI m
-  } deriving (Generic)
+    { sfNil :: DefI m
+    , sfCons :: DefI m
+    , sfHeadTag :: T.Tag
+    , sfTailTag :: T.Tag
+    , sfTrue :: DefI m
+    , sfFalse :: DefI m
+    } deriving (Generic)
 instance Binary (SpecialFunctions m)
 
 type Pane m = DefI m
 
 data Code f m = Code
-  { panes :: f [Pane m]
-  , clipboards :: f [DefI m]
-  , globals :: f [DefI m]
-  , specialFunctions :: f (SpecialFunctions m)
-  , preJumps :: f [WidgetId.Id]
-  , preCursor :: f WidgetId.Id
-  , postCursor :: f WidgetId.Id
-  , tags :: f [T.Tag]
-  }
+    { panes :: f [Pane m]
+    , clipboards :: f [DefI m]
+    , globals :: f [DefI m]
+    , specialFunctions :: f (SpecialFunctions m)
+    , preJumps :: f [WidgetId.Id]
+    , preCursor :: f WidgetId.Id
+    , postCursor :: f WidgetId.Id
+    , tags :: f [T.Tag]
+    }
 onCode :: (forall a. Binary a => f a -> g a) -> Code f m -> Code g m
 onCode f (Code x0 x1 x2 x3 x4 x5 x6 x7) =
-  Code (f x0) (f x1) (f x2) (f x3) (f x4) (f x5) (f x6) (f x7)
+    Code (f x0) (f x1) (f x2) (f x3) (f x4) (f x5) (f x6) (f x7)
 
 data Revision f m = Revision
-  { branches :: f [Branch m]
-  , currentBranch :: f (Branch m)
-  , cursor :: f WidgetId.Id
-  , redos :: f [Version m]
-  , view :: f (View m)
-  }
+    { branches :: f [Branch m]
+    , currentBranch :: f (Branch m)
+    , cursor :: f WidgetId.Id
+    , redos :: f [Version m]
+    , view :: f (View m)
+    }
 onRevision :: (forall a. Binary a => f a -> g a) -> Revision f m -> Revision g m
 onRevision f (Revision x0 x1 x2 x3 x4) =
-  Revision (f x0) (f x1) (f x2) (f x3) (f x4)
+    Revision (f x0) (f x1) (f x2) (f x3) (f x4)
 
 type CodeProps m = Code (MkProperty m) m
 type RevisionProps m = Revision (MkProperty m) m
@@ -75,11 +75,11 @@ assocTagOrder :: MonadA m => T.Tag -> MkProperty m Int
 assocTagOrder = Transaction.assocDataRefDef 0 "Order" . UniqueId.toGuid
 
 data PresentationMode = OO | Verbose | Infix
-  deriving (Eq, Ord, Enum, Bounded, Show, Generic)
+    deriving (Eq, Ord, Enum, Bounded, Show, Generic)
 instance Binary PresentationMode
 
 assocPresentationMode ::
-  (UniqueId.ToGuid a, MonadA m) =>
-  a -> Transaction.MkProperty m PresentationMode
+    (UniqueId.ToGuid a, MonadA m) =>
+    a -> Transaction.MkProperty m PresentationMode
 assocPresentationMode =
     Transaction.assocDataRefDef OO "PresentationMode" . UniqueId.toGuid
