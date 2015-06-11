f9cb1526c399893048df14da8cea642a15f32913
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 14:09:31 +0300

    Lamdu.Data.Definition: 4-space indent

--- Lamdu/Data/Definition.hs
+++ Lamdu/Data/Definition.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE DeriveGeneric, DeriveFunctor, DeriveFoldable, DeriveTraversable, TemplateHaskell #-}
 module Lamdu.Data.Definition
-  ( FFIName(..)
-  , Builtin(..)
-  , ExportedType(..), _NoExportedType, _ExportedType
-  , Expr(..), expr, exprType
-  , Body(..), _BodyExpr, _BodyBuiltin
-  , Definition(..), defBody, defPayload
-  ) where
+    ( FFIName(..)
+    , Builtin(..)
+    , ExportedType(..), _NoExportedType, _ExportedType
+    , Expr(..), expr, exprType
+    , Body(..), _BodyExpr, _BodyBuiltin
+    , Definition(..), defBody, defPayload
+    ) where
 
 import qualified Control.Lens as Lens
 import           Data.Binary (Binary(..))
@@ -16,35 +16,35 @@ import           GHC.Generics (Generic)
 import           Lamdu.Expr.Scheme (Scheme)
 
 data FFIName = FFIName
-  { fModule :: [String]
-  , fName :: String
-  } deriving (Generic, Eq, Ord)
+    { fModule :: [String]
+    , fName :: String
+    } deriving (Generic, Eq, Ord)
 
 instance Show FFIName where
-  show (FFIName path name) = concatMap (++".") path ++ name
+    show (FFIName path name) = concatMap (++".") path ++ name
 
 data Builtin = Builtin
-  { bName :: FFIName
-  , bType :: Scheme
-  } deriving (Generic, Show)
+    { bName :: FFIName
+    , bType :: Scheme
+    } deriving (Generic, Show)
 
 data ExportedType = NoExportedType | ExportedType Scheme
-  deriving (Generic, Show)
+    deriving (Generic, Show)
 
 data Expr valExpr = Expr
-  { _expr :: valExpr
-  , _exprType :: ExportedType
-  } deriving (Generic, Show, Functor, Foldable, Traversable)
+    { _expr :: valExpr
+    , _exprType :: ExportedType
+    } deriving (Generic, Show, Functor, Foldable, Traversable)
 
 data Body valExpr
-  = BodyExpr (Expr valExpr)
-  | BodyBuiltin Builtin
-  deriving (Generic, Show, Functor, Foldable, Traversable)
+    = BodyExpr (Expr valExpr)
+    | BodyBuiltin Builtin
+    deriving (Generic, Show, Functor, Foldable, Traversable)
 
 data Definition valExpr a = Definition
-  { _defBody :: Body valExpr
-  , _defPayload :: a
-  } deriving (Generic, Functor, Foldable, Traversable)
+    { _defBody :: Body valExpr
+    , _defPayload :: a
+    } deriving (Generic, Functor, Foldable, Traversable)
 
 instance Binary FFIName
 instance Binary Builtin
