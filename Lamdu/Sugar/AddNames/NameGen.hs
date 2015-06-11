83fa76b5bf2c8ad5c453f92a3637291f1a6922c3
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:29:56 +0300

    Sugar.AddNames.NameGen: 4-space indent, refactor let to where

--- Lamdu/Sugar/AddNames/NameGen.hs
+++ Lamdu/Sugar/AddNames/NameGen.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Lamdu.Sugar.AddNames.NameGen
-  ( NameGen, initial
-  , IsFunction(..), existingName, newName
-  ) where
+    ( NameGen, initial
+    , IsFunction(..), existingName, newName
+    ) where
 
 import Control.Applicative ((<$>))
 import Control.Arrow ((&&&))
@@ -16,39 +16,40 @@ import qualified Data.Map as Map
 data IsFunction = Function | NotFunction
 
 data NameGen g = NameGen
-  { _ngUnusedNames :: [String]
-  , _ngUnusedFuncNames :: [String]
-  , _ngUsedNames :: Map g String
-  }
+    { _ngUnusedNames :: [String]
+    , _ngUnusedFuncNames :: [String]
+    , _ngUsedNames :: Map g String
+    }
 Lens.makeLenses ''NameGen
 
 initial :: Ord g => NameGen g
 initial =
-  NameGen indepNames indepFuncNames Map.empty
-  where
-    indepFuncNames = numberCycle ["f", "g", "h"]
-    indepNames = numberCycle ["x", "y", "z", "w", "u", "v"]
-    numberCycle s = (s ++) . concat . zipWith appendAll [0::Int ..] $ repeat s
-    appendAll num = map (++ show num)
+    NameGen indepNames indepFuncNames Map.empty
+    where
+        indepFuncNames = numberCycle ["f", "g", "h"]
+        indepNames = numberCycle ["x", "y", "z", "w", "u", "v"]
+        numberCycle s = (s ++) . concat . zipWith appendAll [0::Int ..] $ repeat s
+        appendAll num = map (++ show num)
 
 existingName :: (Ord g, Show g) => g -> State (NameGen g) String
 existingName g =
-  fromMaybe ("TodoError:" ++ show g) <$>
-  Lens.uses ngUsedNames (Map.lookup g)
+    fromMaybe ("TodoError:" ++ show g) <$>
+    Lens.uses ngUsedNames (Map.lookup g)
 
 newName :: Ord g => (String -> Bool) -> IsFunction -> g -> State (NameGen g) String
-newName acceptName isFunc g = do
-  let
-    loop = do
-      name <- state (head &&& tail)
-      if acceptName name
-        then return name
-        else loop
-  name <- Lens.zoom names loop
-  ngUsedNames %= Map.insert g name
-  return name
-  where
-    names =
-      case isFunc of
-      NotFunction -> ngUnusedNames
-      Function -> ngUnusedFuncNames
+newName acceptName isFunc g =
+    do
+        name <- Lens.zoom names loop
+        ngUsedNames %= Map.insert g name
+        return name
+    where
+        loop =
+            do
+                name <- state (head &&& tail)
+                if acceptName name
+                    then return name
+                    else loop
+        names =
+            case isFunc of
+            NotFunction -> ngUnusedNames
+            Function -> ngUnusedFuncNames
