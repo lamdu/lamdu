9c2c11cbb28053f4e0fefd2e31d87e53ccc46f73
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:39:34 +0300

    Sugar.NearestHoles: 4-space indent

--- Lamdu/Sugar/NearestHoles.hs
+++ Lamdu/Sugar/NearestHoles.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE TemplateHaskell, RankNTypes #-}
 module Lamdu.Sugar.NearestHoles
-  ( NearestHoles(..), prev, next
-  , none
-  , add
-  ) where
+    ( NearestHoles(..), prev, next
+    , none
+    , add
+    ) where
 
 import Control.Applicative.Utils (when)
 import Control.Lens (LensLike)
@@ -17,68 +17,68 @@ import qualified Lamdu.Sugar.Lens as SugarLens
 import qualified Lamdu.Sugar.Types as Sugar
 
 markStoredHoles ::
-  Sugar.Expression name m a ->
-  Sugar.Expression name m (Bool, a)
+    Sugar.Expression name m a ->
+    Sugar.Expression name m (Bool, a)
 markStoredHoles expr =
-  expr
-  <&> (,) False
-  & SugarLens.holePayloads . Sugar.plData . _1 .~ True
-  & SugarLens.subExprPayloads %~ removeNonStoredMarks
-  where
-    removeNonStoredMarks pl =
-      case pl ^. Sugar.plActions of
-      Nothing -> pl & Sugar.plData . _1 .~ False
-      Just _ -> pl
+    expr
+    <&> (,) False
+    & SugarLens.holePayloads . Sugar.plData . _1 .~ True
+    & SugarLens.subExprPayloads %~ removeNonStoredMarks
+    where
+        removeNonStoredMarks pl =
+            case pl ^. Sugar.plActions of
+            Nothing -> pl & Sugar.plData . _1 .~ False
+            Just _ -> pl
 
 data NearestHoles = NearestHoles
-  { _prev :: Maybe Sugar.EntityId
-  , _next :: Maybe Sugar.EntityId
-  } deriving (Eq, Show)
+    { _prev :: Maybe Sugar.EntityId
+    , _next :: Maybe Sugar.EntityId
+    } deriving (Eq, Show)
 Lens.makeLenses ''NearestHoles
 
 none :: NearestHoles
 none = NearestHoles Nothing Nothing
 
 add ::
-  MonadA m =>
-  (forall a b.
-   Lens.Traversal
-   (f (Sugar.Expression name m a))
-   (f (Sugar.Expression name m b))
-   (Sugar.Expression name m a)
-   (Sugar.Expression name m b)) ->
-  f (Sugar.Expression name m (NearestHoles -> r)) ->
-  f (Sugar.Expression name m r)
+    MonadA m =>
+    (forall a b.
+      Lens.Traversal
+      (f (Sugar.Expression name m a))
+      (f (Sugar.Expression name m b))
+      (Sugar.Expression name m a)
+      (Sugar.Expression name m b)) ->
+    f (Sugar.Expression name m (NearestHoles -> r)) ->
+    f (Sugar.Expression name m r)
 add exprs s =
-  s
-  & exprs . Lens.mapped %~ toNearestHoles
-  & exprs %~ markStoredHoles
-  & passAll (exprs . SugarLens.subExprPayloads)
-  & passAll (Lens.backwards (exprs . SugarLens.subExprPayloads))
-  & exprs . Lens.mapped %~ snd
-  where
-    toNearestHoles f prevHole nextHole = f (NearestHoles prevHole nextHole)
+    s
+    & exprs . Lens.mapped %~ toNearestHoles
+    & exprs %~ markStoredHoles
+    & passAll (exprs . SugarLens.subExprPayloads)
+    & passAll (Lens.backwards (exprs . SugarLens.subExprPayloads))
+    & exprs . Lens.mapped %~ snd
+    where
+        toNearestHoles f prevHole nextHole = f (NearestHoles prevHole nextHole)
 
 type M = State (Maybe Sugar.EntityId)
 
 passAll ::
-  LensLike M s t
-  (Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a))
-  (Sugar.Payload m (Bool, a)) -> s -> t
+    LensLike M s t
+    (Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a))
+    (Sugar.Payload m (Bool, a)) -> s -> t
 passAll sugarPls s =
-  s
-  & sugarPls %%~ setEntityId
-  & (`evalState` Nothing)
+    s
+    & sugarPls %%~ setEntityId
+    & (`evalState` Nothing)
 
 setEntityId ::
-  Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a) ->
-  M (Sugar.Payload m (Bool, a))
+    Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a) ->
+    M (Sugar.Payload m (Bool, a))
 setEntityId pl =
-  do
-    oldEntityId <- State.get
-    when isStoredHole $ State.put $ Just $ pl ^. Sugar.plEntityId
-    pl
-      & Sugar.plData . _2 %~ ($ oldEntityId)
-      & return
-  where
-    isStoredHole = pl ^. Sugar.plData . _1
+    do
+        oldEntityId <- State.get
+        when isStoredHole $ State.put $ Just $ pl ^. Sugar.plEntityId
+        pl
+            & Sugar.plData . _2 %~ ($ oldEntityId)
+            & return
+    where
+        isStoredHole = pl ^. Sugar.plData . _1
