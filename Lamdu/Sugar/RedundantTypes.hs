44554f70c1b275ea243727352c955a0cfd3143c8
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:46:48 +0300

    Sugar.RedundantTypes: 4-space alignment

--- Lamdu/Sugar/RedundantTypes.hs
+++ Lamdu/Sugar/RedundantTypes.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE RankNTypes #-}
-
 module Lamdu.Sugar.RedundantTypes
-  ( redundantTypes
-  ) where
+    ( redundantTypes
+    ) where
 
 import Control.Applicative (Applicative(..), (<$>))
 import Control.Lens (Traversal')
@@ -12,28 +11,27 @@ import qualified Control.Lens as Lens
 
 redundantTypesDefaultTop :: Bool -> Traversal' (Expression name m a) (Payload m a)
 redundantTypesDefaultTop topRedundant f (Expression body pl) =
-  case body of
-  BodyGetVar (GetVarNamed NamedVar { _nvVarType = GetFieldParameter }) -> redundant
-  BodyGetVar (GetVarNamed NamedVar { _nvVarType = GetParameter }) -> redundant
-  BodyLiteralInteger {} -> redundant
-  BodyRecord{} -> redundant
-  BodyList{} -> redundantChildren
-  BodyApply (Apply func specialArgs annotatedArgs) ->
-    ( Apply
-      <$> ( func & redundantTypesDefaultTop True %%~ f )
-      <*> ( specialArgs & Lens.traversed recurse )
-      <*> ( annotatedArgs & Lens.traversed . Lens.traversed %%~ recurse )
-    )
-    <&> BodyApply & mk
-  _ -> mk recBody
-  where
-    recurse = redundantTypes f
-    mk newBody =
-      Expression <$> newBody <*> (if topRedundant then f else pure) pl
-    recBody = body & Lens.traversed recurse
-    redundant = Expression <$> recBody <*> f pl
-    redundantChildren =
-      body & Lens.traversed . redundantTypesDefaultTop True %%~ f & mk
+    case body of
+    BodyGetVar (GetVarNamed NamedVar { _nvVarType = GetFieldParameter }) -> redundant
+    BodyGetVar (GetVarNamed NamedVar { _nvVarType = GetParameter }) -> redundant
+    BodyLiteralInteger {} -> redundant
+    BodyRecord{} -> redundant
+    BodyList{} -> redundantChildren
+    BodyApply (Apply func specialArgs annotatedArgs) ->
+        Apply
+        <$> ( func & redundantTypesDefaultTop True %%~ f )
+        <*> ( specialArgs & Lens.traversed recurse )
+        <*> ( annotatedArgs & Lens.traversed . Lens.traversed %%~ recurse )
+        <&> BodyApply & mk
+    _ -> mk recBody
+    where
+        recurse = redundantTypes f
+        mk newBody =
+            Expression <$> newBody <*> (if topRedundant then f else pure) pl
+        recBody = body & Lens.traversed recurse
+        redundant = Expression <$> recBody <*> f pl
+        redundantChildren =
+            body & Lens.traversed . redundantTypesDefaultTop True %%~ f & mk
 
 redundantTypes :: Traversal' (Expression name m a) (Payload m a)
 redundantTypes = redundantTypesDefaultTop False
