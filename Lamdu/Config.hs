8f306677f97152c80ed7f10569137aec4f204659
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:49:38 +0300

    Lamdu.Config: 4-space indent

--- Lamdu/Config.hs
+++ Lamdu/Config.hs
@@ -18,18 +18,18 @@ import           Graphics.UI.Bottle.ModKey (ModKey(..))
 import qualified Lamdu.GUI.VersionControl.Config as VersionControl
 
 data Layers = Layers
-  { layerMin
-  , layerCursor
-  , layerAnnotations
-  , layerChoiceBG
-  , layerHoleBG
-  , layerDarkHoleBG
-  , layerNameCollisionBG
-  , layerValFrameBG
-  , layerParensHighlightBG
-  , layerActivePane
-  , layerMax :: Anim.Layer
-  } deriving (Eq, Generic, Show)
+    { layerMin
+    , layerCursor
+    , layerAnnotations
+    , layerChoiceBG
+    , layerHoleBG
+    , layerDarkHoleBG
+    , layerNameCollisionBG
+    , layerValFrameBG
+    , layerParensHighlightBG
+    , layerActivePane
+    , layerMax :: Anim.Layer
+    } deriving (Eq, Generic, Show)
 instance ToJSON Layers
 instance FromJSON Layers
 
@@ -37,148 +37,148 @@ layerInterval :: Layers -> Int
 layerInterval Layers{..} = layerMax - layerMin
 
 data Help = Help
-  { helpTextColor :: Draw.Color
-  , helpTextSize :: Double
-  , helpInputDocColor :: Draw.Color
-  , helpBGColor :: Draw.Color
-  , helpKeys :: [ModKey]
-  } deriving (Eq, Generic, Show)
+    { helpTextColor :: Draw.Color
+    , helpTextSize :: Double
+    , helpInputDocColor :: Draw.Color
+    , helpBGColor :: Draw.Color
+    , helpKeys :: [ModKey]
+    } deriving (Eq, Generic, Show)
 instance ToJSON Help
 instance FromJSON Help
 
 data Zoom = Zoom
-  { shrinkKeys :: [ModKey]
-  , enlargeKeys :: [ModKey]
-  , enlargeFactor :: Double
-  , shrinkFactor :: Double
-  } deriving (Eq, Generic, Show)
+    { shrinkKeys :: [ModKey]
+    , enlargeKeys :: [ModKey]
+    , enlargeFactor :: Double
+    , shrinkFactor :: Double
+    } deriving (Eq, Generic, Show)
 instance ToJSON Zoom
 instance FromJSON Zoom
 
 data Pane = Pane
-  { paneInactiveTintColor :: Draw.Color
-  , paneActiveBGColor :: Draw.Color
-  , paneCloseKeys :: [ModKey]
-  , paneMoveDownKeys :: [ModKey]
-  , paneMoveUpKeys :: [ModKey]
-  , -- Need some padding on top because of on-top hovers, this decides
-    -- how much:
-    paneHoverPadding :: Draw.R
-  , newDefinitionKeys :: [ModKey]
-  , newDefinitionActionColor :: Draw.Color
-  } deriving (Eq, Generic, Show)
+    { paneInactiveTintColor :: Draw.Color
+    , paneActiveBGColor :: Draw.Color
+    , paneCloseKeys :: [ModKey]
+    , paneMoveDownKeys :: [ModKey]
+    , paneMoveUpKeys :: [ModKey]
+    , -- Need some padding on top because of on-top hovers, this decides
+        -- how much:
+        paneHoverPadding :: Draw.R
+    , newDefinitionKeys :: [ModKey]
+    , newDefinitionActionColor :: Draw.Color
+    } deriving (Eq, Generic, Show)
 instance ToJSON Pane
 instance FromJSON Pane
 
 data Hole = Hole
-  { holePickAndMoveToNextHoleKeys :: [ModKey]
-  , holeJumpToNextKeys :: [ModKey]
-  , holeJumpToPrevKeys :: [ModKey]
-  , holeResultCount :: Int
-  , holeResultPadding :: Vector2 Double
-  , holeResultInjectedScaleExponent :: Double
-  , holeExtraSymbolColorUnselected :: Draw.Color
-  , holeExtraSymbolColorSelected :: Draw.Color
-  , holeOpenBGColor :: Draw.Color
-  , holeSearchTermBGColor :: Draw.Color
-  , holePickResultKeys :: [ModKey]
-  , holeDarkPadding :: Vector2 Double
-  , holeDarkBGColor :: Draw.Color
-  , holeUnwrapKeys :: [ModKey]
-  , holeOpenKeys :: [ModKey]
-  , holeCloseKeys :: [ModKey]
-  , holePasteKeys :: [ModKey]
-  , holeHoveringWrapperScaleFactor :: Vector2 Double
-  } deriving (Eq, Generic, Show)
+    { holePickAndMoveToNextHoleKeys :: [ModKey]
+    , holeJumpToNextKeys :: [ModKey]
+    , holeJumpToPrevKeys :: [ModKey]
+    , holeResultCount :: Int
+    , holeResultPadding :: Vector2 Double
+    , holeResultInjectedScaleExponent :: Double
+    , holeExtraSymbolColorUnselected :: Draw.Color
+    , holeExtraSymbolColorSelected :: Draw.Color
+    , holeOpenBGColor :: Draw.Color
+    , holeSearchTermBGColor :: Draw.Color
+    , holePickResultKeys :: [ModKey]
+    , holeDarkPadding :: Vector2 Double
+    , holeDarkBGColor :: Draw.Color
+    , holeUnwrapKeys :: [ModKey]
+    , holeOpenKeys :: [ModKey]
+    , holeCloseKeys :: [ModKey]
+    , holePasteKeys :: [ModKey]
+    , holeHoveringWrapperScaleFactor :: Vector2 Double
+    } deriving (Eq, Generic, Show)
 instance ToJSON Hole
 instance FromJSON Hole
 
 data Name = Name
-  { collisionSuffixTextColor :: Draw.Color
-  , collisionSuffixBGColor :: Draw.Color
-  , collisionSuffixScaleFactor :: Vector2 Double
-  , autoNameOriginFGColor :: Draw.Color
-  , nameOriginFGColor :: Draw.Color
-  , definitionColor :: Draw.Color
-  , parameterColor :: Draw.Color
-  , recordTagColor :: Draw.Color
-  , paramTagColor :: Draw.Color
-  } deriving (Eq, Generic, Show)
+    { collisionSuffixTextColor :: Draw.Color
+    , collisionSuffixBGColor :: Draw.Color
+    , collisionSuffixScaleFactor :: Vector2 Double
+    , autoNameOriginFGColor :: Draw.Color
+    , nameOriginFGColor :: Draw.Color
+    , definitionColor :: Draw.Color
+    , parameterColor :: Draw.Color
+    , recordTagColor :: Draw.Color
+    , paramTagColor :: Draw.Color
+    } deriving (Eq, Generic, Show)
 instance ToJSON Name
 instance FromJSON Name
 
 data Config = Config
-  { layers :: Layers
-  , help :: Help
-  , zoom :: Zoom
-  , pane :: Pane
-  , versionControl :: VersionControl.Config
-  , hole :: Hole
-  , name :: Name
+    { layers :: Layers
+    , help :: Help
+    , zoom :: Zoom
+    , pane :: Pane
+    , versionControl :: VersionControl.Config
+    , hole :: Hole
+    , name :: Name
 
-  , animationTimePeriodSec :: Double
-  , animationRemainInPeriod :: Double
+    , animationTimePeriodSec :: Double
+    , animationRemainInPeriod :: Double
 
-  , backgroundColor :: Draw.Color
-  , baseColor :: Draw.Color
-  , baseTextSize :: Double
-  , spaceWidth :: Double
+    , backgroundColor :: Draw.Color
+    , baseColor :: Draw.Color
+    , baseTextSize :: Double
+    , spaceWidth :: Double
 
-  , quitKeys :: [ModKey]
-  , debugModeKeys :: [ModKey]
-  , nextInfoModeKeys :: [ModKey]
-  , previousCursorKeys :: [ModKey]
+    , quitKeys :: [ModKey]
+    , debugModeKeys :: [ModKey]
+    , nextInfoModeKeys :: [ModKey]
+    , previousCursorKeys :: [ModKey]
 
-  , invalidCursorBGColor :: Draw.Color
+    , invalidCursorBGColor :: Draw.Color
 
-  , addNextParamKeys :: [ModKey]
-  , jumpToDefinitionKeys :: [ModKey]
-  , replaceKeys :: [ModKey]
-  , delForwardKeys :: [ModKey]
-  , delBackwardKeys :: [ModKey]
-  , wrapKeys :: [ModKey]
+    , addNextParamKeys :: [ModKey]
+    , jumpToDefinitionKeys :: [ModKey]
+    , replaceKeys :: [ModKey]
+    , delForwardKeys :: [ModKey]
+    , delBackwardKeys :: [ModKey]
+    , wrapKeys :: [ModKey]
 
-  , parenHighlightColor :: Draw.Color
-  , literalIntColor :: Draw.Color
-  , typeIndicatorErrorColor :: Draw.Color
-  , typeIndicatorMatchColor :: Draw.Color
-  , typeIndicatorFrameWidth :: Vector2 Double
-  , foreignModuleColor :: Draw.Color
-  , foreignVarColor :: Draw.Color
+    , parenHighlightColor :: Draw.Color
+    , literalIntColor :: Draw.Color
+    , typeIndicatorErrorColor :: Draw.Color
+    , typeIndicatorMatchColor :: Draw.Color
+    , typeIndicatorFrameWidth :: Vector2 Double
+    , foreignModuleColor :: Draw.Color
+    , foreignVarColor :: Draw.Color
 
-  , acceptDefinitionTypeForFirstTimeColor :: Draw.Color
-  , acceptDefinitionTypeKeys :: [ModKey]
+    , acceptDefinitionTypeForFirstTimeColor :: Draw.Color
+    , acceptDefinitionTypeKeys :: [ModKey]
 
-  , whereAddItemKeys :: [ModKey]
-  , whereItemPadding :: Vector2 Double
+    , whereAddItemKeys :: [ModKey]
+    , whereItemPadding :: Vector2 Double
 
-  , cutKeys :: [ModKey]
+    , cutKeys :: [ModKey]
 
-  , typeTint :: Draw.Color
-  , typeBoxBGColor :: Draw.Color
-  , valFrameBGColor :: Draw.Color
-  , valFramePadding :: Vector2 Double
-  , valAnnotationSpacing :: Double
-  , typeFrameBGColor :: Draw.Color
-  , verticalSpacing :: Double
-  , cursorBGColor :: Draw.Color
-  , grammarColor :: Draw.Color
+    , typeTint :: Draw.Color
+    , typeBoxBGColor :: Draw.Color
+    , valFrameBGColor :: Draw.Color
+    , valFramePadding :: Vector2 Double
+    , valAnnotationSpacing :: Double
+    , typeFrameBGColor :: Draw.Color
+    , verticalSpacing :: Double
+    , cursorBGColor :: Draw.Color
+    , grammarColor :: Draw.Color
 
-  , listAddItemKeys :: [ModKey]
+    , listAddItemKeys :: [ModKey]
 
-  , jumpLHStoRHSKeys :: [ModKey]
-  , jumpRHStoLHSKeys :: [ModKey]
+    , jumpLHStoRHSKeys :: [ModKey]
+    , jumpRHStoLHSKeys :: [ModKey]
 
-  , enterSubexpressionKeys :: [ModKey]
-  , leaveSubexpressionKeys :: [ModKey]
+    , enterSubexpressionKeys :: [ModKey]
+    , leaveSubexpressionKeys :: [ModKey]
 
-  , recordOpenKeys :: [ModKey]
-  , recordTailColor :: Draw.Color
-  , recordAddFieldKeys :: [ModKey]
+    , recordOpenKeys :: [ModKey]
+    , recordTailColor :: Draw.Color
+    , recordAddFieldKeys :: [ModKey]
 
-  , presentationChoiceScaleFactor :: Vector2 Double
-  , presentationChoiceColor :: Draw.Color
-  } deriving (Eq, Generic, Show)
+    , presentationChoiceScaleFactor :: Vector2 Double
+    , presentationChoiceColor :: Draw.Color
+    } deriving (Eq, Generic, Show)
 instance ToJSON Config
 instance FromJSON Config
 
