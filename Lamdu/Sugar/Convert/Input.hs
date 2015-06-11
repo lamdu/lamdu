65d1b8d3b1344080d778d9c092d6e7a3c781eb3a
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:19:47 +0300

    Sugar.Convert.Input: 4-space indent

--- Lamdu/Sugar/Convert/Input.hs
+++ Lamdu/Sugar/Convert/Input.hs
@@ -1,9 +1,9 @@
 -- | Preprocess of input to sugar
 {-# LANGUAGE RecordWildCards, DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Lamdu.Sugar.Convert.Input
-  ( Payload(..), entityId, inferred, mStored, evalResults, evalLamArgs, userData
-  , mkPayload, mkUnstoredPayload
-  ) where
+    ( Payload(..), entityId, inferred, mStored, evalResults, evalLamArgs, userData
+    , mkPayload, mkUnstoredPayload
+    ) where
 
 import Control.Lens (Lens, Lens')
 import Control.Lens.Operators
@@ -21,8 +21,7 @@ import qualified Lamdu.Expr.IRef as ExprIRef
 import qualified Lamdu.Infer as Infer
 import qualified Lamdu.Sugar.Internal.EntityId as EntityId
 
-data Payload m a
-  = Payload
+data Payload m a = Payload
     { _entityId :: EntityId
     , _inferred :: Infer.Payload
     , _mStored :: Maybe (ExprIRef.ValIProperty m)
@@ -51,20 +50,20 @@ userData :: Lens (Payload m a) (Payload m b) a b
 userData f Payload{..} = f _userData <&> \_userData -> Payload{..}
 
 mkPayload ::
-  a -> Infer.Payload ->
-  Map ScopeId (ComputedVal ()) -> Map ScopeId (ComputedVal ()) ->
-  ExprIRef.ValIProperty m -> Payload m a
+    a -> Infer.Payload ->
+    Map ScopeId (ComputedVal ()) -> Map ScopeId (ComputedVal ()) ->
+    ExprIRef.ValIProperty m -> Payload m a
 mkPayload _userData _inferred _evalResults _evalLamArgs stored =
-  Payload{..}
-  where
-    _guid = IRef.guid $ ExprIRef.unValI $ Property.value stored
-    _entityId = EntityId.ofValI $ Property.value stored
-    _mStored = Just stored
+    Payload{..}
+    where
+        _guid = IRef.guid $ ExprIRef.unValI $ Property.value stored
+        _entityId = EntityId.ofValI $ Property.value stored
+        _mStored = Just stored
 
 mkUnstoredPayload :: a -> Infer.Payload -> Guid -> EntityId -> Payload m a
 mkUnstoredPayload _userData _inferred _guid _entityId =
-  Payload{..}
-  where
-    _mStored = Nothing
-    _evalResults = Map.empty
-    _evalLamArgs = Map.empty
+    Payload{..}
+    where
+        _mStored = Nothing
+        _evalResults = Map.empty
+        _evalLamArgs = Map.empty
