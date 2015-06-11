274a5411db5d94edce9d650022733c26229b75a2
Eyal Lotem <eyal.lotem@gmail.com>
2015-06-11 13:29:42 +0300

    Sugar.AddNames.CPS: 4-space indent

--- Lamdu/Sugar/AddNames/CPS.hs
+++ Lamdu/Sugar/AddNames/CPS.hs
@@ -1,16 +1,16 @@
 {-# LANGUAGE DeriveFunctor, RankNTypes #-}
 module Lamdu.Sugar.AddNames.CPS
-  ( CPS(..)
-  ) where
+    ( CPS(..)
+    ) where
 
 import Control.Applicative (Applicative(..))
 
 data CPS m a = CPS { runCPS :: forall r. m r -> m (a, r) }
-  deriving (Functor)
+    deriving (Functor)
 
 instance Functor m => Applicative (CPS m) where
-  pure x = CPS $ fmap ((,) x)
-  CPS cpsf <*> CPS cpsx =
-    CPS (fmap foo . cpsf . cpsx)
-    where
-      foo (f, (x, r)) = (f x, r)
+    pure x = CPS $ fmap ((,) x)
+    CPS cpsf <*> CPS cpsx =
+        CPS (fmap foo . cpsf . cpsx)
+        where
+            foo (f, (x, r)) = (f x, r)
