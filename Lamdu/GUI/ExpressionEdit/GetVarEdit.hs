6c0193d8badaeb8cf9ff6e2b6c94b6887bfc1aea
Yair Chuchem <yairchu@gmail.com>
2015-06-15 19:22:32 +0300

    sort imports

--- Lamdu/GUI/ExpressionEdit/GetVarEdit.hs
+++ Lamdu/GUI/ExpressionEdit/GetVarEdit.hs
@@ -3,24 +3,24 @@ module Lamdu.GUI.ExpressionEdit.GetVarEdit
     ( make
     ) where
 
-import Control.Applicative ((<$>))
-import Control.Lens.Operators
-import Control.MonadA (MonadA)
-import Data.Monoid ((<>))
-import Lamdu.GUI.ExpressionGui (ExpressionGui)
-import Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
-import Lamdu.Sugar.AddNames.Types (Name(..))
+import           Control.Applicative ((<$>))
+import           Control.Lens.Operators
+import           Control.MonadA (MonadA)
 import qualified Data.ByteString.Char8 as SBS8
+import           Data.Monoid ((<>))
+import qualified Lamdu.Config as Config
+import qualified Lamdu.Data.Ops as DataOps
+import           Lamdu.GUI.ExpressionGui (ExpressionGui)
+import qualified Lamdu.GUI.ExpressionGui as ExpressionGui
+import           Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
+import qualified Lamdu.GUI.ExpressionGui.Monad as ExprGuiM
+import qualified Lamdu.GUI.WidgetIds as WidgetIds
+import           Lamdu.Sugar.AddNames.Types (Name(..))
+import qualified Lamdu.Sugar.Types as Sugar
 import qualified Graphics.DrawingCombinators as Draw
 import qualified Graphics.UI.Bottle.EventMap as E
 import qualified Graphics.UI.Bottle.Widget as Widget
-import qualified Lamdu.Config as Config
-import qualified Lamdu.Data.Ops as DataOps
 import qualified Graphics.UI.Bottle.Widgets as BWidgets
-import qualified Lamdu.GUI.ExpressionGui as ExpressionGui
-import qualified Lamdu.GUI.ExpressionGui.Monad as ExprGuiM
-import qualified Lamdu.GUI.WidgetIds as WidgetIds
-import qualified Lamdu.Sugar.Types as Sugar
 
 makeSimpleView ::
     MonadA m => Draw.Color -> Name m -> Widget.Id ->
