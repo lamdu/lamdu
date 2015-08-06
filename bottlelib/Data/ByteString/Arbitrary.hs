a2cc50bd56f4c041a7add6f9bb909b7ee75f5f07
Eyal Lotem <eyal.lotem@gmail.com>
2015-08-06 16:24:02 +0300

    .cabal file lists all .hs modules
    (Remove dead Data.ByteString.Arbitrary)

--- bottlelib/Data/ByteString/Arbitrary.hs
+++
@@ -1,7 +0,0 @@
-{-# OPTIONS -fno-warn-orphans #-} -- Arbitrary ByteString
-module Data.ByteString.Arbitrary () where
-
-import Test.QuickCheck (Arbitrary(..))
-
-instance Arbitrary SBS.ByteString where
-    arbitrary = fmap SBS.pack arbitrary
