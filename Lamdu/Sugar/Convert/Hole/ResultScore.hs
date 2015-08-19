8c6c89cc9b69045c868470527a36e05d7fd3160a
Yair Chuchem <yairchu@gmail.com>
2015-08-19 15:43:29 +0300

    ResultScore doesnt differentiate same composites with different order/tags

--- Lamdu/Sugar/Convert/Hole/ResultScore.hs
+++ Lamdu/Sugar/Convert/Hole/ResultScore.hs
@@ -16,15 +16,14 @@ resultTypeScore (TVar _) = [0]
 resultTypeScore TInt = [1]
 resultTypeScore (TInst _ p) = 2 : maximum ([] : map resultTypeScore (Map.elems p))
 resultTypeScore (TFun a r) = 2 : max (resultTypeScore a) (resultTypeScore r)
-resultTypeScore (TSum c) =
-    compositeTypeScore c
-resultTypeScore (TRecord c) =
-    compositeTypeScore c
+resultTypeScore (TSum c) = 2 : compositeTypeScore c
+resultTypeScore (TRecord c) = 2 : compositeTypeScore c
 
 compositeTypeScore :: Composite t -> [Int]
-compositeTypeScore (CEmpty) = [2]
+compositeTypeScore (CEmpty) = []
 compositeTypeScore (CVar _) = [1]
-compositeTypeScore (CExtend _ t r) = 2 : max (resultTypeScore t) (compositeTypeScore r)
+compositeTypeScore (CExtend _ t r) =
+    max (resultTypeScore t) (compositeTypeScore r)
 
 resultScore :: Val Infer.Payload -> [Int]
 resultScore val@(Val pl body) =
