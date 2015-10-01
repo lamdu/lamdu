b584078a103de620e896f5577891f89e57ae9b17
Eyal Lotem <eyal.lotem@gmail.com>
2015-10-02 00:15:13 +0300

    Sugar.Convert: Extract the loadInfer/mkContext/run from DefExpr to Sugar.Convert

--- Lamdu/Sugar/Convert.hs
+++ Lamdu/Sugar/Convert.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE FlexibleContexts, OverloadedStrings, TypeFamilies, Rank2Types #-}
+{-# LANGUAGE NoImplicitPrelude, FlexibleContexts, OverloadedStrings, TypeFamilies, Rank2Types #-}
 module Lamdu.Sugar.Convert
     ( convertDefI
     ) where
 
+import qualified Control.Lens as Lens
+import           Control.Lens.Operators
 import           Control.MonadA (MonadA)
 import           Data.CurAndPrev (CurAndPrev)
 import           Data.Store.Guid (Guid)
+import qualified Data.Store.Property as Property
 import           Data.Store.Transaction (Transaction)
 import qualified Data.Store.Transaction as Transaction
 import qualified Lamdu.Data.Anchors as Anchors
@@ -15,10 +18,19 @@ import           Lamdu.Expr.IRef (DefI)
 import qualified Lamdu.Expr.IRef as ExprIRef
 import qualified Lamdu.Expr.UniqueId as UniqueId
 import           Lamdu.Expr.Val (Val(..))
+import qualified Lamdu.Expr.Val as V
+import qualified Lamdu.Infer as Infer
 import qualified Lamdu.Sugar.Convert.DefExpr as ConvertDefExpr
+import qualified Lamdu.Sugar.Convert.Expression as ConvertExpr
+import           Lamdu.Sugar.Convert.Infer (loadInfer)
+import           Lamdu.Sugar.Convert.Monad (Context(..))
+import qualified Lamdu.Sugar.Convert.Monad as ConvertM
 import           Lamdu.Sugar.Internal
 import qualified Lamdu.Sugar.Internal.EntityId as EntityId
 import           Lamdu.Sugar.Types
+import           Text.PrettyPrint.HughesPJClass (pPrint)
+
+import           Prelude.Compat
 
 convertDefIBuiltin ::
     MonadA m => Definition.Builtin -> DefI m ->
@@ -33,6 +45,34 @@ convertDefIBuiltin (Definition.Builtin name scheme) defI =
         setName =
             Transaction.writeIRef defI .
             Definition.BodyBuiltin . (`Definition.Builtin` scheme)
+
+mkContext ::
+    MonadA m =>
+    DefI m -> Anchors.Code (Transaction.MkProperty m) m -> Infer.Context ->
+    Context m
+mkContext defI cp inferContext =
+    Context
+    { _scInferContext = inferContext
+    , _scDefI = defI
+    , _scCodeAnchors = cp
+    , _scTagParamInfos = mempty
+    , _scMExtractDestPos = Nothing
+    , _scNullParams = mempty
+    , _scReinferCheckRoot =
+          do
+              defBody <- Transaction.readIRef defI
+              case defBody of
+                  Definition.BodyBuiltin {} -> return True
+                  Definition.BodyExpr (Definition.Expr valI _) ->
+                      ExprIRef.readVal valI
+                      <&> fmap (flip (,) ())
+                      <&> ExprIRef.addProperties (error "TODO: DefExpr root setIRef")
+                      <&> fmap fst
+                      >>= -- TODO: loadInfer is for sugar, we don't need sugar here
+                          loadInfer mempty
+                      <&> Lens.has Lens._Right
+    , scConvertSubexpression = ConvertExpr.convert
+    }
 
 convertDefI ::
     MonadA m =>
@@ -51,5 +91,11 @@ convertDefI evalRes cp (Definition.Definition body defI) =
     where
         convertDefBody (Definition.BodyBuiltin builtin) =
             return $ convertDefIBuiltin builtin defI
-        convertDefBody (Definition.BodyExpr expr) =
-            ConvertDefExpr.convert evalRes cp expr defI
+        convertDefBody (Definition.BodyExpr (Definition.Expr val typ)) =
+            do
+                (valInferred, newInferContext) <-
+                    loadInfer evalRes val
+                    <&> either (error . ("Type inference failed: " ++) . show . pPrint) id
+                let exprI = val ^. V.payload . Property.pVal
+                ConvertDefExpr.convert exprI (Definition.Expr valInferred typ) defI
+                    & ConvertM.run (mkContext defI cp newInferContext)
--- Lamdu/Sugar/Convert/DefExpr.hs
+++ Lamdu/Sugar/Convert/DefExpr.hs
@@ -6,68 +6,30 @@ module Lamdu.Sugar.Convert.DefExpr
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.MonadA (MonadA)
-import           Data.CurAndPrev (CurAndPrev(..))
 import           Data.Store.Guid (Guid)
 import qualified Data.Store.IRef as IRef
 import qualified Data.Store.Property as Property
-import           Data.Store.Transaction (Transaction)
 import qualified Data.Store.Transaction as Transaction
 import           Lamdu.Builtins.Anchors (recurseVar)
-import qualified Lamdu.Data.Anchors as Anchors
 import qualified Lamdu.Data.Definition as Definition
-import           Lamdu.Eval.Results (EvalResults)
-import           Lamdu.Expr.IRef (DefI)
-import qualified Lamdu.Expr.IRef as ExprIRef
+import           Lamdu.Expr.IRef (DefI, ValI)
 import           Lamdu.Expr.Scheme (Scheme(..))
 import qualified Lamdu.Expr.Scheme as Scheme
 import           Lamdu.Expr.Val (Val(..))
 import qualified Lamdu.Expr.Val as V
 import qualified Lamdu.Infer as Infer
 import qualified Lamdu.Sugar.Convert.Binder as ConvertBinder
-import qualified Lamdu.Sugar.Convert.Expression as ConvertExpr
-import           Lamdu.Sugar.Convert.Infer (loadInfer)
 import qualified Lamdu.Sugar.Convert.Input as Input
-import           Lamdu.Sugar.Convert.Monad (Context(..))
+import           Lamdu.Sugar.Convert.Monad (ConvertM)
 import qualified Lamdu.Sugar.Convert.Monad as ConvertM
 import           Lamdu.Sugar.Internal
 import qualified Lamdu.Sugar.Internal.EntityId as EntityId
 import           Lamdu.Sugar.Types
-import           Text.PrettyPrint.HughesPJClass (pPrint)
 
 import           Prelude.Compat
 
-type T = Transaction
-
-mkContext ::
-    MonadA m =>
-    DefI m -> Anchors.Code (Transaction.MkProperty m) m -> Infer.Context ->
-    Context m
-mkContext defI cp inferContext =
-    Context
-    { _scInferContext = inferContext
-    , _scDefI = defI
-    , _scCodeAnchors = cp
-    , _scTagParamInfos = mempty
-    , _scMExtractDestPos = Nothing
-    , _scNullParams = mempty
-    , _scReinferCheckRoot =
-          do
-              defBody <- Transaction.readIRef defI
-              case defBody of
-                  Definition.BodyBuiltin {} -> return True
-                  Definition.BodyExpr (Definition.Expr valI _) ->
-                      ExprIRef.readVal valI
-                      <&> fmap (flip (,) ())
-                      <&> ExprIRef.addProperties (error "TODO: DefExpr root setIRef")
-                      <&> fmap fst
-                      >>= -- TODO: loadInfer is for sugar, we don't need sugar here
-                          loadInfer mempty
-                      <&> Lens.has Lens._Right
-    , scConvertSubexpression = ConvertExpr.convert
-    }
-
 makeExprDefTypeInfo ::
-    MonadA m => ExprIRef.ValI m -> DefI m -> Definition.ExportedType -> Scheme -> DefinitionTypeInfo m
+    MonadA m => ValI m -> DefI m -> Definition.ExportedType -> Scheme -> DefinitionTypeInfo m
 makeExprDefTypeInfo _ _ (Definition.ExportedType defType) inferredType
     | defType `Scheme.alphaEq` inferredType = DefinitionExportedTypeInfo defType
 makeExprDefTypeInfo defValI defI defType inferredType =
@@ -82,25 +44,21 @@ makeExprDefTypeInfo defValI defI defType inferredType =
 
 convert ::
     MonadA m =>
-    CurAndPrev (EvalResults (ExprIRef.ValI m)) ->
-    Anchors.CodeProps m ->
-    Definition.Expr (Val (ExprIRef.ValIProperty m)) ->
-    DefI m -> T m (DefinitionBody Guid m (ExpressionU m [EntityId]))
-convert evalRes cp (Definition.Expr val defType) defI =
+    ValI m -> Definition.Expr (Val (Input.Payload m a)) -> DefI m ->
+    ConvertM m (DefinitionBody Guid m (ExpressionU m [EntityId]))
+convert exprI (Definition.Expr val defType) defI =
     do
-        (valInferred, newInferContext) <-
-            loadInfer evalRes val
-            <&> either (error . ("Type inference failed: " ++) . show . pPrint) id
+        sugarContext <- ConvertM.readContext
+        let inferContext = sugarContext ^. ConvertM.scInferContext
         content <-
-            valInferred <&> addStoredEntityIds
+            val <&> addStoredEntityIds
             & ConvertBinder.convertBinder (Just recurseVar) defGuid
-            & ConvertM.run (mkContext defI cp newInferContext)
         return $ DefinitionBodyExpression DefinitionExpression
             { _deContent = content
             , _deTypeInfo =
                 makeExprDefTypeInfo exprI defI defType $
-                Infer.makeScheme newInferContext $
-                valInferred ^. V.payload . Input.inferredType
+                Infer.makeScheme inferContext $
+                val ^. V.payload . Input.inferredType
             }
     where
         addStoredEntityIds x =
@@ -108,5 +66,4 @@ convert evalRes cp (Definition.Expr val defType) defI =
             & Input.userData .~
                 (x ^.. Input.mStored . Lens._Just
                   <&> EntityId.ofValI . Property.value)
-        exprI = val ^. V.payload . Property.pVal
         defGuid = IRef.guid defI
