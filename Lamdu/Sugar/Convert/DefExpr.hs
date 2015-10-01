0bcadb5912f6bf120c8cc1fbbf872ae9623b7c8f
Eyal Lotem <eyal.lotem@gmail.com>
2015-10-01 20:19:39 +0300

    Extract Sugar.Convert.Infer from Sugar.Convert.DefExpr

--- Lamdu/Sugar/Convert/DefExpr.hs
+++ Lamdu/Sugar/Convert/DefExpr.hs
@@ -3,12 +3,8 @@ module Lamdu.Sugar.Convert.DefExpr
     ( convert
     ) where
 
-import           Prelude.Compat
-
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
-import           Control.Lens.Tuple
-import           Control.Monad.Trans.Either (runEitherT)
 import           Control.MonadA (MonadA)
 import           Data.CurAndPrev (CurAndPrev(..))
 import           Data.Store.Guid (Guid)
@@ -19,10 +15,9 @@ import qualified Data.Store.Transaction as Transaction
 import           Lamdu.Builtins.Anchors (recurseVar)
 import qualified Lamdu.Data.Anchors as Anchors
 import qualified Lamdu.Data.Definition as Definition
-import           Lamdu.Eval.Results (EvalResults, erExprValues, erAppliesOfLam)
+import           Lamdu.Eval.Results (EvalResults)
 import           Lamdu.Expr.IRef (DefI)
 import qualified Lamdu.Expr.IRef as ExprIRef
-import qualified Lamdu.Expr.IRef.Infer as IRefInfer
 import           Lamdu.Expr.Scheme (Scheme(..))
 import qualified Lamdu.Expr.Scheme as Scheme
 import           Lamdu.Expr.Val (Val(..))
@@ -30,14 +25,16 @@ import qualified Lamdu.Expr.Val as V
 import qualified Lamdu.Infer as Infer
 import qualified Lamdu.Sugar.Convert.Binder as ConvertBinder
 import qualified Lamdu.Sugar.Convert.Expression as ConvertExpr
+import           Lamdu.Sugar.Convert.Infer (loadInfer)
 import qualified Lamdu.Sugar.Convert.Input as Input
 import           Lamdu.Sugar.Convert.Monad (Context(..))
 import qualified Lamdu.Sugar.Convert.Monad as ConvertM
-import qualified Lamdu.Sugar.Convert.ParamList as ParamList
 import           Lamdu.Sugar.Internal
 import qualified Lamdu.Sugar.Internal.EntityId as EntityId
 import           Lamdu.Sugar.Types
 import           Text.PrettyPrint.HughesPJClass (pPrint)
+
+import           Prelude.Compat
 
 type T = Transaction
 
@@ -82,28 +79,6 @@ makeExprDefTypeInfo defValI defI defType inferredType =
         Definition.BodyExpr $
         Definition.Expr defValI $ Definition.ExportedType inferredType
     }
-
-loadInfer ::
-    MonadA m =>
-    CurAndPrev (EvalResults (ExprIRef.ValI m)) ->
-    Val (ExprIRef.ValIProperty m) ->
-    T m (Either IRefInfer.Error (Val (Input.Payload m ()), Infer.Context))
-loadInfer evalRes val =
-    IRefInfer.loadInfer recurseVar val
-    <&> _1 . Lens.mapped %~ mkPayload
-    >>= ParamList.loadForLambdas
-    & runEitherT
-    where
-        mkPayload (inferPl, valIProp) =
-            Input.mkPayload () inferPl
-            (evalRes <&> exprEvalRes execId)
-            valIProp
-            where
-                execId = Property.value valIProp
-        exprEvalRes pl r =
-            Input.EvalResultsForExpr
-            (r ^. erExprValues . Lens.at pl . Lens._Just)
-            (r ^. erAppliesOfLam . Lens.at pl . Lens._Just)
 
 convert ::
     MonadA m =>
---
+++ Lamdu/Sugar/Convert/Infer.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Lamdu.Sugar.Convert.Infer
+    ( loadInfer
+    ) where
+
+import qualified Control.Lens as Lens
+import           Control.Lens.Operators
+import           Control.Lens.Tuple
+import           Control.Monad.Trans.Either (runEitherT)
+import           Control.MonadA (MonadA)
+import           Data.CurAndPrev (CurAndPrev(..))
+import qualified Data.Store.Property as Property
+import           Data.Store.Transaction (Transaction)
+import           Lamdu.Builtins.Anchors (recurseVar)
+import           Lamdu.Eval.Results (EvalResults, erExprValues, erAppliesOfLam)
+import qualified Lamdu.Expr.IRef as ExprIRef
+import qualified Lamdu.Expr.IRef.Infer as IRefInfer
+import           Lamdu.Expr.Val (Val(..))
+import qualified Lamdu.Infer as Infer
+import qualified Lamdu.Sugar.Convert.Input as Input
+import qualified Lamdu.Sugar.Convert.ParamList as ParamList
+import           Prelude.Compat
+
+type T = Transaction
+
+loadInfer ::
+    MonadA m =>
+    CurAndPrev (EvalResults (ExprIRef.ValI m)) ->
+    Val (ExprIRef.ValIProperty m) ->
+    T m (Either IRefInfer.Error (Val (Input.Payload m ()), Infer.Context))
+loadInfer evalRes val =
+    IRefInfer.loadInfer recurseVar val
+    <&> _1 . Lens.mapped %~ mkPayload
+    >>= ParamList.loadForLambdas
+    & runEitherT
+    where
+        mkPayload (inferPl, valIProp) =
+            Input.mkPayload () inferPl
+            (evalRes <&> exprEvalRes execId)
+            valIProp
+            where
+                execId = Property.value valIProp
+        exprEvalRes pl r =
+            Input.EvalResultsForExpr
+            (r ^. erExprValues . Lens.at pl . Lens._Just)
+            (r ^. erAppliesOfLam . Lens.at pl . Lens._Just)
