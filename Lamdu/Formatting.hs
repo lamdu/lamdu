a2515bf5034c2f3a292662b73ef0f4319871bfb8
Yair Chuchem <yairchu@gmail.com>
2015-12-09 16:42:18 +0200

    fix formatting of Infinity

--- Lamdu/Formatting.hs
+++ Lamdu/Formatting.hs
@@ -6,8 +6,9 @@ import Text.Printf (printf)
 
 formatNum :: Double -> String
 formatNum x
-    | fromIntegral i == x = show i
-    | otherwise = printf "%f" x
+    | fromIntegral i /= x = printf "%f" x
+    | isInfinite x = ['-' | x < 0] ++ "Inf"
+    | otherwise = show i
     where
         i :: Integer
         i = truncate x
