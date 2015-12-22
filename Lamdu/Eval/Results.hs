7c89a4402ecd8f3c13796228b2260b19657495ef
Eyal Lotem <eyal.lotem@gmail.com>
2015-12-22 19:02:13 +0200

    Lamdu.Eval.Results: Use TH to generate lenses

--- Lamdu/Eval/Results.hs
+++ Lamdu/Eval/Results.hs
@@ -1,13 +1,12 @@
-{-# LANGUAGE NoImplicitPrelude, RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell, NoImplicitPrelude, RecordWildCards #-}
 module Lamdu.Eval.Results where
 
-import           Prelude.Compat
-
-import           Control.Lens (Lens')
-import           Control.Lens.Operators
+import qualified Control.Lens as Lens
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Lamdu.Eval.Val (EvalResult, ScopeId)
+
+import           Prelude.Compat
 
 data EvalResults pl =
     EvalResults
@@ -15,11 +14,7 @@ data EvalResults pl =
     , _erAppliesOfLam :: Map pl (Map ScopeId [(ScopeId, EvalResult ())])
     } deriving Show
 
-erExprValues :: Lens' (EvalResults pl) (Map pl (Map ScopeId (EvalResult ())))
-erExprValues f EvalResults{..} = f _erExprValues <&> \_erExprValues -> EvalResults{..}
-
-erAppliesOfLam :: Lens' (EvalResults pl) (Map pl (Map ScopeId [(ScopeId, EvalResult ())]))
-erAppliesOfLam f EvalResults{..} = f _erAppliesOfLam <&> \_erAppliesOfLam -> EvalResults{..}
+Lens.makeLenses ''EvalResults
 
 empty :: EvalResults pl
 empty =
