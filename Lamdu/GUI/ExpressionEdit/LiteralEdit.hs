dc404654604860606e6a695f67b99007841c90d5
Eyal Lotem <eyal.lotem@gmail.com>
2016-01-05 16:06:29 +0200

    LiteralEdit: Refactor

--- Lamdu/GUI/ExpressionEdit/LiteralEdit.hs
+++ Lamdu/GUI/ExpressionEdit/LiteralEdit.hs
@@ -6,14 +6,13 @@ module Lamdu.GUI.ExpressionEdit.LiteralEdit
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.MonadA (MonadA)
-import qualified Data.ByteString as SBS
 import           Data.Store.Guid (Guid)
 import qualified Data.Store.Transaction as Transaction
 import qualified Graphics.UI.Bottle.EventMap as E
 import           Graphics.UI.Bottle.ModKey (ModKey(..))
-import           Graphics.UI.Bottle.Widget (Widget)
 import qualified Graphics.UI.Bottle.Widget as Widget
 import qualified Graphics.UI.Bottle.Widgets as BWidgets
+import qualified Graphics.UI.Bottle.Widgets.TextEdit as TextEdit
 import qualified Graphics.UI.Bottle.WidgetsEnvT as WE
 import qualified Graphics.UI.GLFW as GLFW
 import           Lamdu.Formatting (formatNum, formatBytes, formatText)
@@ -24,6 +23,7 @@ import           Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
 import qualified Lamdu.GUI.ExpressionGui.Monad as ExprGuiM
 import qualified Lamdu.GUI.ExpressionGui.Types as ExprGuiT
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
+import           Lamdu.Style (Style)
 import qualified Lamdu.Style as Style
 import qualified Lamdu.Sugar.Types as Sugar
 
@@ -42,10 +42,10 @@ mkEditEventMap valText setToHole =
 
 makeGeneric ::
     MonadA m =>
-    (ExprGuiM m (Widget (T m)) -> ExprGuiM m (Widget (T m))) ->
+    (Style -> TextEdit.Style) ->
     String -> Sugar.Payload m ExprGuiT.Payload ->
     ExprGuiM m (ExpressionGui m)
-makeGeneric setStyle valText pl =
+makeGeneric getStyle valText pl =
     BWidgets.makeFocusableTextView valText myId
     & setStyle . ExprGuiM.widgetEnv
     <&> Widget.weakerEvents editEventMap
@@ -60,47 +60,15 @@ makeGeneric setStyle valText pl =
             Just Sugar.AlreadyAHole -> error "Literal val is a hole?!"
             Just Sugar.AlreadyAppliedToHole -> error "Literal val is an apply?!"
             Nothing -> mempty -- not modifiable
-
-makeNum ::
-    MonadA m =>
-    Double -> Sugar.Payload m ExprGuiT.Payload ->
-    ExprGuiM m (ExpressionGui m)
-makeNum =
-    makeGeneric setStyle . formatNum
-    where
         setStyle action =
             do
                 style <- ExprGuiM.readStyle
-                action & ExprGuiM.localEnv (WE.envTextStyle .~ Style.styleNum style)
-
-makeBytes ::
-    MonadA m =>
-    SBS.ByteString -> Sugar.Payload m ExprGuiT.Payload ->
-    ExprGuiM m (ExpressionGui m)
-makeBytes =
-    makeGeneric setStyle . formatBytes
-    where
-        setStyle action =
-            do
-                style <- ExprGuiM.readStyle
-                action & ExprGuiM.localEnv (WE.envTextStyle .~ Style.styleBytes style)
-
-makeText ::
-    MonadA m =>
-    String -> Sugar.Payload m ExprGuiT.Payload ->
-    ExprGuiM m (ExpressionGui m)
-makeText =
-    makeGeneric setStyle . formatText
-    where
-        setStyle action =
-            do
-                style <- ExprGuiM.readStyle
-                action & ExprGuiM.localEnv (WE.envTextStyle .~ Style.styleText style)
+                action & ExprGuiM.localEnv (WE.envTextStyle .~ getStyle style)
 
 make ::
     MonadA m =>
     Sugar.Literal -> Sugar.Payload m ExprGuiT.Payload ->
     ExprGuiM m (ExpressionGui m)
-make (Sugar.LiteralNum x) = makeNum x
-make (Sugar.LiteralBytes x) = makeBytes x
-make (Sugar.LiteralText x) = makeText x
+make (Sugar.LiteralNum x) = formatNum x & makeGeneric Style.styleNum
+make (Sugar.LiteralBytes x) = formatBytes x & makeGeneric Style.styleBytes
+make (Sugar.LiteralText x) = formatText x & makeGeneric Style.styleText
