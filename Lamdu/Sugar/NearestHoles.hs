lamdu/lamdu

commit 4dc5e9e158adce4d462aa753190fb6ec88d4fd55

Sugar: Always stored
* Remove tons of Maybe wrappers and handlers for the non-existing case
  of non-stored Sugar

* Currently it only exists in search term generation, which uses "fake"
  sugaring just to generate names, incurring an inference penalty, this
  will be fixed in a further commit

Lamdu/Sugar/NearestHoles.hs
@@ -5,32 +5,16 @@ module Lamdu.Sugar.NearestHoles
     , add
     ) where
 
-import           Prelude.Compat
-
-import           Control.Applicative.Utils (when)
 import           Control.Lens (LensLike)
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
-import           Control.Lens.Tuple
 import           Control.Monad.Trans.State (State, evalState)
 import qualified Control.Monad.Trans.State as State
 import           Control.MonadA (MonadA)
 import qualified Lamdu.Sugar.Lens as SugarLens
 import qualified Lamdu.Sugar.Types as Sugar
 
-markStoredHoles ::
-    Sugar.Expression name m a ->
-    Sugar.Expression name m (Bool, a)
-markStoredHoles expr =
-    expr
-    <&> (,) False
-    & SugarLens.holePayloads . Sugar.plData . _1 .~ True
-    & SugarLens.subExprPayloads %~ removeNonStoredMarks
-    where
-        removeNonStoredMarks pl =
-            case pl ^. Sugar.plActions of
-            Nothing -> pl & Sugar.plData . _1 .~ False
-            Just _ -> pl
+import           Prelude.Compat
 
 data NearestHoles = NearestHoles
     { _prev :: Maybe Sugar.EntityId
@@ -54,10 +38,8 @@ add ::
 add exprs s =
     s
     & exprs . Lens.mapped %~ toNearestHoles
-    & exprs %~ markStoredHoles
     & passAll (exprs . SugarLens.subExprPayloads)
     & passAll (Lens.backwards (exprs . SugarLens.subExprPayloads))
-    & exprs . Lens.mapped %~ snd
     where
         toNearestHoles f prevHole nextHole = f (NearestHoles prevHole nextHole)
 
@@ -65,22 +47,20 @@ type M = State (Maybe Sugar.EntityId)
 
 passAll ::
     LensLike M s t
-    (Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a))
-    (Sugar.Payload m (Bool, a)) -> s -> t
+    (Sugar.Payload m (Maybe Sugar.EntityId -> a))
+    (Sugar.Payload m a) -> s -> t
 passAll sugarPls s =
     s
     & sugarPls %%~ setEntityId
     & (`evalState` Nothing)
 
 setEntityId ::
-    Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a) ->
-    M (Sugar.Payload m (Bool, a))
+    Sugar.Payload m (Maybe Sugar.EntityId -> a) ->
+    M (Sugar.Payload m a)
 setEntityId pl =
     do
         oldEntityId <- State.get
-        when isStoredHole $ State.put $ Just $ pl ^. Sugar.plEntityId
+        State.put $ Just $ pl ^. Sugar.plEntityId
         pl
-            & Sugar.plData . _2 %~ ($ oldEntityId)
+            & Sugar.plData %~ ($ oldEntityId)
             & return
-    where
-        isStoredHole = pl ^. Sugar.plData . _1

Lamdu/GUI/ExpressionEdit.hs
@@ -3,7 +3,6 @@ module Lamdu.GUI.ExpressionEdit
     ( make
     ) where
 
-import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.Lens.Tuple
 import           Control.MonadA (MonadA)
@@ -54,14 +53,9 @@ shrinkIfHigherThanLine w =
 
 make :: MonadA m => ExprGuiT.SugarExpr m -> ExprGuiM m (ExpressionGui m)
 make sExpr =
-    assignCursor $
-    do
-        gui <- makeEditor body pl
-        maybeShrink gui <&> ExpressionGui.egWidget %~ maybeDoesntTakeFocus
+    makeEditor body pl >>= maybeShrink
+    & assignCursor
     where
-        maybeDoesntTakeFocus
-            | Lens.has Lens._Nothing (pl ^. Sugar.plActions) = Widget.doesntTakeFocus
-            | otherwise = id
         Sugar.Expression body pl = sExpr
         exprHiddenEntityIds =
             List.delete (pl ^. Sugar.plEntityId)

Lamdu/GUI/ExpressionEdit/ApplyEdit.hs
@@ -37,10 +37,9 @@ prefixPrecedence :: Int
 prefixPrecedence = 10
 
 mkOverrideModifyEventMap ::
-    MonadA m => Maybe (Sugar.Actions m) ->
+    MonadA m => Sugar.Actions m ->
     ExprGuiM m (ExpressionGui m -> ExpressionGui m)
-mkOverrideModifyEventMap Nothing = return id
-mkOverrideModifyEventMap (Just actions) =
+mkOverrideModifyEventMap actions =
     do
         config <- ExprGuiM.readConfig
         ExpressionGui.egWidget %~

Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -9,7 +9,7 @@ import           Control.Applicative ((<|>), liftA2)
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.Lens.Tuple
-import           Control.Monad (guard, join)
+import           Control.Monad (guard)
 import           Control.MonadA (MonadA)
 import           Data.CurAndPrev (CurAndPrev, current)
 import           Data.List.Utils (nonEmptyAll)
@@ -57,22 +57,23 @@ nonOperatorName _ = False
 
 makeBinderNameEdit ::
     MonadA m =>
-    Maybe (Sugar.BinderActions m) ->
+    Sugar.BinderActions m ->
     Widget.EventHandlers (Transaction m) ->
     (String, Sugar.EntityId) ->
     Name m -> Widget.Id ->
     ExprGuiM m (ExpressionGui m)
-makeBinderNameEdit mBinderActions rhsJumperEquals rhs name myId =
+makeBinderNameEdit binderActions rhsJumperEquals rhs name myId =
     do
         config <- ExprGuiM.readConfig
         rhsJumper <- jumpToRHS (Config.jumpLHStoRHSKeys config) rhs
         ExpressionGui.makeNameOriginEdit name myId
             <&> jumpToRHSViaEquals name
             <&> Widget.weakerEvents
-                (ParamEdit.eventMapAddFirstParam config mAddFirstParam <> rhsJumper)
+                (ParamEdit.eventMapAddFirstParam config
+                 (binderActions ^. Sugar.baAddFirstParam) <>
+                 rhsJumper)
             <&> ExpressionGui.fromValueWidget
     where
-        mAddFirstParam = mBinderActions ^? Lens._Just . Sugar.baAddFirstParam
         jumpToRHSViaEquals n widget
             | nonOperatorName n =
                 widget
@@ -155,10 +156,7 @@ readBinderChosenScope ::
     MonadA m =>
     Sugar.Binder name m expr -> ExprGuiM m (Maybe Sugar.BinderParamScopeId)
 readBinderChosenScope binder =
-    binder ^. Sugar.bMChosenScopeProp
-    & Lens._Just %%~ Transaction.getP
-    & ExprGuiM.transaction
-    <&> join
+    binder ^. Sugar.bChosenScopeProp & Transaction.getP & ExprGuiM.transaction
 
 mkChosenScopeCursor ::
     MonadA m =>
@@ -223,18 +221,13 @@ makeScopeNavEdit binder myId curCursor =
                 mapM mkArrow scopes
                 >>= ExpressionGui.hboxSpaced
                 >>= ExpressionGui.makeFocusableView myId
-                <&>
-                ExpressionGui.egWidget %~ Widget.weakerEvents
-                ( mkScopeEventMap leftKeys rightKeys
-                  & maybe mempty (`mappend` blockEventMap)
-                )
+                <&> ExpressionGui.egWidget %~ Widget.weakerEvents
+                    (mkScopeEventMap leftKeys rightKeys `mappend` blockEventMap)
                 <&> Just
-                <&> (,)
-                    (mkScopeEventMap prevScopeKeys nextScopeKeys
-                     & fromMaybe mempty)
+                <&> (,) (mkScopeEventMap prevScopeKeys nextScopeKeys)
             _ -> return (mempty, Nothing)
     where
-        mkScopeEventMap l r = makeScopeEventMap l r curCursor <$> mSetScope
+        mkScopeEventMap l r = makeScopeEventMap l r curCursor setScope
         leftKeys = [ModKey mempty GLFW.Key'Left]
         rightKeys = [ModKey mempty GLFW.Key'Right]
         scopes :: [(String, Maybe Sugar.BinderParamScopeId)]
@@ -242,10 +235,7 @@ makeScopeNavEdit binder myId curCursor =
             [ ("◀", sMPrevParamScope curCursor)
             , ("▶", sMNextParamScope curCursor)
             ]
-        mSetScope =
-            binder ^. Sugar.bMChosenScopeProp
-            <&> Transaction.setP
-            <&> (. Just)
+        setScope = Transaction.setP (binder ^. Sugar.bChosenScopeProp) . Just
 
 makeMParamsEdit ::
     MonadA m =>
@@ -347,7 +337,7 @@ make name binder myId =
         jumpHolesEventMap <-
             ExprEventMap.jumpHolesEventMap (binderContentNearestHoles body)
         defNameEdit <-
-            makeBinderNameEdit (binder ^. Sugar.bMActions)
+            makeBinderNameEdit (binder ^. Sugar.bActions)
             rhsJumperEquals rhs name myId
             <&> ExpressionGui.addBelow 0 (map ((,) 0) presentationEdits)
             <&> ExpressionGui.egWidget %~ Widget.weakerEvents jumpHolesEventMap
@@ -374,18 +364,16 @@ makeLetEdit ::
 makeLetEdit item =
     do
         config <- ExprGuiM.readConfig
-        let actionsEventMap
-                | Just lActions <- item ^. Sugar.lActions =
+        let actionsEventMap =
                 mconcat
                 [ Widget.keysEventMapMovesCursor (Config.delKeys config)
                   (E.Doc ["Edit", "Let clause", "Delete"]) $
-                  bodyId <$ lActions ^. Sugar.laSetToInner
+                  bodyId <$ item ^. Sugar.lActions . Sugar.laSetToInner
                 , Widget.keysEventMapMovesCursor (Config.extractKeys config)
                   (E.Doc ["Edit", "Let clause", "Extract to outer scope"]) $
                   WidgetIds.fromEntityId . Sugar.lfrNewEntity <$>
-                  lActions ^. Sugar.laFloat
+                  item ^. Sugar.lActions . Sugar.laFloat
                 ]
-                | otherwise = mempty
         let usageEventMap =
                 mconcat
                 [ Widget.keysEventMapMovesCursor (Config.inlineKeys config)
@@ -423,20 +411,15 @@ makeBinderBodyEdit ::
     Sugar.BinderParams name m ->
     Sugar.BinderBody (Name m) m (ExprGuiT.SugarExpr m) ->
     ExprGuiM m (ExpressionGui m)
-makeBinderBodyEdit params (Sugar.BinderBody mActions content) =
+makeBinderBodyEdit params (Sugar.BinderBody addOuterLet content) =
     do
         config <- ExprGuiM.readConfig
         savePos <- ExprGuiM.mkPrejumpPosSaver
         let newLetEventMap =
-                case mActions of
-                Nothing -> mempty
-                Just Sugar.BinderBodyActions { _bbaAddOuterLet } ->
-                    do
-                        savePos
-                        _bbaAddOuterLet
-                        <&> WidgetIds.fromEntityId <&> WidgetIds.nameEditOf
-                    & Widget.keysEventMapMovesCursor (Config.letAddItemKeys config)
-                      (E.Doc ["Edit", "Let clause", "Add"])
+                savePos >> addOuterLet
+                <&> WidgetIds.fromEntityId <&> WidgetIds.nameEditOf
+                & Widget.keysEventMapMovesCursor (Config.letAddItemKeys config)
+                  (E.Doc ["Edit", "Let clause", "Add"])
         makeBinderContentEdit params content
             <&> ExpressionGui.egWidget %~ Widget.weakerEvents newLetEventMap
 
@@ -449,11 +432,10 @@ makeBinderContentEdit params (Sugar.BinderLet l) =
     do
         config <- ExprGuiM.readConfig
         let delEventMap =
-                maybe mempty
-                (Widget.keysEventMapMovesCursor (Config.delKeys config)
-                 (E.Doc ["Edit", "Delete let expression"])
-                 . fmap WidgetIds.fromEntityId . (^. Sugar.laSetToHole))
-                (l ^. Sugar.lActions)
+                l ^. Sugar.lActions . Sugar.laSetToHole
+                <&> WidgetIds.fromEntityId
+                & Widget.keysEventMapMovesCursor (Config.delKeys config)
+                  (E.Doc ["Edit", "Delete let expression"])
         mOuterScopeId <- ExprGuiM.readMScopeId
         let letBodyScope = liftA2 lookupMKey mOuterScopeId (l ^. Sugar.lBodyScope)
         [ makeLetEdit l
@@ -492,10 +474,8 @@ namedParamEditInfo paramInfo =
     { ParamEdit.iMakeNameEdit =
       ExpressionGui.makeNameOriginEdit (paramInfo ^. Sugar.npiName)
       <&> Lens.mapped %~ ExpressionGui.fromValueWidget
-    , ParamEdit.iMAddNext =
-      paramInfo ^? Sugar.npiMActions . Lens._Just . Sugar.fpAddNext
-    , ParamEdit.iMDel =
-      paramInfo ^? Sugar.npiMActions . Lens._Just . Sugar.fpDelete
+    , ParamEdit.iMAddNext = paramInfo ^. Sugar.npiActions . Sugar.fpAddNext & Just
+    , ParamEdit.iDel = paramInfo ^. Sugar.npiActions . Sugar.fpDelete
     }
 
 nullParamEditInfo :: MonadA m => Sugar.NullParamInfo m -> ParamEdit.Info m
@@ -506,9 +486,7 @@ nullParamEditInfo (Sugar.NullParamInfo mActions) =
       ExpressionGui.grammarLabel "◗" (Widget.toAnimId myId)
       >>= ExpressionGui.makeFocusableView myId
     , ParamEdit.iMAddNext = Nothing
-    , ParamEdit.iMDel =
-      mActions ^? Lens._Just . Sugar.npDeleteLambda
-      <&> Lens.mapped .~ Sugar.ParamDelResultDelVar
+    , ParamEdit.iDel = Sugar.ParamDelResultDelVar <$ mActions ^. Sugar.npDeleteLambda
     }
 
 makeParamsEdit ::

Lamdu/GUI/ExpressionEdit/CaseEdit.hs
@@ -49,7 +49,7 @@ make ::
     Sugar.Case (Name m) m (ExprGuiT.SugarExpr m) ->
     Sugar.Payload m ExprGuiT.Payload ->
     ExprGuiM m (ExpressionGui m)
-make (Sugar.Case mArg alts caseTail mAddAlt cEntityId) pl =
+make (Sugar.Case mArg alts caseTail addAlt cEntityId) pl =
     ExpressionGui.stdWrapParentExpr pl $ \myId ->
     let headerId = Widget.joinId myId ["header"]
     in ExprGuiM.assignCursor myId (destCursorId alts headerId) $
@@ -73,13 +73,12 @@ make (Sugar.Case mArg alts caseTail mAddAlt cEntityId) pl =
         (mActiveTag, header) <-
             case mArg of
             Sugar.LambdaCase -> headerLabel "λ:" <&> (,) Nothing
-            Sugar.CaseWithArg (Sugar.CaseArg arg mToLambdaCase) ->
+            Sugar.CaseWithArg (Sugar.CaseArg arg toLambdaCase) ->
                 do
                     argEdit <-
                         ExprGuiM.makeSubexpression (const 0) arg
                         <&> ExpressionGui.egWidget %~ Widget.weakerEvents
-                            (maybe mempty (toLambdaCaseEventMap config)
-                                mToLambdaCase)
+                            (toLambdaCaseEventMap config toLambdaCase)
                     caseLabel <- headerLabel ":"
                     mTag <-
                         ExpressionGui.evaluationResult (arg ^. Sugar.rPayload)
@@ -90,17 +89,16 @@ make (Sugar.Case mArg alts caseTail mAddAlt cEntityId) pl =
             do
                 altsGui <- makeAltsWidget mActiveTag alts myId
                 case caseTail of
-                    Sugar.ClosedCase mDeleteTail ->
+                    Sugar.ClosedCase deleteTail ->
                         altsGui
                         & ExpressionGui.egWidget %~
                           Widget.weakerEvents
-                          (maybe mempty (caseOpenEventMap config) mDeleteTail)
+                          (caseOpenEventMap config deleteTail)
                         & return
                     Sugar.CaseExtending rest ->
                         altsGui
                         & makeOpenCase rest (Widget.toAnimId myId)
-        let addAltEventMap Nothing = mempty
-            addAltEventMap (Just addAlt) =
+        let addAltEventMap =
                 ExprGuiM.holePickersAction resultPickers >> addAlt
                 <&> (^. Sugar.caarNewTag . Sugar.tagInstance)
                 <&> WidgetIds.fromEntityId
@@ -111,15 +109,14 @@ make (Sugar.Case mArg alts caseTail mAddAlt cEntityId) pl =
         [header, vspace, altsGui]
             & ExpressionGui.vboxTopFocalAlignedTo 0
             & ExpressionGui.addValFrame myId
-            <&> ExpressionGui.egWidget %~
-                Widget.weakerEvents (addAltEventMap mAddAlt)
+            <&> ExpressionGui.egWidget %~ Widget.weakerEvents addAltEventMap
 
 makeAltRow ::
     MonadA m =>
     Maybe Tag ->
     Sugar.CaseAlt (Name m) m (Sugar.Expression (Name m) m ExprGuiT.Payload) ->
     ExprGuiM m [ExpressionGui m]
-makeAltRow mActiveTag (Sugar.CaseAlt mDelete tag altExpr) =
+makeAltRow mActiveTag (Sugar.CaseAlt delete tag altExpr) =
     do
         config <- ExprGuiM.readConfig
         altRefGui <-
@@ -131,7 +128,7 @@ makeAltRow mActiveTag (Sugar.CaseAlt mDelete tag altExpr) =
                     (WidgetIds.fromEntityId (tag ^. Sugar.tagInstance))
                 else return
         altExprGui <- ExprGuiM.makeSubexpression (const 0) altExpr
-        let itemEventMap = maybe mempty (caseDelEventMap config) mDelete
+        let itemEventMap = caseDelEventMap config delete
         space <- ExpressionGui.stdSpace
         [ altRefGui & ExpressionGui.egAlignment . _1 .~ 1
             , space

Lamdu/GUI/ExpressionEdit/EventMap.hs
@@ -38,14 +38,14 @@ make ::
     ExprGuiM m (EventHandlers (T m))
 make holePickers pl =
     mconcat <$> sequenceA
-    [ maybe (return mempty)
-      ( actionsEventMap holePickers
-      $ if ExprGuiT.plOfHoleResult pl then HoleResult else NotHoleResult
-      )
-      (pl ^. Sugar.plActions)
+    [ actionsEventMap holePickers isHoleResult (pl ^. Sugar.plActions)
     , jumpHolesEventMapIfSelected pl
     , replaceOrComeToParentEventMap pl
     ]
+    where
+        isHoleResult
+            | ExprGuiT.plOfHoleResult pl = HoleResult
+            | otherwise = NotHoleResult
 
 mkEventMapWithPickers ::
     (Functor f, MonadA m) =>
@@ -105,7 +105,7 @@ replaceOrComeToParentEventMap pl =
         isSelected <- ExprGuiM.isExprSelected pl
         return $
             if isSelected
-            then maybe mempty (replaceEventMap config) $ pl ^. Sugar.plActions
+            then replaceEventMap config (pl ^. Sugar.plActions)
             else
                 Widget.keysEventMapMovesCursor (Config.delKeys config)
                 (E.Doc ["Navigation", "Select parent"]) selectParent

Lamdu/GUI/ExpressionEdit/HoleEdit.hs
@@ -6,14 +6,13 @@ module Lamdu.GUI.ExpressionEdit.HoleEdit
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.MonadA (MonadA)
-import           Data.Store.Transaction (Transaction)
 import qualified Data.Store.Transaction as Transaction
 import qualified Graphics.UI.Bottle.Widget as Widget
 import qualified Graphics.UI.Bottle.Widgets.Layout as Layout
 import qualified Graphics.UI.Bottle.WidgetsEnvT as WE
 import qualified Lamdu.Config as Config
 import           Lamdu.GUI.ExpressionEdit.HoleEdit.Common (addDarkBackground)
-import           Lamdu.GUI.ExpressionEdit.HoleEdit.Info (HoleInfo(..), EditableHoleInfo(..))
+import           Lamdu.GUI.ExpressionEdit.HoleEdit.Info (HoleInfo(..))
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.SearchArea as SearchArea
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.State as HoleState
 import           Lamdu.GUI.ExpressionEdit.HoleEdit.WidgetIds (WidgetIds(..))
@@ -27,21 +26,6 @@ import qualified Lamdu.GUI.ExpressionGui.Types as ExprGuiT
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import           Lamdu.Sugar.Names.Types (Name(..))
 import qualified Lamdu.Sugar.Types as Sugar
-
-type T = Transaction
-
-mkEditableHoleInfo ::
-    MonadA m => HoleInfo m -> Sugar.HoleActions (Name m) m -> T m (EditableHoleInfo m)
-mkEditableHoleInfo holeInfo actions =
-    do
-        stateProp <-
-            HoleState.assocStateRef (actions ^. Sugar.holeGuid) ^.
-            Transaction.mkProperty
-        EditableHoleInfo
-            { ehiActions = actions
-            , ehiState = stateProp
-            , ehiInfo = holeInfo
-            } & return
 
 makeWrapper ::
     MonadA m => Sugar.Payload m ExprGuiT.Payload ->
@@ -101,10 +85,20 @@ make ::
 make hole pl =
     do
         Config.Hole{..} <- ExprGuiM.readConfig <&> Config.hole
-        mEditableHoleInfo <-
-            hole ^. Sugar.holeMActions
-            & Lens._Just %%~ mkEditableHoleInfo holeInfo
-            & ExprGuiM.transaction
+
+        stateProp <-
+            HoleState.assocStateRef (hole ^. Sugar.holeActions . Sugar.holeGuid)
+            ^. Transaction.mkProperty & ExprGuiM.transaction
+
+        let holeInfo = HoleInfo
+                { hiEntityId = pl ^. Sugar.plEntityId
+                , hiState = stateProp
+                , hiInferredType = pl ^. Sugar.plAnnotation . Sugar.aInferredType
+                , hiHole = hole
+                , hiIds = WidgetIds{..}
+                , hiNearestHoles = pl ^. Sugar.plData . ExprGuiT.plNearestHoles
+                }
+
         do
             mWrapperGui <- makeWrapper pl holeInfo
             case mWrapperGui of
@@ -115,7 +109,7 @@ make hole pl =
                         isSelected <- ExprGuiM.widgetEnv $ WE.isSubCursor hidHole
                         let layout f = do
                                 searchAreaGui <-
-                                    SearchArea.makeStdWrapped pl holeInfo mEditableHoleInfo
+                                    SearchArea.makeStdWrapped pl holeInfo
                                 f WidgetIds{..} wrapperGui searchAreaGui
                                     <&> (`Layout.hoverInPlaceOf` unfocusedWrapperGui)
                         if wrapperGui ^. ExpressionGui.egWidget . Widget.isFocused
@@ -124,14 +118,7 @@ make hole pl =
                                      layout addWrapperAbove
                                  else
                                      return unfocusedWrapperGui
-                Nothing -> SearchArea.makeStdWrapped pl holeInfo mEditableHoleInfo
+                Nothing -> SearchArea.makeStdWrapped pl holeInfo
             & assignHoleCursor WidgetIds{..} (hole ^. Sugar.holeMArg)
     where
-        holeInfo = HoleInfo
-            { hiEntityId = pl ^. Sugar.plEntityId
-            , hiInferredType = pl ^. Sugar.plAnnotation . Sugar.aInferredType
-            , hiHole = hole
-            , hiIds = WidgetIds{..}
-            , hiNearestHoles = pl ^. Sugar.plData . ExprGuiT.plNearestHoles
-            }
         WidgetIds{..} = HoleWidgetIds.make (pl ^. Sugar.plEntityId)

Lamdu/GUI/ExpressionEdit/HoleEdit/EventMap.hs
@@ -24,7 +24,7 @@ import qualified Graphics.UI.GLFW as GLFW
 import           Lamdu.CharClassification (operatorChars, digitChars)
 import           Lamdu.Config (Config)
 import qualified Lamdu.Config as Config
-import           Lamdu.GUI.ExpressionEdit.HoleEdit.Info (HoleInfo(..), EditableHoleInfo(..))
+import           Lamdu.GUI.ExpressionEdit.HoleEdit.Info (HoleInfo(..))
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.Info as HoleInfo
 import           Lamdu.GUI.ExpressionEdit.HoleEdit.ShownResult (PickedResult(..), ShownResult(..))
 import           Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
@@ -170,12 +170,12 @@ emptyPickEventMap Config.Hole{..} =
 
 makeOpenEventMaps ::
     MonadA m =>
-    EditableHoleInfo m -> Maybe (ShownResult m) ->
+    HoleInfo m -> Maybe (ShownResult m) ->
     ExprGuiM m
     ( Widget.EventHandlers (T m)
     , Widget.EventHandlers (T m)
     )
-makeOpenEventMaps editableHoleInfo mShownResult =
+makeOpenEventMaps holeInfo mShownResult =
     do
         holeConfig <- ExprGuiM.readConfig <&> Config.hole
         -- below ad-hoc and search term edit:
@@ -187,7 +187,5 @@ makeOpenEventMaps editableHoleInfo mShownResult =
                 <&> mappend (pickEventMap holeConfig holeInfo shownResult)
         let adHocEdit =
                 adHocTextEditEventMap holeConfig
-                (HoleInfo.ehiSearchTermProperty editableHoleInfo)
+                (HoleInfo.hiSearchTermProperty holeInfo)
         pure (eventMap, adHocEdit <> eventMap)
-    where
-        holeInfo = ehiInfo editableHoleInfo

commit df84fb3a00d84f49db129ab3547905b6c7e66d02

Oops: NearestHoles (tabs) was broken by 4dc5e9e158a

Lamdu/Sugar/NearestHoles.hs
@@ -5,16 +5,26 @@ module Lamdu.Sugar.NearestHoles
     , add
     ) where
 
+import           Prelude.Compat
+
+import           Control.Applicative.Utils (when)
 import           Control.Lens (LensLike)
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
+import           Control.Lens.Tuple
 import           Control.Monad.Trans.State (State, evalState)
 import qualified Control.Monad.Trans.State as State
 import           Control.MonadA (MonadA)
 import qualified Lamdu.Sugar.Lens as SugarLens
 import qualified Lamdu.Sugar.Types as Sugar
 
-import           Prelude.Compat
+markStoredHoles ::
+    Sugar.Expression name m a ->
+    Sugar.Expression name m (Bool, a)
+markStoredHoles expr =
+    expr
+    <&> (,) False
+    & SugarLens.holePayloads . Sugar.plData . _1 .~ True
 
 data NearestHoles = NearestHoles
     { _prev :: Maybe Sugar.EntityId
@@ -38,8 +48,10 @@ add ::
 add exprs s =
     s
     & exprs . Lens.mapped %~ toNearestHoles
+    & exprs %~ markStoredHoles
     & passAll (exprs . SugarLens.subExprPayloads)
     & passAll (Lens.backwards (exprs . SugarLens.subExprPayloads))
+    & exprs . Lens.mapped %~ snd
     where
         toNearestHoles f prevHole nextHole = f (NearestHoles prevHole nextHole)
 
@@ -47,20 +59,22 @@ type M = State (Maybe Sugar.EntityId)
 
 passAll ::
     LensLike M s t
-    (Sugar.Payload m (Maybe Sugar.EntityId -> a))
-    (Sugar.Payload m a) -> s -> t
+    (Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a))
+    (Sugar.Payload m (Bool, a)) -> s -> t
 passAll sugarPls s =
     s
     & sugarPls %%~ setEntityId
     & (`evalState` Nothing)
 
 setEntityId ::
-    Sugar.Payload m (Maybe Sugar.EntityId -> a) ->
-    M (Sugar.Payload m a)
+    Sugar.Payload m (Bool, Maybe Sugar.EntityId -> a) ->
+    M (Sugar.Payload m (Bool, a))
 setEntityId pl =
     do
         oldEntityId <- State.get
-        State.put $ Just $ pl ^. Sugar.plEntityId
+        when isStoredHole $ State.put $ Just $ pl ^. Sugar.plEntityId
         pl
-            & Sugar.plData %~ ($ oldEntityId)
+            & Sugar.plData . _2 %~ ($ oldEntityId)
             & return
+    where
+        isStoredHole = pl ^. Sugar.plData . _1