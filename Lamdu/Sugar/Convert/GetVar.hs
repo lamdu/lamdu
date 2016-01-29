3c162c55e49b66812489585c6849ed88ca63eebb
Yair Chuchem <yairchu@gmail.com>
2016-01-29 15:47:35 +0200

    Convert.GetVar: refactor to extract matcher functions and use MatcherT

--- Lamdu/Sugar/Convert/GetVar.hs
+++ Lamdu/Sugar/Convert/GetVar.hs
@@ -5,8 +5,13 @@ module Lamdu.Sugar.Convert.GetVar
 
 import qualified Control.Lens as Lens
 import           Control.Lens.Operators
+import           Control.Monad (guard)
+import           Control.Monad.Trans.Class (MonadTrans(..))
+import           Control.Monad.Trans.Either.Utils (runMatcherT, justToLeft)
+import           Control.Monad.Trans.Maybe (MaybeT)
 import           Control.MonadA (MonadA)
 import           Data.Maybe (fromMaybe)
+import           Data.Maybe.Utils (maybeToMPlus)
 import           Data.Store.Guid (Guid)
 import           Lamdu.Builtins.Anchors (recurseVar)
 import qualified Lamdu.Expr.Lens as ExprLens
@@ -22,65 +27,80 @@ import           Lamdu.Sugar.Types
 
 import           Prelude.Compat
 
+convertRecurse ::
+    MonadA m => V.Var -> MaybeT (ConvertM m) (GetVar Guid m)
+convertRecurse param =
+    do
+        guard (param == recurseVar)
+        defI <-
+            ConvertM.readContext & lift
+            <&> (^. ConvertM.scDefI)
+            <&> fromMaybe (error "recurseVar used not in definition context?!")
+        GetBinder BinderVar
+            { _bvNameRef =
+                NameRef
+                { _nrName = UniqueId.toGuid defI
+                , _nrGotoDefinition = pure $ EntityId.ofIRef defI
+                }
+            , _bvForm = GetDefinition
+            , _bvInline = CannotInline
+            } & return
+
 usesAround :: Eq a => a -> [a] -> [a]
 usesAround x xs =
     drop 1 after ++ before
     where
         (before, after) = break (== x) xs
 
-convertH ::
-    MonadA m => ConvertM.Context m -> V.Var -> Input.Payload m a -> GetVar Guid m
-convertH sugarContext param exprPl
-    | param == recurseVar =
-      GetBinder BinderVar
-      { _bvNameRef =
-            NameRef
-            { _nrName = UniqueId.toGuid defI
-            , _nrGotoDefinition = pure $ EntityId.ofIRef defI
-            }
-      , _bvForm = GetDefinition
-      , _bvInline = CannotInline
-      }
+paramNameRef :: MonadA m => V.Var -> NameRef Guid m
+paramNameRef param =
+    NameRef
+    { _nrName = UniqueId.toGuid param
+    , _nrGotoDefinition = pure $ EntityId.ofLambdaParam param
+    }
 
-    | Just inline <- scopeInfo ^. ConvertM.siLetItems . Lens.at param =
-      GetBinder BinderVar
-      { _bvNameRef = paramNameRef
-      , _bvForm = GetLet
-      , _bvInline =
-        inline & _CannotInlineDueToUses %~ usesAround (exprPl ^. Input.entityId)
-      }
+convertGetBinder ::
+    MonadA m => V.Var -> Input.Payload m a -> MaybeT (ConvertM m) (GetVar Guid m)
+convertGetBinder param exprPl =
+    do
+        inline <-
+            lift ConvertM.readContext
+            <&> (^. ConvertM.scScopeInfo . ConvertM.siLetItems . Lens.at param)
+            >>= maybeToMPlus
+        GetBinder BinderVar
+            { _bvNameRef = paramNameRef param
+            , _bvForm = GetLet
+            , _bvInline =
+                inline
+                & _CannotInlineDueToUses %~ usesAround (exprPl ^. Input.entityId)
+            } & return
 
-    | isGetParamRecord =
-      GetParamsRecord ParamsRecordVar
-      { _prvFieldNames =
-          exprPl ^.. Input.inferredType . ExprLens._TRecord . ExprLens.compositeTags <&> UniqueId.toGuid
-      }
-
-    | otherwise =
-      GetParam (Param paramNameRef GetParameter NormalBinder)
-
-    where
-        paramNameRef =
-            NameRef
-            { _nrName = UniqueId.toGuid param
-            , _nrGotoDefinition = pure $ EntityId.ofLambdaParam param
-            }
-        isGetParamRecord =
-            scopeInfo ^..
-            siTagParamInfos . Lens.traversed . ConvertM._TagFieldParam
-            & map tpiFromParameters
-            & elem param
-        scopeInfo = sugarContext ^. ConvertM.scScopeInfo
-        defI =
-            sugarContext ^. ConvertM.scDefI
-            & fromMaybe (error "recurseVar used not in definition context?!")
+convertParamsRecord ::
+    MonadA m => V.Var -> Input.Payload m a -> MaybeT (ConvertM m) (GetVar Guid m)
+convertParamsRecord param exprPl =
+    do
+        lift ConvertM.readContext
+            <&> (^.. ConvertM.scScopeInfo . siTagParamInfos . Lens.traversed
+                . ConvertM._TagFieldParam)
+            <&> map tpiFromParameters
+            <&> elem param
+            >>= guard
+        GetParamsRecord ParamsRecordVar
+            { _prvFieldNames =
+                exprPl
+                ^.. Input.inferredType . ExprLens._TRecord . ExprLens.compositeTags
+                <&> UniqueId.toGuid
+            } & return
 
 convert ::
     MonadA m =>
     V.Var -> Input.Payload m a -> ConvertM m (ExpressionU m a)
 convert param exprPl =
     do
-        sugarContext <- ConvertM.readContext
-        convertH sugarContext param exprPl
-            & BodyGetVar
-            & addActions exprPl
+        convertRecurse param & justToLeft
+        convertGetBinder param exprPl & justToLeft
+        convertParamsRecord param exprPl & justToLeft
+        GetParam (Param (paramNameRef param) GetParameter NormalBinder) & return
+    & runMatcherT
+    <&> BodyGetVar
+    >>= addActions exprPl
