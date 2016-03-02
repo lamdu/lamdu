c64767ae7708f1d25cfe7b4d0a22a658a8c3f421
Eyal Lotem <eyal.lotem@gmail.com>
2016-03-02 02:17:37 +0200

    Setup.hs builds submodules/node/node which is also installed as a data file

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,28 @@
+{-# OPTIONS -Wall #-}
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Verbosity
+import Distribution.Simple.Utils
+import System.Directory
+
+buildNode :: Verbosity -> IO ()
+buildNode verbosity =
+    do
+        setCurrentDirectory "submodules/node"
+        rawSystemExit verbosity "./configure" ["--prefix=/tmp"]
+        rawSystemExit verbosity "make" ["-j4"]
+        setCurrentDirectory "../.."
+
+main :: IO ()
+main = do
+    e <- doesFileExist "submodules/node/node"
+    if e
+        then defaultMain
+        else defaultMainWithHooks simpleUserHooks
+            { postBuild =
+              \args buildFlags pkgDesc localBuildInfo ->
+              do
+                  postBuild simpleUserHooks args buildFlags pkgDesc localBuildInfo
+                  let verbosity = fromFlagOrDefault normal $ buildVerbosity buildFlags
+                  buildNode verbosity
+            }
