ce9baa147ba38ab335b702e23fabc5e0aa426536
Yair Chuchem <yairchu@gmail.com>
2016-04-06 02:39:23 +0200

    codejam export generates a zipfile

--- Lamdu/Data/Export/Codejam.hs
+++ Lamdu/Data/Export/Codejam.hs
@@ -4,20 +4,22 @@ module Lamdu.Data.Export.Codejam
     ( exportFancy
     ) where
 
-import           Codec.Picture (Image, PixelRGB8(..), withImage, writePng)
+import qualified Codec.Archive.Zip as Zip
+import           Codec.Picture (Image, PixelRGB8(..), withImage, encodePng)
 import           Control.Lens.Operators
 import qualified Data.Aeson.Encode.Pretty as AesonPretty
 import qualified Data.ByteString.Lazy as LBS
 import           Data.List (isPrefixOf)
 import           Data.Store.Transaction (Transaction)
 import qualified Data.Store.Transaction as Transaction
+import           Data.String (IsString(..))
+import           Data.Time.Clock.POSIX (getPOSIXTime)
 import qualified Foreign as F
+import qualified Graphics.Rendering.OpenGL as GL
 import qualified Lamdu.Data.DbLayout as DbLayout
 import           Lamdu.Data.Export.JSON (jsonExportRepl)
 import qualified Lamdu.Expr.IRef as ExprIRef
 import           Lamdu.Expr.Val (Val)
-import qualified Graphics.Rendering.OpenGL as GL
-import           System.Directory (createDirectory, copyFile)
 
 import           Prelude.Compat
 
@@ -55,17 +57,26 @@ readRepl =
 exportFancy :: T DbLayout.ViewM (IO ())
 exportFancy =
     do
-        exportedCode <- jsonExportRepl
+        exportedCode <-
+            jsonExportRepl <&> AesonPretty.encodePretty
         _todoRepl <- readRepl
+        -- TODO also export RESULT of repl to separate output.txt file
         return $
             do
-                createDirectory "codeExport"
-                takeScreenshot >>= writePng "codeExport/screenshot.png"
-                AesonPretty.encodePretty exportedCode &
-                    LBS.writeFile "codeExport/source.lamdu"
-                createDirectory "codeExport/js"
-                copyFile "js/rts.js" "codeExport/js/rts.js"
-                readFile "doc/CodeJamReadMe.md"
-                    <&> removeReadmeMeta
-                    >>= writeFile "codeExport/README.md"
+                now <- getPOSIXTime <&> round
+                screenshot <- takeScreenshot <&> encodePng
+                readme <-
+                    readFile "doc/CodeJamReadMe.md"
+                    <&> removeReadmeMeta <&> fromString
+                rts <- readFile "js/rts.js" <&> fromString
                 -- TODO create fast js runable in codeExport/js/main.js
+                let addFile archive (filename, contents) =
+                        Zip.addEntryToArchive
+                        (Zip.toEntry ("export/" ++ filename) now contents)
+                        archive
+                foldl addFile Zip.emptyArchive
+                    [ ("source.lamdu", exportedCode)
+                    , ("screenshot.png", screenshot)
+                    , ("README.md", readme)
+                    , ("js/rts.js", rts)
+                    ] & Zip.fromArchive & LBS.writeFile "export.zip"
