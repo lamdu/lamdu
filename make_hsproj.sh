29563364a7df8f703b568eb0be3ddec6e71542e4
Eyal Lotem <eyal.lotem@gmail.com>
2016-04-19 14:57:17 +0300

    Remove stale references to submodules/AlgoW

--- make_hsproj.sh
+++ make_hsproj.sh
@@ -5,8 +5,3 @@ cd bottlelib
 find * -type dir -exec mkdir -p ../lamdu.hsproj/{} ";"
 find * -name "*.hs" -exec ln -f {} ../lamdu.hsproj/{} ";"
 cd ..
-
-cd submodules/AlgoW
-find Data Lamdu Text -type dir -exec mkdir -p ../../lamdu.hsproj/{} ";"
-find Data Lamdu Text -name "*.hs" -exec ln -f {} ../../lamdu.hsproj/{} ";"
-cd ../..
