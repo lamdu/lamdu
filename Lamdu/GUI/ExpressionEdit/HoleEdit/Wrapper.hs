lamdu/lamdu

commit aa430cb41c4d93f7b53a0a1c57593d9a2b5e62a7

only focused widgets have event maps
removed comment "When unfocused, event map is usually irrelevant, but can too be used" which contradicts this - but is this still true?

Lamdu/GUI/ExpressionEdit/HoleEdit/Wrapper.hs
@@ -4,6 +4,7 @@ module Lamdu.GUI.ExpressionEdit.HoleEdit.Wrapper
     ( make
     ) where
 
+import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.Lens.Tuple
 import           Data.Monoid ((<>))
@@ -83,7 +84,7 @@ make WidgetIds{..} arg =
         let argIsFocused = argGui ^. ExpressionGui.egWidget & Widget.isFocused
         unwrapEventMap <- makeUnwrapEventMap arg WidgetIds{..}
         argGui
-            & ExpressionGui.egWidget . Widget.eventMap %~
+            & ExpressionGui.egWidget . Widget.mFocus . Lens._Just . Widget.eventMap %~
                 modifyWrappedEventMap config argIsFocused arg WidgetIds{..}
             & ExpressionGui.pad (frameWidth & _2 .~ 0)
             & ExpressionGui.egWidget %~

Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -76,7 +76,7 @@ makeBinderNameEdit binderActions rhsJumperEquals rhs name myId =
         jumpToRHSViaEquals n widget
             | nonOperatorName n =
                 widget
-                & Widget.eventMap %~ E.filterChars (/= '=')
+                & Widget.mFocus . Lens._Just . Widget.eventMap %~ E.filterChars (/= '=')
                 & Widget.weakerEvents rhsJumperEquals
             | otherwise = widget
 

Lamdu/GUI/ExpressionEdit/HoleEdit/Open.hs
@@ -319,11 +319,10 @@ makeHoleResultWidget resultId holeResult =
                 -- event maps of any result (we actually use the first one's
                 -- event map)
                 ExprGuiM.localEnv (WE.envCursor .~ idWithinResultWidget) mkWidget
-                <&> (^. Widget.eventMap)
+                <&> (^. Widget.mFocus . Lens._Just . Widget.eventMap)
         widget <-
             mkWidget
             <&> Widget.animFrame %~ Anim.mapIdentities (<> (resultSuffix # Widget.toAnimId resultId))
-            <&> Widget.eventMap .~ mempty
             >>= makeFocusable resultId
             >>= ExprGuiM.widgetEnv . BWidgets.liftLayerInterval
         return (widget, mkEventMap)

Lamdu/GUI/ExpressionEdit/HoleEdit/SearchTerm.hs
@@ -5,6 +5,7 @@ module Lamdu.GUI.ExpressionEdit.HoleEdit.SearchTerm
     ( make
     ) where
 
+import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.Monad (when)
 import qualified Data.Monoid as Monoid
@@ -59,7 +60,8 @@ make holeInfo =
         config <- ExprGuiM.readConfig
         let holeConfig@Config.Hole{..} = Config.hole config
         makeSearchTermPropEdit WidgetIds{..} (HoleInfo.hiSearchTermProperty holeInfo)
-            <&> Widget.eventMap %~ EventMap.disallowCharsFromSearchTerm holeConfig searchTerm
+            <&> Widget.mFocus . Lens._Just . Widget.eventMap
+                %~ EventMap.disallowCharsFromSearchTerm holeConfig searchTerm
             <&> addBackground (Widget.toAnimId hidOpenSearchTerm)
                 (Config.layers config) holeSearchTermBGColor
             <&> ExpressionGui.fromValueWidget

Lamdu/GUI/ExpressionGui.hs
@@ -405,9 +405,9 @@ makeNameEditWith onActiveEditor (Name nameSrc nameCollision setName name) myId =
             NameSourceAutoGenerated -> ""
             NameSourceStored -> name
         makeWordEdit =
-            BWidgets.makeWordEdit <&>
-            Lens.mapped . Lens.mapped . Widget.eventMap %~
-            E.filterChars (`notElem` disallowedNameChars)
+            BWidgets.makeWordEdit
+            <&> Lens.mapped . Lens.mapped . Widget.mFocus . Lens._Just . Widget.eventMap
+                %~ E.filterChars (`notElem` disallowedNameChars)
 
 stdWrap ::
     Monad m => Sugar.Payload m ExprGuiT.Payload ->

Lamdu/GUI/Scroll.hs
@@ -30,5 +30,6 @@ focusAreaIntoWindow winSize widget =
                 l :: Lens' (Vector2 Widget.R) Widget.R
                 l = Lens.cloneLens rawLens
         focalPoint =
-            widget ^. Widget.mFocalArea <&> (^. Rect.center)
+            widget ^? Widget.mFocus . Lens._Just . Widget.focalArea
+            <&> (^. Rect.center)
             & fromMaybe 0

bottlelib/Graphics/UI/Bottle/Main.hs
@@ -70,7 +70,8 @@ mainLoopWidget win widgetTickHandler mkWidgetUnmemod getConfig =
                     when anyUpdate newWidget
                     widget <- getWidget size
                     EventResult runInMainThread tickResults <-
-                        sequenceA (widget ^. Widget.eventMap . E.emTickHandlers)
+                        sequenceA
+                        (widget ^. Widget.mFocus . Lens._Just . Widget.eventMap . E.emTickHandlers)
                         ^. m
                     unless (null tickResults) newWidget
                     return MainAnim.EventResult
@@ -83,7 +84,7 @@ mainLoopWidget win widgetTickHandler mkWidgetUnmemod getConfig =
             , MainAnim.eventHandler = \event ->
                 do
                     widget <- getWidget size
-                    let eventMap = widget ^. Widget.eventMap
+                    let eventMap = widget ^. Widget.mFocus . Lens._Just . Widget.eventMap
                     mWidgetRes <- E.lookup (GLFW.getClipboardString win) event eventMap
                     EventResult runInMainThread mAnimIdMapping <-
                         (sequenceA mWidgetRes <&> fmap (^. Widget.eAnimIdMapping)) ^. m

bottlelib/Graphics/UI/Bottle/Widget.hs
@@ -19,7 +19,8 @@ module Graphics.UI.Bottle.Widget
     , keysEventMapMovesCursor
 
     -- Widget type and lenses:
-    , Widget(..), view, mEnter, eventMap, mFocalArea
+    , Widget(..), view, mEnter, mFocus
+    , Focus(..), eventMap, focalArea
     , animLayers, animFrame, size, width, height, events
 
     , isFocused
@@ -92,33 +93,34 @@ data EnterResult a = EnterResult
     , _enterResultEvent :: a
     }
 
+data Focus a = Focus
+    { _focalArea :: Rect
+    , _eventMap :: EventMap a
+    }
+
 data Widget a = Widget
     { _view :: View
     , _mEnter :: Maybe (Direction -> EnterResult a) -- Nothing if we're not enterable
-    , _eventMap :: EventMap a
-    , _mFocalArea :: Maybe Rect
+    , _mFocus :: Maybe (Focus a)
     }
 
 -- When focused, mEnter may still be relevant, e.g: FlyNav in an
 -- active textedit, to move to a different text-edit position.
 
--- When unfocused, event map is usually irrelevant, but can too be
--- used
-
 Lens.makeLenses ''EnterResult
 Lens.makeLenses ''EventResult
+Lens.makeLenses ''Focus
 Lens.makeLenses ''Widget
 
 isFocused :: Widget a -> Bool
-isFocused = Lens.has (mFocalArea . Lens._Just)
+isFocused = Lens.has (mFocus . Lens._Just)
 
 empty :: Widget f
 empty =
     Widget
     { _view = View.empty
     , _mEnter = Nothing
-    , _eventMap = mempty
-    , _mFocalArea = Nothing
+    , _mFocus = Nothing
     }
 
 {-# INLINE animFrame #-}
@@ -151,21 +153,23 @@ events :: Lens.Setter (Widget a) (Widget b) a b
 events =
     Lens.sets atEvents
     where
-        atEvents f widget = widget
+        atEvents :: (a -> b) -> Widget a -> Widget b
+        atEvents f widget =
+            widget
             { _mEnter =
-                      (Lens.mapped . Lens.mapped . enterResultEvent %~ f) $
-                      _mEnter widget
-            , _eventMap = f <$> _eventMap widget
+                  (Lens.mapped . Lens.mapped . enterResultEvent %~ f) $
+                  _mEnter widget
+            , _mFocus =
+                widget ^. mFocus & Lens._Just . eventMap . Lens.mapped %~ f
             }
 
 fromView :: View -> Widget f
 fromView v =
     Widget
-        { _mFocalArea = Nothing
-        , _view = v
-        , _eventMap = mempty
-        , _mEnter = Nothing
-        }
+    { _mFocus = Nothing
+    , _view = v
+    , _mEnter = Nothing
+    }
 
 takesFocus ::
     Functor f =>
@@ -181,13 +185,13 @@ takesFocus enterFunc widget =
 doesntTakeFocus :: Widget a -> Widget a
 doesntTakeFocus = mEnter .~ Nothing
 
--- ^ If doesn't take focus, event map is ignored
+-- ^ If doesn't take focus, does nothing
 strongerEvents :: EventMap a -> Widget a -> Widget a
-strongerEvents eMap = eventMap %~ (eMap `mappend`)
+strongerEvents eMap = mFocus . Lens._Just . eventMap %~ (eMap `mappend`)
 
--- ^ If doesn't take focus, event map is ignored
+-- ^ If doesn't take focus, does nothing
 weakerEvents :: EventMap a -> Widget a -> Widget a
-weakerEvents eMap = eventMap %~ (`mappend` eMap)
+weakerEvents eMap = mFocus . Lens._Just . eventMap %~ (`mappend` eMap)
 
 backgroundColor :: Int -> AnimId -> Draw.Color -> Widget a -> Widget a
 backgroundColor layer animId color =
@@ -244,14 +248,14 @@ translate pos widget =
         Direction.coordinates . Rect.topLeft -~ pos
     & mEnter . Lens._Just . Lens.mapped .
         enterResultRect . Rect.topLeft +~ pos
-    & mFocalArea . Lens._Just . Rect.topLeft +~ pos
+    & mFocus . Lens._Just . focalArea . Rect.topLeft +~ pos
     & view %~ View.translate pos
 
 scale :: Vector2 R -> Widget f -> Widget f
 scale mult widget =
     widget
     & view %~ View.scale mult
-    & mFocalArea . Lens._Just . Rect.topLeftAndSize *~ mult
+    & mFocus . Lens._Just . focalArea . Rect.topLeftAndSize *~ mult
     & mEnter . Lens._Just . Lens.mapped . enterResultRect . Rect.topLeftAndSize *~ mult
     & mEnter . Lens._Just . Lens.argument . Direction.coordinates . Rect.topLeftAndSize //~ mult
 
@@ -292,7 +296,12 @@ respondToCursorBy f env
     | otherwise = id
 
 respondToCursor :: Widget f -> Widget f
-respondToCursor widget = widget & mFocalArea .~ Just (Rect 0 (widget ^. size))
+respondToCursor widget =
+    widget & mFocus .~ Just
+        Focus
+        { _focalArea = Rect 0 (widget ^. size)
+        , _eventMap = mempty
+        }
 
 cursorAnimId :: AnimId
 cursorAnimId = ["background"]
@@ -304,10 +313,10 @@ data CursorConfig = CursorConfig
 
 renderWithCursor :: CursorConfig -> Widget a -> Anim.Frame
 renderWithCursor CursorConfig{..} widget =
-    maybe mempty renderCursor (widget ^. mFocalArea)
+    maybe mempty renderCursor (widget ^? mFocus . Lens._Just . focalArea)
     & mappend (widget ^. animFrame)
     where
-        renderCursor focalArea =
+        renderCursor area =
             Anim.backgroundColor cursorAnimId cursorLayer cursorColor
-            (focalArea ^. Rect.size)
-            & Anim.translate (focalArea ^. Rect.topLeft)
+            (area ^. Rect.size)
+            & Anim.translate (area ^. Rect.topLeft)

commit a488db8ba1692cb605ea21464374d1447d0a7d60

fixed going into hole args broken in aa430cb41

Lamdu/GUI/ExpressionEdit/HoleEdit/Wrapper.hs
@@ -84,8 +84,6 @@ make WidgetIds{..} arg =
         let argIsFocused = argGui ^. ExpressionGui.egWidget & Widget.isFocused
         unwrapEventMap <- makeUnwrapEventMap arg WidgetIds{..}
         argGui
-            & ExpressionGui.egWidget . Widget.mFocus . Lens._Just . Widget.eventMap %~
-                modifyWrappedEventMap config argIsFocused arg WidgetIds{..}
             & ExpressionGui.pad (frameWidth & _2 .~ 0)
             & ExpressionGui.egWidget %~
                 Widget.addInnerFrame
@@ -93,6 +91,8 @@ make WidgetIds{..} arg =
                 frameId frameColor frameWidth
             & ExpressionGui.egWidget %%~
                 ExprGuiM.widgetEnv . BWidgets.makeFocusableView hidWrapper
+            <&> ExpressionGui.egWidget . Widget.mFocus . Lens._Just . Widget.eventMap %~
+                modifyWrappedEventMap config argIsFocused arg WidgetIds{..}
             <&> ExpressionGui.egWidget %~ Widget.weakerEvents unwrapEventMap
     where
         frameId = Widget.toAnimId hidWrapper <> ["hole frame"]