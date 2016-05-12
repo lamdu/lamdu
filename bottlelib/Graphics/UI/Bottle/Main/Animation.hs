45e5389572c2ad44de07f142da1c729a694e0421
Eyal Lotem <Eyal Lotem>
2016-05-12 03:18:16 +0300

    Revert "Animate to initial state"
    This reverts commit 8d238552aee8430db2ad8edcf791f2db378b1528.

--- bottlelib/Graphics/UI/Bottle/Main/Animation.hs
+++ bottlelib/Graphics/UI/Bottle/Main/Animation.hs
@@ -205,7 +205,8 @@ mainLoop win getAnimationConfig animHandlers =
         tvars <-
             ThreadVars
             <$>
-                ( initialAnimState mempty >>= newTVarIO
+                ( makeFrame (animHandlers initialWinSize)
+                  >>= initialAnimState >>= newTVarIO
                 )
             <*> newTVarIO EventsData
                 { _edHaveTicks = False
