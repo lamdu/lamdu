e8394b397aad0a900c251338133f134fca4d0f48
Yair Chuchem <yairchu@gmail.com>
2017-01-28 01:27:22 +0200

    OutdatedDefs supports functions whose arguments only changed

--- Lamdu/Sugar/Convert/DefExpr/OutdatedDefs.hs
+++ Lamdu/Sugar/Convert/DefExpr/OutdatedDefs.hs
@@ -5,9 +5,11 @@ module Lamdu.Sugar.Convert.DefExpr.OutdatedDefs
 
 import qualified Control.Lens as Lens
 import qualified Data.Map as Map
+import qualified Data.Monoid as Monoid
 import qualified Data.Store.Property as Property
 import           Data.Store.Transaction (Transaction)
 import qualified Data.Store.Transaction as Transaction
+import           Lamdu.Calc.Type (Type)
 import qualified Lamdu.Calc.Type as T
 import           Lamdu.Calc.Type.Scheme (Scheme, schemeType, alphaEq)
 import qualified Lamdu.Calc.Val as V
@@ -18,36 +20,89 @@ import qualified Lamdu.Data.Ops as DataOps
 import qualified Lamdu.Data.Ops.Subexprs as SubExprs
 import           Lamdu.Expr.IRef (DefI, ValIProperty)
 import qualified Lamdu.Expr.IRef as ExprIRef
+import qualified Lamdu.Expr.Lens as ExprLens
 import           Lamdu.Sugar.Types (DefinitionOutdatedType(..))
 
 import           Lamdu.Prelude
 
 type T = Transaction
 
-changeFuncResult :: Monad m => V.Var -> Val (ValIProperty m) -> T m ()
-changeFuncResult usedDefVar (Val pl (V.BLeaf (V.LVar v))) =
-    when (v == usedDefVar) (void (DataOps.wrap pl))
-changeFuncResult usedDefVar
-    (Val pl (V.BApp (V.Apply (Val _ (V.BLeaf (V.LVar v))) arg)))
-    | v == usedDefVar =
-        do
-            DataOps.wrap pl & void
-            changeFuncResult usedDefVar arg
-changeFuncResult usedDefVar val =
-    val ^. Val.body & traverse_ (changeFuncResult usedDefVar)
+data IsHoleArg = IsHoleArg | NotHoleArg deriving Eq
 
-isFuncResultChange :: Scheme -> Scheme -> Bool
-isFuncResultChange prevType newType =
+holeWrapped :: Lens.Traversal' (Val a) (Val a)
+holeWrapped =
+    ExprLens.valApply .
+    Lens.filtered (Lens.has (V.applyFunc . ExprLens.valHole)) .
+    V.applyArg
+
+recursivelyFixExpr ::
+    Monad m =>
+    (IsHoleArg -> Val a -> Maybe ((IsHoleArg -> Val a -> m ()) -> m ())) ->
+    Val a -> m ()
+recursivelyFixExpr mFix =
+    go NotHoleArg
+    where
+        go isHoleArg expr =
+            case mFix isHoleArg expr of
+            Just fix -> fix go
+            Nothing -> recurse expr
+        recurse val =
+            case val ^? holeWrapped of
+            Just arg -> go IsHoleArg arg
+            Nothing -> traverse_ (go NotHoleArg) (val ^. Val.body)
+
+changeFuncRes :: Monad m => V.Var -> Val (ValIProperty m) -> T m ()
+changeFuncRes usedDefVar =
+    recursivelyFixExpr mFix
+    where
+        mFix NotHoleArg (Val pl (V.BLeaf (V.LVar v)))
+            | v == usedDefVar = DataOps.wrap pl & void & const & Just
+        mFix isHoleArg
+            (Val pl (V.BApp (V.Apply (Val _ (V.BLeaf (V.LVar v))) arg)))
+            | v == usedDefVar =
+            Just $
+            \go ->
+            do
+                when (isHoleArg == NotHoleArg) (void (DataOps.wrap pl))
+                go NotHoleArg arg
+        mFix _ _ = Nothing
+
+wrap :: Monad m => Val (ValIProperty m) -> T m ()
+wrap val
+    | Lens.has holeWrapped val
+    || Lens.has ExprLens.valHole val = return ()
+    | otherwise = val ^. Val.payload & DataOps.wrap & void
+
+changeFuncArg :: Monad m => V.Var -> Val (ValIProperty m) -> T m ()
+changeFuncArg usedDefVar =
+    recursivelyFixExpr mFix
+    where
+        mFix NotHoleArg (Val pl (V.BLeaf (V.LVar v)))
+            | v == usedDefVar = DataOps.wrap pl & void & const & Just
+        mFix _ (Val _ (V.BApp (V.Apply (Val _ (V.BLeaf (V.LVar v))) arg)))
+            | v == usedDefVar = Just $ \go -> wrap arg >> go IsHoleArg arg
+        mFix _ _ = Nothing
+
+isPartSame ::
+    (Lens.Getting (Monoid.First Type) Type Type) -> Scheme -> Scheme -> Bool
+isPartSame part preType newType =
     do
-        prevArg <- prevType & schemeType %%~ (^? T._TFun . _1)
-        newArg <- newType & schemeType %%~ (^? T._TFun . _1)
-        alphaEq prevArg newArg & guard
+        prePart <- preType & schemeType %%~ (^? part)
+        newPart <- newType & schemeType %%~ (^? part)
+        alphaEq prePart newPart & guard
     & Lens.has Lens._Just
+
+isFuncResChange :: Scheme -> Scheme -> Bool
+isFuncResChange = isPartSame (T._TFun . _1)
+
+isFuncArgChange :: Scheme -> Scheme -> Bool
+isFuncArgChange = isPartSame (T._TFun . _2)
 
 fixDefExpr ::
     Monad m => Scheme -> Scheme -> V.Var -> Val (ValIProperty m) -> T m ()
 fixDefExpr prevType newType usedDefVar defExpr
-    | isFuncResultChange prevType newType = changeFuncResult usedDefVar defExpr
+    | isFuncResChange prevType newType = changeFuncRes usedDefVar defExpr
+    | isFuncArgChange prevType newType = changeFuncArg usedDefVar defExpr
     | otherwise = SubExprs.onGetVars (DataOps.wrap <&> void) usedDefVar defExpr
 
 updateDefType ::
