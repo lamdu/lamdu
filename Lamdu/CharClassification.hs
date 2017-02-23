lamdu/lamdu

commit b05b858e8548c1c3caa318e82b0d911a15835d91

add missing operator chars

Lamdu/CharClassification.hs
@@ -3,7 +3,7 @@ module Lamdu.CharClassification
     ) where
 
 operatorChars :: String
-operatorChars = "\\+-*/^=><&|%$:.,!;"
+operatorChars = "\\+-*/^=><&|%$:.,!;#?@~"
 
 bracketChars :: String
 bracketChars = "()[]{}"

commit 3523d4b81f07d5ce6ee05b097f0250963ae37464

fix entering byte literals
broken since commit b05b858e8

Lamdu/CharClassification.hs
@@ -1,5 +1,5 @@
 module Lamdu.CharClassification
-    ( operatorChars, bracketChars, digitChars
+    ( operatorChars, bracketChars, digitChars, hexDigitChars
     ) where
 
 operatorChars :: String
@@ -10,3 +10,6 @@ bracketChars = "()[]{}"
 
 digitChars :: String
 digitChars = ['0'..'9']
+
+hexDigitChars :: String
+hexDigitChars = ['a'..'f'] ++ ['A' .. 'F'] ++ digitChars

Lamdu/GUI/ExpressionEdit/HoleEdit/EventMap.hs
@@ -21,7 +21,7 @@ import           Graphics.UI.Bottle.Widget (Widget)
 import qualified Graphics.UI.Bottle.Widget as Widget
 import qualified Graphics.UI.Bottle.Widgets.Grid as Grid
 import qualified Graphics.UI.GLFW as GLFW
-import           Lamdu.CharClassification (operatorChars, bracketChars, digitChars)
+import           Lamdu.CharClassification (operatorChars, bracketChars, digitChars, hexDigitChars)
 import           Lamdu.Config (Config)
 import qualified Lamdu.Config as Config
 import           Lamdu.GUI.ExpressionEdit.HoleEdit.Info (HoleInfo(..))
@@ -100,6 +100,9 @@ disallowCharsFromSearchTerm Config.Hole{..} holeInfo searchTerm =
             '-':x:xs
                 | x `elem` digitChars ->
                     digitChars ++ ['.' | not ("." `isInfixOf` xs)] & allowOnly
+            "#" | isLeafHole -> allowOnly (operatorChars ++ hexDigitChars)
+            '#':x:_
+                | x `elem` hexDigitChars -> allowOnly hexDigitChars
             x:xs
                 | x `elem` operatorChars -> allowOnly operatorChars
                 | x `elem` bracketChars -> allowOnly bracketChars