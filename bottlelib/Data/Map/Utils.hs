07b7e222ef448cc289c2738b770620a7c173c7d8
Eyal Lotem <eyal.lotem@gmail.com>
2017-02-23 14:46:04 +0200

    MapUtils: Add generalization of List.partition

--- bottlelib/Data/Map/Utils.hs
+++ bottlelib/Data/Map/Utils.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Data.Map.Utils
     ( setMapIntersection
+    , partition
     ) where
 
+import           Control.Lens.Operators
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import           Data.Set (Set)
@@ -10,3 +12,10 @@ import           Prelude.Compat
 
 setMapIntersection :: Ord k => Set k -> Map k a -> Map k a
 setMapIntersection s m = m `Map.intersection` Map.fromSet (const ()) s
+
+-- | Generalized Data.List.partition
+partition :: Ord k => (a -> k) -> [a] -> Map k [a]
+partition mkKey xs =
+    xs <&> singletonMap & Map.unionsWith (++)
+    where
+        singletonMap x = Map.singleton (mkKey x) [x]
