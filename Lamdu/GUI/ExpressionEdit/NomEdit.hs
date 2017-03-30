lamdu/lamdu

commit 9ede153c2beb7104ce71975bdc94601c9e56449a

can add let items inside ToNom
ToNoms introduce type variables into scope, hence they are binders.

Lamdu/GUI/ExpressionEdit/NomEdit.hs
@@ -11,6 +11,7 @@ import qualified Graphics.UI.Bottle.Widget.Aligned as AlignedWidget
 import qualified Graphics.UI.Bottle.Widget.TreeLayout as TreeLayout
 import qualified Graphics.UI.Bottle.WidgetsEnvT as WE
 import qualified Lamdu.Config as Config
+import qualified Lamdu.GUI.ExpressionEdit.BinderEdit as BinderEdit
 import           Lamdu.GUI.ExpressionGui
     ( ExpressionGui, Precedence, precBefore, precAfter, (<||), (||>) )
 import qualified Lamdu.GUI.ExpressionGui as ExpressionGui
@@ -19,6 +20,7 @@ import qualified Lamdu.GUI.ExpressionGui.Monad as ExprGuiM
 import qualified Lamdu.GUI.ExpressionGui.Types as ExprGuiT
 import qualified Lamdu.GUI.Precedence as Prec
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
+import qualified Lamdu.Sugar.Lens as SugarLens
 import           Lamdu.Sugar.Names.Types (Name(..))
 import qualified Lamdu.Sugar.Types as Sugar
 
@@ -30,17 +32,29 @@ data ShowName = NameHovering | NameShowing | NameCollapsed
 
 makeToNom ::
     Monad m =>
-    Sugar.Nominal (Name m) (ExprGuiT.SugarExpr m) ->
+    Sugar.Nominal (Name m) (Sugar.BinderBody (Name m) m (ExprGuiT.SugarExpr m)) ->
     Sugar.Payload m ExprGuiT.Payload ->
     ExprGuiM m (ExpressionGui m)
-makeToNom = mkNomGui precBefore "«" (\a b -> [a, b]) (||>)
+makeToNom nom pl =
+    traverse (BinderEdit.makeBinderBodyEdit Sugar.BinderWithoutParams) nom
+    >>= mkNomGui precBefore "«" (\a b -> [a, b]) (||>) valId pl
+    where
+        valId =
+            nom ^. Sugar.nVal . Sugar.bbContent . SugarLens.binderContentExpr
+                . Sugar.rPayload . Sugar.plEntityId & WidgetIds.fromEntityId
+
 
 makeFromNom ::
     Monad m =>
     Sugar.Nominal (Name m) (ExprGuiT.SugarExpr m) ->
     Sugar.Payload m ExprGuiT.Payload ->
     ExprGuiM m (ExpressionGui m)
-makeFromNom = mkNomGui precAfter "»" (\a b -> [b, a]) (flip (<||))
+makeFromNom nom pl =
+    nom
+    & traverse (ExprGuiM.makeSubexpression (precAfter .~ nomPrecedence+1))
+    >>= mkNomGui precAfter "»" (\a b -> [b, a]) (flip (<||)) valId pl
+    where
+        valId = nom ^. Sugar.nVal . Sugar.rPayload . Sugar.plEntityId & WidgetIds.fromEntityId
 
 nomPrecedence :: Int
 nomPrecedence = 9
@@ -51,10 +65,11 @@ mkNomGui ::
     Text ->
     (ExpressionGui m -> ExpressionGui m -> [ExpressionGui m]) ->
     (AlignedWidget (T m Widget.EventResult) -> ExpressionGui m -> ExpressionGui m) ->
-    Sugar.Nominal (Name m) (ExprGuiT.SugarExpr m) ->
+    Widget.Id ->
     Sugar.Payload m ExprGuiT.Payload ->
+    Sugar.Nominal (Name m) (ExpressionGui m) ->
     ExprGuiM m (ExpressionGui m)
-mkNomGui nameSidePrecLens str asList hCombine nom@(Sugar.Nominal _ val) pl =
+mkNomGui nameSidePrecLens str asList hCombine valId pl (Sugar.Nominal tid val) =
     ExprGuiM.withLocalPrecedence (nameSidePrecLens .~ 0) $
     ExpressionGui.stdWrapParentExpr pl $
     \myId ->
@@ -77,11 +92,9 @@ mkNomGui nameSidePrecLens str asList hCombine nom@(Sugar.Nominal _ val) pl =
                     else AlignedWidget.widget %~ Widget.takesFocus (const (pure nameId))
                 )
             <*> (ExpressionGui.makeFocusableView nameId
-                 <*> mkNameGui nom nameId)
-            <*> ExprGuiM.makeSubexpression (nameSidePrecLens .~ nomPrecedence+1) val
+                 <*> mkNameGui tid nameId)
+            ?? val
     & ExprGuiM.assignCursor myId valId
-    where
-        valId = val ^. Sugar.rPayload . Sugar.plEntityId & WidgetIds.fromEntityId
 
 expandingName ::
     Monad m =>
@@ -128,9 +141,8 @@ expandingName vertOrder (#>) needParen nomId showName =
             | otherwise = Nothing
 
 mkNameGui ::
-    Monad m => Sugar.Nominal (Name m) a -> Widget.Id ->
-    ExprGuiM m (AlignedWidget b)
-mkNameGui (Sugar.Nominal tidg _val) nameId =
+    Monad m => Sugar.TIdG (Name m) -> Widget.Id -> ExprGuiM m (AlignedWidget b)
+mkNameGui tidg nameId =
     ExpressionGui.makeNameView (tidg ^. Sugar.tidgName) (Widget.toAnimId nameId)
     <&> Widget.fromView
     <&> AlignedWidget.fromCenteredWidget

Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE NamedFieldPuns, RecordWildCards, NoImplicitPrelude, OverloadedStrings, LambdaCase #-}
 module Lamdu.GUI.ExpressionEdit.BinderEdit
     ( make
+    , makeBinderBodyEdit
     , Parts(..), makeParts
     , nonOperatorName
     ) where

Lamdu/GUI/RedundantAnnotations.hs
@@ -59,9 +59,10 @@ markAnnotationsToDisplay (Expression oldBody pl) =
         Expression newBody pl & dontShowAnnotation
     BodyFromNom _ ->
         Expression (newBody <&> dontShowEval) pl
-    BodyToNom (Nominal _ val) ->
+    BodyToNom (Nominal _ binder) ->
         pl
-        & showAnn . T.showInEvalMode .~ val ^. rPayload . showAnn . T.showInEvalMode
+        & showAnn . T.showInEvalMode .~
+            binder ^. bbContent . SugarLens.binderContentExpr . rPayload . showAnn . T.showInEvalMode
         & Expression (newBody <&> dontShowEval)
     BodyInject _ ->
         Expression newBody pl & dontShowEval

Lamdu/Sugar/Convert/Binder.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude, FlexibleContexts, OverloadedStrings, TypeFamilies, RankNTypes, RecordWildCards #-}
 module Lamdu.Sugar.Convert.Binder
-    ( convertDefinitionBinder, convertLam
+    ( convertDefinitionBinder, convertLam, convertBinderBody
     ) where
 
 import qualified Control.Lens as Lens
@@ -98,7 +98,7 @@ convertRedex expr redex =
             mkLetItemActions (expr ^. Val.payload . Input.stored)
             (redex <&> (^. Input.stored))
         letBody <-
-            makeBinderBody body
+            convertBinderBody body
             & localVarsUnderExtractDestPos [param]
             & localNewExtractDestPos expr
             & ConvertM.local (scScopeInfo . siLetItems <>~
@@ -138,11 +138,11 @@ makeBinderContent expr =
         <&> BinderExpr
     Just redex -> convertRedex expr redex <&> BinderLet
 
-makeBinderBody ::
+convertBinderBody ::
     (Monad m, Monoid a) =>
     Val (Input.Payload m a) ->
     ConvertM m (BinderBody UUID m (ExpressionU m a))
-makeBinderBody expr =
+convertBinderBody expr =
     do
         content <- makeBinderContent expr
         BinderBody
@@ -160,7 +160,7 @@ makeBinder :: (Monad m, Monoid a) =>
 makeBinder chosenScopeProp mPresentationModeProp ConventionalParams{..} funcBody =
     do
         binderBody <-
-            makeBinderBody funcBody
+            convertBinderBody funcBody
             & localVarsUnderExtractDestPos (cpMLamParam ^.. Lens._Just)
         return Binder
             { _bParams = _cpParams

Lamdu/Sugar/Convert/Nominal.hs
@@ -7,6 +7,7 @@ import           Control.Monad.Trans.Either.Utils (runMatcherT, justToLeft)
 import           Data.UUID.Types (UUID)
 import qualified Lamdu.Calc.Val as V
 import           Lamdu.Calc.Val.Annotated (Val(..))
+import qualified Lamdu.Sugar.Convert.Binder as ConvertBinder
 import           Lamdu.Sugar.Convert.Expression.Actions (addActionsWithSetToInner)
 import qualified Lamdu.Sugar.Convert.Input as Input
 import           Lamdu.Sugar.Convert.Monad (ConvertM)
@@ -18,31 +19,31 @@ import           Lamdu.Sugar.Types
 
 import           Lamdu.Prelude
 
-convertFromNom ::
-    (Monad m, Monoid a) => V.Nom (Val (Input.Payload m a)) ->
-    Input.Payload m a -> ConvertM m (ExpressionU m a)
-convertFromNom = convert BodyFromNom
-
-convert ::
-    (Monad m, Monoid a) =>
-    (Nominal UUID (ExpressionU m a) -> BodyU m b) ->
-    V.Nom (Val (Input.Payload m a)) ->
-    Input.Payload m b -> ConvertM m (ExpressionU m b)
-convert f (V.Nom tid val) exprPl =
+convertNom :: V.Nom expr -> Nominal UUID expr
+convertNom (V.Nom tid val) =
     Nominal
     { _nTId = ConvertTIdG.convert tid
     , _nVal = val
     }
-    & traverse ConvertM.convertSubexpression
-    <&> f
-    >>= addActionsWithSetToInner exprPl val
+
+convertFromNom ::
+    (Monad m, Monoid a) =>
+    V.Nom (Val (Input.Payload m a)) -> Input.Payload m a ->
+    ConvertM m (ExpressionU m a)
+convertFromNom nom pl =
+    traverse ConvertM.convertSubexpression nom
+    <&> convertNom <&> BodyFromNom
+    >>= addActionsWithSetToInner pl (nom ^. V.nomVal)
 
 convertToNom ::
     (Monad m, Monoid a) =>
     V.Nom (Val (Input.Payload m a)) -> Input.Payload m a ->
     ConvertM m (ExpressionU m a)
-convertToNom nom exprPl =
+convertToNom nom pl =
     do
-        ConvertText.text nom exprPl & justToLeft
-        convert BodyToNom nom exprPl & lift
+        ConvertText.text nom pl & justToLeft
+        traverse ConvertBinder.convertBinderBody nom
+            <&> convertNom <&> BodyToNom
+            >>= addActionsWithSetToInner pl (nom ^. V.nomVal)
+            & lift
     & runMatcherT

Lamdu/Sugar/Names/Walk.hs
@@ -173,8 +173,8 @@ toBody expr = \case
     BodyCase         x -> traverse expr x >>= (cAlts . traverse . caTag) toTagG <&> BodyCase
     BodyApply        x -> traverse expr x >>= (aAnnotatedArgs . traverse . aaTag) toTagG <&> BodyApply
     BodyHole         x -> traverse expr x >>= holeActions toHoleActions <&> BodyHole
-    BodyToNom        x -> traverse expr x >>= nTId toTIdG <&> BodyToNom
     BodyFromNom      x -> traverse expr x >>= nTId toTIdG <&> BodyFromNom
+    BodyToNom        x -> traverse (toBinderBody expr) x >>= nTId toTIdG <&> BodyToNom
     BodyGetVar       x -> toGetVar x <&> BodyGetVar
     BodyLiteral      x -> pure $ BodyLiteral x
     BodyLam          x -> toLam expr x <&> BodyLam

Lamdu/Sugar/Types/Expression.hs
@@ -309,7 +309,7 @@ data Body name m expr
     | BodyCase (Case name m expr)
     | BodyInject (Inject name expr)
     | BodyGetVar (GetVar name m)
-    | BodyToNom (Nominal name expr)
+    | BodyToNom (Nominal name (BinderBody name m expr))
     | BodyFromNom (Nominal name expr)
     | BodyInjectedExpression -- Used for hole results
     deriving (Functor, Foldable, Traversable)

commit b0b7622223b98da96586ad62374b3c765505b9c2

NomEdit: fix invalid cursor bug
Bug introduced last month in 9ede153c2beb7

Lamdu/GUI/ExpressionEdit/NomEdit.hs
@@ -36,13 +36,12 @@ makeToNom ::
     Sugar.Payload m ExprGuiT.Payload ->
     ExprGuiM m (ExpressionGui m)
 makeToNom nom pl =
-    traverse (BinderEdit.makeBinderBodyEdit Sugar.BinderWithoutParams) nom
-    >>= mkNomGui precBefore "«" (\a b -> [a, b]) (||>) valId pl
+    nom <&> BinderEdit.makeBinderBodyEdit Sugar.BinderWithoutParams
+    & mkNomGui precBefore "«" (\a b -> [a, b]) (||>) valId pl
     where
         valId =
             nom ^. Sugar.nVal . Sugar.bbContent . SugarLens.binderContentExpr
                 . Sugar.rPayload . Sugar.plEntityId & WidgetIds.fromEntityId
-
 
 makeFromNom ::
     Monad m =>
@@ -50,9 +49,8 @@ makeFromNom ::
     Sugar.Payload m ExprGuiT.Payload ->
     ExprGuiM m (ExpressionGui m)
 makeFromNom nom pl =
-    nom
-    & traverse (ExprGuiM.makeSubexpression (precAfter .~ nomPrecedence+1))
-    >>= mkNomGui precAfter "»" (\a b -> [b, a]) (flip (<||)) valId pl
+    nom <&> ExprGuiM.makeSubexpression (precAfter .~ nomPrecedence+1)
+    & mkNomGui precAfter "»" (\a b -> [b, a]) (flip (<||)) valId pl
     where
         valId = nom ^. Sugar.nVal . Sugar.rPayload . Sugar.plEntityId & WidgetIds.fromEntityId
 
@@ -67,7 +65,7 @@ mkNomGui ::
     (AlignedWidget (T m Widget.EventResult) -> ExpressionGui m -> ExpressionGui m) ->
     Widget.Id ->
     Sugar.Payload m ExprGuiT.Payload ->
-    Sugar.Nominal (Name m) (ExpressionGui m) ->
+    Sugar.Nominal (Name m) (ExprGuiM m (ExpressionGui m)) ->
     ExprGuiM m (ExpressionGui m)
 mkNomGui nameSidePrecLens str asList hCombine valId pl (Sugar.Nominal tid val) =
     ExprGuiM.withLocalPrecedence (nameSidePrecLens .~ 0) $
@@ -93,7 +91,7 @@ mkNomGui nameSidePrecLens str asList hCombine valId pl (Sugar.Nominal tid val) =
                 )
             <*> (ExpressionGui.makeFocusableView nameId
                  <*> mkNameGui tid nameId)
-            ?? val
+            <*> val
     & ExprGuiM.assignCursor myId valId
 
 expandingName ::