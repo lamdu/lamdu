lamdu/lamdu

commit 2347959e581e103868abdc9c26a5b66732c28c70

New simplified layers mechanism.
Seems to work ok but some stuff might be in the wrong layer now..

bottlelib/Graphics/UI/Bottle/View.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE NoImplicitPrelude, RecordWildCards, RankNTypes, OverloadedStrings, TemplateHaskell #-}
 module Graphics.UI.Bottle.View
-    ( View(..)
+    ( View(..), make
     , empty
-    , size, animFrame
+    , size, animLayers
+    , Layers(..), layers
+    , render
+    , animFrames
     , width, height
     , pad, assymetricPad
     , Size, R
@@ -16,23 +19,49 @@ import qualified Control.Lens as Lens
 import           Control.Lens.Operators
 import           Control.Lens.Tuple
 import           Control.Monad (void)
+import           Data.Monoid ((<>))
 import           Data.Vector.Vector2 (Vector2(..))
 import qualified Graphics.DrawingCombinators as Draw
-import           Graphics.UI.Bottle.Animation (AnimId, Layer, R)
+import           Graphics.UI.Bottle.Animation (AnimId, R)
 import qualified Graphics.UI.Bottle.Animation as Anim
 
 import           Prelude.Compat
 
 type Size = Anim.Size
 
+-- | Layers is a list of animation frames that overlay on top of each
+-- other (first element is most obscured one). When composing Views,
+-- the layers at the same list index are composed together and all
+-- obscure the layers from a lower index.
+newtype Layers = Layers { _layers :: [Anim.Frame] }
+Lens.makeLenses ''Layers
+
+instance Monoid Layers where
+    mempty = Layers []
+    mappend xs (Layers []) = xs
+    mappend (Layers []) ys = ys
+    mappend (Layers (x:xs)) (Layers (y:ys)) =
+        Layers (x<>y : rest ^. layers)
+        where
+            rest = Layers xs <> Layers ys
+
 data View = View
     { _size :: Size
-    , _animFrame :: Anim.Frame
+    , _animLayers :: Layers
     }
 Lens.makeLenses ''View
 
+make :: Size -> Anim.Frame -> View
+make sz frame = View sz (Layers [frame])
+
+render :: View -> Anim.Frame
+render view = view ^. animFrames
+
+animFrames :: Lens.Traversal' View Anim.Frame
+animFrames = animLayers . layers . traverse
+
 empty :: View
-empty = View 0 mempty
+empty = make 0 mempty
 
 width :: Lens' View R
 width = size . _1
@@ -42,9 +71,9 @@ height = size . _2
 
 -- | Add a diagonal line (top-left to right-bottom). Useful as a
 -- "deletion" GUI annotation
-addDiagonal :: R -> AnimId -> Layer -> Draw.Color -> View -> View
-addDiagonal thickness animId layer color view=
-    view & animFrame <>~ line
+addDiagonal :: R -> AnimId -> Draw.Color -> View -> View
+addDiagonal thickness animId color view=
+    view & animLayers . layers . Lens.reversed . Lens.ix 0 <>~ line
     where
         line =
             Draw.convexPoly
@@ -58,13 +87,12 @@ addDiagonal thickness animId layer color view=
             & Draw.tint color
             & void
             & Anim.simpleFrame animId
-            & Anim.layers +~ layer
             & Anim.scale (view ^. size)
 
-backgroundColor :: Layer -> AnimId -> Draw.Color -> View -> View
-backgroundColor layer animId color view =
+backgroundColor :: AnimId -> Draw.Color -> View -> View
+backgroundColor animId color view =
     view
-    & animFrame <>~ Anim.backgroundColor bgAnimId layer color (view ^. size)
+    & animLayers . layers . Lens.ix 0 <>~ Anim.backgroundColor bgAnimId color (view ^. size)
     where
         bgAnimId = animId ++ ["bg"]
 
@@ -72,13 +100,13 @@ scale :: Vector2 Draw.R -> View -> View
 scale ratio view =
     view
     & size *~ ratio
-    & animFrame %~ Anim.scale ratio
+    & animFrames %~ Anim.scale ratio
 
 pad :: Vector2 R -> View -> View
 pad p = assymetricPad p p
 
 translate :: Vector2 R -> View -> View
-translate pos = animFrame %~ Anim.translate pos
+translate pos = animFrames %~ Anim.translate pos
 
 assymetricPad :: Vector2 R -> Vector2 R -> View -> View
 assymetricPad leftAndTop rightAndBottom view =
@@ -87,4 +115,4 @@ assymetricPad leftAndTop rightAndBottom view =
     & translate leftAndTop
 
 tint :: Draw.Color -> View -> View
-tint color = animFrame . Anim.unitImages %~ Draw.tint color
+tint color = animFrames . Anim.unitImages %~ Draw.tint color

Lamdu/Config.hs
@@ -1,43 +1,21 @@
 {-# OPTIONS -O0 #-}
 {-# LANGUAGE DeriveGeneric, RecordWildCards #-}
 module Lamdu.Config
-    ( Layers(..)
-    , Help(..), Zoom(..), Export(..), Pane(..), Hole(..)
+    ( Help(..), Zoom(..), Export(..), Pane(..), Hole(..)
     , Name(..), Eval(..), Indent(..)
     , LiteralText(..)
     , LightLambda(..)
     , Config(..)
     , delKeys
-    , layerInterval
     ) where
 
 import qualified Data.Aeson.Types as Aeson
 import           Data.Vector.Vector2 (Vector2(..))
 import           GHC.Generics (Generic)
 import qualified Graphics.DrawingCombinators as Draw
-import qualified Graphics.UI.Bottle.Animation as Anim
 import           Graphics.UI.Bottle.ModKey (ModKey(..))
 import           Lamdu.Font (FontSize, Fonts(..))
 import qualified Lamdu.GUI.VersionControl.Config as VersionControl
-
-data Layers = Layers
-    { layerMin
-    , layerAnnotations
-    , layerTypeIndicatorFrame
-    , layerChoiceBG
-    , layerHoleBG
-    , layerDarkHoleBG
-    , layerNameCollisionBG
-    , layerValFrameBG
-    , layerActivePane
-    , layerMax :: Anim.Layer
-    } deriving (Eq, Generic, Show)
-instance Aeson.ToJSON Layers where
-    toJSON = Aeson.genericToJSON Aeson.defaultOptions
-instance Aeson.FromJSON Layers
-
-layerInterval :: Layers -> Int
-layerInterval Layers{..} = layerMax - layerMin
 
 data Help = Help
     { helpTextSize :: FontSize
@@ -166,7 +144,6 @@ instance Aeson.FromJSON Indent
 data Config = Config
     { fonts :: Fonts FilePath
     , baseTextSize :: FontSize
-    , layers :: Layers
     , help :: Help
     , zoom :: Zoom
     , export :: Export

Lamdu/GUI/CodeEdit.hs
@@ -377,9 +377,8 @@ makePaneWidget defS =
         let Config.Pane{paneActiveBGColor,paneInactiveTintColor} = Config.pane config
         let colorizeInactivePane = Widget.tint paneInactiveTintColor
         let colorizeActivePane =
-                Widget.backgroundColor
-                (Config.layerActivePane (Config.layers config))
-                WidgetIds.activePaneBackground paneActiveBGColor
+                Widget.backgroundColor WidgetIds.activePaneBackground
+                paneActiveBGColor
         let colorize widget
                 | Widget.isFocused widget = colorizeActivePane widget
                 | otherwise = colorizeInactivePane widget

Lamdu/GUI/DefinitionEdit.hs
@@ -137,7 +137,7 @@ typeIndicator color myId =
     <&>
     \config width ->
     Anim.unitSquare (Widget.toAnimId (typeIndicatorId myId))
-    & View 1
+    & View.make 1
     & View.scale (Vector2 width (realToFrac (Config.typeIndicatorFrameWidth config ^. _2)))
     & View.tint color
 

Lamdu/GUI/EvalView.hs
@@ -150,7 +150,7 @@ makeRecExtend animId typ recExtend =
                         GridView.verticalAlign 0.5 [sqr, v] & return
                     where
                         sqr =
-                            View 1 (Anim.unitSquare (animId ++ ["line"]))
+                            View.make 1 (Anim.unitSquare (animId ++ ["line"]))
                             & View.scale (Vector2 barWidth 1)
             GridView.verticalAlign 0.5 [fieldsView, restView] & return
     where

Lamdu/GUI/ExpressionEdit/ApplyEdit.hs
@@ -66,7 +66,7 @@ infixMarker (Vector2 w h) =
 addInfixMarker :: Widget.Id -> Widget a -> Widget a
 addInfixMarker widgetId widget =
     widget
-    & Widget.animFrame
+    & Widget.bottomFrame
     <>~ Anim.simpleFrame frameId (infixMarker (widget ^. Widget.size))
     where
         frameId = Widget.toAnimId widgetId ++ ["infix"]

Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -27,7 +27,6 @@ import qualified Graphics.UI.Bottle.Widgets.FocusDelegator as FocusDelegator
 import qualified Graphics.UI.Bottle.WidgetsEnvT as WE
 import qualified Graphics.UI.GLFW as GLFW
 import           Lamdu.CharClassification (operatorChars)
-import           Lamdu.Config (Config)
 import qualified Lamdu.Config as Config
 import qualified Lamdu.GUI.CodeEdit.Settings as CESettings
 import qualified Lamdu.GUI.ExpressionEdit.EventMap as ExprEventMap
@@ -78,8 +77,8 @@ makeBinderNameEdit binderActions rhsJumperEquals rhs name myId =
                 & Widget.weakerEvents rhsJumperEquals
             | otherwise = widget
 
-presentationModeChoiceConfig :: Config -> Choice.Config
-presentationModeChoiceConfig config = Choice.Config
+presentationModeChoiceConfig :: Choice.Config
+presentationModeChoiceConfig = Choice.Config
     { Choice.cwcFDConfig =
         FocusDelegator.Config
         { FocusDelegator.focusChildKeys = [ModKey mempty GLFW.Key'Enter]
@@ -89,7 +88,6 @@ presentationModeChoiceConfig config = Choice.Config
         }
     , Choice.cwcOrientation = Box.Vertical
     , Choice.cwcExpandMode = Choice.ExplicitEntry
-    , Choice.cwcBgLayer = Config.layerChoiceBG $ Config.layers config
     }
 
 mkPresentationModeEdit ::
@@ -107,7 +105,7 @@ mkPresentationModeEdit myId prop = do
             return (presentationMode, widget)
     pairs <- traverse mkPair [Sugar.OO, Sugar.Verbose, Sugar.Infix 5]
     BWidgets.makeChoiceWidget (Transaction.setP prop) pairs cur
-        (presentationModeChoiceConfig config) myId
+        presentationModeChoiceConfig myId
         & ExprGuiM.widgetEnv
         <&> Widget.scale (realToFrac <$> Config.presentationChoiceScaleFactor config)
 

Lamdu/GUI/ExpressionEdit/CaseEdit.hs
@@ -143,7 +143,7 @@ makeAltsWidget mActiveTag alts _ =
 separationBar :: Config -> Widget.R -> Anim.AnimId -> View
 separationBar config width animId =
     Anim.unitSquare (animId <> ["tailsep"])
-    & View 1
+    & View.make 1
     & View.tint (Config.caseTailColor config)
     & View.scale (Vector2 width 10)
 

commit ad0aa24ad2d7c8672b479dd8d9006e874dda7d44

fix View.backgroundColor for empty views - indentation bars appear again
was broken since 2347959e581e103868abdc9c26a5b66732c28c70

bottlelib/Graphics/UI/Bottle/View.hs
@@ -92,9 +92,12 @@ addDiagonal thickness animId color view=
 backgroundColor :: AnimId -> Draw.Color -> View -> View
 backgroundColor animId color view =
     view
-    & animLayers . layers . Lens.ix 0 <>~ Anim.backgroundColor bgAnimId color (view ^. size)
+    & animLayers . layers %~ addBg
     where
         bgAnimId = animId ++ ["bg"]
+        bg = Anim.backgroundColor bgAnimId color (view ^. size)
+        addBg [] = [bg]
+        addBg (x:xs) = x <> bg : xs
 
 scale :: Vector2 Draw.R -> View -> View
 scale ratio view =