59534675ff4d25c6396b064942887621961af2c9
Yair Chuchem <yairchu@gmail.com>
2017-05-18 19:16:17 +0300

    hello-world example quit command
    When not compiling with -threaded this doesn't quit immediately (bug)

--- gui-examples/hello-world.hs
+++ gui-examples/hello-world.hs
@@ -5,9 +5,12 @@ module Main where
 import           Control.Lens
 import           Data.Vector.Vector2 (Vector2(..))
 import qualified Graphics.DrawingCombinators as Draw
+import           Graphics.UI.Bottle.EventMap as EventMap
 import qualified Graphics.UI.Bottle.Main as Main
-import           Graphics.UI.Bottle.Widget (Widget, Size)
+import           Graphics.UI.Bottle.MetaKey (MetaKey(..), noMods)
+import           Graphics.UI.Bottle.Widget (Widget, Size, EventResult, keysEventMap, strongerEvents, respondToCursor)
 import qualified Graphics.UI.Bottle.Widgets.TextView as TextView
+import qualified Graphics.UI.GLFW as GLFW
 import qualified Graphics.UI.GLFW.Utils as GLFWUtils
 
 import           Prelude.Compat
@@ -19,7 +22,7 @@ main =
         Main.mainLoopWidget win hello Main.defaultOptions
     & GLFWUtils.withGLFW
 
-hello :: Size -> IO (Widget a)
+hello :: Functor f => Size -> IO (Widget (f EventResult))
 hello size =
     do
         font <- Draw.openFont (min 200 (realToFrac (size ^. _2))) "fonts/DejaVuSans.ttf"
@@ -29,4 +32,13 @@ hello size =
                 , TextView._styleFont = font
                 , TextView._styleUnderline = Nothing
                 }
-        TextView.makeWidget textStyle "Hello World!" ["hello"] & return
+        TextView.makeWidget textStyle "Hello World!" ["hello"]
+            & respondToCursor
+            & strongerEvents quitEventMap
+            & return
+
+quitEventMap :: Functor f => EventMap (f EventResult)
+quitEventMap =
+    keysEventMap
+    [MetaKey noMods GLFW.Key'Q] (EventMap.Doc ["Quit"])
+    (error "Quit")
