44b2e3993cdf0aad4b8e150e11b7e6cba6e38fbb
Yair Chuchem <yairchu@gmail.com>
2017-05-18 16:54:29 +0300

    resizable hello world
    blocking font size due to freetype-gl limits

--- gui-examples/gui-demo.hs
+++ gui-examples/gui-demo.hs
@@ -2,10 +2,11 @@
 
 module Main where
 
-import           Control.Lens.Operators
+import           Control.Lens
 import           Data.Vector.Vector2 (Vector2(..))
 import qualified Graphics.DrawingCombinators as Draw
 import qualified Graphics.UI.Bottle.Main as Main
+import           Graphics.UI.Bottle.Widget (Widget, Size)
 import qualified Graphics.UI.Bottle.Widgets.TextView as TextView
 import qualified Graphics.UI.GLFW.Utils as GLFWUtils
 
@@ -14,14 +15,18 @@ import           Prelude.Compat
 main :: IO ()
 main =
     do
-        win <- GLFWUtils.createWindow "GUI Demo" Nothing (Vector2 800 150)
-        font <- Draw.openFont 120 "fonts/DejaVuSans.ttf"
+        win <- GLFWUtils.createWindow "GUI Demo" Nothing (Vector2 800 100)
+        Main.mainLoopWidget win hello Main.defaultOptions
+    & GLFWUtils.withGLFW
+
+hello :: Size -> IO (Widget a)
+hello size =
+    do
+        font <- Draw.openFont (min 200 (realToFrac (size ^. _2))) "fonts/DejaVuSans.ttf"
         let textStyle =
                 TextView.Style
                 { TextView._styleColor = Draw.Color 1 1 1 1
                 , TextView._styleFont = font
                 , TextView._styleUnderline = Nothing
                 }
-        let hello = TextView.makeWidget textStyle "Hello World!" ["hello"]
-        Main.mainLoopWidget win (const (return hello)) Main.defaultOptions
-    & GLFWUtils.withGLFW
+        TextView.makeWidget textStyle "Hello World!" ["hello"] & return
