eec47423a06d027375050456cfed8d68ebbe5bb6
Yair Chuchem <yairchu@gmail.com>
2017-05-25 16:26:16 +0300

    further hello-world cleanup

--- gui-examples/hello-world.hs
+++ gui-examples/hello-world.hs
@@ -2,15 +2,12 @@
 
 module Main where
 
-import           Control.Lens.Operators
-import           Control.Lens.Tuple
 import           Data.MRUMemo (memoIO)
 import           Data.Vector.Vector2 (Vector2(..))
 import qualified Graphics.DrawingCombinators as Draw
 import qualified Graphics.UI.Bottle.Main as Main
 import           Graphics.UI.Bottle.Widget (Widget, Size, EventResult, strongerEvents, respondToCursor)
 import qualified Graphics.UI.Bottle.Widgets.TextView as TextView
-import           Graphics.UI.Bottle.Zoom (Zoom)
 import qualified Graphics.UI.Bottle.Zoom as Zoom
 import qualified Graphics.UI.GLFW.Utils as GLFWUtils
 
@@ -21,23 +18,22 @@ fontPath = "fonts/DejaVuSans.ttf"
 
 main :: IO ()
 main =
-    do
+    GLFWUtils.withGLFW $ do
         win <- GLFWUtils.createWindow "Hello World" Nothing (Vector2 800 400)
         cachedOpenFont <-
             memoIO $ \size ->
-            Draw.openFont (min 100 (realToFrac (size ^. _2))) fontPath
+            Draw.openFont (min 100 size) fontPath
         Main.defaultOptions fontPath
             >>= Main.mainLoopWidget win (hello cachedOpenFont)
-    & GLFWUtils.withGLFW
 
 hello ::
     Functor m =>
-    (Size -> IO Draw.Font) -> Zoom -> Size -> IO (Widget (m EventResult))
+    (Float -> IO Draw.Font) -> Zoom.Zoom -> Size -> IO (Widget (m EventResult))
 hello getFont zoom _size =
     do
         sizeFactor <- Zoom.getSizeFactor zoom
         font <- getFont (sizeFactor * 20)
-        TextView.makeWidget (TextView.whiteText font) "Hello World!" ["hello"]
-            & respondToCursor
-            & strongerEvents Main.quitEventMap
-            & return
+        return $
+            strongerEvents Main.quitEventMap $
+            respondToCursor $
+            TextView.makeWidget (TextView.whiteText font) "Hello World!" ["hello"]
