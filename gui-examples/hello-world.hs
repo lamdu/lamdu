86f98b19a79ee6a64054eb7760013d740fcb5645
Eyal Lotem <eyal.lotem@gmail.com>
2017-05-25 13:56:32 +0300

    hello-world: Cache font loading

--- gui-examples/hello-world.hs
+++ gui-examples/hello-world.hs
@@ -4,6 +4,7 @@ module Main where
 
 import           Control.Lens.Operators
 import           Control.Lens.Tuple
+import           Data.MRUMemo (memoIO)
 import           Data.Vector.Vector2 (Vector2(..))
 import qualified Graphics.DrawingCombinators as Draw
 import           Graphics.UI.Bottle.EventMap as EventMap
@@ -19,22 +20,29 @@ import qualified Graphics.UI.GLFW.Utils as GLFWUtils
 
 import           Prelude.Compat
 
+openFont :: Size -> IO Draw.Font
+openFont size =
+    Draw.openFont (min 100 (realToFrac (size ^. _2))) "fonts/DejaVuSans.ttf"
+
 main :: IO ()
 main =
     do
-        win <- GLFWUtils.createWindow "Hello World" Nothing (Vector2 800 100)
+        win <- GLFWUtils.createWindow "Hello World" Nothing (Vector2 800 400)
         addHelp <- EventMapDoc.makeToggledHelpAdder EventMapDoc.HelpNotShown
-        Main.mainLoopWidget win (hello addHelp) Main.defaultOptions
+        cachedOpenFont <- memoIO openFont
+        Main.mainLoopWidget win (hello cachedOpenFont addHelp) Main.defaultOptions
     & GLFWUtils.withGLFW
 
 hello ::
     Functor m =>
-    (EventMapDoc.Config -> Size -> Widget (m EventResult) -> IO (Widget (m EventResult))) ->
+    (Size -> IO Draw.Font) ->
+    (EventMapDoc.Config -> Size -> Widget (m EventResult) ->
+     IO (Widget (m EventResult))) ->
     Zoom -> Size -> IO (Widget (m EventResult))
-hello addHelp zoom size =
+hello getFont addHelp zoom size =
     do
         sizeFactor <- Zoom.getSizeFactor zoom
-        font <- Draw.openFont (min 100 (sizeFactor * realToFrac (size ^. _2))) "fonts/DejaVuSans.ttf"
+        font <- getFont (sizeFactor * realToFrac (size ^. _2))
         TextView.makeWidget (TextView.whiteText font) "Hello World!" ["hello"]
             & respondToCursor
             & strongerEvents quitEventMap
