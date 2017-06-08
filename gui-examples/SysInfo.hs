80b125e28d91d1cb025be94bc9177bd6f0a35f33
Yair Chuchem <yairchu@gmail.com>
2017-06-08 13:30:48 +0300

    add gui-examples/SysInfo.hs
    trying to investigate why things look off in Parallels VM hosted Ubuntu..

---
+++ gui-examples/SysInfo.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Main where
+
+import qualified Graphics.UI.GLFW as GLFW
+import qualified Graphics.UI.GLFW.Utils as GLFWUtils
+
+import Lamdu.Prelude
+
+main :: IO ()
+main =
+    do
+        putStrLn ""
+        monitor <-
+            GLFW.getPrimaryMonitor
+            >>= maybe (fail "Cannot get primary monitor") return
+        GLFW.getMonitorPhysicalSize monitor
+            <&> (\(x,y) -> "Monitor physical size: " ++ show x ++ "mm x " ++ show y ++ "mm")
+            >>= putStrLn
+        putStrLn ""
+        GLFW.getVideoMode monitor
+            >>= maybe (fail "Can't get video mode of monitor") return
+            <&> show
+            <&> ("Video mode: " ++)
+            >>= putStrLn
+        putStrLn ""
+    & GLFWUtils.withGLFW
