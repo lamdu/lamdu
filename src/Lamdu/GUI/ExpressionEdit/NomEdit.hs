lamdu/lamdu

commit 245278da55fcfda46818ddbd41a05d840cd665c4

Default alignment point is top-left of text

src/Lamdu/GUI/ExpressionEdit/NomEdit.hs
@@ -6,7 +6,7 @@ module Lamdu.GUI.ExpressionEdit.NomEdit
 import qualified Control.Lens as Lens
 import           Data.Store.Transaction (Transaction)
 import qualified Graphics.UI.Bottle.Widget as Widget
-import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget)
+import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget(..))
 import qualified Graphics.UI.Bottle.Widget.Aligned as AlignedWidget
 import qualified Graphics.UI.Bottle.Widget.TreeLayout as TreeLayout
 import qualified Lamdu.Config.Theme as Theme
@@ -106,7 +106,7 @@ expandingName ::
     )
 expandingName vertOrder (#>) needParen nomId showName =
     do
-        space <- ExpressionGui.stdHSpace <&> AlignedWidget.fromCenteredWidget
+        space <- ExpressionGui.stdHSpace <&> AlignedWidget 0
         addBg <- ExpressionGui.addValBGWithColor Theme.valNomBGColor
         horizWithFallback <- ExpressionGui.horizVertFallback mParenInfo
         return $
@@ -139,5 +139,4 @@ mkNameGui ::
     Monad m => Sugar.TIdG (Name m) -> Widget.Id -> ExprGuiM m (AlignedWidget b)
 mkNameGui tidg nameId =
     ExpressionGui.makeNameView (tidg ^. Sugar.tidgName) (Widget.toAnimId nameId)
-    <&> Widget.fromView
-    <&> AlignedWidget.fromCenteredWidget
+    <&> Widget.fromView <&> AlignedWidget 0

src/Graphics/UI/Bottle/Widget/Aligned.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE NoImplicitPrelude, TypeFamilies, TemplateHaskell, RankNTypes, FlexibleContexts, DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Graphics.UI.Bottle.Widget.Aligned
-    ( AlignedWidget, alignment, aWidget
+    ( AlignedWidget(..), alignment, aWidget
     , asTuple
-    , empty, fromCenteredWidget, fromCenteredView
+    , empty
     , scaleAround, scale
     , hoverInPlaceOf
     , AbsAlignedWidget, absAlignedWidget
@@ -16,7 +16,6 @@ import           Data.Vector.Vector2 (Vector2(..))
 import           Graphics.UI.Bottle.Alignment (Alignment(..))
 import qualified Graphics.UI.Bottle.Alignment as Alignment
 import qualified Graphics.UI.Bottle.EventMap as E
-import           Graphics.UI.Bottle.View (View)
 import qualified Graphics.UI.Bottle.View as View
 import           Graphics.UI.Bottle.Widget (Widget)
 import qualified Graphics.UI.Bottle.Widget as Widget
@@ -47,14 +46,8 @@ instance View.HasView (AlignedWidget a) where view = aWidget . View.view
 instance E.HasEventMap AlignedWidget where eventMap = aWidget . E.eventMap
 instance Widget.HasWidget AlignedWidget where widget = aWidget
 
-fromCenteredWidget :: Widget a -> AlignedWidget a
-fromCenteredWidget = AlignedWidget 0.5
-
-fromCenteredView :: View -> AlignedWidget a
-fromCenteredView = fromCenteredWidget . Widget.fromView
-
 empty :: AlignedWidget a
-empty = fromCenteredWidget Widget.empty
+empty = AlignedWidget 0 Widget.empty
 
 -- | scale = scaleAround 0.5
 --   scaleFromTopMiddle = scaleAround (Vector2 0.5 0)

src/Graphics/UI/Bottle/Widget/TreeLayout.hs
@@ -33,7 +33,7 @@ module Graphics.UI.Bottle.Widget.TreeLayout
     , alignedWidget, alignment, modeWidths
 
     -- * Leaf generation
-    , fromAlignedWidget, fromCenteredWidget, fromCenteredView, empty
+    , fromAlignedWidget, fromWidget, fromView, empty
     ) where
 
 import qualified Control.Lens as Lens
@@ -43,7 +43,7 @@ import           Graphics.UI.Bottle.View (View)
 import qualified Graphics.UI.Bottle.View as View
 import           Graphics.UI.Bottle.Widget (Widget)
 import qualified Graphics.UI.Bottle.Widget as Widget
-import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget)
+import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget(..))
 import qualified Graphics.UI.Bottle.Widget.Aligned as AlignedWidget
 
 import           Lamdu.Prelude
@@ -102,16 +102,14 @@ alignment = alignedWidget . AlignedWidget.alignment
 fromAlignedWidget :: AlignedWidget a -> TreeLayout a
 fromAlignedWidget = TreeLayout . const
 
--- | Lifts a Widget into a 'TreeLayout' with an alignment point at the
--- widget's center
-fromCenteredWidget :: Widget a -> TreeLayout a
-fromCenteredWidget = fromAlignedWidget . AlignedWidget.fromCenteredWidget
+-- | Lifts a Widget into a 'TreeLayout' with an alignment point at the top left
+fromWidget :: Widget a -> TreeLayout a
+fromWidget = fromAlignedWidget . AlignedWidget 0
 
--- | Lifts a View into a 'TreeLayout' with an alignment point at the
--- view's center
-fromCenteredView :: View -> TreeLayout a
-fromCenteredView = fromCenteredWidget . Widget.fromView
+-- | Lifts a View into a 'TreeLayout' with an alignment point at the top left
+fromView :: View -> TreeLayout a
+fromView = fromWidget . Widget.fromView
 
 -- | The empty 'TreeLayout'
 empty :: TreeLayout a
-empty = fromCenteredView View.empty
+empty = fromView View.empty

src/Lamdu/GUI/CodeEdit.hs
@@ -161,13 +161,10 @@ make width env =
         workArea <- loadWorkArea env & transaction
         replGui <- makeReplEdit env (workArea ^. Sugar.waRepl)
         panesEdits <- workArea ^. Sugar.waPanes & traverse (makePaneEdit env)
-        newDefinitionButton <-
-            makeNewDefinitionButton <&> fmap mLiftTrans
-            <&> TreeLayout.fromCenteredWidget
+        newDefinitionButton <- makeNewDefinitionButton <&> fmap mLiftTrans <&> TreeLayout.fromWidget
         eventMap <- panesEventMap env
         ExpressionGui.vboxTopFocalSpaced
-            ?? (replGui : panesEdits ++ [newDefinitionButton]
-                <&> TreeLayout.alignment . _1 .~ 0)
+            ?? (replGui : panesEdits ++ [newDefinitionButton])
             <&> E.weakerEvents eventMap
     & ExprGuiM.run ExpressionEdit.make
       (codeProps env) (config env) (theme env) (settings env) (style env)

src/Lamdu/GUI/DefinitionEdit.hs
@@ -102,8 +102,7 @@ make def =
             Sugar.DefinitionBodyExpression bodyExpr ->
                 makeExprDefinition def bodyExpr
             Sugar.DefinitionBodyBuiltin builtin ->
-                makeBuiltinDefinition def builtin
-                <&> TreeLayout.fromCenteredWidget
+                makeBuiltinDefinition def builtin <&> TreeLayout.fromWidget
             <&> addDeletionDiagonal
         case defState of
             Sugar.LiveDefinition -> return defGui
@@ -111,7 +110,7 @@ make def =
                 do
                     buttonGui <-
                         myId <$ Property.set defStateProp Sugar.LiveDefinition
-                        & undeleteButton <&> TreeLayout.fromCenteredWidget
+                        & undeleteButton <&> TreeLayout.fromWidget
                     ExpressionGui.vboxTopFocal [defGui, buttonGui] & return
     & Reader.local (View.animIdPrefix .~ Widget.toAnimId myId)
     where

src/Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -21,7 +21,7 @@ import           Graphics.UI.Bottle.MetaKey (MetaKey(..), noMods, toModKey)
 import qualified Graphics.UI.Bottle.View as View
 import           Graphics.UI.Bottle.Widget (Widget)
 import qualified Graphics.UI.Bottle.Widget as Widget
-import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget)
+import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget(..))
 import qualified Graphics.UI.Bottle.Widget.Aligned as AlignedWidget
 import qualified Graphics.UI.Bottle.Widget.TreeLayout as TreeLayout
 import qualified Graphics.UI.Bottle.Widgets.Box as Box
@@ -69,7 +69,7 @@ makeBinderNameEdit binderActions rhsJumperEquals name color myId =
             <&> E.weakerEvents
                 (ParamEdit.eventMapAddFirstParam config
                  (binderActions ^. Sugar.baAddFirstParam))
-            <&> TreeLayout.fromCenteredWidget
+            <&> TreeLayout.fromWidget
     where
         jumpToRHSViaEquals n
             | nonOperatorName n = E.strongerEvents rhsJumperEquals
@@ -341,12 +341,9 @@ make name color binder myId =
         defNameEdit <-
             makeBinderNameEdit (binder ^. Sugar.bActions) rhsJumperEquals
             name color myId
-            <&> TreeLayout.alignment . _1 .~ 0
             <&> TreeLayout.alignedWidget %~
                 AlignedWidget.addAfter AlignedWidget.Vertical
-                (presentationEdits
-                <&> AlignedWidget.fromCenteredWidget
-                <&> AlignedWidget.alignment . _1 .~ 0)
+                (presentationEdits <&> AlignedWidget 0)
             <&> E.weakerEvents jumpHolesEventMap
         mLhsEdit <-
             case mParamsEdit of
@@ -479,8 +476,8 @@ namedParamEditInfo :: Monad m => Draw.Color -> Sugar.NamedParamInfo (Name m) m -
 namedParamEditInfo color paramInfo =
     ParamEdit.Info
     { ParamEdit.iMakeNameEdit =
-      ExpressionGui.makeNameOriginEdit (paramInfo ^. Sugar.npiName) color
-      <&> Lens.mapped %~ TreeLayout.fromCenteredWidget
+        ExpressionGui.makeNameOriginEdit (paramInfo ^. Sugar.npiName) color
+        <&> Lens.mapped %~ TreeLayout.fromWidget
     , ParamEdit.iMAddNext = paramInfo ^. Sugar.npiActions . Sugar.fpAddNext & Just
     , ParamEdit.iMOrderBefore = paramInfo ^. Sugar.npiActions . Sugar.fpMOrderBefore
     , ParamEdit.iMOrderAfter = paramInfo ^. Sugar.npiActions . Sugar.fpMOrderAfter

src/Lamdu/GUI/ExpressionEdit/CaseEdit.hs
@@ -11,6 +11,7 @@ import qualified Graphics.UI.Bottle.EventMap as E
 import           Graphics.UI.Bottle.View (View(..))
 import qualified Graphics.UI.Bottle.View as View
 import qualified Graphics.UI.Bottle.Widget as Widget
+import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget(..))
 import qualified Graphics.UI.Bottle.Widget.Aligned as AlignedWidget
 import qualified Graphics.UI.Bottle.Widget.TreeLayout as TreeLayout
 import           Lamdu.Calc.Type (Tag)
@@ -159,14 +160,12 @@ makeOpenCase rest animId altsGui =
                 targetWidth = alts ^. View.width
             in
             alts
-            & AlignedWidget.alignment . _1 .~ 0
             & AlignedWidget.addAfter AlignedWidget.Vertical
-            ( [ separationBar theme (max minWidth targetWidth) animId
-                & Widget.fromView & AlignedWidget.fromCenteredWidget
-              , AlignedWidget.fromCenteredWidget vspace
-              , restLayout
-              ] <&> (AlignedWidget.alignment . _1 .~ 0)
-            )
+            [ separationBar theme (max minWidth targetWidth) animId
+                & Widget.fromView & AlignedWidget 0
+            , AlignedWidget 0 vspace
+            , restLayout
+            ]
 
 caseOpenEventMap ::
     Monad m =>

src/Lamdu/GUI/ExpressionEdit/GetVarEdit.hs
@@ -11,7 +11,7 @@ import qualified Graphics.UI.Bottle.EventMap as E
 import           Graphics.UI.Bottle.Font (Underline(..))
 import qualified Graphics.UI.Bottle.View as View
 import qualified Graphics.UI.Bottle.Widget as Widget
-import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget)
+import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget(..))
 import qualified Graphics.UI.Bottle.Widget.Aligned as AlignedWidget
 import           Graphics.UI.Bottle.Widget.TreeLayout (TreeLayout)
 import qualified Graphics.UI.Bottle.Widget.TreeLayout as TreeLayout
@@ -44,7 +44,7 @@ makeSimpleView ::
 makeSimpleView name myId =
     (Widget.makeFocusableView ?? myId)
     <*> (ExpressionGui.makeNameView name (Widget.toAnimId myId) <&> Widget.fromView)
-    <&> TreeLayout.fromCenteredWidget
+    <&> TreeLayout.fromWidget
 
 makeParamsRecord ::
     Monad m => Widget.Id -> Sugar.ParamsRecordVar (Name m) ->
@@ -54,8 +54,7 @@ makeParamsRecord myId paramsRecordVar =
         theme <- ExprGuiM.readTheme
         let Theme.Name{..} = Theme.name theme
         sequence
-            [ TextView.makeLabel "Params {"
-              <&> TreeLayout.fromCenteredView
+            [ TextView.makeLabel "Params {" <&> TreeLayout.fromView
             , ExpressionGui.combineSpaced
               <*>
               ( fieldNames
@@ -66,8 +65,7 @@ makeParamsRecord myId paramsRecordVar =
                     & Reader.local (TextView.color .~ parameterColor)
                 )
               )
-            , TextView.makeLabel "}"
-              <&> TreeLayout.fromCenteredView
+            , TextView.makeLabel "}" <&> TreeLayout.fromView
             ] <&> ExpressionGui.combine
     where
         Sugar.ParamsRecordVar fieldNames = paramsRecordVar
@@ -113,23 +111,21 @@ definitionTypeChangeBox ::
     ExprGuiM m (AlignedWidget (T m Widget.EventResult))
 definitionTypeChangeBox info getVarId =
     do
-        headerLabel <- TextView.makeLabel "Type was:" <&> AlignedWidget.fromCenteredView
+        headerLabel <- TextView.makeLabel "Type was:" <&> fromView
         typeWhenUsed <-
             mkTypeWidget "typeWhenUsed" (info ^. Sugar.defTypeWhenUsed)
-        spacing <- ExpressionGui.stdVSpace <&> AlignedWidget.fromCenteredWidget
+        spacing <- ExpressionGui.stdVSpace <&> AlignedWidget 0
         sepLabel <-
             (Widget.makeFocusableView ?? myId)
-            <*> (TextView.makeLabel "Update to:" <&> AlignedWidget.fromCenteredView)
+            <*> (TextView.makeLabel "Update to:" <&> fromView)
         typeCurrent <- mkTypeWidget "typeCurrent" (info ^. Sugar.defTypeCurrent)
         config <- ExprGuiM.readConfig
         theme <- ExprGuiM.readTheme
         let padding = realToFrac <$> Theme.valFramePadding theme
         let box =
                 [headerLabel, typeWhenUsed, spacing, sepLabel, typeCurrent]
-                <&> AlignedWidget.alignment .~ 0
                 & AlignedWidget.vbox 0
                 & View.pad padding
-                & AlignedWidget.alignment .~ 0
                 & Hover.addBackground animId (Theme.hoverBGColor theme)
         -- TODO: unify config's button press keys
         let keys = Config.newDefinitionButtonPressKeys (Config.pane config)
@@ -140,9 +136,10 @@ definitionTypeChangeBox info getVarId =
                 (Widget.keysEventMapMovesCursor keys
                  (E.Doc ["Edit", "Update definition type"]) update)
     where
+        fromView = AlignedWidget 0 . Widget.fromView
         mkTypeWidget idSuffix scheme =
             TypeView.make (scheme ^. schemeType) (animId ++ [idSuffix])
-            <&> Widget.fromView <&> AlignedWidget.fromCenteredWidget
+            <&> Widget.fromView <&> AlignedWidget 0
         myId = Widget.joinId getVarId ["type change"]
         animId = Widget.toAnimId myId
 

commit a73f3b2aef00cd91b3155cdb62854f3a99fa2ec8

fix to-nom alignment
to-nom's alignment was broken which could be seen when resizing the window
with stdlib's takeWhile open.
alignment was broken since 245278da55fcfda46818ddbd41a05d840cd665c4

src/Lamdu/GUI/ExpressionEdit/NomEdit.hs
@@ -128,10 +128,12 @@ expandingName vertOrder (#>) needParen nomId showName =
                     NameHovering -> nameShowing `AlignedWidget.hoverInPlaceOf` label
                     #> (AlignedWidget.fromView 0 space #> subexprGui)
                 vert =
-                    TreeLayout.fromAlignedWidget nameShowing
+                    TreeLayout.fromAlignedWidget (nameShowing & AlignedWidget.alignment .~ 0)
                     `vertOrder` subexprGui
                     & TreeLayout.vbox
-            in horiz `horizWithFallback` vert
+            in
+            horiz `horizWithFallback` vert
+            & TreeLayout.alignment .~ 0
     where
         mParenInfo
             | needParen = Widget.toAnimId nomId & Just