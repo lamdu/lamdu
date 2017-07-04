lamdu/lamdu

commit af140d3d3d4d38f1b35a20f21253d50af34af866

vboxSpaced generalized and moved to lib

src/Lamdu/GUI/ExpressionEdit/ApplyEdit.hs
@@ -206,10 +206,9 @@ mkBoxed apply nearestHoles mkFuncRow =
             case apply ^. Sugar.aRelayedArgs of
             [] -> return []
             args -> mkRelayedArgs nearestHoles args <&> (:[])
-        vbox <- ExpressionGui.vboxTopFocalSpaced
         ExpressionGui.addValFrame
-            ?? vbox
-                (funcRow : argRows ++ relayedArgs <&> TreeLayout.alignment . _1 .~ 0)
+            <*> (TreeLayout.vboxSpaced ?? funcRow : argRows ++ relayedArgs)
+            <&> TreeLayout.alignment . _1 .~ 0
 
 makeSimple ::
     Monad m =>

src/Graphics/UI/Bottle/Widget/TreeLayout.hs
@@ -36,10 +36,11 @@ module Graphics.UI.Bottle.Widget.TreeLayout
     , fromAlignedWidget, fromWidget, fromView, empty
 
     -- * Combinators
-    , vbox
+    , vbox, vboxSpaced
     ) where
 
 import qualified Control.Lens as Lens
+import qualified Data.List as List
 import           Graphics.UI.Bottle.Alignment (Alignment)
 import qualified Graphics.UI.Bottle.EventMap as E
 import           Graphics.UI.Bottle.View (View)
@@ -48,6 +49,7 @@ import           Graphics.UI.Bottle.Widget (Widget)
 import qualified Graphics.UI.Bottle.Widget as Widget
 import           Graphics.UI.Bottle.Widget.Aligned (AlignedWidget(..))
 import qualified Graphics.UI.Bottle.Widget.Aligned as AlignedWidget
+import qualified Graphics.UI.Bottle.Widgets.Spacer as Spacer
 
 import           Lamdu.Prelude
 
@@ -133,3 +135,12 @@ vbox (gui:guis) =
     & gui ^. render
     & AlignedWidget.addAfter AlignedWidget.Vertical
         (guis ^.. traverse . render ?? cp)
+
+vboxSpaced ::
+    (MonadReader env m, Spacer.HasStdSpacing env) =>
+    m ([TreeLayout a] -> TreeLayout a)
+vboxSpaced =
+    Spacer.stdVSpaceView
+    <&> fromView
+    <&> List.intersperse
+    <&> Lens.mapped %~ vbox

src/Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -349,7 +349,7 @@ make name color binder myId =
             case mParamsEdit of
             Nothing -> return Nothing
             Just paramsEdit ->
-                ExpressionGui.vboxTopFocalSpaced
+                TreeLayout.vboxSpaced
                 ?? (paramsEdit : fmap TreeLayout.fromWidget mScopeEdit ^.. Lens._Just
                     <&> TreeLayout.alignment . _1 .~ 0.5)
                 <&> TreeLayout.alignment . _1 .~ 0
@@ -462,7 +462,7 @@ makeBinderContentEdit (Sugar.BinderLet l) =
         mOuterScopeId <- ExprGuiM.readMScopeId
         let letBodyScope = liftA2 lookupMKey mOuterScopeId (l ^. Sugar.lBodyScope)
         ExpressionGui.parentDelegator letEntityId
-            <*> ( ExpressionGui.vboxTopFocalSpaced
+            <*> ( TreeLayout.vboxSpaced
                   <*>
                   ( sequence
                     [ makeLetEdit l <&> E.weakerEvents moveToInnerEventMap

src/Lamdu/GUI/ExpressionEdit/CaseEdit.hs
@@ -94,7 +94,7 @@ make (Sugar.Case mArg alts caseTail addAlt _cEntityId) pl =
                   (E.Doc ["Edit", "Case", "Add Alt"])
                 & ExprGuiM.withHolePicker resultPicker
         ExpressionGui.addValFrame
-            <*> (ExpressionGui.vboxTopFocalSpaced ??
+            <*> (TreeLayout.vboxSpaced ??
                  ([header, altsGui] <&> TreeLayout.alignment . _1 .~ 0))
             <&> E.weakerEvents addAltEventMap
     & Widget.assignCursor myId (destCursorId alts headerId)
@@ -131,7 +131,7 @@ makeAltsWidget _ [] myId =
     (Widget.makeFocusableView ?? Widget.joinId myId ["Ø"])
     <*> (ExpressionGui.grammarLabel "Ø" <&> TreeLayout.fromView)
 makeAltsWidget mActiveTag alts _myId =
-    ExpressionGui.vboxTopFocalSpaced <*> mapM (makeAltRow mActiveTag) alts
+    TreeLayout.vboxSpaced <*> mapM (makeAltRow mActiveTag) alts
 
 separationBar :: Theme -> Widget.R -> Anim.AnimId -> View
 separationBar theme width animId =

src/Lamdu/GUI/ExpressionEdit/RecordEdit.hs
@@ -98,7 +98,7 @@ makeFieldsWidget [] myId =
     (Widget.makeFocusableView ?? myId)
     <*> (ExpressionGui.grammarLabel "()" <&> TreeLayout.fromView)
 makeFieldsWidget fields _ =
-    ExpressionGui.vboxTopFocalSpaced <*> mapM makeFieldRow fields
+    TreeLayout.vboxSpaced <*> mapM makeFieldRow fields
 
 separationBar :: Theme -> Widget.R -> Anim.AnimId -> View
 separationBar theme width animId =

commit 9597726904ca08b61dfa7a5a4e2e24ca0e88bb8e

oops fix alignment broken in af140d3d3d

src/Lamdu/GUI/ExpressionEdit/ApplyEdit.hs
@@ -207,8 +207,7 @@ mkBoxed apply nearestHoles mkFuncRow =
             [] -> return []
             args -> mkRelayedArgs nearestHoles args <&> (:[])
         ExpressionGui.addValFrame
-            <*> (TreeLayout.vboxSpaced ?? funcRow : argRows ++ relayedArgs)
-            <&> TreeLayout.alignment . _1 .~ 0
+            <*> (TreeLayout.vboxSpaced ?? (funcRow : argRows ++ relayedArgs <&> TreeLayout.alignment . _1 .~ 0))
 
 makeSimple ::
     Monad m =>