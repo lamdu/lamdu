lamdu/lamdu

commit 59045554c326bdf402af1766743b57525305d03c

cleanup: FieldParam contains TagG

src/Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -314,9 +314,12 @@ makeParts funcApplyLimit binder delVarBackwardsId myId =
             & ExprGuiM.withLocalMScopeId binderScopeId
     where
         destId =
-            params ^? SugarLens.binderNamedParams . Sugar.fpId
-            <&> WidgetIds.fromEntityId
-            & fromMaybe bodyId
+            case params of
+            Sugar.BinderWithoutParams -> bodyId
+            Sugar.NullParam{} -> bodyId
+            Sugar.VarParam v -> v ^. Sugar.fpInfo . Sugar.vpiId & WidgetIds.fromEntityId
+            Sugar.FieldParams ps ->
+                ps ^?! traverse . Sugar.fpInfo . Sugar.fpiTag . Sugar.tagInstance & WidgetIds.fromEntityId
         params = binder ^. Sugar.bParams
         body = binder ^. Sugar.bBody
         bodyContent = body ^. Sugar.bbContent
@@ -479,20 +482,23 @@ makeBinderContentEdit (Sugar.BinderLet l) =
         letEntityId = l ^. Sugar.lEntityId & WidgetIds.fromEntityId
         body = l ^. Sugar.lBody
 
-namedParamEditInfo :: Monad m => Draw.Color -> Sugar.NamedParamInfo (Name m) m -> ParamEdit.Info m
-namedParamEditInfo color paramInfo =
+namedParamEditInfo ::
+    Monad m =>
+    Draw.Color -> Sugar.EntityId -> Name m -> Sugar.FuncParamActions m -> ParamEdit.Info m
+namedParamEditInfo color entityId name actions =
     ParamEdit.Info
     { ParamEdit.iMakeNameEdit =
-        ExpressionGui.makeNameOriginEdit (paramInfo ^. Sugar.npiName) color
+        ExpressionGui.makeNameOriginEdit name color
         <&> Lens.mapped %~ Responsive.fromWithTextPos
-    , ParamEdit.iMAddNext = paramInfo ^. Sugar.npiActions . Sugar.fpAddNext & Just
-    , ParamEdit.iMOrderBefore = paramInfo ^. Sugar.npiActions . Sugar.fpMOrderBefore
-    , ParamEdit.iMOrderAfter = paramInfo ^. Sugar.npiActions . Sugar.fpMOrderAfter
-    , ParamEdit.iDel = paramInfo ^. Sugar.npiActions . Sugar.fpDelete
+    , ParamEdit.iMAddNext = actions ^. Sugar.fpAddNext & Just
+    , ParamEdit.iMOrderBefore = actions ^. Sugar.fpMOrderBefore
+    , ParamEdit.iMOrderAfter = actions ^. Sugar.fpMOrderAfter
+    , ParamEdit.iDel = actions ^. Sugar.fpDelete
+    , ParamEdit.iId = WidgetIds.fromEntityId entityId
     }
 
-nullParamEditInfo :: Monad m => Sugar.NullParamActions m -> ParamEdit.Info m
-nullParamEditInfo mActions =
+nullParamEditInfo :: Monad m => Widget.Id -> Sugar.NullParamActions m -> ParamEdit.Info m
+nullParamEditInfo widgetId mActions =
     ParamEdit.Info
     { ParamEdit.iMakeNameEdit =
       \myId ->
@@ -503,6 +509,7 @@ nullParamEditInfo mActions =
     , ParamEdit.iMOrderBefore = Nothing
     , ParamEdit.iMOrderAfter = Nothing
     , ParamEdit.iDel = Sugar.ParamDelResultDelVar <$ mActions ^. Sugar.npDeleteLambda
+    , ParamEdit.iId = widgetId
     }
 
 makeParamsEdit ::
@@ -518,13 +525,16 @@ makeParamsEdit annotationOpts nearestHoles delVarBackwardsId lhsId rhsId params 
             Sugar.BinderWithoutParams -> return []
             Sugar.NullParam p ->
                 fromParamList ExprGuiT.showAnnotationWhenVerbose delVarBackwardsId rhsId
-                [p & Sugar.fpInfo %~ nullParamEditInfo]
+                [p & Sugar.fpInfo %~ nullParamEditInfo lhsId]
             Sugar.VarParam p ->
                 fromParamList ExprGuiT.alwaysShowAnnotations delVarBackwardsId rhsId
-                [p & Sugar.fpInfo %~ namedParamEditInfo paramColor]
+                [p & Sugar.fpInfo %~
+                    \x -> namedParamEditInfo paramColor (x ^. Sugar.vpiId) (x ^. Sugar.vpiName) (x ^. Sugar.vpiActions)]
             Sugar.FieldParams ps ->
-                ps ^.. Lens.traversed . _2
-                & traverse . Sugar.fpInfo %~ namedParamEditInfo paramColor
+                ps
+                & traverse . Sugar.fpInfo %~
+                    (\x -> namedParamEditInfo paramColor
+                        (x ^. Sugar.fpiTag . Sugar.tagInstance) (x ^. Sugar.fpiTag . Sugar.tagGName) (x ^. Sugar.fpiActions))
                 & fromParamList ExprGuiT.alwaysShowAnnotations lhsId rhsId
     where
         fromParamList showParamAnnotation delDestFirst delDestLast paramList =
@@ -534,5 +544,5 @@ makeParamsEdit annotationOpts nearestHoles delVarBackwardsId lhsId rhsId params 
                         ParamEdit.make annotationOpts showParamAnnotation prevId nextId param
                         <&> E.weakerEvents jumpHolesEventMap
                 ExpressionGui.listWithDelDests delDestFirst delDestLast
-                    (WidgetIds.fromEntityId . (^. Sugar.fpId)) paramList
+                    (ParamEdit.iId . (^. Sugar.fpInfo)) paramList
                     & traverse mkParam

src/Lamdu/GUI/ExpressionEdit/LambdaEdit.hs
@@ -110,7 +110,12 @@ mkLightLambda params myId =
             else mkShrunk paramIds myId
                  <&> \mk _mParamsEdit mScopeEdit -> mk mScopeEdit
     where
-        paramIds = params ^.. SugarLens.binderNamedParams . Sugar.fpId
+        paramIds =
+            case params of
+            Sugar.BinderWithoutParams -> []
+            Sugar.NullParam{} -> []
+            Sugar.VarParam p -> [p ^. Sugar.fpInfo . Sugar.vpiId]
+            Sugar.FieldParams ps -> ps <&> (^. Sugar.fpInfo . Sugar.fpiTag . Sugar.tagInstance)
 
 make ::
     Monad m =>

src/Lamdu/GUI/ParamEdit.hs
@@ -98,6 +98,7 @@ data Info m = Info
     , iMAddNext :: Maybe (T m Sugar.ParamAddResult)
     , iMOrderBefore :: Maybe (T m ())
     , iMOrderAfter :: Maybe (T m ())
+    , iId :: Widget.Id
     }
 
 -- exported for use in definition sugaring.
@@ -130,6 +131,5 @@ make annotationOpts showAnnotation prevId nextId param =
               -- <&> Responsive.alignment . _1 .~ 0.5
             )
     where
-        entityId = param ^. Sugar.fpId
-        myId = WidgetIds.fromEntityId entityId
+        myId = iId info
         info = param ^. Sugar.fpInfo

src/Lamdu/Sugar/Convert/Binder.hs
@@ -187,8 +187,7 @@ convertLam lam exprPl =
             (exprPl ^. Input.stored & Property.value & Anchors.assocScopeRef)
             Nothing convParams (lam ^. V.lamResult)
         let paramUUIDs =
-                binder ^.. bParams . SugarLens.binderNamedParams .
-                Lens.traversed . npiName
+                binder ^.. bParams . _FieldParams . traverse . fpInfo . fpiTag . tagGName
                 & Set.fromList
         let lambda
                 | useNormalLambda paramUUIDs binder =
@@ -213,8 +212,10 @@ useNormalLambda paramUUIDs binder =
         forbiddenLightLamSubExprs :: Lens.Traversal' (Body name m a) ()
         forbiddenLightLamSubExprs =
             Lens.failing (_BodyHole . Lens.united)
-            (_BodyLam . lamBinder . bParams . SugarLens.binderNamedParams .
+            (_BodyLam . lamBinder . bParams . namedParams .
              Lens.united)
+        namedParams :: Lens.Traversal' (BinderParams name m) ()
+        namedParams = Lens.failing (_VarParam . Lens.united) (_FieldParams . Lens.united)
 
 allParamsUsed :: Set UUID -> Binder UUID m (Expression UUID m a) -> Bool
 allParamsUsed paramUUIDs binder =

src/Lamdu/Sugar/Convert/Binder/Params.hs
@@ -305,27 +305,29 @@ convertRecordParams binderKind fieldParams lam@(V.Lam param _) pl =
         mkFieldParamInfo fp = mkParamInfo param fp <&> ConvertM.TagFieldParam
         storedLam = mkStoredLam lam pl
         mkParam fp =
-            ( fpTag fp
-            , FuncParam
-                { _fpInfo =
-                  NamedParamInfo
-                  { _npiName = UniqueId.toUUID $ fpTag fp
-                  , _npiActions = fieldParamActions binderKind tags fp storedLam
-                  }
-                , _fpId = fpIdEntityId param fp
-                , _fpAnnotation =
-                    Annotation
-                    { _aInferredType = fpFieldType fp
-                    , _aMEvaluationResult =
-                        fpValue fp <&>
-                        \x ->
-                        do
-                            Map.null x & not & guard
-                            x ^.. Lens.traversed . Lens.traversed
-                                & Map.fromList & Just
+            FuncParam
+            { _fpInfo =
+                FieldParamInfo
+                { _fpiActions = fieldParamActions binderKind tags fp storedLam
+                , _fpiTag =
+                    TagG
+                    { _tagInstance = fpIdEntityId param fp
+                    , _tagVal = fpTag fp
+                    , _tagGName = UniqueId.toUUID $ fpTag fp
                     }
                 }
-            )
+            , _fpAnnotation =
+                Annotation
+                { _aInferredType = fpFieldType fp
+                , _aMEvaluationResult =
+                    fpValue fp <&>
+                    \x ->
+                    do
+                        Map.null x & not & guard
+                        x ^.. Lens.traversed . Lens.traversed
+                            & Map.fromList & Just
+                }
+            }
 
 removeCallsToVar :: Monad m => V.Var -> Val (ValIProperty m) -> T m ()
 removeCallsToVar funcVar val =
@@ -439,15 +441,12 @@ makeNonRecordParamActions binderKind storedLam =
             , _fpMOrderAfter = Nothing
             }
 
-mkFuncParam ::
-    Monad m =>
-    EntityId -> Input.Payload m a -> info -> ConvertM m (FuncParam info)
-mkFuncParam paramEntityId lamExprPl info =
+mkFuncParam :: Monad m => Input.Payload m a -> info -> ConvertM m (FuncParam info)
+mkFuncParam lamExprPl info =
     do
         noms <- ConvertM.readContext <&> (^. ConvertM.scNominalsMap)
         return FuncParam
             { _fpInfo = info
-            , _fpId = paramEntityId
             , _fpAnnotation =
                 Annotation
                 { _aInferredType = typ
@@ -480,13 +479,14 @@ convertNonRecordParam binderKind lam@(V.Lam param _) lamExprPl =
                   funcParamActions ^. fpDelete
                   & void
                   & NullParamActions
-                  & mkFuncParam paramEntityId lamExprPl
+                  & mkFuncParam lamExprPl
                   <&> NullParam
             _ ->
-                NamedParamInfo
-                { _npiName = UniqueId.toUUID param
-                , _npiActions = funcParamActions
-                } & mkFuncParam paramEntityId lamExprPl
+                VarParamInfo
+                { _vpiName = UniqueId.toUUID param
+                , _vpiActions = funcParamActions
+                , _vpiId = paramEntityId
+                } & mkFuncParam lamExprPl
                 <&> VarParam
         pure ConventionalParams
             { cpTags = mempty
@@ -522,9 +522,9 @@ postProcessActions ::
 postProcessActions post x =
     x
     & cpAddFirstParam %~ (<* post)
-    & cpParams . SugarLens.binderNamedParamsActions . fpAddNext %~ (<* post)
+    & cpParams . SugarLens.binderFuncParamActions . fpAddNext %~ (<* post)
     & if Lens.has (cpParams . _FieldParams) x
-        then cpParams . SugarLens.binderNamedParamsActions . fpDelete %~ (<* post)
+        then cpParams . SugarLens.binderFuncParamActions . fpDelete %~ (<* post)
         else id
 
 convertLamParams ::

src/Lamdu/Sugar/Lens.hs
@@ -4,8 +4,7 @@ module Lamdu.Sugar.Lens
     , payloadsOf
     , holePayloads, holeArgs
     , defSchemes
-    , binderNamedParams
-    , binderNamedParamsActions
+    , binderFuncParamActions
     , binderFuncParamAdds
     , binderFuncParamDeletes
     , binderLetActions
@@ -96,20 +95,12 @@ defBodySchemes f (DefinitionBodyExpression de) =
 defSchemes :: Lens.Traversal' (Definition name m expr) Scheme
 defSchemes = drBody . defBodySchemes
 
-binderNamedParams ::
-    Lens.Traversal
-    (BinderParams a m)
-    (BinderParams b m)
-    (FuncParam (NamedParamInfo a m))
-    (FuncParam (NamedParamInfo b m))
-binderNamedParams _ BinderWithoutParams = pure BinderWithoutParams
-binderNamedParams _ (NullParam a) = pure (NullParam a)
-binderNamedParams f (VarParam p) = VarParam <$> f p
-binderNamedParams f (FieldParams ps) = FieldParams <$> (Lens.traverse . _2) f ps
-
-binderNamedParamsActions ::
+binderFuncParamActions ::
     Lens.Traversal' (BinderParams name m) (FuncParamActions m)
-binderNamedParamsActions = binderNamedParams . fpInfo . npiActions
+binderFuncParamActions _ BinderWithoutParams = pure BinderWithoutParams
+binderFuncParamActions _ (NullParam a) = pure (NullParam a)
+binderFuncParamActions f (VarParam p) = (fpInfo . vpiActions) f p <&> VarParam
+binderFuncParamActions f (FieldParams ps) = (traverse . fpInfo . fpiActions) f ps <&> FieldParams
 
 binderLetActions ::
     Lens.Traversal'
@@ -123,14 +114,14 @@ binderFuncParamAdds ::
     (Transaction m ParamAddResult)
 binderFuncParamAdds f Binder{..} =
     (\_bParams _bActions -> Binder{..})
-    <$> (_bParams & binderNamedParamsActions . fpAddNext %%~ f)
+    <$> (_bParams & binderFuncParamActions . fpAddNext %%~ f)
     <*> (_bActions & baAddFirstParam %%~ f)
 
 binderFuncParamDeletes ::
     Lens.Traversal'
     (Binder name m (Expression name m a))
     (Transaction m ParamDelResult)
-binderFuncParamDeletes = bParams . binderNamedParamsActions . fpDelete
+binderFuncParamDeletes = bParams . binderFuncParamActions . fpDelete
 
 binderContentExpr :: Lens' (BinderContent name m a) a
 binderContentExpr f (BinderLet l) = l & lBody . bbContent . binderContentExpr %%~ f <&> BinderLet

src/Lamdu/Sugar/Names/Walk.hs
@@ -242,18 +242,9 @@ withBinderParams BinderWithoutParams = pure BinderWithoutParams
 withBinderParams (NullParam a) = pure (NullParam a)
 withBinderParams (VarParam fp) =
     opWithParamName (isFunctionType (fp ^. fpAnnotation . aInferredType))
-    (fp ^. fpInfo . npiName)
-    <&> VarParam . \newName -> fp & fpInfo . npiName .~ newName
-withBinderParams (FieldParams xs) = onTagParams xs <&> FieldParams
-
-onTagParams ::
-    MonadNaming m =>
-    [(T.Tag, FuncParam (NamedParamInfo (OldName m) p))] ->
-    CPS m [(T.Tag, FuncParam (NamedParamInfo (NewName m) p))]
-onTagParams =
-    (traverse . second . fpInfo . npiName) opWithTagName
-    where
-        second f (x, y) = (,) x <$> f y
+    (fp ^. fpInfo . vpiName)
+    <&> VarParam . \newName -> fp & fpInfo . vpiName .~ newName
+withBinderParams (FieldParams xs) = (traverse . fpInfo . fpiTag . tagGName) opWithTagName xs <&> FieldParams
 
 toDefinitionBody ::
     MonadNaming m => (a -> m b) ->

src/Lamdu/Sugar/OrderTags.hs
@@ -95,11 +95,11 @@ orderExpr e =
     >>= Sugar.rBody %%~ orderBody
     >>= Sugar.rBody . Lens.traversed %%~ orderExpr
 
-orderParams :: Monad m => Order m [(T.Tag, Sugar.FuncParam info)]
+orderParams :: Monad m => Order m [Sugar.FuncParam (Sugar.FieldParamInfo name m)]
 orderParams xs =
     xs
-    & Lens.traversed . _2 . Sugar.fpAnnotation . Sugar.aInferredType %%~ orderType
-    >>= orderByTag (^. _1)
+    & Lens.traversed . Sugar.fpAnnotation . Sugar.aInferredType %%~ orderType
+    >>= orderByTag (^. Sugar.fpInfo . Sugar.fpiTag . Sugar.tagVal)
 
 orderBinder ::
     Monad m => Order m (Sugar.Binder name m a)

commit 373132871ef67dc250770b23f8dccc276e2f9f18

fix navigation bug introduced in 59045554c326

src/Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -509,7 +509,7 @@ nullParamEditInfo widgetId mActions =
     , ParamEdit.iMOrderBefore = Nothing
     , ParamEdit.iMOrderAfter = Nothing
     , ParamEdit.iDel = Sugar.ParamDelResultDelVar <$ mActions ^. Sugar.npDeleteLambda
-    , ParamEdit.iId = widgetId
+    , ParamEdit.iId = Widget.joinId widgetId ["param"]
     }
 
 makeParamsEdit ::