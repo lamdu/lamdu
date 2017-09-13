6dc139c22dc4f7b661ebba74d51e036b3d646bd6
Eyal Lotem <eyal.lotem@gmail.com>
2017-09-13 22:44:08 +0300

    OrderedSet: Export filter, Traversable instance

--- src/Data/Set/Ordered.hs
+++ src/Data/Set/Ordered.hs
@@ -1,16 +1,25 @@
-{-# LANGUAGE NoImplicitPrelude, DeriveFoldable #-}
+{-# LANGUAGE NoImplicitPrelude, TemplateHaskell, DeriveFoldable, DeriveTraversable #-}
 module Data.Set.Ordered
     ( OrderedSet
     , singleton
+    , filter
     ) where
 
-import Lamdu.Prelude
+import qualified Control.Lens as Lens
+import qualified Lamdu.Prelude as Prelude
+import           Lamdu.Prelude hiding (filter)
 
-newtype OrderedSet a = OrderedSet [a]
-    deriving (Show, Eq, Ord, Foldable)
+newtype OrderedSet a = OrderedSet { _orderedSet :: [a] }
+    deriving (Show, Eq, Ord, Functor, Foldable, Traversable)
+Lens.makeLenses ''OrderedSet
+
 instance Eq a => Monoid (OrderedSet a) where
     mempty = OrderedSet []
-    OrderedSet xs `mappend` OrderedSet ys = OrderedSet $ xs ++ filter (`notElem` xs) ys
+    OrderedSet xs `mappend` OrderedSet ys =
+        OrderedSet $ xs ++ Prelude.filter (`notElem` xs) ys
 
 singleton :: a -> OrderedSet a
 singleton = OrderedSet . (:[])
+
+filter :: (a -> Bool) -> OrderedSet a -> OrderedSet a
+filter p = orderedSet %~ Prelude.filter p
