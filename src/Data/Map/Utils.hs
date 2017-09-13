31c965b6650deb38fbb0f67012d62d0ee544ee7d
Eyal Lotem <eyal.lotem@gmail.com>
2017-09-14 02:09:13 +0300

    Data.Map.Utils: Add unionWithM

--- src/Data/Map/Utils.hs
+++ src/Data/Map/Utils.hs
@@ -2,6 +2,7 @@
 module Data.Map.Utils
     ( setMapIntersection
     , partition
+    , unionWithM
     ) where
 
 import qualified Data.Map as Map
@@ -17,3 +18,17 @@ partition mkKey xs =
     xs <&> singletonMap & Map.unionsWith (++)
     where
         singletonMap x = Map.singleton (mkKey x) [x]
+
+unionWithM :: (Applicative f, Ord k) => (a -> a -> f a) -> Map k a -> Map k a -> f (Map k a)
+unionWithM f m0 m1 =
+    go
+    (Map.toAscList m0)
+    (Map.toAscList m1)
+    <&> Map.fromAscList
+    where
+        go [] ns = pure ns
+        go ms [] = pure ms
+        go ((k0,v0):ms) ((k1,v1):ns)
+            | k0 < k1 = go ms ((k1,v1):ns) <&> ((k0,v0):)
+            | k1 < k0 = go ((k0,v0):ms) ns <&> ((k1,v1):)
+            | otherwise = (:) . (,) k0 <$> f v0 v1 <*> go ms ns
