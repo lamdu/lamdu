f0a098112c75849c28fe148bd91950beba3e7758
Eyal Lotem <eyal.lotem@gmail.com>
2017-09-20 15:40:18 +0300

    LightLambda: Generalize types

--- src/Lamdu/GUI/LightLambda.hs
+++ src/Lamdu/GUI/LightLambda.hs
@@ -4,14 +4,15 @@ module Lamdu.GUI.LightLambda
     ) where
 
 import           GUI.Momentu.Widgets.TextView (Underline(..))
+import qualified GUI.Momentu.Widgets.TextView as TextView
 import           Lamdu.Config.Theme (Theme)
 import qualified Lamdu.Config.Theme as Theme
-import           Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
 import qualified Lamdu.GUI.ExpressionGui.Monad as ExprGuiM
 
 import           Lamdu.Prelude
 
-withUnderline :: Monad m => Theme -> ExprGuiM m a -> ExprGuiM m a
+withUnderline ::
+    (MonadReader env m, TextView.HasStyle env) => Theme -> m a -> m a
 withUnderline theme =
     Underline
     { _underlineColor = Theme.lightLambdaUnderlineColor (Theme.codeForegroundColors theme)
