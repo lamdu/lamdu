80912683c2a10c03d765b0dc772cb52bdb7d9e86
Eyal Lotem <eyal.lotem@gmail.com>
2017-09-27 16:56:50 +0300

    Precedence: Remove dead code

--- src/Lamdu/Precedence.hs
+++ src/Lamdu/Precedence.hs
@@ -19,14 +19,6 @@ Lens.makeLenses ''Precedence
 make :: Int -> Precedence
 make p = Precedence p p
 
--- instance Num Precedence where
---     fromInteger x = Precedence (fromInteger x) (fromInteger x)
---     x + y = Precedence (_before x + _before y) (_after x + _after y)
---     (*) = error "instance Num Precedence.*"
---     (-) = error "instance Num Precedence.-"
---     abs = error "instance Num Precedence.abs"
---     signum = error "instance Num Precedence.signum"
-
 newtype MyPrecedence = MyPrecedence Precedence deriving Show
 newtype ParentPrecedence = ParentPrecedence Precedence deriving Show
 
