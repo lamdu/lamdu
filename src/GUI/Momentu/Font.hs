3a3bb7cf99c32ac6eeb2d85cecb2040b946a2646
Eyal Lotem <eyal.lotem@gmail.com>
2017-10-06 00:25:30 +0300

    Momentu: Move font-related stuff from DrawingCombinators.Utils to Momentu.Font

--- src/GUI/Momentu/Font.hs
+++ src/GUI/Momentu/Font.hs
@@ -1,20 +1,24 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, DeriveTraversable, OverloadedStrings #-}
 -- | A font attached to its size
 
 module GUI.Momentu.Font
     ( Underline(..), underlineColor, underlineWidth
     , render
-    , RenderedText(..), DrawUtils.renderedText, DrawUtils.renderedTextSize
-    , TextSize(..), DrawUtils.bounding, DrawUtils.advance
-    , DrawUtils.textSize
+    , RenderedText(..), renderedTextSize, renderedText
+    , TextSize(..), bounding, advance
     , height
+    , textSize
+    , renderText
     ) where
 
+import           Control.Applicative (liftA2)
 import qualified Control.Lens as Lens
+import           Data.Text
+import qualified Data.Text as Text
 import           Data.Vector.Vector2 (Vector2(..))
 import           Graphics.DrawingCombinators ((%%))
 import qualified Graphics.DrawingCombinators as Draw
-import           Graphics.DrawingCombinators.Utils (TextSize(..), Image, RenderedText, renderedText, renderedTextSize)
+import           Graphics.DrawingCombinators.Utils (Image)
 import qualified Graphics.DrawingCombinators.Utils as DrawUtils
 
 import           Lamdu.Prelude
@@ -25,8 +29,31 @@ data Underline = Underline
     }
 Lens.makeLenses ''Underline
 
+data TextSize a = TextSize
+    { _bounding :: a
+    , _advance :: a
+    } deriving (Functor, Foldable, Traversable)
+Lens.makeLenses ''TextSize
+
+instance Applicative TextSize where
+    pure x = TextSize x x
+    TextSize f1 f2 <*> TextSize x1 x2 = TextSize (f1 x1) (f2 x2)
+instance Num a => Num (TextSize a) where
+    fromInteger = pure . fromInteger
+    (+) = liftA2 (+)
+    (*) = liftA2 (*)
+    abs = fmap abs
+    signum = fmap signum
+    negate = fmap negate
+
+data RenderedText a = RenderedText
+    { _renderedTextSize :: TextSize (Vector2 Draw.R)
+    , _renderedText :: a
+    }
+Lens.makeLenses ''RenderedText
+
 height :: Draw.Font -> Draw.R
-height = DrawUtils.fontHeight
+height = Draw.fontHeight
 
 render ::
     Draw.Font -> Draw.Color -> Maybe Underline -> Text ->
@@ -34,9 +61,9 @@ render ::
 render font color mUnderline str =
     r
     & renderedText <>~
-        maybe mempty (drawUnderline font (r ^. renderedTextSize . DrawUtils.bounding)) mUnderline
+        maybe mempty (drawUnderline font (r ^. renderedTextSize . bounding)) mUnderline
     where
-        r = DrawUtils.renderText font attrs str
+        r = renderText font attrs str
         attrs =
             Draw.defTextAttrs
             { Draw.gamma = 1.0
@@ -52,3 +79,37 @@ drawUnderline font size (Underline color relativeWidth) =
     where
         width = relativeWidth * height font
         descender = Draw.fontDescender font
+
+textWidth :: Draw.Font -> Text -> TextSize Draw.R
+textWidth font str =
+    TextSize
+    { _bounding =
+      Draw.textBoundingWidth font str
+      -- max with advance because spaces are counted only in advance
+      -- but not the bounding width
+      & max adv
+    , _advance = adv
+    }
+    where
+        adv = Draw.textAdvance font str
+
+textSize :: Draw.Font -> Text -> TextSize (Vector2 Draw.R)
+textSize font str =
+    (`Vector2` totalHeight) <$> textWidth font str
+    where
+        totalHeight = height font * fromIntegral numLines
+        numLines = 1 + Text.count "\n" str
+
+renderText :: Draw.Font -> Draw.TextAttrs -> Text -> RenderedText Image
+renderText font textAttrs str =
+    RenderedText
+    { _renderedTextSize = textSize font str
+    , _renderedText =
+        Draw.text font str textAttrs
+        & void
+        -- Text is normally at height -0.5..1.5.  We move it to be -textHeight..0
+        & (DrawUtils.translate (Vector2 0 (-Draw.fontHeight font - Draw.fontDescender font)) %%)
+        -- We want to reverse it so that higher y is down, and it is also
+        -- moved to 0..2
+        & (DrawUtils.scale (Vector2 1 (-1)) %%)
+    }
--- src/Graphics/DrawingCombinators/Utils.hs
+++ src/Graphics/DrawingCombinators/Utils.hs
@@ -3,23 +3,15 @@
 module Graphics.DrawingCombinators.Utils
     ( Image
     , square
-    , TextSize(..), bounding, advance
-    , Draw.fontHeight, textSize
-    , RenderedText(..), renderedTextSize, renderedText
-    , renderText
     , scale, translate
     , Draw.clearRender
     ) where
 
-import           Control.Applicative (liftA2)
-import qualified Control.Lens as Lens
 import           Data.Aeson.TH (deriveJSON)
 import           Data.Aeson.Types (defaultOptions)
-import qualified Data.Text as Text
 import           Data.Vector.Vector2 (Vector2(..))
 import           Foreign.C.Types.Instances ()
 import           GHC.Generics (Generic)
-import           Graphics.DrawingCombinators ((%%))
 import qualified Graphics.DrawingCombinators as Draw
 
 import           Lamdu.Prelude
@@ -43,60 +35,3 @@ translate (Vector2 x y) = Draw.translate (roundR x, roundR y)
 
 square :: Image
 square = void $ Draw.convexPoly [ (0, 0), (1, 0), (1, 1), (0, 1) ]
-
-data TextSize a = TextSize
-    { _bounding :: a
-    , _advance :: a
-    } deriving (Functor, Foldable, Traversable)
-instance Applicative TextSize where
-    pure x = TextSize x x
-    TextSize f1 f2 <*> TextSize x1 x2 = TextSize (f1 x1) (f2 x2)
-instance Num a => Num (TextSize a) where
-    fromInteger = pure . fromInteger
-    (+) = liftA2 (+)
-    (*) = liftA2 (*)
-    abs = fmap abs
-    signum = fmap signum
-    negate = fmap negate
-
-Lens.makeLenses ''TextSize
-
-textWidth :: Draw.Font -> Text -> TextSize Draw.R
-textWidth font str =
-    TextSize
-    { _bounding =
-      Draw.textBoundingWidth font str
-      -- max with advance because spaces are counted only in advance
-      -- but not the bounding width
-      & max adv
-    , _advance = adv
-    }
-    where
-        adv = Draw.textAdvance font str
-
-data RenderedText a = RenderedText
-    { _renderedTextSize :: TextSize (Vector2 Draw.R)
-    , _renderedText :: a
-    }
-Lens.makeLenses ''RenderedText
-
-textSize :: Draw.Font -> Text -> TextSize (Vector2 Draw.R)
-textSize font str =
-    (`Vector2` height) <$> textWidth font str
-    where
-        height = Draw.fontHeight font * fromIntegral numLines
-        numLines = 1 + Text.count "\n" str
-
-renderText :: Draw.Font -> Draw.TextAttrs -> Text -> RenderedText Image
-renderText font textAttrs str =
-    RenderedText
-    { _renderedTextSize = textSize font str
-    , _renderedText =
-        Draw.text font str textAttrs
-        & void
-        -- Text is normally at height -0.5..1.5.  We move it to be -textHeight..0
-        & (translate (Vector2 0 (-Draw.fontHeight font - Draw.fontDescender font)) %%)
-        -- We want to reverse it so that higher y is down, and it is also
-        -- moved to 0..2
-        & (scale (Vector2 1 (-1)) %%)
-    }
