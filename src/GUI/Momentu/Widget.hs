lamdu/lamdu

commit bba9ee8794ff22aceb247661b787bbf0b198bbb6

New cleaner pre-events mechanism.
Instead of using MonadWriter the pre-events are stored in the widget,
and are added to events added to it.

src/GUI/Momentu/Widget.hs
@@ -1,7 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 module GUI.Momentu.Widget
     ( module Types
-    , eventMapMaker
 
     , Id(..), Id.joinId, makeSubId
 
@@ -22,6 +22,13 @@ module GUI.Momentu.Widget
     , takesFocus
     , enterFuncAddVirtualCursor
 
+    -- Event maps:
+    , strongerEvents
+    , weakerEvents
+    , weakerEventsWithContext
+    , addPreEvent
+    , eventMapMaker
+
     -- Operations:
     , translate
     , translateFocused, combineEnterPoints
@@ -36,17 +43,20 @@ module GUI.Momentu.Widget
     , glueStates
     ) where
 
+import           Control.Applicative (liftA2)
 import qualified Control.Lens as Lens
+import qualified Data.Text as Text
 import           Data.Vector.Vector2 (Vector2(..))
 import           GUI.Momentu.Animation (AnimId, R, Size)
 import           GUI.Momentu.Direction (Direction)
 import qualified GUI.Momentu.Direction as Direction
 import qualified GUI.Momentu.Element as Element
 import           GUI.Momentu.EventMap (EventMap)
-import           GUI.Momentu.State (VirtualCursor(..), Update, HasCursor(..))
-import qualified GUI.Momentu.State as State
+import qualified GUI.Momentu.EventMap as E
 import           GUI.Momentu.Rect (Rect(..))
 import qualified GUI.Momentu.Rect as Rect
+import           GUI.Momentu.State (VirtualCursor(..), Update, HasCursor(..))
+import qualified GUI.Momentu.State as State
 import           GUI.Momentu.View (View(..))
 import           GUI.Momentu.Widget.Id (Id(..))
 import qualified GUI.Momentu.Widget.Id as Id
@@ -97,6 +107,65 @@ enterFuncAddVirtualCursor destRect =
             Direction.Point p     -> Rect p 0 & Just
             <&> VirtualCursor
 
+-- | Take a manual `mappend` function to avoid needing "Monoid (f a)"
+-- constraint in callers, who can give the Applicative-Monoid instance
+-- for a generic Applicative without requiring a cumbersome
+-- "Applicative (f a)" constraint
+addPreEventToEventMap :: (a -> a -> a) -> PreEvent a -> EventMap a -> EventMap a
+addPreEventToEventMap append preEvent e =
+    e
+    & actionText %~ concatDescs (preEvent ^. pDesc)
+    <&> append (preEvent ^. pAction)
+    where
+        actionText = E.emDocs . E.docStrs . Lens.reversed . Lens.element 0
+        concatDescs x y = filter (not . Text.null) [x, y] & Text.intercalate ", "
+
+addPreEvent :: Monoid a => PreEvent a -> Widget a -> Widget a
+addPreEvent preEvent =
+    wState . _StateFocused . Lens.mapped %~ onFocused
+    where
+        onFocused f =
+            f
+            & fPreEvents %~ (preEvent:)
+            & fEventMap %~ onMkEventMap
+        onMkEventMap mk ctx =
+            ctx
+            & ePrevTextRemainder %~ (preEvent ^. pTextRemainder <>)
+            & mk
+            & addPreEventToEventMap mappend preEvent
+
+addEvents ::
+    (Applicative f, Monoid a, HasWidget w) =>
+    (EventMap (f a) -> EventMap (f a) -> EventMap (f a)) ->
+    EventMap (f a) -> w (f a) -> w (f a)
+addEvents append e =
+    widget . wState . _StateFocused . Lens.mapped %~ onFocused
+    where
+        onFocused f =
+            f & fEventMap . Lens.mapped %~ append e'
+            where
+                e' = foldr (addPreEventToEventMap (liftA2 mappend)) e (f ^. fPreEvents)
+
+strongerEvents ::
+    (Applicative f, Monoid a, HasWidget w) => EventMap (f a) -> w (f a) -> w (f a)
+strongerEvents = addEvents mappend
+
+weakerEvents ::
+    (Applicative f, Monoid a, HasWidget w) => EventMap (f a) -> w (f a) -> w (f a)
+weakerEvents = addEvents (flip mappend)
+
+weakerEventsWithContext ::
+    (Applicative f, Monoid a, HasWidget w) =>
+    (EventContext -> EventMap (f a)) -> w (f a) -> w (f a)
+weakerEventsWithContext mkEvents =
+    widget . wState . _StateFocused . Lens.mapped %~ onFocused
+    where
+        onFocused f =
+            f & fEventMap . Lens.imapped %@~ add
+            where
+                add ctx =
+                    (<> foldr (addPreEventToEventMap (liftA2 mappend)) (mkEvents ctx) (f ^. fPreEvents))
+
 translateFocused ::
     Functor f =>
     Vector2 R -> (Surrounding -> Focused (f Update)) ->
@@ -115,7 +184,8 @@ padToSizeAlign newSize alignment w =
 setFocused :: HasWidget w => w a -> w a
 setFocused = widget %~ \w -> setFocusedWith (Rect 0 (w ^. wSize)) mempty w
 
-setFocusedWith :: Rect -> (EventContext -> EventMap a) -> Widget a -> Widget a
+setFocusedWith ::
+    Rect -> (EventContext -> EventMap a) -> Widget a -> Widget a
 setFocusedWith rect eventMap =
     wState %~
     \s ->
@@ -124,6 +194,7 @@ setFocusedWith rect eventMap =
         const Focused
         { _fFocalAreas = [rect]
         , _fEventMap = eventMap
+        , _fPreEvents = mempty
         , _fMEnterPoint = u ^. uMEnter <&> (. Direction.Point)
         , _fLayers = u ^. uLayers
         }

Lamdu.cabal
@@ -120,7 +120,6 @@ Executable lamdu
                   , GUI.Momentu.Main.Image
                   , GUI.Momentu.MetaKey
                   , GUI.Momentu.ModKey
-                  , GUI.Momentu.PreEvent
                   , GUI.Momentu.Rect
                   , GUI.Momentu.Responsive
                   , GUI.Momentu.Responsive.Expression

gui-examples/choice.hs
@@ -78,7 +78,7 @@ makeWidget choiceRef getFont mainEnv =
                 & M.tint color
                 & M.scale 100
         box `M.above` choiceWidget
-            & M.strongerEvents M.quitEventMap
+            & M.weakerEvents M.quitEventMap
             & return
 
 choiceConfig :: Choice.Config

gui-examples/hello-world.hs
@@ -33,5 +33,5 @@ hello getFont env =
             ^. M.tValue
             & Widget.fromView
             & Widget.setFocused
-            & M.strongerEvents M.quitEventMap
+            & M.weakerEvents M.quitEventMap
             & return

src/GUI/Momentu.hs
@@ -10,7 +10,7 @@ module GUI.Momentu
 
     -- | Events
     , EventMap.Doc(..)
-    , EventMap.weakerEvents, EventMap.strongerEvents
+    , Widget.weakerEvents
 
     -- | Animations
     , AnimId

src/GUI/Momentu/EventMap.hs
@@ -14,7 +14,6 @@ module GUI.Momentu.EventMap
     , dropEventMap
     , deleteKey, deleteKeys
     , filterChars, filter, mapMaybe
-    , HasEventMap(..), weakerEvents, strongerEvents
     ) where
 
 import qualified Control.Lens as Lens
@@ -183,19 +182,19 @@ mapMaybe p (EventMap m dropHandlers charGroups mAllChars) =
         f (Doesn'tWantClipboard val) = p val <&> Doesn'tWantClipboard
         f (WantsClipboard func) = (>>= p) . func & WantsClipboard & Just
 
-filter :: HasEventMap f => (a -> Bool) -> f a -> f a
+filter :: (a -> Bool) -> EventMap a -> EventMap a
 filter p =
-    eventMap %~ mapMaybe f
+    mapMaybe f
     where
         f x
             | p x = Just x
             | otherwise = Nothing
 
-filterChars :: HasEventMap f => (Char -> Bool) -> f a -> f a
+filterChars :: (Char -> Bool) -> EventMap a -> EventMap a
 filterChars p val =
     val
-    & eventMap . emAllCharsHandler . Lens.traversed . chDocHandler . dhHandler %~ f
-    & eventMap . emCharGroupHandlers %~ filterCharGroups p
+    & emAllCharsHandler . Lens.traversed . chDocHandler . dhHandler %~ f
+    & emCharGroupHandlers %~ filterCharGroups p
     where
         f handler c = do
             guard $ p c
@@ -216,10 +215,10 @@ mkModKey = ModKey
 filterByKey :: (k -> Bool) -> Map k v -> Map k v
 filterByKey p = Map.filterWithKey (const . p)
 
-deleteKey :: HasEventMap f => KeyEvent -> f a -> f a
-deleteKey key = eventMap . emKeyMap %~ Map.delete key
+deleteKey :: KeyEvent -> EventMap a -> EventMap a
+deleteKey key = emKeyMap %~ Map.delete key
 
-deleteKeys :: HasEventMap f => [KeyEvent] -> f a -> f a
+deleteKeys :: [KeyEvent] -> EventMap a -> EventMap a
 deleteKeys = foldr ((.) . deleteKey) id
 
 lookup :: Applicative f => f (Maybe Clipboard) -> Events.Event -> EventMap a -> f (Maybe a)
@@ -324,12 +323,3 @@ pasteOnKey :: ModKey -> Doc -> (Clipboard -> a) -> EventMap a
 pasteOnKey key doc handler =
     WantsClipboard (Just . handler)
     & keyEventMapH (KeyEvent ModKey.KeyState'Pressed key) doc
-
-class HasEventMap f where eventMap :: Lens.Setter' (f a) (EventMap a)
-instance HasEventMap EventMap where eventMap = id
-
-strongerEvents :: HasEventMap f => EventMap a -> f a -> f a
-strongerEvents eMap = eventMap %~ (eMap `mappend`)
-
-weakerEvents :: HasEventMap f => EventMap a -> f a -> f a
-weakerEvents eMap = eventMap %~ (`mappend` eMap)

src/GUI/Momentu/Main.hs
@@ -217,7 +217,7 @@ mainLoopWidget win mkWidgetUnmemod options =
                         , _eWindowSize = size
                         , _eState = s
                         }
-                        <&> E.strongerEvents zoomEventMap
+                        <&> Widget.eventMapMaker . Lens.mapped %~ (zoomEventMap <>)
                         >>= addHelp helpStyle size
         mkWidgetRef <- mkW >>= newIORef
         let newWidget = mkW >>= writeIORef mkWidgetRef

src/GUI/Momentu/PreEvent.hs
@@ -1,55 +1,0 @@
--- | For effective text-like keyboard based input, current choices
--- should be finalized without an explicit action, upon further
--- action.  For example when creating a list, and choosing an item by
--- moving the cursor to it, pressing "," or "+" to add another item
--- should finalize the selection of the currently focused item.  This
--- selection of current item is the PreEvent to the other event.
-
-{-# LANGUAGE NoImplicitPrelude, OverloadedStrings, TypeFamilies #-}
-
-module GUI.Momentu.PreEvent
-    ( PreEvents(..), withPreEvents
-    , HasPreEvents(..)
-    ) where
-
-import qualified Control.Lens as Lens
-import qualified Data.Text as Text
-import           GUI.Momentu.EventMap (EventMap)
-import qualified GUI.Momentu.EventMap as E
-
-import           Lamdu.Prelude
-
-class HasPreEvents m where
-    type Event m
-    listenPreEvents :: m a -> m (a, PreEvents (Event m))
-
-concatDescs :: Text -> Text -> Text
-concatDescs x y = filter (not . Text.null) [x, y] & Text.intercalate ", "
-
-data PreEvents a = PreEvents
-    { pDesc :: E.Subtitle
-    , pAction :: a
-    , pTextRemainder :: Text
-    }
-instance Monoid a => Monoid (PreEvents a) where
-    mempty = PreEvents mempty mempty mempty
-    mappend x y =
-        PreEvents
-        { pDesc = concatDescs (pDesc x) (pDesc y)
-        , pAction = pAction x <> pAction y
-        , pTextRemainder = pTextRemainder x <> pTextRemainder y
-        }
-
-actionText :: Lens.Traversal' (EventMap a) E.Subtitle
-actionText = E.emDocs . E.docStrs . Lens.reversed . Lens.element 0
-
-withPreEvents :: Applicative f => PreEvents (f ()) -> (Text, EventMap (f a) -> EventMap (f a))
-withPreEvents pre =
-    ( pTextRemainder pre
-    , onEventMap
-    )
-    where
-        onEventMap eventMap =
-            eventMap
-            & actionText %~ concatDescs (pDesc pre)
-            <&> (pAction pre *>)

commit 0081912c4945d57a08ba2ccaaaaa1c3897158a42

oops fix new pre-event mechanism's text remainder
typing 1+2..10 was broken since bba9ee8794

src/GUI/Momentu/Widget.hs
@@ -164,7 +164,11 @@ weakerEventsWithContext mkEvents =
             f & fEventMap . Lens.imapped %@~ add
             where
                 add ctx =
-                    (<> foldr (addPreEventToEventMap (liftA2 mappend)) (mkEvents ctx) (f ^. fPreEvents))
+                    ctx
+                    & ePrevTextRemainder <>~ (f ^. fPreEvents . traverse . pTextRemainder)
+                    & mkEvents
+                    & (foldr (addPreEventToEventMap (liftA2 mappend)) ?? (f ^. fPreEvents))
+                    & flip mappend
 
 translateFocused ::
     Functor f =>