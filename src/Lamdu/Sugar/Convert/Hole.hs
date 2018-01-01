lamdu/lamdu

commit 1ecdf26101e28180e58560ad962c565ce795bcc7

holeOptionLiteral creates a single result

src/Lamdu/Sugar/Convert/Hole.hs
@@ -50,7 +50,6 @@ import qualified Lamdu.Expr.IRef as ExprIRef
 import qualified Lamdu.Expr.Lens as ExprLens
 import qualified Lamdu.Expr.Load as Load
 import qualified Lamdu.Expr.Pure as P
-import qualified Lamdu.Expr.Pure as Pure
 import qualified Lamdu.Infer as Infer
 import qualified Lamdu.Infer.Trans as InferT
 import           Lamdu.Infer.Unify (unify)
@@ -318,23 +317,41 @@ mkLeafActions ::
 mkLeafActions exprPl =
     do
         sugarContext <- ConvertM.readContext
-        let mkOption =
-                pure . mkHoleOption sugarContext Nothing exprPl . SeedExpr
-        let mkLiteralOption =
-                mkOption . Val () . V.BLeaf . V.LLiteral . PrimVal.fromKnown
+        let mkOption val =
+                pure
+                ( HoleResultScore 0 []
+                , fixedVal <&> convPl
+                    & mkHoleResult sugarContext (pure ()) (exprPl ^. Input.stored)
+                )
+                where
+                    typ = val ^. Val.payload
+                    fixedVal
+                        | inferredType == typ || Lens.has T._TVar inferredType = val
+                        | otherwise =
+                            V.Apply (Val (T.TFun typ inferredType) (V.BLeaf V.LHole)) val
+                            & V.BApp
+                            & Val inferredType
         pure LeafHoleActions
             { _holeOptionLiteral =
                 \case
-                LiteralNum (Identity x) -> PrimVal.Float x & mkLiteralOption
-                LiteralBytes (Identity x) -> PrimVal.Bytes x & mkLiteralOption
+                LiteralNum (Identity x) -> PrimVal.Float x & literalExpr & mkOption
+                LiteralBytes (Identity x) -> PrimVal.Bytes x & literalExpr & mkOption
                 LiteralText (Identity x) ->
                     encodeUtf8 x
                     & PrimVal.Bytes
-                    & PrimVal.fromKnown
-                    & V.LLiteral & Pure.leaf
-                    & Pure.toNom Builtins.textTid
+                    & literalExpr
+                    & V.Nom Builtins.textTid
+                    & V.BToNom
+                    & Val (T.TInst Builtins.textTid mempty)
                     & mkOption
             }
+    where
+        literalExpr v =
+            V.LLiteral prim & V.BLeaf & Val (T.TInst (prim ^. V.primType) mempty)
+            where
+                prim = PrimVal.fromKnown v
+        convPl t = (Infer.Payload t Infer.emptyScope, (Nothing, NotInjected))
+        inferredType = exprPl ^. Input.inferred . Infer.plType
 
 getLocalScopeGetVars :: ConvertM.Context m -> V.Var -> [Val ()]
 getLocalScopeGetVars sugarContext par

src/Lamdu/Sugar/Types/Hole.hs
@@ -45,7 +45,7 @@ data Literal f
     | LiteralText (f Text)
 
 newtype LeafHoleActions m resultExpr = LeafHoleActions
-    { _holeOptionLiteral :: Literal Identity -> m (HoleOption m resultExpr)
+    { _holeOptionLiteral :: Literal Identity -> m (HoleResultScore, m (HoleResult m resultExpr))
     } deriving Functor
 
 data Unwrap m

src/Lamdu/GUI/ExpressionEdit/HoleEdit/EventMap.hs
@@ -8,8 +8,6 @@ module Lamdu.GUI.ExpressionEdit.HoleEdit.EventMap
 import qualified Control.Lens as Lens
 import qualified Data.Char as Char
 import           Data.Functor.Identity (Identity(..))
-import           Data.List.Class (List)
-import qualified Data.List.Class as List
 import qualified Data.Store.Transaction as Transaction
 import qualified Data.Text as Text
 import           GUI.Momentu (MetaKey(..))
@@ -93,14 +91,6 @@ disallowCharsFromSearchTerm ::
 disallowCharsFromSearchTerm holeKind getSearchTerm =
     E.filter (allowedSearchTerm holeKind . getSearchTerm)
 
-listTHead :: List t => b -> t b -> List.ItemM t b
-listTHead nil l =
-    l
-    & List.runList
-    <&> \case
-        List.Nil -> nil
-        List.Cons item _ -> item
-
 -- TODO: This is ugly, maybe Sugar.HoleOption should
 -- have a canonical result?
 toLiteralTextEventMap ::
@@ -111,11 +101,7 @@ toLiteralTextEventMap actions =
     E.keysEventMapMovesCursor toLiteralTextKeys
     (E.Doc ["Edit", "Create Text Literal"]) $
     do
-        (_score, mkResult) <-
-            Sugar.LiteralText (Identity "")
-            & actions ^. Sugar.holeOptionLiteral
-            <&> (^. Sugar.hoResults)
-            >>= listTHead (error "Literal hole option has no results?!")
+        (_score, mkResult) <- Sugar.LiteralText (Identity "") & actions ^. Sugar.holeOptionLiteral
         result <- mkResult
         result ^. Sugar.holeResultPick
         let argExpr =

src/Lamdu/GUI/ExpressionEdit/HoleEdit/ResultGroups.hs
@@ -184,25 +184,34 @@ mkGroup option =
             }
 
 tryBuildLiteral ::
-    (Format a, Monad m) => (Identity a -> Sugar.Literal Identity) ->
+    (Format a, Monad m) =>
+    Text ->
+    (Identity a -> Sugar.Literal Identity) ->
     Sugar.HoleKind (T m) (ExpressionN m ()) e -> Text ->
-    Maybe (T m (Sugar.HoleOption (T m) (ExpressionN m ())))
-tryBuildLiteral mkLiteral holeKind searchTerm =
+    Maybe (T m (Group m))
+tryBuildLiteral identText mkLiteral holeKind searchTerm =
     holeKind ^? Sugar._LeafHole . Sugar.holeOptionLiteral <*> literal
+    <&> Lens.mapped %~ f
     where
         literal =
             tryParse searchTerm
             <&> Identity
             <&> mkLiteral
+        f x =
+            Group
+            { _groupSearchTerms = [searchTerm]
+            , _groupResults = pure x
+            , _groupId = WidgetIds.hash identText
+            }
 
 literalGroups ::
     Monad m =>
     Sugar.HoleKind (T m) (ExpressionN m ()) e -> Text ->
-    [T m (Sugar.HoleOption (T m) (ExpressionN m ()))]
+    [T m (Group m)]
 literalGroups holeKind searchTerm =
-    [ tryBuildLiteral Sugar.LiteralNum holeKind searchTerm
-    , tryBuildLiteral Sugar.LiteralBytes holeKind searchTerm
-    , tryBuildLiteral Sugar.LiteralText holeKind searchTerm
+    [ tryBuildLiteral "Num"   Sugar.LiteralNum holeKind searchTerm
+    , tryBuildLiteral "Bytes" Sugar.LiteralBytes holeKind searchTerm
+    , tryBuildLiteral "Text"  Sugar.LiteralText holeKind searchTerm
     ] ^.. Lens.traverse . Lens._Just
 
 insensitivePrefixOf :: Text -> Text -> Bool
@@ -233,7 +242,7 @@ makeAllGroups ::
     T m [Group m]
 makeAllGroups hole searchTerm =
     (++)
-    <$> (literalGroups (hole ^. Sugar.holeKind) searchTerm & sequenceA >>= mapM mkGroup)
+    <$> (literalGroups (hole ^. Sugar.holeKind) searchTerm & sequenceA)
     <*> (hole ^. Sugar.holeOptions >>= mapM mkGroup <&> holeMatches searchTerm)
 
 groupOrdering :: Text -> Group m -> [Bool]

src/Lamdu/Sugar/Names/Walk.hs
@@ -7,6 +7,7 @@ module Lamdu.Sugar.Names.Walk
     , toWorkArea, toDef, toExpression, toBody
     ) where
 
+import qualified Control.Lens as Lens
 import qualified Data.Set as Set
 import           Data.Store.Transaction (Transaction)
 import           Lamdu.Calc.Type (Type)
@@ -181,7 +182,7 @@ toLeafHoleActions ::
 toLeafHoleActions ha@LeafHoleActions {..} =
     do
         run <- opRun
-        pure ha { _holeOptionLiteral = _holeOptionLiteral <&> (>>= run . toHoleOption) }
+        pure ha { _holeOptionLiteral = _holeOptionLiteral <&> Lens.mapped . _2 %~ (>>= run . toHoleResult) }
 
 toHoleKind ::
     MonadNaming m =>

commit 03dd57c2b3dc5629e5605c41d49810e1a7f73a8d

oops fix creation of text literals
broken since 1ecdf26101e2

src/Lamdu/Sugar/Convert/Hole.hs
@@ -38,7 +38,7 @@ import qualified Lamdu.Builtins.PrimVal as PrimVal
 import           Lamdu.Calc.Type (Type(..))
 import qualified Lamdu.Calc.Type as T
 import qualified Lamdu.Calc.Type.Nominal as N
-import           Lamdu.Calc.Type.Scheme (schemeType)
+import           Lamdu.Calc.Type.Scheme (schemeType, mono)
 import qualified Lamdu.Calc.Val as V
 import           Lamdu.Calc.Val.Annotated (Val(..))
 import qualified Lamdu.Calc.Val.Annotated as Val
@@ -317,11 +317,11 @@ mkLeafActions ::
 mkLeafActions exprPl =
     do
         sugarContext <- ConvertM.readContext
-        let mkOption val =
+        let mkOption updateDeps val =
                 pure
                 ( HoleResultScore 0 []
                 , fixedVal <&> convPl
-                    & mkHoleResult sugarContext (pure ()) (exprPl ^. Input.stored)
+                    & mkHoleResult sugarContext updateDeps (exprPl ^. Input.stored)
                 )
                 where
                     typ = val ^. Val.payload
@@ -331,11 +331,12 @@ mkLeafActions exprPl =
                             V.Apply (Val (T.TFun typ inferredType) (V.BLeaf V.LHole)) val
                             & V.BApp
                             & Val inferredType
+        let addTextDep = Property.pureModify (sugarContext ^. ConvertM.scFrozenDeps) (<> textDep)
         pure LeafHoleActions
             { _holeOptionLiteral =
                 \case
-                LiteralNum (Identity x) -> PrimVal.Float x & literalExpr & mkOption
-                LiteralBytes (Identity x) -> PrimVal.Bytes x & literalExpr & mkOption
+                LiteralNum (Identity x) -> PrimVal.Float x & literalExpr & mkOption (pure ())
+                LiteralBytes (Identity x) -> PrimVal.Bytes x & literalExpr & mkOption (pure ())
                 LiteralText (Identity x) ->
                     encodeUtf8 x
                     & PrimVal.Bytes
@@ -343,7 +344,7 @@ mkLeafActions exprPl =
                     & V.Nom Builtins.textTid
                     & V.BToNom
                     & Val (T.TInst Builtins.textTid mempty)
-                    & mkOption
+                    & mkOption addTextDep
             }
     where
         literalExpr v =
@@ -352,6 +353,15 @@ mkLeafActions exprPl =
                 prim = PrimVal.fromKnown v
         convPl t = (Infer.Payload t Infer.emptyScope, (Nothing, NotInjected))
         inferredType = exprPl ^. Input.inferred . Infer.plType
+        textDep =
+            mempty
+            { Infer._depsNominals =
+                Map.singleton Builtins.textTid
+                N.Nominal
+                { N._nomType = T.TInst Builtins.bytesTid mempty & mono & N.NominalType
+                , N._nomParams = mempty
+                }
+            }
 
 getLocalScopeGetVars :: ConvertM.Context m -> V.Var -> [Val ()]
 getLocalScopeGetVars sugarContext par