lamdu/lamdu

commit 649f2fa6611b0716eddb19f3283c31fb2c4760c7

makeOpenSearchAreaGui does less

src/Lamdu/GUI/ExpressionEdit/HoleEdit/SearchArea.hs
@@ -8,35 +8,43 @@ module Lamdu.GUI.ExpressionEdit.HoleEdit.SearchArea
     ( make, allowedSearchTermCommon
     ) where
 
-import           Data.Store.Transaction (Transaction)
-import           GUI.Momentu.EventMap (EventMap)
-import           GUI.Momentu.ModKey (ModKey(..))
-import           Lamdu.Config (HasConfig)
-import           Lamdu.GUI.ExpressionEdit.HoleEdit.Open (makeOpenSearchAreaGui)
-import           Lamdu.GUI.ExpressionEdit.HoleEdit.WidgetIds (WidgetIds(..))
-import           Lamdu.GUI.ExpressionGui (ExpressionGui, ExpressionN)
-import           Lamdu.GUI.ExpressionGui.Annotation (maybeAddAnnotationPl)
-import           Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
 import qualified Control.Lens as Lens
+import           Control.Monad.Transaction (transaction)
 import qualified Data.Char as Char
 import qualified Data.Monoid as Monoid
+import           Data.Store.Transaction (Transaction)
 import qualified Data.Text as Text
+import           GUI.Momentu.Align (WithTextPos)
 import qualified GUI.Momentu.Align as Align
+import qualified GUI.Momentu.Element as Element
+import           GUI.Momentu.EventMap (EventMap)
 import qualified GUI.Momentu.EventMap as E
 import qualified GUI.Momentu.Hover as Hover
 import qualified GUI.Momentu.MetaKey as MetaKey
+import           GUI.Momentu.ModKey (ModKey(..))
 import qualified GUI.Momentu.Responsive as Responsive
 import qualified GUI.Momentu.State as GuiState
+import           GUI.Momentu.Widget (Widget)
 import qualified GUI.Momentu.Widget as Widget
 import qualified GUI.Momentu.Widgets.FocusDelegator as FocusDelegator
 import qualified GUI.Momentu.Widgets.Menu as Menu
+import qualified GUI.Momentu.Widgets.Spacer as Spacer
 import qualified Lamdu.CharClassification as Chars
+import           Lamdu.Config (HasConfig)
 import qualified Lamdu.Config as Config
+import qualified Lamdu.Config.Theme as Theme
+import           Lamdu.GUI.ExpressionEdit.HoleEdit.Open (makeOpenSearchAreaGui, ResultOption(..))
+import           Lamdu.GUI.ExpressionEdit.HoleEdit.ResultGroups (ResultGroup(..), Result(..))
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.ResultGroups as ResultGroups
+import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.ResultWidget as ResultWidget
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.SearchTerm as SearchTerm
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.State as HoleState
+import           Lamdu.GUI.ExpressionEdit.HoleEdit.WidgetIds (WidgetIds(..))
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.WidgetIds as HoleWidgetIds
+import           Lamdu.GUI.ExpressionGui (ExpressionGui, ExpressionN)
 import qualified Lamdu.GUI.ExpressionGui as ExprGui
+import           Lamdu.GUI.ExpressionGui.Annotation (maybeAddAnnotationPl)
+import           Lamdu.GUI.ExpressionGui.Monad (ExprGuiM)
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
@@ -76,6 +84,45 @@ searchTermEditEventMap widgetIds allowedTerms =
     where
         notOp = Text.any (`notElem` Chars.operator)
 
+makeShownResult ::
+    Monad m =>
+    Sugar.Payload f ExprGui.Payload -> Result (T m) ->
+    ExprGuiM m
+    ( EventMap (T m GuiState.Update)
+    , WithTextPos (Widget (T m GuiState.Update))
+    )
+makeShownResult pl result =
+    do
+        -- Warning: rHoleResult should be ran at most once!
+        -- Running it more than once caused a horrible bug (bugfix: 848b6c4407)
+        res <- rHoleResult result & transaction
+        theme <- Theme.hole <$> Lens.view Theme.theme
+        stdSpacing <- Spacer.getSpaceSize
+        let padding = Theme.holeResultPadding theme <&> realToFrac & (* stdSpacing)
+        ResultWidget.make pl (rId result) res <&> _2 %~ Element.pad padding
+
+makeResultOption ::
+    Monad m =>
+    Sugar.Payload f ExprGui.Payload ->
+    ResultGroup (T m) ->
+    ExprGuiM m (ResultOption m)
+makeResultOption pl results =
+    makeShownResult pl (results ^. ResultGroups.rgMain)
+    <&>
+    \(pickMain, mainResultWidget) ->
+    ResultOption
+    { _roOption =
+        Menu.Option
+        { Menu._oId = results ^. ResultGroups.rgPrefixId
+        , Menu._oWidget = pure mainResultWidget
+        , Menu._oSubmenuWidgets =
+            case results ^. ResultGroups.rgExtra of
+            [] -> Menu.SubmenuEmpty
+            extras -> Menu.SubmenuItems (traverse (makeShownResult pl) extras <&> map snd)
+        }
+    , _roPickMainEventMap = pickMain
+    }
+
 -- Has a typeView under the search term
 make ::
     Monad m =>
@@ -111,6 +158,7 @@ make options mOptionLiteral pl allowedTerms =
                 -- here
                 (fdWrap <&> (Lens.mapped %~))
                 <*> ( ResultGroups.makeAll options mOptionLiteral widgetIds
+                        >>= traverse (makeResultOption pl)
                         >>= makeOpenSearchAreaGui searchTermEventMap allowedTerms pl)
                 <&> Lens.mapped %~ inPlaceOfClosed . (^. Align.tValue)
             else

src/Lamdu/GUI/ExpressionEdit/HoleEdit/Open.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings, TemplateHaskell #-}
 -- | The search area (search term + results) of an open/active hole.
 
 module Lamdu.GUI.ExpressionEdit.HoleEdit.Open
-    ( makeOpenSearchAreaGui
+    ( makeOpenSearchAreaGui, ResultOption(..), roOption, roPickMainEventMap
     ) where
 
 import qualified Control.Lens as Lens
@@ -27,9 +27,6 @@ import qualified GUI.Momentu.Widgets.Spacer as Spacer
 import           Lamdu.Config (HasConfig)
 import qualified Lamdu.Config as Config
 import qualified Lamdu.Config.Theme as Theme
-import           Lamdu.GUI.ExpressionEdit.HoleEdit.ResultGroups (ResultGroup(..), Result(..))
-import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.ResultGroups as ResultGroups
-import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.ResultWidget as ResultWidget
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.SearchTerm as SearchTerm
 import qualified Lamdu.GUI.ExpressionEdit.HoleEdit.State as HoleState
 import           Lamdu.GUI.ExpressionEdit.HoleEdit.WidgetIds (WidgetIds(..))
@@ -45,48 +42,10 @@ import           Lamdu.Prelude
 type T = Transaction
 
 data ResultOption m = ResultOption
-    { roOption :: !(Menu.Option (ExprGuiM m) (T m GuiState.Update))
-    , roPickMainEventMap :: !(EventMap (T m GuiState.Update))
+    { _roOption :: !(Menu.Option (ExprGuiM m) (T m GuiState.Update))
+    , _roPickMainEventMap :: !(EventMap (T m GuiState.Update))
     }
-
-makeShownResult ::
-    Monad m =>
-    Sugar.Payload f ExprGui.Payload -> Result (T m) ->
-    ExprGuiM m
-    ( EventMap (T m GuiState.Update)
-    , WithTextPos (Widget (T m GuiState.Update))
-    )
-makeShownResult pl result =
-    do
-        -- Warning: rHoleResult should be ran at most once!
-        -- Running it more than once caused a horrible bug (bugfix: 848b6c4407)
-        res <- rHoleResult result & transaction
-        theme <- Theme.hole <$> Lens.view Theme.theme
-        stdSpacing <- Spacer.getSpaceSize
-        let padding = Theme.holeResultPadding theme <&> realToFrac & (* stdSpacing)
-        ResultWidget.make pl (rId result) res <&> _2 %~ Element.pad padding
-
-makeResultOption ::
-    Monad m =>
-    Sugar.Payload f ExprGui.Payload ->
-    ResultGroup (T m) ->
-    ExprGuiM m (ResultOption m)
-makeResultOption pl results =
-    makeShownResult pl (results ^. ResultGroups.rgMain)
-    <&>
-    \(pickMain, mainResultWidget) ->
-    ResultOption
-    { roOption =
-        Menu.Option
-        { Menu._oId = results ^. ResultGroups.rgPrefixId
-        , Menu._oWidget = pure mainResultWidget
-        , Menu._oSubmenuWidgets =
-            case results ^. ResultGroups.rgExtra of
-            [] -> Menu.SubmenuEmpty
-            extras -> Menu.SubmenuItems (traverse (makeShownResult pl) extras <&> map snd)
-        }
-    , roPickMainEventMap = pickMain
-    }
+Lens.makeLenses ''ResultOption
 
 assignCursor :: Monad m => WidgetIds -> [Widget.Id] -> ExprGuiM m a -> ExprGuiM m a
 assignCursor widgetIds resultIds action =
@@ -144,18 +103,17 @@ makeOpenSearchAreaGui ::
     EventMap (T m GuiState.Update) ->
     (Text -> Bool) ->
     Sugar.Payload (T m) ExprGui.Payload ->
-    Menu.OptionList (ResultGroup (T m)) ->
+    Menu.OptionList (ResultOption m) ->
     ExprGuiM m (Menu.Placement -> WithTextPos (Widget (T m GuiState.Update)))
-makeOpenSearchAreaGui searchTermEventMap allowedTerms pl optList =
+makeOpenSearchAreaGui searchTermEventMap allowedTerms pl opts =
     do
-        opts <- traverse (makeResultOption pl) optList
         vspace <- Annotation.annotationSpacer
         pickFirstResult <-
             case opts ^. Menu.olOptions of
             [] -> emptyPickEventMap
-            (x:_) -> roPickMainEventMap x & return
+            (x:_) -> x ^. roPickMainEventMap & pure
         let options =
-                opts <&> roOption
+                opts <&> (^. roOption)
                 <&> Menu.optionWidgets . Lens.mapped .
                     Widget.eventMapMaker . Lens.mapped %~ (searchTermEventMap <>)
         typeView <- makeInferredTypeAnnotation pl holeAnimId
@@ -165,7 +123,7 @@ makeOpenSearchAreaGui searchTermEventMap allowedTerms pl optList =
             <&> \searchTermWidget placement ->
                 searchTermWidget <&> hoverMenu placement
     & Reader.local (Element.animIdPrefix .~ WidgetId.toAnimId (hidOpen widgetIds))
-    & assignCursor widgetIds (optList ^. Menu.olOptions <&> rId . (^. ResultGroups.rgMain))
+    & assignCursor widgetIds (opts ^.. traverse . roOption . Menu.oId)
     where
         widgetIds = pl ^. Sugar.plEntityId & HoleWidgetIds.make
         holeAnimId = hidHole widgetIds & Widget.toAnimId

commit 9f4e007b93932f7710ba092d9dc346d5e870b9c6

fix hole cursor bug introduced in 649f2fa6

src/Lamdu/GUI/ExpressionEdit/HoleEdit/ResultWidget.hs
@@ -103,7 +103,7 @@ make ::
     Sugar.HoleResult (T m) (Sugar.Expression (Name (T m)) (T m) ()) ->
     ExprGuiM m
     ( EventMap (T m GuiState.Update)
-    , WithTextPos (Widget (T m GuiState.Update))
+    , ExprGuiM m (WithTextPos (Widget (T m GuiState.Update)))
     )
 make pl resultId holeResult =
     do
@@ -130,16 +130,18 @@ make pl resultId holeResult =
                 , Widget._pTextRemainder = searchStringRemainder
                 }
         remUnwanted <- removeUnwanted
-        postProcessSugar (pl ^. Sugar.plData . ExprGui.plMinOpPrec) holeResultConverted
-            & ExprGuiM.makeSubexpression
-            <&> Widget.enterResultCursor .~ resultId
-            <&> Widget.widget . Widget.eventMapMaker . Lens.mapped %~ remUnwanted
-            & GuiState.assignCursor resultId idWithinResultWidget
-            <&> applyResultLayout
-            <&> setFocalAreaToFullSize
-            <&> Align.tValue %~ Widget.addPreEvent preEvent
-            <&> Align.tValue . Widget.eventMapMaker . Lens.mapped %~ mappend pickEventMap
-            <&> (,) pickEventMap
+        pure
+            ( pickEventMap
+            , postProcessSugar (pl ^. Sugar.plData . ExprGui.plMinOpPrec) holeResultConverted
+                & ExprGuiM.makeSubexpression
+                <&> Widget.enterResultCursor .~ resultId
+                <&> Widget.widget . Widget.eventMapMaker . Lens.mapped %~ remUnwanted
+                & GuiState.assignCursor resultId idWithinResultWidget
+                <&> applyResultLayout
+                <&> setFocalAreaToFullSize
+                <&> Align.tValue %~ Widget.addPreEvent preEvent
+                <&> Align.tValue . Widget.eventMapMaker . Lens.mapped %~ mappend pickEventMap
+            )
     where
         pick = idWithinResultWidget <$ holeResult ^. Sugar.holeResultPick
         widgetIds = pl ^. Sugar.plEntityId & HoleWidgetIds.make

src/Lamdu/GUI/ExpressionEdit/HoleEdit/SearchArea.hs
@@ -93,7 +93,7 @@ makeShownResult ::
     Sugar.Payload f ExprGui.Payload -> Result (T m) ->
     ExprGuiM m
     ( EventMap (T m GuiState.Update)
-    , WithTextPos (Widget (T m GuiState.Update))
+    , ExprGuiM m (WithTextPos (Widget (T m GuiState.Update)))
     )
 makeShownResult pl result =
     do
@@ -103,7 +103,7 @@ makeShownResult pl result =
         theme <- Theme.hole <$> Lens.view Theme.theme
         stdSpacing <- Spacer.getSpaceSize
         let padding = Theme.holeResultPadding theme <&> realToFrac & (* stdSpacing)
-        ResultWidget.make pl (rId result) res <&> _2 %~ Element.pad padding
+        ResultWidget.make pl (rId result) res <&> _2 . Lens.mapped %~ Element.pad padding
 
 makeResultOption ::
     Monad m =>
@@ -113,16 +113,16 @@ makeResultOption ::
 makeResultOption pl results =
     makeShownResult pl (results ^. ResultGroups.rgMain)
     <&>
-    \(pickMain, mainResultWidget) ->
+    \(pickMain, mkMainResultWidget) ->
     ResultOption
     { _roOption =
         Menu.Option
         { Menu._oId = results ^. ResultGroups.rgPrefixId
-        , Menu._oWidget = pure mainResultWidget
+        , Menu._oWidget = mkMainResultWidget
         , Menu._oSubmenuWidgets =
             case results ^. ResultGroups.rgExtra of
             [] -> Menu.SubmenuEmpty
-            extras -> Menu.SubmenuItems (traverse (makeShownResult pl) extras <&> map snd)
+            extras -> Menu.SubmenuItems (traverse (makeShownResult pl) extras >>= traverse snd)
         }
     , _roPickMainEventMap = pickMain
     }