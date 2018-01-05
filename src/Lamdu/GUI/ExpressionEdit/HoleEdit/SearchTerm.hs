2782bbaaec4de3e128374b7350a8dfa7c5552344
Yair Chuchem <yairchu@gmail.com>
2018-01-05 16:34:37 +0200

    remove unneeded context

--- src/Lamdu/GUI/ExpressionEdit/HoleEdit/SearchTerm.hs
+++ src/Lamdu/GUI/ExpressionEdit/HoleEdit/SearchTerm.hs
@@ -13,14 +13,13 @@ import qualified GUI.Momentu.State as GuiState
 import qualified GUI.Momentu.Widget as Widget
 import qualified GUI.Momentu.Widgets.TextEdit as TextEdit
 import qualified GUI.Momentu.Widgets.Menu.Search as SearchMenu
-import           Lamdu.Config (HasConfig)
 import           Lamdu.Config.Theme (HasTheme)
 import qualified Lamdu.Config.Theme as Theme
 
 import           Lamdu.Prelude
 
 make ::
-    ( MonadReader env m, HasTheme env , HasConfig env, TextEdit.HasStyle env, GuiState.HasState env
+    ( MonadReader env m, HasTheme env, TextEdit.HasStyle env, GuiState.HasState env
     , Applicative f
     ) =>
     Widget.Id -> (Text -> Bool) -> m (WithTextPos (Widget (f GuiState.Update)))
