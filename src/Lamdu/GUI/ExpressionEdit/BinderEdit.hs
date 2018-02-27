lamdu/lamdu

commit 78345d62c16a38bcba6129c15514b389ab2c0ea1

Fix whole binder actions
Actions should apply only when whole binder selected, not on each lhs component

src/Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -339,27 +339,26 @@ make pMode lhsEventMap name color binder myId =
                 <&> Widget.strongerEvents rhsJumperEquals
                 <&> Just
         equals <- TextView.makeLabel "="
+        wholeBinderSelected <- GuiState.isSubCursor ?? wholeBinderId
         addWholeBinderActions <-
             case binder ^. Sugar.bActions . Sugar.baMNodeActions of
-            Nothing -> pure id
-            Just nodeActions ->
-                (.)
-                <$> ExprEventMap.addWith ExprEventMap.defaultOptions
-                    ExprEventMap.ExprInfo
-                    { ExprEventMap.exprInfoActions = nodeActions
-                    , ExprEventMap.exprInfoNearestHoles = nearestHoles
-                    , ExprEventMap.exprInfoIsHoleResult = False
-                    , ExprEventMap.exprInfoMinOpPrec = 0
-                    }
-                <*> parentDelegator (Widget.joinId myId ["whole binder"])
-        Options.boxSpaced ?? Options.disambiguationNone
-            <&>
-            (\hbox ->
-            hbox
-            [ hbox (defNameEdit : (mParamEdit ^.. Lens._Just) ++ [Responsive.fromTextView equals])
-                & Widget.weakerEvents lhsEventMap
-            , bodyEdit
-            ] )
+            Just nodeActions | wholeBinderSelected ->
+                ExprEventMap.addWith ExprEventMap.defaultOptions
+                ExprEventMap.ExprInfo
+                { ExprEventMap.exprInfoActions = nodeActions
+                , ExprEventMap.exprInfoNearestHoles = nearestHoles
+                , ExprEventMap.exprInfoIsHoleResult = False
+                , ExprEventMap.exprInfoMinOpPrec = 0
+                }
+            _ -> pure id
+        let layoutWithBody hbox =
+                hbox
+                [ hbox (defNameEdit : (mParamEdit ^.. Lens._Just) ++ [Responsive.fromTextView equals])
+                    & Widget.weakerEvents lhsEventMap
+                , bodyEdit
+                ]
+        parentDelegator wholeBinderId
+            <*> (Options.boxSpaced ?? Options.disambiguationNone <&> layoutWithBody)
             <&> addWholeBinderActions
             <&> Widget.weakerEvents eventMap
     & Reader.local (Element.animIdPrefix .~ Widget.toAnimId myId)
@@ -368,6 +367,7 @@ make pMode lhsEventMap name color binder myId =
         Just lamId ->
             GuiState.assignCursorPrefix (WidgetIds.fromEntityId lamId) (const bodyId)
     where
+        wholeBinderId = Widget.joinId myId ["whole binder"]
         presentationChoiceId = Widget.joinId myId ["presentation"]
         body = binder ^. Sugar.bBody . Sugar.bbContent
         bodyId = body ^. SugarLens.binderContentEntityId & WidgetIds.fromEntityId

commit ded6e41d2d821b9777ff0aec31528fade673c328

fix extract on let item name
was broken since 78345d62c16a38bcba6129c15514b389ab2c0ea1

src/Lamdu/GUI/ExpressionEdit/BinderEdit.hs
@@ -396,14 +396,20 @@ makeLetEdit item =
         theme <- Lens.view Theme.theme
         let letColor = Theme.letColor (Theme.name theme)
         let eventMap =
+                foldMap
+                ( E.keysEventMapMovesCursor (Config.extractKeys config)
+                    (E.Doc ["Edit", "Let clause", "Extract to outer scope"])
+                    . fmap (ExprEventMap.extractCursor . Sugar.efrNewEntity)
+                ) (item ^? Sugar.lValue . Sugar.bActions . Sugar.baMNodeActions . Lens._Just . Sugar.extract)
+                <>
                 E.keysEventMapMovesCursor (Config.delKeys config)
                 (E.Doc ["Edit", "Let clause", "Delete"])
                 (bodyId <$ item ^. Sugar.lActions . Sugar.laDelete)
                 <>
                 foldMap
                 ( E.keysEventMapMovesCursor (Config.inlineKeys config)
-                  (E.Doc ["Navigation", "Jump to first use"])
-                  . pure . WidgetIds.fromEntityId
+                    (E.Doc ["Navigation", "Jump to first use"])
+                    . pure . WidgetIds.fromEntityId
                 ) (item ^? Sugar.lUsages . Lens.ix 0)
         letLabel <- Styled.grammarLabel "let"
         space <- Spacer.stdHSpace