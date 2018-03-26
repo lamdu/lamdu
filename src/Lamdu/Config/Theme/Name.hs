5757618080fa705c5f20c8bc56bbd064bfc86f4d
Yair Chuchem <yairchu@gmail.com>
2018-03-26 11:56:38 +0300

    fix compilation with ghcid

--- src/Lamdu/Config/Theme/Name.hs
+++ src/Lamdu/Config/Theme/Name.hs
@@ -5,8 +5,10 @@ module Lamdu.Config.Theme.Name where
 import qualified Control.Lens as Lens
 import           Data.Aeson.TH (deriveJSON)
 import           Data.Aeson.Types (defaultOptions)
+#ifndef NO_CODE
 import qualified Data.Aeson.Types as Aeson
 import           Data.Aeson.Utils (removePrefix)
+#endif
 import           Data.Vector.Vector2 (Vector2)
 import qualified GUI.Momentu.Draw as Draw
 
