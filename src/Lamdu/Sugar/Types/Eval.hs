f2b6b2f56183d53606881b41f3a8a7f4363aef21
Eyal Lotem <eyal.lotem@gmail.com>
2018-03-31 10:50:44 +0300

    Types.Eval: Minor refactor

--- src/Lamdu/Sugar/Types/Eval.hs
+++ src/Lamdu/Sugar/Types/Eval.hs
@@ -1,25 +1,27 @@
 -- | Sugared evaluation results
-
 module Lamdu.Sugar.Types.Eval
-    ( ChildScopes, ParamScopes, EvaluationScopes, ScopeId
+    ( EvalScopes
+    , ChildScopes, ParamScopes, EvaluationScopes, ScopeId
     ) where
 
 import           Data.CurAndPrev (CurAndPrev)
 import qualified Lamdu.Calc.Type as T
 import           Lamdu.Data.Anchors (BinderParamScopeId)
-import qualified Lamdu.Eval.Results as ER
 import           Lamdu.Eval.Results (ScopeId)
+import qualified Lamdu.Eval.Results as ER
 
 import           Lamdu.Prelude
 
 -- TODO: Actually sugar the eval results
 
+type EvalScopes a = CurAndPrev (Map ScopeId a)
+
 -- This is a mapping from a parent scope to the inner scope in:
 -- * A redex lambda body (executed exactly once)
 -- * Also used for if-else sugar where else-if scopes are executed no more than once
-type ChildScopes = CurAndPrev (Map ScopeId ScopeId)
+type ChildScopes = EvalScopes ScopeId
 
-type ParamScopes = CurAndPrev (Map ScopeId [BinderParamScopeId])
+type ParamScopes = EvalScopes [BinderParamScopeId]
 
 -- TODO: Is the `Maybe` here relevant? Is an empty map the same?
 -- Remove `Maybe` or document the difference
