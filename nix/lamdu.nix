20bf5ee1a0c5b04cd766191f20b3d066971a3c85
Răzvan Flavius Panda <razvan.panda@gmail.com>
2018-04-01 19:23:24 +0100

    Update Nix packaging #91

--- nix/lamdu.nix
+++ nix/lamdu.nix
@@ -1,13 +1,13 @@
-{ mkDerivation, aeson, aeson-pretty, AlgoW, base, base-compat
+{ mkDerivation, aeson, aeson-diff, aeson-pretty, AlgoW, base, base-compat
 , base-orphans, base16-bytestring, binary, bytestring, containers
 , cryptohash-sha256, data-default, deepseq, deepseq-generics
-, directory, filepath, GLFW-b, graphics-drawingcombinators
+, directory, edit-distance, filepath, GLFW-b, graphics-drawingcombinators
 , hashable, JuicyPixels, lamdu-calculus, language-ecmascript, lens
 , leveldb-haskell, List, mtl, nodejs-exec, OpenGL
 , optparse-applicative, pretty, process, random, safe-exceptions
-, StateVar, stdenv, stm, text, time, transformers
-, unordered-containers, uuid, uuid-types, vector, wl-pprint
-, zip-archive, imagemagick
+, StateVar, stdenv, stm, test-framework, test-framework-hunit
+, test-framework-quickcheck2, text, time, transformers, unordered-containers
+, uuid, uuid-types, vector, wl-pprint, zip-archive, imagemagick, HUnit
 }:
 mkDerivation {
   pname = "Lamdu";
@@ -17,14 +17,15 @@ mkDerivation {
   isExecutable = true;
   enableSeparateDataOutput = true;
   executableHaskellDepends = [
-    aeson aeson-pretty AlgoW base base-compat base-orphans
+    aeson aeson-diff aeson-pretty AlgoW base base-compat base-orphans
     base16-bytestring binary bytestring containers cryptohash-sha256
-    data-default deepseq deepseq-generics directory filepath GLFW-b
-    graphics-drawingcombinators hashable JuicyPixels lamdu-calculus
+    data-default deepseq deepseq-generics directory edit-distance filepath GLFW-b
+    graphics-drawingcombinators hashable HUnit JuicyPixels lamdu-calculus
     language-ecmascript lens leveldb-haskell List mtl nodejs-exec
     OpenGL optparse-applicative pretty process random safe-exceptions
-    StateVar stm text time transformers unordered-containers uuid
-    uuid-types vector wl-pprint zip-archive
+    StateVar stm test-framework test-framework-hunit test-framework-quickcheck2
+    text time transformers unordered-containers uuid uuid-types vector wl-pprint
+    zip-archive
   ];
   executablePkgconfigDepends = [ imagemagick ];
   homepage = "http://www.lamdu.org";
