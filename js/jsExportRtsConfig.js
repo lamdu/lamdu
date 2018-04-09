lamdu/lamdu

commit 2d0173de1f1e2b0a425037d52f56edae73785a1c

Extract protocol.js

js/rts.js
@@ -2,8 +2,6 @@
 /* jshint esversion: 6 */
 "use strict";
 
-process.stdout._handle.setBlocking(true);
-
 var conf = require('./rtsConfig.js');
 
 // Tag names must match those in Lamdu.Builtins.Anchors
@@ -69,49 +67,6 @@ var bytesFromAscii = function (str) {
     return arr;
 };
 
-var encode = function() {
-    var cacheId = 0;
-    return function (x) {
-        var replacer = function(key, value) {
-            if (value == null) { // or undefined due to auto-coercion
-                return {};
-            }
-            if (typeof value == "number" && !isFinite(value)) {
-                return { "number": String(value) };
-            }
-            if ((typeof value != "object" && typeof value != "function") ||
-                key === "array" || key === "bytes") {
-                return value;
-            }
-            if (value.hasOwnProperty("cacheId")) {
-                if (value.cacheId == -1) {
-                    // Opaque value
-                    return {};
-                }
-                return { cachedVal: value.cacheId };
-            }
-            value.cacheId = cacheId++;
-            if (Function.prototype.isPrototypeOf(value)) {
-                return { func: value.cacheId };
-            }
-            if (Array.prototype.isPrototypeOf(value)) {
-                return {
-                    array: value,
-                    cacheId: value.cacheId,
-                };
-            }
-            if (Uint8Array.prototype.isPrototypeOf(value)) {
-                return {
-                    bytes: Array.from(value),
-                    cacheId: value.cacheId,
-                };
-            }
-            return value;
-        };
-        return JSON.stringify(x, replacer);
-    };
-}();
-
 var toString = function (bytes) {
     // This is a nodejs only method to convert a UInt8Array to a string.
     // For the browser we'll need to augment this.
@@ -143,28 +98,17 @@ var mutVoidWithError = function (inner) {
     };
 };
 
+var protocol = require('./protocol.js');
+
 module.exports = {
     logRepl: conf.logRepl,
     logReplErr: conf.logReplErr,
     logResult: function (scope, exprId, result) {
-        process.stdout.write(encode({
-            event:"Result",
-            scope:scope,
-            exprId:exprId,
-            result:result
-            }));
-        process.stdout.write("\n");
+        protocol.sendResult(scope, exprId, result);
         return result;
     },
     logNewScope: function (parentScope, scope, lamId, arg) {
-        process.stdout.write(encode({
-            event:"NewScope",
-            parentScope:parentScope,
-            scope:scope,
-            lamId:lamId,
-            arg:arg
-            }));
-        process.stdout.write("\n");
+        protocol.sendNewScope(parentScope, scope, lamId, arg);
     },
     memo: function (thunk) {
         var done = false;

js/protocol.js
@@ -1,0 +1,73 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+"use strict";
+
+process.stdout._handle.setBlocking(true);
+
+var encode = function() {
+    var cacheId = 0;
+    return function (x) {
+        var replacer = function(key, value) {
+            if (value == null) { // or undefined due to auto-coercion
+                return {};
+            }
+            if (typeof value == "number" && !isFinite(value)) {
+                return { "number": String(value) };
+            }
+            if ((typeof value != "object" && typeof value != "function") ||
+                key === "array" || key === "bytes") {
+                return value;
+            }
+            if (value.hasOwnProperty("cacheId")) {
+                if (value.cacheId == -1) {
+                    // Opaque value
+                    return {};
+                }
+                return { cachedVal: value.cacheId };
+            }
+            value.cacheId = cacheId++;
+            if (Function.prototype.isPrototypeOf(value)) {
+                return { func: value.cacheId };
+            }
+            if (Array.prototype.isPrototypeOf(value)) {
+                return {
+                    array: value,
+                    cacheId: value.cacheId,
+                };
+            }
+            if (Uint8Array.prototype.isPrototypeOf(value)) {
+                return {
+                    bytes: Array.from(value),
+                    cacheId: value.cacheId,
+                };
+            }
+            return value;
+        };
+        return JSON.stringify(x, replacer);
+    };
+}();
+
+var sendEvent = function (obj) {
+    process.stdout.write(encode(obj));
+    process.stdout.write("\n");
+};
+
+module.exports = {
+    sendResult: function(scope, exprId, result) {
+        sendEvent({
+            event:"Result",
+            scope:scope,
+            exprId:exprId,
+            result:result,
+        });
+    },
+    sendNewScope: function(parentScope, scope, lamId, arg) {
+        sendEvent({
+            event:"NewScope",
+            parentScope:parentScope,
+            scope:scope,
+            lamId:lamId,
+            arg:arg
+        });
+    },
+};

commit 61d4d52cf0a26fd01c70d869078a0aac4d53413d

Fix JS export broken earlier in 2d0173de1f1e2

js/jsExportRtsConfig.js
@@ -13,4 +13,7 @@ module.exports = {
     },
     logRepl: function (x) { console.log(String.fromCharCode.apply(null, x)); },
     logReplErr: function (x) { console.log("Error: " + String.fromCharCode.apply(null, x)); },
+    // Not used in JS export:
+    logResult: undefined,
+    logNewScope: undefined
 };

js/rts.js
@@ -67,19 +67,13 @@ var mutVoidWithError = function (inner) {
     };
 };
 
-var protocol = require('./protocol.js');
 var conf = require('./rtsConfig.js');
 
 module.exports = {
     logRepl: conf.logRepl,
     logReplErr: conf.logReplErr,
-    logResult: function (scope, exprId, result) {
-        protocol.sendResult(scope, exprId, result);
-        return result;
-    },
-    logNewScope: function (parentScope, scope, lamId, arg) {
-        protocol.sendNewScope(parentScope, scope, lamId, arg);
-    },
+    logResult: conf.logResult,
+    logNewScope: conf.logNewScope,
     memo: function (thunk) {
         var done = false;
         var memo;

js/rtsConfig.js
@@ -24,4 +24,11 @@ module.exports = {
     logReplErr: function (err) {
         protocol.sendCompletionError(err);
     },
+    logResult: function (scope, exprId, result) {
+        protocol.sendResult(scope, exprId, result);
+        return result;
+    },
+    logNewScope: function (parentScope, scope, lamId, arg) {
+        protocol.sendNewScope(parentScope, scope, lamId, arg);
+    },
 };

src/Lamdu/Data/Export/JS.hs
@@ -128,6 +128,7 @@ exportFancy evalResults =
                     readDataFile "doc/JSExportReadMe.md"
                     <&> removeReadmeMeta <&> fromString
                 rts <- readDataFile "js/rts.js" <&> fromString
+                rtsAnchors <- readDataFile "js/anchors.js" <&> fromString
                 rtsConf <- readDataFile "js/jsExportRtsConfig.js" <&> fromString
                 let addFile archive (filename, contents) =
                         Zip.addEntryToArchive
@@ -140,5 +141,6 @@ exportFancy evalResults =
                     , ("README.md", readme)
                     , ("js/main.js", replJs)
                     , ("js/rts.js", rts)
+                    , ("js/anchors.js", rtsAnchors)
                     , ("js/rtsConfig.js", rtsConf)
                     ] & Zip.fromArchive & LBS.writeFile "export.zip"