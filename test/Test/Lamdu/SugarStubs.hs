2d0257676ad55bb4623627d84a57b7d20515e173
Eyal Lotem <eyal.lotem@gmail.com>
2018-04-19 22:00:04 +0300

    Extract Test.Lamdu.SugarStubs

---
+++ test/Test/Lamdu/SugarStubs.hs
@@ -0,0 +1,178 @@
+-- | Build sugar expressions easily
+
+module Test.Lamdu.SugarStubs where
+
+import           Control.Monad.Unit (Unit(Unit))
+import           Data.CurAndPrev (CurAndPrev(CurAndPrev))
+import           Data.Functor.Identity (Identity)
+import           Data.Property (Property(Property))
+import           Data.UUID.Types (UUID)
+import qualified Lamdu.Calc.Type as T
+import           Lamdu.Sugar.Names.Add (InternalName(..))
+import qualified Lamdu.Sugar.Types as Sugar
+import           Test.Lamdu.Instances ()
+import           Lamdu.Prelude
+
+pane ::
+    Sugar.Definition name i Unit (Sugar.Expression name i Unit a) ->
+    Sugar.Pane name i Unit a
+pane body =
+    Sugar.Pane
+    { Sugar._paneDefinition = body
+    , Sugar._paneClose = Unit
+    , Sugar._paneMoveDown = Nothing
+    , Sugar._paneMoveUp = Nothing
+    }
+
+tagInfo :: UUID -> T.Tag -> Sugar.TagInfo InternalName
+tagInfo var tag =
+    Sugar.TagInfo
+    { Sugar._tagName = taggedEntityName var tag
+    , Sugar._tagInstance = "dummy"
+    , Sugar._tagVal = tag
+    }
+
+mkTag :: UUID -> T.Tag -> Sugar.Tag InternalName Identity Unit
+mkTag var tag =
+    Sugar.Tag
+    { Sugar._tagSelection = tagSelection
+    , Sugar._tagInfo = tagInfo var tag
+    }
+
+def ::
+    Sugar.Type InternalName -> UUID -> T.Tag ->
+    Sugar.Binder InternalName Identity Unit expr ->
+    Sugar.Definition InternalName Identity Unit expr
+def typ var tag body =
+    Sugar.Definition
+    { Sugar._drName = mkTag var tag
+    , Sugar._drDefI = "def"
+    , Sugar._drDefinitionState = Property Sugar.LiveDefinition (const Unit) & pure
+    , Sugar._drEntityId = "dummy"
+    , Sugar._drBody =
+        Sugar.DefinitionBodyExpression Sugar.DefinitionExpression
+        { Sugar._deType =
+            Sugar.Scheme
+            { Sugar._schemeForAll = mempty
+            , Sugar._schemeConstraints = mempty
+            , Sugar._schemeType = typ
+            }
+        , Sugar._dePresentationMode = Nothing
+        , Sugar._deContent = body
+        }
+    }
+
+repl :: Sugar.Expression name i o a -> Sugar.Repl name i o a
+repl x =
+    Sugar.Repl
+    { Sugar._replExpr = x
+    , Sugar._replResult = CurAndPrev Nothing Nothing
+    }
+
+mkFuncParam ::
+    (UUID, T.Tag, Sugar.Type name) ->
+    Sugar.FuncParam name (Sugar.ParamInfo InternalName Identity Unit)
+mkFuncParam (paramVar, paramTag, paramType) =
+    Sugar.FuncParam
+    { Sugar._fpAnnotation = annotation paramType
+    , Sugar._fpInfo =
+        Sugar.ParamInfo
+        { Sugar._piTag = mkTag paramVar paramTag
+        , Sugar._piActions =
+            Sugar.FuncParamActions
+            { Sugar._fpAddNext = Sugar.AddNext tagSelection
+            , Sugar._fpDelete = Unit
+            , Sugar._fpMOrderBefore = Nothing
+            , Sugar._fpMOrderAfter = Nothing
+            }
+        }
+    }
+
+binderExpr ::
+    [(UUID, T.Tag, Sugar.Type InternalName)] ->
+    Sugar.Expression InternalName Identity Unit () ->
+    Sugar.Binder InternalName Identity Unit
+    (Sugar.Expression InternalName Identity Unit ())
+binderExpr params body =
+    Sugar.Binder
+    { Sugar._bChosenScopeProp = Property Nothing (const Unit) & pure
+    , Sugar._bLamId =
+        case params of
+        [] -> Nothing
+        _:_ -> Just "dummy"
+    , Sugar._bBodyScopes = CurAndPrev mempty mempty & Sugar.BinderBodyScope
+    , Sugar._bActions =
+        Sugar.BinderActions
+        { Sugar._baAddFirstParam = Sugar.PrependParam tagSelection
+        , Sugar._baMNodeActions = Just nodeActions
+        }
+    , Sugar._bParams = params <&> mkFuncParam & Sugar.Params
+    , Sugar._bBody =
+        Sugar.BinderBody
+        { Sugar._bbAddOuterLet = Unit
+        , Sugar._bbContent = Sugar.BinderExpr body
+        }
+    }
+
+funcType :: Sugar.Type InternalName -> Sugar.Type InternalName -> Sugar.Type InternalName
+funcType param res =
+    Sugar.Type
+    { Sugar._tPayload = "dummy"
+    , Sugar._tBody = Sugar.TFun param res
+    }
+
+expr ::
+    Sugar.Type name ->
+    Sugar.Body name Identity Unit
+    (Sugar.Expression name Identity Unit ()) ->
+    Sugar.Expression name Identity Unit ()
+expr typ body =
+    Sugar.Expression { Sugar._rBody = body, Sugar._rPayload = mkPayload typ }
+
+numType :: Sugar.Type InternalName
+numType =
+    Sugar.Type
+    { Sugar._tPayload = "dummy"
+    , Sugar._tBody = Sugar.TInst (Sugar.TId (taggedEntityName "numTid" "num") "num") mempty
+    }
+
+mkPayload :: Sugar.Type name -> Sugar.Payload name Identity Unit ()
+mkPayload typ =
+    Sugar.Payload
+    { Sugar._plAnnotation = annotation typ
+    , Sugar._plEntityId = "dummy"
+    , Sugar._plActions = nodeActions
+    , Sugar._plData = ()
+    }
+
+annotation :: Sugar.Type name -> Sugar.Annotation name
+annotation typ =
+    Sugar.Annotation
+    { Sugar._aInferredType = typ
+    , Sugar._aMEvaluationResult = CurAndPrev Nothing Nothing
+    }
+
+nodeActions :: Sugar.NodeActions name Identity Unit
+nodeActions =
+    Sugar.NodeActions
+    { Sugar._detach = Sugar.DetachAction Unit
+    , Sugar._mSetToHole = Nothing
+    , Sugar._extract = Unit
+    , Sugar._mReplaceParent = Nothing
+    , Sugar._wrapInRecord = tagSelection
+    }
+
+taggedEntityName :: UUID -> T.Tag -> InternalName
+taggedEntityName ctx tag =
+    InternalName
+    { _inContext = Just ctx
+    , _inTag = tag
+    }
+
+tagSelection :: Sugar.TagSelection name Identity Unit ()
+tagSelection =
+    Sugar.TagSelection
+    { Sugar._tsOptions = pure []
+    , Sugar._tsNewTag = const Unit
+    , Sugar._tsAnon = Nothing
+    }
--- test/TestDisambiguation.hs
+++ test/TestDisambiguation.hs
@@ -7,11 +7,9 @@ module TestDisambiguation (test) where
 import           Control.Monad.Unit (Unit(..))
 import           Control.Monad.Trans.FastWriter (Writer, runWriter)
 import           Control.Monad.Writer (MonadWriter(..))
-import           Data.CurAndPrev (CurAndPrev(..))
 import           Data.Functor.Identity (Identity(..))
 import           Data.Property (Property(..), MkProperty(..))
 import           Data.String (IsString(..))
-import           Data.UUID.Types (UUID)
 import qualified Lamdu.Calc.Type as T
 import           Lamdu.Name (Name)
 import qualified Lamdu.Name as Name
@@ -23,6 +21,7 @@ import           Test.Framework (Test, testGroup)
 import           Test.Framework.Providers.HUnit (testCase)
 import           Test.HUnit (assertString)
 import           Test.Lamdu.Instances ()
+import qualified Test.Lamdu.SugarStubs as Stub
 
 import           Lamdu.Prelude
 
@@ -74,202 +73,37 @@ getNameProp tag =
     Property (fromString (show tag)) (const Unit)
     & Identity & MkProperty
 
-pane ::
-    Sugar.Definition name i Unit (Sugar.Expression name i Unit a) ->
-    Sugar.Pane name i Unit a
-pane body =
-    Sugar.Pane
-    { Sugar._paneDefinition = body
-    , Sugar._paneClose = Unit
-    , Sugar._paneMoveDown = Nothing
-    , Sugar._paneMoveUp = Nothing
-    }
-
-tagInfo :: UUID -> T.Tag -> Sugar.TagInfo InternalName
-tagInfo var tag =
-    Sugar.TagInfo
-    { Sugar._tagName = taggedEntityName var tag
-    , Sugar._tagInstance = "dummy"
-    , Sugar._tagVal = tag
-    }
-
-mkTag :: UUID -> T.Tag -> Sugar.Tag InternalName Identity Unit
-mkTag var tag =
-    Sugar.Tag
-    { Sugar._tagSelection = tagSelection
-    , Sugar._tagInfo = tagInfo var tag
-    }
-
-def ::
-    Sugar.Type InternalName -> UUID -> T.Tag ->
-    Sugar.Binder InternalName Identity Unit expr ->
-    Sugar.Definition InternalName Identity Unit expr
-def typ var tag body =
-    Sugar.Definition
-    { Sugar._drName = mkTag var tag
-    , Sugar._drDefI = "def"
-    , Sugar._drDefinitionState = Property Sugar.LiveDefinition (const Unit) & Identity
-    , Sugar._drEntityId = "dummy"
-    , Sugar._drBody =
-        Sugar.DefinitionBodyExpression Sugar.DefinitionExpression
-        { Sugar._deType =
-            Sugar.Scheme
-            { Sugar._schemeForAll = mempty
-            , Sugar._schemeConstraints = mempty
-            , Sugar._schemeType = typ
-            }
-        , Sugar._dePresentationMode = Nothing
-        , Sugar._deContent = body
-        }
-    }
-
-repl :: Sugar.Expression name i o a -> Sugar.Repl name i o a
-repl x =
-    Sugar.Repl
-    { Sugar._replExpr = x
-    , Sugar._replResult = CurAndPrev Nothing Nothing
-    }
-
-mkFuncParam ::
-    (UUID, T.Tag, Sugar.Type name) ->
-    Sugar.FuncParam name (Sugar.ParamInfo InternalName Identity Unit)
-mkFuncParam (paramVar, paramTag, paramType) =
-    Sugar.FuncParam
-    { Sugar._fpAnnotation = annotation paramType
-    , Sugar._fpInfo =
-        Sugar.ParamInfo
-        { Sugar._piTag = mkTag paramVar paramTag
-        , Sugar._piActions =
-            Sugar.FuncParamActions
-            { Sugar._fpAddNext = Sugar.AddNext tagSelection
-            , Sugar._fpDelete = Unit
-            , Sugar._fpMOrderBefore = Nothing
-            , Sugar._fpMOrderAfter = Nothing
-            }
-        }
-    }
-
-binderExpr ::
-    [(UUID, T.Tag, Sugar.Type InternalName)] ->
-    Sugar.Expression InternalName Identity Unit () ->
-    Sugar.Binder InternalName Identity Unit
-    (Sugar.Expression InternalName Identity Unit ())
-binderExpr params body =
-    Sugar.Binder
-    { Sugar._bChosenScopeProp = Property Nothing (const Unit) & Identity
-    , Sugar._bLamId =
-        case params of
-        [] -> Nothing
-        _:_ -> Just "dummy"
-    , Sugar._bBodyScopes = CurAndPrev mempty mempty & Sugar.BinderBodyScope
-    , Sugar._bActions =
-        Sugar.BinderActions
-        { Sugar._baAddFirstParam = Sugar.PrependParam tagSelection
-        , Sugar._baMNodeActions = Just nodeActions
-        }
-    , Sugar._bParams = params <&> mkFuncParam & Sugar.Params
-    , Sugar._bBody =
-        Sugar.BinderBody
-        { Sugar._bbAddOuterLet = Unit
-        , Sugar._bbContent = Sugar.BinderExpr body
-        }
-    }
-
-funcType :: Sugar.Type InternalName -> Sugar.Type InternalName -> Sugar.Type InternalName
-funcType param res =
-    Sugar.Type
-    { Sugar._tPayload = "dummy"
-    , Sugar._tBody = Sugar.TFun param res
-    }
-
-expr ::
-    Sugar.Type name ->
-    Sugar.Body name Identity Unit
-    (Sugar.Expression name Identity Unit ()) ->
-    Sugar.Expression name Identity Unit ()
-expr typ body =
-    Sugar.Expression { Sugar._rBody = body, Sugar._rPayload = mkPayload typ }
-
-numType :: Sugar.Type InternalName
-numType =
-    Sugar.Type
-    { Sugar._tPayload = "dummy"
-    , Sugar._tBody = Sugar.TInst (Sugar.TId (taggedEntityName "numTid" "num") "num") mempty
-    }
-
-mkPayload :: Sugar.Type name -> Sugar.Payload name Identity Unit ()
-mkPayload typ =
-    Sugar.Payload
-    { Sugar._plAnnotation = annotation typ
-    , Sugar._plEntityId = "dummy"
-    , Sugar._plActions = nodeActions
-    , Sugar._plData = ()
-    }
-
-annotation :: Sugar.Type name -> Sugar.Annotation name
-annotation typ =
-    Sugar.Annotation
-    { Sugar._aInferredType = typ
-    , Sugar._aMEvaluationResult = CurAndPrev Nothing Nothing
-    }
-
-nodeActions :: Sugar.NodeActions name Identity Unit
-nodeActions =
-    Sugar.NodeActions
-    { Sugar._detach = Sugar.DetachAction Unit
-    , Sugar._mSetToHole = Nothing
-    , Sugar._extract = Unit
-    , Sugar._mReplaceParent = Nothing
-    , Sugar._wrapInRecord = tagSelection
-    }
-
-taggedEntityName :: UUID -> T.Tag -> InternalName
-taggedEntityName ctx tag =
-    InternalName
-    { _inContext = Just ctx
-    , _inTag = tag
-    }
-
-tagSelection :: Sugar.TagSelection name Identity Unit ()
-tagSelection =
-    Sugar.TagSelection
-    { Sugar._tsOptions = Identity []
-    , Sugar._tsNewTag = const Unit
-    , Sugar._tsAnon = Nothing
-    }
-
-
 --- test inputs:
 
 workArea396 :: IO ()
 workArea396 =
     Sugar.WorkArea
-    { Sugar._waRepl = repl lamExpr
+    { Sugar._waRepl = Stub.repl lamExpr
     , Sugar._waPanes =
-        [ binderExpr [("paneVar", "num", numType)] leafExpr
-            & def lamType "def" "def"
-            & pane
+        [ Stub.binderExpr [("paneVar", "num", Stub.numType)] leafExpr
+            & Stub.def lamType "def" "def"
+            & Stub.pane
         ]
     , Sugar._waGlobals = pure []
     } & testWorkArea assertNoCollisions
     where
-        lamType = funcType numType numType
-        leafExpr = expr numType Sugar.BodyPlaceHolder
+        lamType = Stub.funcType Stub.numType Stub.numType
+        leafExpr = Stub.expr Stub.numType Sugar.BodyPlaceHolder
         lamExpr =
             Sugar.BodyLam Sugar.Lambda
             { Sugar._lamMode = Sugar.NormalBinder
-            , Sugar._lamBinder = binderExpr [("lamVar", "num", numType)] leafExpr
-            } & expr lamType
+            , Sugar._lamBinder = Stub.binderExpr [("lamVar", "num", Stub.numType)] leafExpr
+            } & Stub.expr lamType
 
 workAreaGlobals :: IO ()
 workAreaGlobals =
     Sugar.WorkArea
-    { Sugar._waRepl = repl trivialExpr
+    { Sugar._waRepl = Stub.repl trivialExpr
     , Sugar._waPanes =
         -- 2 defs sharing the same tag with different Vars/UUIDs,
         -- should collide with ordinary suffixes
-        [ def numType "def1" "def" trivialBinder & pane
-        , def numType "def2" "def" trivialBinder & pane
+        [ Stub.def Stub.numType "def1" "def" trivialBinder & Stub.pane
+        , Stub.def Stub.numType "def2" "def" trivialBinder & Stub.pane
         ]
     , Sugar._waGlobals = pure []
     } & testWorkArea verifyName
@@ -283,5 +117,5 @@ workAreaGlobals =
                 [ "Unexpected/bad collision for name", show text
                 , show textCollision, show tagCollision
                 ] & assertString
-        trivialBinder = binderExpr [] trivialExpr
-        trivialExpr = expr numType Sugar.BodyPlaceHolder
+        trivialBinder = Stub.binderExpr [] trivialExpr
+        trivialExpr = Stub.expr Stub.numType Sugar.BodyPlaceHolder
