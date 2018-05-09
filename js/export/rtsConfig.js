8bb179af9d5ab33d385904afa381e7b89778017a
Eyal Lotem <eyal.lotem@gmail.com>
2018-05-09 18:18:00 +0300

    JSExport: Repl output: Do not stringify non-strings

--- js/export/rtsConfig.js
+++ js/export/rtsConfig.js
@@ -12,7 +12,12 @@ module.exports = {
             return nameMap[x];
         return x;
     },
-    logRepl: function (x) { console.log(String.fromCharCode.apply(null, x)); },
+    logRepl: function (x) {
+        console.log(
+            x instanceof Uint8Array
+            ? String.fromCharCode.apply(null, x)
+            : x);
+    },
     logReplErr: function (x) { console.log("Error: " + String.fromCharCode.apply(null, x)); },
     // Not used in JS export:
     logResult: undefined,
