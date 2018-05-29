7af155cdbca8fb8eb3b0e7f0a045341ab3b4d39f
Yair Chuchem <yairchu@gmail.com>
2018-05-30 00:22:39 +0300

    reuse Data.Aeson.Config.load
    tests would now work properly with imports in default theme

--- test/Test/Lamdu/Theme.hs
+++ test/Test/Lamdu/Theme.hs
@@ -1,7 +1,6 @@
 module Test.Lamdu.Theme (load) where
 
-import qualified Data.Aeson as Aeson
-import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Aeson.Config as AesonConfig
 import qualified Data.Text as Text
 import           Lamdu.Config.Theme (Theme)
 import qualified Lamdu.Paths as Paths
@@ -15,6 +14,4 @@ load =
     Paths.getDataFileName "config.json"
     <&> takeDirectory
     <&> (\x -> x </> "themes" </> Text.unpack Themes.initial ++ ".json")
-    >>= LBS.readFile
-    <&> Aeson.eitherDecode'
-    >>= either (fail . mappend "Failed loading default theme: ") pure
+    >>= AesonConfig.load
