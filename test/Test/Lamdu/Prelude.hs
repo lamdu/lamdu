518859590bf0d2b6eb0f0c4c8ae9a6ea60ff84a9
Eyal Lotem <eyal.lotem@gmail.com>
2018-06-03 13:25:41 +0300

    Test.Lamdu.Prelude: Add runTest for interactive mode testing

--- test/Test/Lamdu/Prelude.hs
+++ test/Test/Lamdu/Prelude.hs
@@ -2,6 +2,7 @@
 
 module Test.Lamdu.Prelude
     ( module X
+    , runTest
     ) where
 
 import Lamdu.Prelude as X
@@ -11,3 +12,9 @@ import Test.Framework.Providers.QuickCheck2 as X (testProperty)
 import Test.HUnit as X (assertString, assertEqual)
 import Test.Lamdu.Instances ()
 import Text.PrettyPrint.HughesPJClass as X (prettyShow)
+
+-- | Useful for running test in a terminal-incapable terminal, with
+-- colors disabled
+runTest :: Test -> IO ()
+runTest test =
+    defaultMainWithOpts [test] mempty { ropt_color_mode = Just ColorNever }
