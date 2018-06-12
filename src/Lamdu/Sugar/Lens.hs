lamdu/lamdu

commit 9b69d4546ade1e72cc5f2a459948694295fb1fdb

Sugar.Expression: Payload before body

src/Lamdu/Sugar/Lens.hs
@@ -23,10 +23,10 @@ subExprPayloads ::
     (Expression name i o a)
     (Expression name i o b)
     (Payload name i o a) (Payload name i o b)
-subExprPayloads f val@(Expression body pl) =
+subExprPayloads f val@(Expression pl body) =
     Expression
-    <$> (Lens.traversed .> subExprPayloads) f body
-    <*> Lens.indexed f (void val) pl
+    <$> Lens.indexed f (void val) pl
+    <*> (Lens.traversed .> subExprPayloads) f body
 
 payloadsIndexedByPath ::
     Lens.IndexedTraversal
@@ -37,10 +37,10 @@ payloadsIndexedByPath ::
 payloadsIndexedByPath f =
     go []
     where
-        go path val@(Expression body pl) =
+        go path val@(Expression pl body) =
             Expression
-            <$> Lens.traversed (go newPath) body
-            <*> Lens.indexed f newPath pl
+            <$> Lens.indexed f newPath pl
+            <*> Lens.traversed (go newPath) body
             where
                 newPath = void val : path
 

src/Lamdu/GUI/AnnotationsPass.hs
@@ -34,7 +34,7 @@ topLevelAnn = rPayload . plData . _1
 markAnnotationsToDisplay ::
     Expression name i o a ->
     Expression name i o (T.ShowAnnotation, a)
-markAnnotationsToDisplay (Expression oldBody pl) =
+markAnnotationsToDisplay (Expression pl oldBody) =
     case newBody of
     BodyPlaceHolder -> set T.neverShowAnnotations
     BodyLiteral LiteralNum {} -> set T.neverShowAnnotations
@@ -59,7 +59,7 @@ markAnnotationsToDisplay (Expression oldBody pl) =
                 else binder ^. bbContent . SugarLens.binderContentExpr .
                         topLevelAnn . T.showInEvalMode
             )
-        & Expression (newBodyWith dontShowEval)
+        & (`Expression` newBodyWith dontShowEval)
     BodyInject _ -> set dontShowEval
     BodyGetVar (GetParamsRecord _) -> set T.showAnnotationWhenVerbose
     BodyGetField _ -> set T.showAnnotationWhenVerbose
@@ -67,23 +67,23 @@ markAnnotationsToDisplay (Expression oldBody pl) =
         app
         & applyFunc . nonHoleAnn .~ T.neverShowAnnotations
         & BodySimpleApply
-        & (`Expression` defPl)
+        & Expression defPl
     BodyLabeledApply _ -> set T.showAnnotationWhenVerbose
     BodyIfElse i ->
         i
         & iIfThen . itThen %~ onCaseAlt
         & iElse %~ onElse
         & BodyIfElse
-        & (`Expression` defPl)
+        & Expression defPl
     BodyHole hole ->
         hole
         & BodyHole
-        & (`Expression` plWith forceShowType)
+        & Expression (plWith forceShowType)
     BodyFragment fragment ->
         fragment
         & fExpr . nonHoleAnn .~ forceShowTypeOrEval
         & BodyFragment
-        & (`Expression` plWith forceShowType)
+        & Expression (plWith forceShowType)
     BodyCase cas ->
         cas
         -- cKind contains the scrutinee which is not always
@@ -92,12 +92,12 @@ markAnnotationsToDisplay (Expression oldBody pl) =
         & cKind . Lens.mapped . nonHoleAnn .~ T.neverShowAnnotations
         & cBody . cItems . Lens.mapped . Lens.mapped %~ onCaseAlt
         & BodyCase
-        & (`Expression` defPl)
+        & Expression defPl
     where
         newBodyWith f = newBody <&> nonHoleAnn .~ f
         plWith ann = pl & plData %~ (,) ann
         defPl = plWith T.showAnnotationWhenVerbose
-        set ann = Expression newBody (plWith ann)
+        set ann = Expression (plWith ann) newBody
         newBody = oldBody <&> markAnnotationsToDisplay
         nonHoleAnn = Lens.filtered (Lens.nullOf (rBody . SugarLens.bodyUnfinished)) . topLevelAnn
         onCaseAlt a =

src/Lamdu/GUI/ExpressionEdit.hs
@@ -32,7 +32,7 @@ import qualified Lamdu.Sugar.Types as Sugar
 import           Lamdu.Prelude
 
 make :: (Monad i, Monad o) => ExprGui.SugarExpr i o -> ExprGuiM i o (ExpressionGui o)
-make (Sugar.Expression body pl) =
+make (Sugar.Expression pl body) =
     makeEditor body pl & assignCursor
     where
         exprHiddenEntityIds = pl ^. Sugar.plData . ExprGui.plHiddenEntityIds

src/Lamdu/GUI/ExpressionEdit/HoleEdit/ValTerms.hs
@@ -90,7 +90,7 @@ bodyNames =
     b -> NamesGet.fromBody b >>= ofName
 
 expr :: Monad i => ExpressionN i o a -> [Text]
-expr (Sugar.Expression body _) =
+expr (Sugar.Expression _ body) =
     bodyShape body <>
     bodyNames body <>
     case body of

src/Lamdu/GUI/ExpressionEdit/InjectEdit.hs
@@ -82,7 +82,7 @@ emptyRec ::
 emptyRec nearestHoles (Sugar.NullaryVal pl closedActions addItem) =
     Sugar.Composite [] (Sugar.ClosedComposite closedActions) addItem
     & Sugar.BodyRecord
-    & (`Sugar.Expression` pl')
+    & Sugar.Expression pl'
     where
         pl' = pl <&> \() -> ExprGui.adhocPayload nearestHoles
 

src/Lamdu/Sugar/Convert/Binder.hs
@@ -261,7 +261,7 @@ markLightParams ::
     Monad m =>
     Set InternalName -> Expression InternalName (T m) (T m) a ->
     Expression InternalName (T m) (T m) a
-markLightParams paramNames (Expression body pl) =
+markLightParams paramNames (Expression pl body) =
     case body of
     BodyGetVar (GetParam n)
         | Set.member (n ^. pNameRef . nrName) paramNames ->
@@ -270,7 +270,7 @@ markLightParams paramNames (Expression body pl) =
             & GetParam & BodyGetVar
     BodyFragment w -> w <&> markLightParams paramNames & BodyFragment
     _ -> body <&> markLightParams paramNames
-    & (`Expression` pl)
+    & Expression pl
 
 -- Let-item or definition (form of <name> [params] = <body>)
 convertBinder ::

src/Lamdu/Sugar/Convert/Inject.hs
@@ -32,10 +32,10 @@ convert (V.Inject tag injected) exprPl =
                     typeProtect <&> EntityId.ofValI
         let val =
                 case injectedS of
-                Expression
+                Expression pl
                     (BodyRecord
                      (Composite []
-                      (ClosedComposite closedCompositeActions) addItem)) pl ->
+                      (ClosedComposite closedCompositeActions) addItem)) ->
                     NullaryVal (void pl) closedCompositeActions addItem
                     & InjectNullary
                 _ ->

src/Lamdu/Sugar/Names/Walk.hs
@@ -423,10 +423,10 @@ toExpression ::
     MonadNaming m =>
     Expression (OldName m) (IM m) o a ->
     m (Expression (NewName m) (IM m) o a)
-toExpression (Expression body pl) =
+toExpression (Expression pl body) =
     Expression
-    <$> toBody toExpression body
-    <*> toPayload pl
+    <$> toPayload pl
+    <*> toBody toExpression body
 
 withParamInfo ::
     MonadNaming m =>

commit d7328196b4bc5d28a815f2e3d4c7c5d8b05cbc88

Fix precedence bug (*)
Bug introduced in 9b69d4546ade1e72cc5f2a459948694295fb1fdb which was supposed to have no functionality change,
however it did change traversal orders and caused this bug.

Bug manifests when typing "x%3" in euler1.

TODO: Write a test that would fail when reverting this fix.

src/Lamdu/Sugar/Lens.hs
@@ -26,9 +26,9 @@ subExprPayloads ::
     (Expression name i o b)
     (Payload name i o a) (Payload name i o b)
 subExprPayloads f val@(Expression pl x) =
-    Expression
-    <$> Lens.indexed f (void val) pl
-    <*> (bodyChildren .> subExprPayloads) f x
+    flip Expression
+    <$> (bodyChildren .> subExprPayloads) f x
+    <*> Lens.indexed f (void val) pl
 
 payloadsIndexedByPath ::
     Lens.IndexedTraversal