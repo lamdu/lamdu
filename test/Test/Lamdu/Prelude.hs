cfd3f2b04e9b80cf8fdb3c26f843ee223155a05f
Eyal Lotem <eyal.lotem@gmail.com>
2018-07-04 13:41:40 +0300

    Test.Prelude: Add assertSetEquals

--- test/Test/Lamdu/Prelude.hs
+++ test/Test/Lamdu/Prelude.hs
@@ -3,18 +3,36 @@
 module Test.Lamdu.Prelude
     ( module X
     , runTest
+    , assertSetEquals
     ) where
 
-import Lamdu.Prelude as X
-import Test.Framework as X
-import Test.Framework.Providers.HUnit as X (testCase)
-import Test.Framework.Providers.QuickCheck2 as X (testProperty)
-import Test.HUnit as X (assertString, assertEqual)
-import Test.Lamdu.Instances ()
-import Text.PrettyPrint.HughesPJClass as X (prettyShow)
+import qualified Control.Lens as Lens
+import           Data.Set as Set
+import           Lamdu.Prelude as X
+import           Test.Framework as X
+import           Test.Framework.Providers.HUnit as X (testCase)
+import           Test.Framework.Providers.QuickCheck2 as X (testProperty)
+import           Test.HUnit as X (assertString, assertEqual)
+import           Test.Lamdu.Instances ()
+import           Text.PrettyPrint.HughesPJClass as X (prettyShow)
 
 -- | Useful for running test in a terminal-incapable terminal, with
 -- colors disabled
 runTest :: Test -> IO ()
 runTest test =
     defaultMainWithOpts [test] mempty { ropt_color_mode = Just ColorNever }
+
+assertSetEquals :: (Ord a, Show a) => String -> Set a -> Set a -> IO ()
+assertSetEquals msg expected actual
+    | expected == actual = pure ()
+    | otherwise =
+          msg
+          <> asMsg "missing" missingElements
+          <> asMsg "has unexpected" extraElements
+          & fail
+    where
+        asMsg prefix s
+            | Set.null s = ""
+            | otherwise = " " <> prefix <> " " <> show (s ^.. Lens.folded)
+        missingElements = expected `Set.difference` actual
+        extraElements = actual `Set.difference` expected
