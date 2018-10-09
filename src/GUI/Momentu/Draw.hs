c895c402422af1671501bf5ea916c1448f67266b
Eyal Lotem <eyal.lotem@gmail.com>
2018-10-09 15:28:51 +0300

    backgroundColor: Refactor

--- src/GUI/Momentu/Draw.hs
+++ src/GUI/Momentu/Draw.hs
@@ -27,9 +27,8 @@ backgroundColor ::
     m (Draw.Color -> a -> a)
 backgroundColor =
     Element.subAnimId ["bg"] <&>
-    \animId color -> Element.setLayers %@~ \sz x ->
-    x
-    & Element.layers %~ addBg (Anim.coloredRectangle animId color & Anim.scale sz)
+    \animId color -> Element.setLayers . Element.layers %@~ \sz ->
+    addBg (Anim.coloredRectangle animId color & Anim.scale sz)
     where
         addBg bg [] = [bg]
         addBg bg (x:xs) = x <> bg : xs
