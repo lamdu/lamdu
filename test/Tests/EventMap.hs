lamdu/lamdu

commit 4c56009ed035c2b82183e4df201969d4120cae4f

EventMap: Remove redundant listToMaybe

src/GUI/Momentu/EventMap.hs
@@ -23,7 +23,6 @@ import qualified Control.Lens.Extended as Lens
 import           Data.Char (isAscii)
 import           Data.Foldable (asum)
 import qualified Data.Map as Map
-import           Data.Maybe (listToMaybe)
 import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
 import           Data.String (IsString(..))
@@ -255,20 +254,18 @@ lookupKeyMap getClipboard dict (Events.KeyEvent k _scanCode keyState modKeys _) 
 
 lookupCharGroup :: [CharGroupHandler a] -> Events.KeyEvent -> Maybe a
 lookupCharGroup charGroups (Events.KeyEvent _k _scanCode keyState _modKeys mchar) =
-    listToMaybe $
     do
         ModKey.KeyState'Pressed <- pure keyState
-        char <- mchar ^.. Lens._Just
-        charGroups ^.. Lens.traverse . cgDocHandler . dhHandler . Lens.ix char
+        char <- mchar
+        charGroups ^? Lens.traverse . cgDocHandler . dhHandler . Lens.ix char
 
 lookupAllCharHandler :: [AllCharsHandler t] -> Events.KeyEvent -> Maybe t
 lookupAllCharHandler allCharHandlers (Events.KeyEvent _k _scanCode keyState _modKeys mchar) =
-    listToMaybe $
     do
         keyState == ModKey.KeyState'Pressed & guard
-        char <- mchar ^.. Lens._Just
-        AllCharsHandler _ handler <- allCharHandlers
-        (handler ^. dhHandler) char ^.. Lens._Just
+        char <- mchar
+        AllCharsHandler _ handler <- allCharHandlers ^? traverse
+        (handler ^. dhHandler) char
 
 charGroup :: HasCallStack => Maybe InputDoc -> Doc -> String -> (Char -> a) -> EventMap a
 charGroup miDoc oDoc chars func =

commit 2e629e5a0053ff163b257b2bf4656200cc42c99a

Add test for bug introduced by 4c56009ed035c2b82183
* Test indeed fails correctly when reverting c93b2e28d30ed020b6958d3f

Lamdu.cabal
@@ -69,6 +69,7 @@ Test-Suite Tests
                , Tests.ColorSchemes
                , Tests.Config
                , Tests.DataFiles
+               , Tests.EventMap
                , Tests.Gui
                , Tests.JsExport
                , Tests.JsRtsTags

test/Tests.hs
@@ -5,6 +5,7 @@ import qualified Tests.Codec
 import qualified Tests.ColorSchemes
 import qualified Tests.Config
 import qualified Tests.DataFiles
+import qualified Tests.EventMap
 import qualified Tests.Gui
 import qualified Tests.JsExport
 import qualified Tests.JsRtsTags
@@ -29,6 +30,7 @@ main =
             , Tests.ColorSchemes.test
             , Tests.Config.test
             , Tests.DataFiles.test
+            , Tests.EventMap.test
             , Tests.Gui.test
             , Tests.JsExport.test
             , Tests.JsRtsTags.test

test/Tests/EventMap.hs
@@ -1,0 +1,34 @@
+module Tests.EventMap where
+
+import           Control.Lens (runIdentity)
+import           GUI.Momentu.EventMap hiding (KeyEvent)
+import qualified Graphics.UI.GLFW as GLFW
+import           Graphics.UI.GLFW.Events (Event(..), KeyEvent(..))
+
+import           Test.Lamdu.Prelude hiding (lookup)
+
+charEvent :: Char -> GLFW.Key -> Event
+charEvent c key = EventKey KeyEvent
+    { keKey = key
+    , keScanCode = 0 -- dummy
+    , keModKeys = mempty
+    , keState = GLFW.KeyState'Pressed
+    , keChar = Just c
+    }
+
+eventMap :: EventMap Integer
+eventMap =
+    mconcat
+    [ charEventMap "A" (Doc ["B"]) (const Nothing)
+    , charEventMap "C" (Doc ["D"]) (const (Just 1))
+    ]
+
+test :: Test
+test =
+    do
+        assertEqual "Lookup" (Just (Doc ["D"])) (res <&> (^. dhDoc))
+        & testCase "event-map-lookup for char handlers"
+    where
+        res =
+            lookup (pure Nothing) (charEvent 'X' GLFW.Key'X) eventMap
+            & runIdentity