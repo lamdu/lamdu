cf1c2b9131a9576dfd83be46c0dd33fa47d20e5c
Eyal Lotem <eyal.lotem@gmail.com>
2018-11-18 02:45:44 +0200

    Build successfully even if git/.git is not available

--- src/main/System/Process/Git.hs
+++ src/main/System/Process/Git.hs
@@ -1,11 +1,13 @@
 -- | Run some git commands, even from another module's TH
 
+-- TODO: Rename to Language.Haskell.TH.Git ?
 module System.Process.Git
     ( hash
     , status
     , dirty
     ) where
 
+import qualified Control.Exception as E
 import qualified Language.Haskell.TH as TH
 import qualified Language.Haskell.TH.Syntax as THS
 import           System.Process (readProcess)
@@ -13,7 +15,10 @@ import           System.Process (readProcess)
 import           Lamdu.Prelude
 
 runGit :: [String] -> TH.Q String
-runGit args = readProcess "git" args "" & TH.runIO
+runGit args =
+    readProcess "git" args ""
+    & E.handle (\e@E.SomeException {} -> "git failed: " ++ show e & pure)
+    & TH.runIO
 
 hash :: TH.ExpQ
 hash = runGit ["rev-parse", "HEAD"] <&> words <&> head >>= TH.stringE
