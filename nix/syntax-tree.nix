7a1b05af93b6c6ea8233b2f218b032f2e907f152
Yair Chuchem <yairchu@gmail.com>
2018-12-04 14:38:19 +0200

    update dep, use constraints package and not constraint
    hopefully this fixes nix build / CI

--- nix/syntax-tree.nix
+++ nix/syntax-tree.nix
@@ -1,5 +1,5 @@
 { mkDerivation, base, base-compat, base16-bytestring, binary
-, bytestring, constraint, containers, deepseq, fetchFromGitHub, hashable
+, bytestring, constraints, containers, deepseq, fetchFromGitHub, hashable
 , lens, pretty, QuickCheck, transformers, stdenv
 }:
 mkDerivation {
@@ -8,11 +8,11 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "syntax-tree";
-    sha256 = "0f2acyif0583iaxqv9v2vvkf8ygwbmwsxw8qy7vq2xj0c5gw3yhs";
-    rev = "8501c027b77f87d9c021ae9c4f08e571976510e8";
+    sha256 = "0qzkrxxl43sf4s76i1syrnjprmqlm9s519402vds033zcl6rqvcr";
+    rev = "87a407715d682a68e1f3326aa3bbfd608735db55";
   };
   libraryHaskellDepends = [
-    base base-compat base16-bytestring binary bytestring constraint
+    base base-compat base16-bytestring binary bytestring constraints
     containers deepseq hashable lens pretty QuickCheck transformers
   ];
   homepage = "https://github.com/lamdu/syntax-tree.git#readme";
