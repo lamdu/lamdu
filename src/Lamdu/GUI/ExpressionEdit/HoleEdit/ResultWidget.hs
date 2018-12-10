lamdu/lamdu

commit 137de64d363cc2248d3f60dd2e512444c62bc00b

replace some uses of SugarLens.binderPayloads

src/Lamdu/GUI/ExpressionEdit/HoleEdit/ResultWidget.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Lamdu.GUI.ExpressionEdit.HoleEdit.ResultWidget
     ( make
     ) where
 
-import           AST (Node, Ann(..))
+import           AST (Node, Ann(..), Children(..), ChildrenWithConstraint)
+import           Control.Lens (Traversal')
 import qualified Control.Lens.Extended as Lens
+import           Data.Constraint (Dict, withDict)
+import           Data.Proxy (Proxy(..))
 import           GUI.Momentu (Widget, WithTextPos(..), TextWidget)
 import qualified GUI.Momentu.Align as Align
 import qualified GUI.Momentu.Element as Element
@@ -108,9 +112,8 @@ make ctx resultId pick holeResultConverted =
             & WidgetIds.fromEntityId
         mFirstHoleInside =
             holeResultConverted ^?
-            SugarLens.binderPayloads .
-            Lens.filteredByIndex (SugarLens._OfExpr . SugarLens.bodyUnfinished) .
-            Sugar.plEntityId <&> WidgetIds.fromEntityId
+            unfinishedPayloads . Sugar.plEntityId
+            <&> WidgetIds.fromEntityId
         pickResult =
             case mFirstHoleInside of
             Nothing ->
@@ -123,3 +126,15 @@ make ctx resultId pick holeResultConverted =
                 { Menu._pickDest = innerEntryPoint
                 , Menu._pickMNextEntry = Just innerEntryPoint
                 }
+
+unfinishedPayloads ::
+    forall t a.
+    SugarLens.SugarExpr t =>
+    Traversal' (Node (Ann a) t) a
+unfinishedPayloads f (Ann a x)
+    | SugarLens.isUnfinished x = f a <&> (`Ann` x)
+    | otherwise =
+        withDict
+        (SugarLens.sugarExprRecursive :: Dict (ChildrenWithConstraint t SugarLens.SugarExpr))
+        (children (Proxy :: Proxy SugarLens.SugarExpr) (unfinishedPayloads f) x)
+        <&> Ann a

Lamdu.cabal
@@ -108,6 +108,7 @@ Library
                   , binary >= 0.7
                   , bytestring
                   , containers >= 0.4
+                  , constraints
                   , data-default >= 0.5
                   , deepseq
                   , deepseq-generics >= 0.1

nix/lamdu.nix
@@ -1,6 +1,6 @@
 { mkDerivation, aeson, aeson-diff, aeson-pretty, AlgoW, base
 , base-compat, base-orphans, base16-bytestring, binary, bytestring
-, Cabal, containers, cryptohash-sha256, data-default, deepseq
+, Cabal, constraints, containers, cryptohash-sha256, data-default, deepseq
 , deepseq-generics, directory, edit-distance, ekg-core
 , executable-path, filepath, generic-random, GLFW-b
 , graphics-drawingcombinators, hashable, HUnit, lamdu-calculus
@@ -41,7 +41,7 @@ mkDerivation {
   enableSeparateDataOutput = true;
   libraryHaskellDepends = [
     aeson aeson-pretty AlgoW base base-compat base-orphans
-    base16-bytestring binary bytestring containers cryptohash-sha256
+    base16-bytestring binary bytestring constraints containers cryptohash-sha256
     data-default deepseq deepseq-generics directory edit-distance
     ekg-core executable-path filepath GLFW-b
     graphics-drawingcombinators hashable lamdu-calculus

nix/syntax-tree.nix
@@ -8,8 +8,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "syntax-tree";
-    sha256 = "1nn0imqfajh6z692cj7gcaax37jm7pr2a5alvv2p0j1lwdzr0k2f";
-    rev = "b0a633c39dc2c0b5e6fa2c1ec5ce75276acb031f";
+    sha256 = "13vwgn4xwh4jb2z10q2fgd7a5dmgzamzpr7fh8c7frkpkz1b2a7m";
+    rev = "5a2027192833dce3979f552cede25b238e865673";
   };
   libraryHaskellDepends = [
     base base-compat binary constraints containers deepseq lens

src/Lamdu/Sugar/Convert/Binder.hs
@@ -5,9 +5,11 @@ module Lamdu.Sugar.Convert.Binder
     ) where
 
 import           AST (Node, Children(..), overChildren, monoChildren)
+import           AST.Class.Recursive (foldMapRecursive)
 import           AST.Functor.Ann (Ann(..), ann, val, annotations)
 import qualified Control.Lens.Extended as Lens
 import qualified Data.Map as Map
+import           Data.Monoid (Any(..))
 import           Data.Property (MkProperty')
 import qualified Data.Property as Property
 import           Data.Proxy (Proxy(..))
@@ -242,17 +244,10 @@ useNormalLambda :: Set InternalName -> Function InternalName i o (Ann a) -> Bool
 useNormalLambda paramNames func
     | Set.size paramNames < 2 = True
     | otherwise =
-        any (func &)
-        [ Lens.has (fBody . val . _BinderLet)
-        , Lens.has (fBody . SugarLens.binderPayloads . Lens.filteredByIndex
-            (SugarLens._OfExpr . forbiddenLightLamSubExprs))
-        , not . allParamsUsed paramNames
-        ]
-    where
-        forbiddenLightLamSubExprs :: Lens.Traversal' (Body name i o (Ann a)) ()
-        forbiddenLightLamSubExprs =
-            Lens.failing SugarLens.bodyUnfinished
-            (_BodyLam . lamFunc . fParams . _Params . Lens.united)
+        (foldMapRecursive (Proxy :: Proxy SugarLens.SugarExpr)
+            (Any . SugarLens.isForbiddenInLightLam) (func ^. fBody . val)
+            ^. Lens._Wrapped)
+        || not (allParamsUsed paramNames func)
 
 allParamsUsed :: Set InternalName -> Function InternalName i o (Ann a) -> Bool
 allParamsUsed paramNames func =

src/Lamdu/Sugar/Lens.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes, TemplateHaskell, ScopedTypeVariables, FlexibleInstances, KindSignatures #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes, TemplateHaskell, ScopedTypeVariables, FlexibleInstances, KindSignatures, DefaultSignatures #-}
 module Lamdu.Sugar.Lens
     ( SugarExpr(..)
     , PayloadOf(..), _OfExpr
@@ -18,9 +18,10 @@ module Lamdu.Sugar.Lens
     ) where
 
 import           AST (Node, LeafNode, Children(..), ChildrenWithConstraint, monoChildren)
-import           AST.Class.Recursive (ChildrenRecursive, hoistBody)
+import           AST.Class.Recursive (Recursive(..), ChildrenRecursive, hoistBody)
 import           AST.Functor.Ann (Ann(..), ann, val)
 import qualified Control.Lens as Lens
+import           Data.Constraint
 import           Data.Functor.Const (Const(..))
 import           Data.Proxy (Proxy(..))
 import           Lamdu.Sugar.Types
@@ -36,9 +37,20 @@ class SugarExpr (t :: (* -> *) -> *) where
     isUnfinished :: t f -> Bool
     isUnfinished _ = False
 
+    isForbiddenInLightLam :: t f -> Bool
+    isForbiddenInLightLam = isUnfinished
+
+    sugarExprRecursive :: Dict (ChildrenWithConstraint t SugarExpr)
+    default sugarExprRecursive ::
+        ChildrenWithConstraint t SugarExpr =>
+        Dict (ChildrenWithConstraint t SugarExpr)
+    sugarExprRecursive = Dict
+
+instance Recursive SugarExpr where
+    recursive _ _ = Sub sugarExprRecursive
+
 instance SugarExpr (Const (NullaryVal name i o))
 instance SugarExpr (Const (GetVar name o))
-
 instance SugarExpr (Else name i o)
 
 instance SugarExpr (Const (BinderVarRef name o)) where
@@ -48,14 +60,19 @@ instance SugarExpr (AssignmentBody name i o) where
     isUnfinished (BodyPlain x) = isUnfinished (x ^. apBody)
     isUnfinished BodyFunction{} = False
 
+instance SugarExpr (Function name i o) where
+    isForbiddenInLightLam = Lens.has (fParams . _Params)
+
 instance SugarExpr (Binder name i o) where
     isUnfinished (BinderExpr x) = isUnfinished x
     isUnfinished BinderLet{} = False
+    isForbiddenInLightLam BinderLet{} = True
+    isForbiddenInLightLam (BinderExpr x) = isForbiddenInLightLam x
 
 instance SugarExpr (Body name i o) where
     isUnfinished BodyHole{} = True
     isUnfinished BodyFragment{} = True
-    isUnfinished (BodyGetVar (GetBinder x)) = Lens.has binderVarRefUnfinished x
+    isUnfinished (BodyGetVar (GetBinder x)) = isUnfinished (Const x)
     isUnfinished _ = False
 
 binderVarRefUnfinished :: Lens.Traversal' (BinderVarRef name m) ()

stack.yaml
@@ -2,7 +2,7 @@ packages:
 - '.'
 - location:
     git: https://github.com/lamdu/syntax-tree
-    commit: b0a633c39dc2c0b5e6fa2c1ec5ce75276acb031f
+    commit: 5a2027192833dce3979f552cede25b238e865673
   extra-dep: True
 - location:
     git: https://github.com/lamdu/lamdu-calculus

commit b405631a1a7fdaf53d221b2513798c5e08d1a21d

oops fix hole results broken in 137de64d363cc2
when typing fragments with holes inside cursor didn't go to the hole

src/Lamdu/GUI/ExpressionEdit/HoleEdit/ResultWidget.hs
@@ -131,10 +131,13 @@ unfinishedPayloads ::
     forall name t a.
     SugarLens.SugarExpr name t =>
     Traversal' (Node (Ann a) t) a
-unfinishedPayloads f (Ann a x)
-    | SugarLens.isUnfinished x = f a <&> (`Ann` x)
-    | otherwise =
-        withDict
-        (SugarLens.sugarExprRecursive :: Dict (ChildrenWithConstraint t (SugarLens.SugarExpr name)))
-        (children (Proxy :: Proxy (SugarLens.SugarExpr name)) (unfinishedPayloads f) x)
-        <&> Ann a
+unfinishedPayloads f (Ann a x) =
+    flip Ann
+    <$>
+    withDict
+    (SugarLens.sugarExprRecursive :: Dict (ChildrenWithConstraint t (SugarLens.SugarExpr name)))
+    (children (Proxy :: Proxy (SugarLens.SugarExpr name)) (unfinishedPayloads f) x)
+    <*>
+    ( if SugarLens.isUnfinished x
+        then f a else pure a
+    )