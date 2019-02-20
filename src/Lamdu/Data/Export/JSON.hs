lamdu/lamdu

commit e8969c60d039d5a424c8ad26eba81d459a997713

Schema change: Opaque nominals implied by lack of declaration

src/Lamdu/Data/Export/JSON.hs
@@ -111,7 +111,7 @@ exportTag tag =
 exportNominal :: Monad m => T.NominalId -> Export m ()
 exportNominal nomId =
     do
-        nominal <- trans (Load.nominal nomId)
+        nominal <- trans (Load.nominal nomId) <&> fromMaybe (error "opaque nominal exported")
         tag <- readAssocTag nomId & trans
         Codec.EntityNominal tag nomId nominal & tell
         & withVisited visitedNominals nomId
@@ -261,7 +261,7 @@ importOne (Codec.EntitySchemaVersion _) =
 
 importEntities :: [Codec.Entity] -> T ViewM ()
 importEntities (Codec.EntitySchemaVersion ver : entities) =
-    if ver == 6
+    if ver == 7
     then traverse_ importOne entities
     else "Unsupported schema version: " ++ show ver & fail
 importEntities _ = "Missing schema version"  & fail

src/Lamdu/Data/Export/JSON/Codec.hs
@@ -33,7 +33,7 @@ import           Lamdu.Calc.Type.Constraints (Constraints(..))
 import qualified Lamdu.Calc.Type.Constraints as Constraints
 import           Lamdu.Calc.Type.FlatComposite (FlatComposite(..))
 import qualified Lamdu.Calc.Type.FlatComposite as FlatComposite
-import           Lamdu.Calc.Type.Nominal (Nominal(..), NominalType(..))
+import           Lamdu.Calc.Type.Nominal (Nominal(..))
 import           Lamdu.Calc.Type.Scheme (Scheme(..))
 import           Lamdu.Calc.Type.Vars (TypeVars(..))
 import qualified Lamdu.Data.Anchors as Anchors
@@ -500,17 +500,9 @@ decodeRepl =
 insertField :: Aeson.ToJSON a => Text -> a -> Aeson.Object -> Aeson.Object
 insertField k v = HashMap.insert k (Aeson.toJSON v)
 
-encodeNominalType :: Encoder NominalType
-encodeNominalType (NominalType scheme) = encodeScheme scheme
-encodeNominalType OpaqueNominal = "OpaqueNominal"
-
-decodeNominalType :: Decoder NominalType
-decodeNominalType (Aeson.String "OpaqueNominal") = pure OpaqueNominal
-decodeNominalType json = decodeScheme json <&> NominalType
-
 encodeNominal :: Nominal -> Aeson.Object
 encodeNominal (Nominal paramsMap nominalType) =
-    "nomType" .= encodeNominalType nominalType :
+    "nomType" .= encodeScheme nominalType :
     encodeSquash Map.null "typeParams"
     (encodeIdentMap T.typeParamId (encodeIdent . T.tvName)) paramsMap
     & HashMap.fromList
@@ -519,7 +511,7 @@ decodeNominal :: ExhaustiveDecoder Nominal
 decodeNominal obj =
     Nominal
     <$> decodeSquashed "typeParams" (decodeIdentMap T.ParamId (fmap T.Var . decodeIdent)) obj
-    <*> (obj .: "nomType" >>= lift . decodeNominalType)
+    <*> (obj .: "nomType" >>= lift . decodeScheme)
 
 encodeTagged :: Text -> (a -> Aeson.Object) -> ((T.Tag, Identifier), a) -> Aeson.Object
 encodeTagged idAttrName encoder ((tag, ident), x) =

src/Lamdu/Data/Export/JSON/Migration/ToVersion7.hs
@@ -1,0 +1,22 @@
+module Lamdu.Data.Export.JSON.Migration.ToVersion7 (migrate) where
+
+import qualified Control.Lens as Lens
+import qualified Data.Aeson as Aeson
+import           Data.Aeson.Lens (_Object, _String)
+import qualified Data.Vector as Vector
+import           Lamdu.Data.Export.JSON.Migration.Common (migrateToVer)
+
+import           Lamdu.Prelude
+
+migrateVal :: Aeson.Value -> Either Text (Maybe Aeson.Value)
+migrateVal val =
+    case val ^? _Object . Lens.ix "nomType" . _String of
+    Just x
+        | x == "OpaqueNominal" -> Right Nothing
+        | otherwise -> Left ("Unexpected nomType: " <> x)
+    _ -> Right (Just val)
+
+migrate :: Aeson.Value -> Either Text Aeson.Value
+migrate =
+    migrateToVer 7
+    (traverse migrateVal <&> Lens._Right %~ Vector.fromList . (^.. Lens.folded . Lens.folded))

Lamdu.cabal
@@ -257,6 +257,7 @@ Library
                   , Lamdu.Data.Export.JSON.Migration.ToVersion4
                   , Lamdu.Data.Export.JSON.Migration.ToVersion5
                   , Lamdu.Data.Export.JSON.Migration.ToVersion6
+                  , Lamdu.Data.Export.JSON.Migration.ToVersion7
                   , Lamdu.Data.Meta
                   , Lamdu.Data.Ops
                   , Lamdu.Data.Ops.Subexprs

nix/AlgoW.nix
@@ -9,8 +9,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "Algorithm-W-Step-By-Step";
-    sha256 = "0i2nr50kcvaa0y2acp834drys2r8lakaysqc66a2giyl88zgqjx4";
-    rev = "9c667d032b550c524404f7b8cde2535705736151";
+    sha256 = "1b6f9wg7wx4x27rhmcp2a6ba62r76vwrsq6mfchbdxna2ayi9ajc";
+    rev = "b6c5b6755cf13d092905a620f4c8b3cedec33bd9";
   };
   libraryHaskellDepends = [
     base base-compat binary bytestring containers deepseq

nix/lamdu-calculus.nix
@@ -8,8 +8,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "lamdu-calculus";
-    sha256 = "1cxy5xmhsn05l7ar85phwgzgmf085frcnflvazhmpm4isk31mw8i";
-    rev = "c40ab92fd00f3a8c31f98de308b0409fc8c036f0";
+    sha256 = "0yhrbf5isgj3giy7hrj9hfmk961n81afv5cnc3q09xiw7wz9k7aj";
+    rev = "73078616010a6763aeb4441dc891812c1de3ad57";
   };
   libraryHaskellDepends = [
     base base-compat base16-bytestring binary bytestring containers

src/Lamdu/Data/Db/Layout.hs
@@ -82,4 +82,4 @@ dbSchemaVersion :: IRef DbM Int
 dbSchemaVersion = IRef.anchor "dbSchemaVersion"
 
 curDbSchemaVersion :: Int
-curDbSchemaVersion = 4
+curDbSchemaVersion = 7

src/Lamdu/Data/Export/JSON/Migration.hs
@@ -13,6 +13,7 @@ import qualified Lamdu.Data.Export.JSON.Migration.ToVersion3 as ToVersion3
 import qualified Lamdu.Data.Export.JSON.Migration.ToVersion4 as ToVersion4
 import qualified Lamdu.Data.Export.JSON.Migration.ToVersion5 as ToVersion5
 import qualified Lamdu.Data.Export.JSON.Migration.ToVersion6 as ToVersion6
+import qualified Lamdu.Data.Export.JSON.Migration.ToVersion7 as ToVersion7
 
 import           Lamdu.Prelude
 
@@ -36,6 +37,7 @@ versionMigrations =
     , ToVersion4.migrate
     , ToVersion5.migrate
     , ToVersion6.migrate
+    , ToVersion7.migrate
     ]
 
 currentVersion :: Int

commit 22eef7a8bf469c5be167c7fbbd300fc4c7dcf789

Oops schema change in e8969c60d039d5 caused opaque nominals to be nameless

src/Lamdu/Data/Export/JSON.hs
@@ -111,7 +111,7 @@ exportTag tag =
 exportNominal :: Monad m => T.NominalId -> Export m ()
 exportNominal nomId =
     do
-        nominal <- trans (Load.nominal nomId) <&> fromMaybe (error "opaque nominal exported")
+        nominal <- trans (Load.nominal nomId)
         tag <- readAssocTag nomId & trans
         Codec.EntityNominal tag nomId nominal & tell
         & withVisited visitedNominals nomId
@@ -243,11 +243,11 @@ importLamVar paramList tag lamUUID var =
     where
         lamI = IRef.unsafeFromUUID lamUUID & ToKnot
 
-importNominal :: T.Tag -> T.NominalId -> Nominal -> T ViewM ()
+importNominal :: T.Tag -> T.NominalId -> Maybe Nominal -> T ViewM ()
 importNominal tag nomId nominal =
     do
         Property.setP (Anchors.assocTag nomId) tag
-        Transaction.writeIRef (ExprIRef.nominalI nomId) nominal
+        traverse_ (Transaction.writeIRef (ExprIRef.nominalI nomId)) nominal
         nomId `insertTo` DbLayout.tids
 
 importOne :: Codec.Entity -> T ViewM ()

src/Lamdu/Data/Export/JSON/Codec.hs
@@ -56,7 +56,7 @@ data Entity
     | EntityRepl (Definition.Expr (Val UUID))
     | EntityDef (Definition (Val UUID) (Meta.PresentationMode, T.Tag, V.Var))
     | EntityTag TagOrder (Maybe Text) T.Tag
-    | EntityNominal T.Tag T.NominalId Nominal
+    | EntityNominal T.Tag T.NominalId (Maybe Nominal)
     | EntityLamVar (Maybe Meta.ParamList) T.Tag UUID V.Var
 Lens.makePrisms ''Entity
 
@@ -519,7 +519,6 @@ encodeTagged idAttrName encoder ((tag, ident), x) =
     & insertField idAttrName (encodeIdent ident)
     & insertField "tag" (encodeTagId tag)
 
-
 decodeTagged :: Text -> ExhaustiveDecoder a -> ExhaustiveDecoder ((T.Tag, Identifier), a)
 decodeTagged idAttrName decoder obj =
     (,)
@@ -609,14 +608,23 @@ decodeTaggedLamVar json =
     <&> \((tag, ident), (lamI, mParamList)) ->
     (mParamList, tag, lamI, V.Var ident)
 
-encodeTaggedNominal :: Encoder ((T.Tag, T.NominalId), Nominal)
-encodeTaggedNominal ((tag, T.NominalId nomId), nom) =
-    encodeTagged "nom" encodeNominal ((tag, nomId), nom) & Aeson.Object
+encodeTaggedNominal :: Encoder ((T.Tag, T.NominalId), Maybe Nominal)
+encodeTaggedNominal ((tag, T.NominalId nomId), mNom) =
+    maybe mempty encodeNominal mNom
+    & insertField "nom" (encodeIdent nomId)
+    & insertField "tag" (encodeTagId tag)
+    & Aeson.Object
 
-decodeTaggedNominal :: Decoder ((T.Tag, T.NominalId), Nominal)
+decodeTaggedNominal :: Decoder ((T.Tag, T.NominalId), Maybe Nominal)
 decodeTaggedNominal json =
-    withObject "nom" (decodeTagged "nom" decodeNominal) json
+    withObject "nom" (decodeTagged "nom" decodeMNom) json
     <&> _1 . _2 %~ T.NominalId
+    where
+        decodeMNom x =
+            jsum'
+            [ decodeNominal x <&> Just
+            , pure Nothing
+            ]
 
 encodeSchemaVersion :: Encoder Int
 encodeSchemaVersion ver =

src/Lamdu/Data/Export/JSON/Migration/ToVersion7.hs
@@ -3,20 +3,20 @@ module Lamdu.Data.Export.JSON.Migration.ToVersion7 (migrate) where
 import qualified Control.Lens as Lens
 import qualified Data.Aeson as Aeson
 import           Data.Aeson.Lens (_Object, _String)
-import qualified Data.Vector as Vector
 import           Lamdu.Data.Export.JSON.Migration.Common (migrateToVer)
 
 import           Lamdu.Prelude
 
-migrateVal :: Aeson.Value -> Either Text (Maybe Aeson.Value)
+migrateVal :: Aeson.Value -> Either Text (Aeson.Value)
 migrateVal val =
     case val ^? _Object . Lens.ix "nomType" . _String of
+    Nothing -> Right val
     Just x
-        | x == "OpaqueNominal" -> Right Nothing
+        | x == "OpaqueNominal" ->
+            val & _Object %~
+            ((Lens.at "nomType" .~ Nothing) . (Lens.at "typeParams" .~ Nothing))
+            & Right
         | otherwise -> Left ("Unexpected nomType: " <> x)
-    _ -> Right (Just val)
 
 migrate :: Aeson.Value -> Either Text Aeson.Value
-migrate =
-    migrateToVer 7
-    (traverse migrateVal <&> Lens._Right %~ Vector.fromList . (^.. Lens.folded . Lens.folded))
+migrate = migrateToVer 7 (traverse migrateVal)