lamdu/lamdu

commit 4c70f316a8e8123b9b7a55c96f498859057583d0

Replace last use of bodyChildPayloads

nix/syntax-tree.nix
@@ -8,8 +8,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "syntax-tree";
-    sha256 = "1r81pwjc0iy16hjfalsmxaghdxmiha3bjp1r475awbai19s5xzsk";
-    rev = "318cede067a076ba31836afd36a9b59791230682";
+    sha256 = "0f2acyif0583iaxqv9v2vvkf8ygwbmwsxw8qy7vq2xj0c5gw3yhs";
+    rev = "8501c027b77f87d9c021ae9c4f08e571976510e8";
   };
   libraryHaskellDepends = [
     base base-compat base16-bytestring binary bytestring constraint

src/Lamdu/Sugar/Annotations.hs
@@ -158,7 +158,9 @@ markBodyAnnotations oldBody =
             & BodyCase
         )
     where
-        newBodyWith f = newBody & SugarLens.bodyChildPayloads . nonHoleIndex . _1 .~ f
+        newBodyWith f =
+            newBody & overChildren (Proxy :: Proxy SugarLens.SugarExpr)
+            (Lens.filtered (SugarLens.isUnfinished . (^. val)) . ann . _1 .~ f)
         nonHoleIndex = Lens.ifiltered (const . Lens.nullOf (SugarLens._OfExpr . SugarLens.bodyUnfinished))
         set x = (x, newBody)
         newBody =

src/Lamdu/Sugar/Lens.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE FlexibleContexts, RankNTypes, TemplateHaskell, ScopedTypeVariables #-}
 module Lamdu.Sugar.Lens
-    ( PayloadOf(..), _OfExpr, _OfLabeledApplyFunc, _OfNullaryVal
+    ( SugarExpr(..)
+    , PayloadOf(..), _OfExpr
     , childPayloads
-    , bodyChildPayloads
     , binderPayloads
     , bodyUnfinished
     , defSchemes
@@ -31,6 +31,33 @@ childPayloads :: ChildrenWithConstraint expr Children =>
     Lens.Traversal' (expr (Ann a)) a
 childPayloads f =
     children (Proxy :: Proxy Children) (ann f)
+
+class SugarExpr t where
+    isUnfinished :: t (Ann a) -> Bool
+    isUnfinished _ = False
+
+instance SugarExpr (Const a)
+instance SugarExpr (Else name i o)
+
+instance SugarExpr (AssignmentBody name i o) where
+    isUnfinished (BodyPlain x) = isUnfinished (x ^. apBody)
+    isUnfinished BodyFunction{} = False
+
+instance SugarExpr (Binder name i o) where
+    isUnfinished (BinderExpr x) = isUnfinished x
+    isUnfinished BinderLet{} = False
+
+instance SugarExpr (Body name i o) where
+    isUnfinished BodyHole{} = True
+    isUnfinished BodyFragment{} = True
+    isUnfinished (BodyGetVar (GetBinder x)) = Lens.has binderVarRefUnfinished x
+    isUnfinished (BodyLabeledApply x) =
+        Lens.has (aFunc . val . Lens._Wrapped . binderVarRefUnfinished) x
+    isUnfinished _ = False
+
+binderVarRefUnfinished :: Lens.Traversal' (BinderVarRef name m) ()
+binderVarRefUnfinished =
+    bvForm . _GetDefinition . Lens.failing _DefDeleted (_DefTypeChanged . Lens.united)
 
 -- TODO: Get rid of most of these.
 -- First step is replacing their usages with `AST.children`
@@ -123,12 +150,6 @@ bodyChildren n l r e b f =
     BodyFragment     x -> fExpr f x <&> BodyFragment
     BodyToNom        x -> traverse b x <&> BodyToNom
 
-parentNodePayload ::
-    (expr (Ann a) -> p) ->
-    Lens.IndexedLens' p (Node (Ann a) expr) a
-parentNodePayload c f (Ann pl x) =
-    Lens.indexed f (c x) pl <&> (`Ann` x)
-
 stripAnnotations :: Recursive expr => expr (Ann a) -> expr (Ann ())
 stripAnnotations = hoistBody (ann .~ ())
 
@@ -143,25 +164,6 @@ elseIndex (SimpleElse x) = stripAnnotations x & OfExpr
 assignmentBodyIndex :: AssignmentBody name i o (Ann a) -> PayloadOf name i o
 assignmentBodyIndex (BodyFunction x) = stripAnnotations x & OfAssignFunction
 assignmentBodyIndex (BodyPlain x) = binderIndex (x ^. apBody)
-
-bodyChildPayloads ::
-    forall name i o a.
-    Lens.IndexedTraversal' (PayloadOf name i o) (Body name i o (Ann a)) a
-bodyChildPayloads f =
-    bodyChildren
-    (leafNodePayload OfNullaryVal f)
-    (Lens.cloneIndexedLens labeledFuncPayloads f)
-    (Lens.cloneIndexedLens relayedPayloads f)
-    (parentNodePayload elseIndex f)
-    (parentNodePayload binderIndex f)
-    (parentNodePayload (OfExpr . stripAnnotations) f)
-    where
-        labeledFuncPayloads ::
-            Lens.AnIndexedLens' (PayloadOf name i o) (LeafNode (Ann a) (BinderVarRef name o)) a
-        labeledFuncPayloads = leafNodePayload OfLabeledApplyFunc
-        relayedPayloads ::
-            Lens.AnIndexedLens' (PayloadOf name i o) (LeafNode (Ann a) (GetVar name o)) a
-        relayedPayloads = leafNodePayload OfRelayedArg
 
 leafNodePayload ::
     (l -> p) ->
@@ -278,10 +280,6 @@ bodyPayloads f =
             (LeafNode (Ann b) (GetVar name o))
             a b
         relayedPayloads = leafNodePayload OfRelayedArg
-
-binderVarRefUnfinished :: Lens.Traversal' (BinderVarRef name m) ()
-binderVarRefUnfinished =
-    bvForm . _GetDefinition . Lens.failing _DefDeleted (_DefTypeChanged . Lens.united)
 
 bodyUnfinished :: Lens.Traversal' (Body name i o (Ann a)) ()
 bodyUnfinished =

stack.yaml
@@ -2,7 +2,7 @@ packages:
 - '.'
 - location:
     git: https://github.com/lamdu/syntax-tree
-    commit: 318cede067a076ba31836afd36a9b59791230682
+    commit: 8501c027b77f87d9c021ae9c4f08e571976510e8
   extra-dep: True
 - location:
     git: https://github.com/lamdu/lamdu-calculus

commit 6ad2c6db6febabf770207959e5e1c3849e41333b

Add failing test
Since 2018.12.04 4c70f316a8e accidentally holes don't have always type annotations shown.
To-nom reproduces this, and navigating to the hole causes a crash.

test/Tests/Sugar.hs
@@ -38,6 +38,7 @@ test =
     , testCreateLetInLetVal
     , testFloatToRepl
     , floatLetWithGlobalRef
+    , testHoleTypeShown
     ]
 
 -- | Verify that a sugar action does not result in a crash
@@ -329,3 +330,11 @@ testCreateLetInLetVal =
             (WorkArea name i o a)
             (Tree (Let name i o) (Ann a))
         theLet = replBody . _BodyLam . lamFunc . fBody . val . _BinderLet
+
+testHoleTypeShown :: Test
+testHoleTypeShown =
+    testCase "hole-type-shown" $
+    do
+        workArea <- testProgram "to-nom.json" convertWorkArea
+        let x = workArea ^?! replBody . _BodyToNom . nVal
+        assertBool "Expected to have type" (Lens.has _AnnotationType (x ^. ann . plAnnotation))