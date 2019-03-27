lamdu/lamdu

commit 63e65e36174ad0bb20968eefd73043c7201ad2b7

lamdu-calculus changes - record and variants both use the same row types

src/Lamdu/Data/Export/JSON/Migration/ToVersion6.hs
@@ -1,0 +1,42 @@
+module Lamdu.Data.Export.JSON.Migration.ToVersion6 (migrate) where
+
+import qualified Control.Lens as Lens
+import qualified Data.Aeson as Aeson
+import           Lamdu.Data.Export.JSON.Migration.Common (version)
+
+import           Lamdu.Prelude
+
+migrateVal :: Aeson.Value -> Either Text Aeson.Value
+migrateVal (Aeson.Object obj) =
+    case (obj ^. Lens.at "recordTypeVars", obj ^. Lens.at "variantTypeVars") of
+    (Nothing, Nothing) -> pure obj
+    (Just (Aeson.Array x), Nothing) -> rowVars x
+    (Nothing, Just (Aeson.Array x)) -> rowVars x
+    (Just (Aeson.Array rs), Just (Aeson.Array vs)) -> rowVars (rs <> vs)
+    _ -> Left "Unexpected format of row vars"
+    >>= traverse migrateVal <&> Aeson.Object
+    where
+        rowVars x =
+            obj
+            & Lens.at "recordTypeVars" .~ Nothing
+            & Lens.at "variantTypeVars" .~ Nothing
+            & Lens.at "rowVars" ?~ Aeson.Array x
+            & pure
+migrateVal (Aeson.Array vals) = traverse migrateVal vals <&> Aeson.Array
+migrateVal (Aeson.String x) = Aeson.String x & pure
+migrateVal (Aeson.Number x) = Aeson.Number x & pure
+migrateVal (Aeson.Bool x) = Aeson.Bool x & pure
+migrateVal Aeson.Null = pure Aeson.Null
+
+migrate :: Aeson.Value -> Either Text Aeson.Value
+migrate (Aeson.Array vals) =
+    case vals ^? Lens._Cons of
+    Just (ver, rest)
+        | ver == version 5 ->
+            traverse migrateVal rest
+            <&> (,) (version 6)
+            <&> (Lens._Cons #)
+            <&> Aeson.Array
+        | otherwise -> Left "Migration from unexpected version"
+    _ -> Left "Array of at least 1 element required"
+migrate _ = Left "top-level should be array"

Lamdu.cabal
@@ -256,6 +256,7 @@ Library
                   , Lamdu.Data.Export.JSON.Migration.ToVersion3
                   , Lamdu.Data.Export.JSON.Migration.ToVersion4
                   , Lamdu.Data.Export.JSON.Migration.ToVersion5
+                  , Lamdu.Data.Export.JSON.Migration.ToVersion6
                   , Lamdu.Data.Meta
                   , Lamdu.Data.Ops
                   , Lamdu.Data.Ops.Subexprs

nix/AlgoW.nix
@@ -9,8 +9,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "Algorithm-W-Step-By-Step";
-    sha256 = "021nwj2zrsk83k994pffkgy94rysvfbz3vj3v4xplz7ig1vq3w1q";
-    rev = "019f40002ac949a1c7f571178c097fc199ecc4ee";
+    sha256 = "18b7zw1h94lxrr8r93q17ds2rpb3rg02cb1dzljhdrhkss5dk5j1";
+    rev = "0e9e4991ddf7b3a59878296281b66e1849c87a84";
   };
   libraryHaskellDepends = [
     base base-compat binary bytestring containers deepseq

nix/lamdu-calculus.nix
@@ -8,8 +8,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "lamdu-calculus";
-    sha256 = "09w3rsnavr78cfjgv3l5jw6nnvm7bakf66fd7hx774gj9ll58hai";
-    rev = "8513ec4e5c2a95be46451b5ac7f048f2bd9273b1";
+    sha256 = "1fwjhjaz6zm5qpnmpcn5drq5kj9bw0ijmp6r9dwas5rsh2sjk8v0";
+    rev = "6b25057d453146561ea5dd5e3f3bcca46c8d9e77";
   };
   libraryHaskellDepends = [
     base base-compat base16-bytestring binary bytestring containers

src/Lamdu/Calc/Term/Utils.hs
@@ -2,8 +2,8 @@
 
 module Lamdu.Calc.Term.Utils
     ( Composite(..), tags, rest
-    , case_, Case
-    , recExtend, Record
+    , case_
+    , recExtend
     , culledSubexprPayloads
     ) where
 
@@ -21,16 +21,13 @@ culledSubexprPayloads cut (Ann pl body)
     | cut pl = []
     | otherwise = pl : body ^. monoChildren . Lens.to (culledSubexprPayloads cut)
 
-data Composite p a = Composite
+data Composite a = Composite
     { _tags :: Map T.Tag a
     , _rest :: Maybe a
     } deriving (Functor, Foldable, Traversable)
 Lens.makeLenses ''Composite
 
-type Case = Composite T.VariantTag
-type Record = Composite T.RecordTag
-
-case_ :: V.Case (Val pl) -> Case (Val pl)
+case_ :: V.Case (Val pl) -> Composite (Val pl)
 case_ (V.Case tag handler r) =
     caseVal r
     & tags . Lens.at tag ?~ handler
@@ -41,7 +38,7 @@ case_ (V.Case tag handler r) =
             V.BCase x -> case_ x
             _ -> Composite mempty (Just v)
 
-recExtend :: V.RecExtend (Val pl) -> Record (Val pl)
+recExtend :: V.RecExtend (Val pl) -> Composite (Val pl)
 recExtend (V.RecExtend tag field r) =
     recExtendVal r
     & tags . Lens.at tag ?~ field

src/Lamdu/Data/Db/Layout.hs
@@ -82,4 +82,4 @@ dbSchemaVersion :: IRef DbM Int
 dbSchemaVersion = IRef.anchor "dbSchemaVersion"
 
 curDbSchemaVersion :: Int
-curDbSchemaVersion = 3
+curDbSchemaVersion = 4

src/Lamdu/Data/Export/JSON.hs
@@ -261,7 +261,7 @@ importOne (Codec.EntitySchemaVersion _) =
 
 importEntities :: [Codec.Entity] -> T ViewM ()
 importEntities (Codec.EntitySchemaVersion ver : entities) =
-    if ver == 5
+    if ver == 6
     then traverse_ importOne entities
     else "Unsupported schema version: " ++ show ver & fail
 importEntities _ = "Missing schema version"  & fail

src/Lamdu/Data/Export/JSON/Codec.hs
@@ -25,7 +25,7 @@ import qualified Data.Vector as Vector
 import           Lamdu.Calc.Identifier (Identifier, identHex, identFromHex)
 import           Lamdu.Calc.Term (Val)
 import qualified Lamdu.Calc.Term as V
-import           Lamdu.Calc.Type (Type, Composite)
+import           Lamdu.Calc.Type (Type, Row)
 import qualified Lamdu.Calc.Type as T
 import           Lamdu.Calc.Type.Constraints (Constraints(..))
 import qualified Lamdu.Calc.Type.Constraints as Constraints
@@ -212,7 +212,7 @@ decodeTagId :: Decoder T.Tag
 decodeTagId Aeson.Null = pure Anchors.anonTag
 decodeTagId json = decodeIdent json <&> T.Tag
 
-encodeFlatComposite :: Encoder (FlatComposite p)
+encodeFlatComposite :: Encoder FlatComposite
 encodeFlatComposite = \case
     FlatComposite fields Nothing -> encodedFields fields
     FlatComposite fields (Just (T.Var name)) ->
@@ -220,7 +220,7 @@ encodeFlatComposite = \case
     where
         encodedFields = encodeIdentMap T.tagName encodeType
 
-decodeFlatComposite :: Decoder (FlatComposite p)
+decodeFlatComposite :: Decoder FlatComposite
 decodeFlatComposite json =
     jsum
     [ do
@@ -236,10 +236,10 @@ decodeFlatComposite json =
     where
         decodeFields = decodeIdentMap T.Tag decodeType
 
-encodeComposite :: Encoder (Composite p)
+encodeComposite :: Encoder Row
 encodeComposite = encodeFlatComposite . FlatComposite.fromComposite
 
-decodeComposite :: Decoder (Composite p)
+decodeComposite :: Decoder Row
 decodeComposite = fmap FlatComposite.toComposite . decodeFlatComposite
 
 encodeType :: Encoder Type
@@ -268,30 +268,28 @@ decodeType json =
           T.TInst nomId params & pure
     ]
 
-encodeCompositeVarConstraints :: Constraints.CompositeVar t -> [Encoded]
+encodeCompositeVarConstraints :: Constraints.CompositeVar -> [Encoded]
 encodeCompositeVarConstraints (Constraints.CompositeVar forbidden) =
     Set.toList forbidden
     <&> T.tagName
     <&> encodeIdent
 
 decodeCompositeConstraints ::
-    [Encoded] -> AesonTypes.Parser (Constraints.CompositeVar t)
+    [Encoded] -> AesonTypes.Parser Constraints.CompositeVar
 decodeCompositeConstraints json =
     traverse decodeIdent json <&> map T.Tag <&> Set.fromList
     <&> Constraints.CompositeVar
 
 encodeTypeVars :: Encoder (TypeVars, Constraints)
-encodeTypeVars (TypeVars tvs rtvs stvs, Constraints recordConstraints variantConstraints) =
+encodeTypeVars (TypeVars tvs rvs, cs) =
     concat
     [ encodeTVs "typeVars" tvs
-    , encodeTVs "recordTypeVars" rtvs
-    , encodeTVs "variantTypeVars" stvs
+    , encodeTVs "rowVars" rvs
     , encodeSquash null "constraints" Aeson.object
-      (encodeConstraints "recordTypeVars" recordConstraints ++
-       encodeConstraints "variantTypeVars" variantConstraints)
+      (encodeConstraints "rowVars" cs)
     ] & Aeson.object
     where
-        encodeConstraints name (Constraints.CompositeVars constraints) =
+        encodeConstraints name (Constraints constraints) =
             encodeSquash Map.null name
             (encodeIdentMap T.tvName encodeCompositeVarConstraints)
             constraints
@@ -307,13 +305,11 @@ decodeTypeVars =
         tvs <-
             TypeVars
             <$> getTVs "typeVars"
-            <*> getTVs "recordTypeVars"
-            <*> getTVs "variantTypeVars"
+            <*> getTVs "rowVars"
         decodedConstraints <-
             decodeSquashed "constraints"
             ( withObject "constraints" $ \constraints ->
-              let  getCs name = decodeConstraints name constraints <&> Constraints.CompositeVars
-              in   Constraints <$> getCs "recordTypeVars" <*> getCs "variantTypeVars"
+              decodeConstraints "rowVars" constraints <&> Constraints
             ) obj
         pure (tvs, decodedConstraints)
     where

commit 7e096a14e8bc8fadb29b937b72ba04fde5269f59

Oops fix data migration of row variable constraints
Broken since 63e65e36174a 2019.01.08

src/Lamdu/Data/Export/JSON/Migration/ToVersion6.hs
@@ -10,9 +10,10 @@ migrateVal :: Aeson.Value -> Either Text Aeson.Value
 migrateVal (Aeson.Object obj) =
     case (obj ^. Lens.at "recordTypeVars", obj ^. Lens.at "variantTypeVars") of
     (Nothing, Nothing) -> pure obj
-    (Just (Aeson.Array x), Nothing) -> rowVars x
-    (Nothing, Just (Aeson.Array x)) -> rowVars x
-    (Just (Aeson.Array rs), Just (Aeson.Array vs)) -> rowVars (rs <> vs)
+    (Just x, Nothing) -> rowVars x
+    (Nothing, Just x) -> rowVars x
+    (Just (Aeson.Array rs), Just (Aeson.Array vs)) -> rowVars (Aeson.Array (rs <> vs))
+    (Just (Aeson.Object rs), Just (Aeson.Object vs)) -> rowVars (Aeson.Object (rs <> vs))
     _ -> Left "Unexpected format of row vars"
     >>= traverse migrateVal <&> Aeson.Object
     where
@@ -20,7 +21,7 @@ migrateVal (Aeson.Object obj) =
             obj
             & Lens.at "recordTypeVars" .~ Nothing
             & Lens.at "variantTypeVars" .~ Nothing
-            & Lens.at "rowVars" ?~ Aeson.Array x
+            & Lens.at "rowVars" ?~ x
             & pure
 migrateVal (Aeson.Array vals) = traverse migrateVal vals <&> Aeson.Array
 migrateVal x = pure x