7472dc4239e4732f38f0dd0350c2ea8c1e837e6c
Yair Chuchem <yairchu@gmail.com>
2019-04-25 17:50:09 +0300

    Revert "Fix black screen on open on macOS Mojave"
    This reverts commit a55f8ee94b5d1f80ed0d8f25e309a4ec4da4f2ed.
    
    https://trello.com/c/he98LZE9/462-ui-used-to-be-smoother
    
    Looks like the work-around is not needed anymore.
    So problem was probably solved either in GLFW-b or macOS?
    
    The work-around caused animation to be really slow in Yair's system (with 5K monitor attached),
    and without it animation is more smooth.

--- src/GUI/Momentu/Render.hs
+++ src/GUI/Momentu/Render.hs
@@ -11,7 +11,6 @@ import           Graphics.Rendering.OpenGL.GL (($=))
 import qualified Graphics.Rendering.OpenGL.GL as GL
 import qualified Graphics.UI.GLFW as GLFW
 import qualified Graphics.UI.GLFW.Utils as GLFW.Utils
-import qualified System.Info as SysInfo
 import           System.TimeIt (timeItT)
 
 import           Lamdu.Prelude
@@ -31,24 +30,9 @@ render win image =
         GL.matrixMode $= GL.Projection
         GL.loadIdentity
         GL.ortho 0 sizeX sizeY 0 (-1) 1
-        (timedRender, ()) <-
-            do
-                Draw.clearRender image
-                platformWorkarounds
-            & timeItT
+        (timedRender, ()) <- timeItT (Draw.clearRender image)
         (timedSwapBuffers, ()) <- timeItT (GLFW.swapBuffers win)
         pure PerfCounters
             { renderTime = timedRender
             , swapBuffersTime = timedSwapBuffers
             }
-    where
-        platformWorkarounds
-            | SysInfo.os == "darwin" =
-                do
-                    -- Work around for https://github.com/glfw/glfw/issues/1334
-                    GL.colorMask GL.$= GL.Color4 GL.Disabled GL.Disabled GL.Disabled GL.Enabled
-                    GL.clearColor GL.$= GL.Color4 1 1 1 1
-                    GL.clear [GL.ColorBuffer]
-                    GL.colorMask GL.$= GL.Color4 GL.Enabled GL.Enabled GL.Enabled GL.Enabled
-            | otherwise = pure ()
-
