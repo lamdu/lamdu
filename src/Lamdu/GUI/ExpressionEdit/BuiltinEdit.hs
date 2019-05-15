02da2ef2a4b00c83e43c5f6cd7baeeb80c6a2bef
Yair Chuchem <yairchu@gmail.com>
2019-05-15 19:20:29 +0300

    BuiltinEdit: fine-grained imports

--- src/Lamdu/GUI/ExpressionEdit/BuiltinEdit.hs
+++ src/Lamdu/GUI/ExpressionEdit/BuiltinEdit.hs
@@ -12,6 +12,8 @@ import qualified GUI.Momentu.Draw as MDraw
 import qualified GUI.Momentu.Element as Element
 import qualified GUI.Momentu.EventMap as E
 import           GUI.Momentu.Glue ((/|/))
+import qualified GUI.Momentu.Glue as Glue
+import qualified GUI.Momentu.I18N as MomentuTexts
 import           GUI.Momentu.MetaKey (MetaKey(..), noMods)
 import qualified GUI.Momentu.MetaKey as MetaKey
 import qualified GUI.Momentu.State as GuiState
@@ -25,22 +27,22 @@ import           Lamdu.Config.Theme (Theme)
 import qualified Lamdu.Config.Theme as Theme
 import qualified Lamdu.Config.Theme.TextColors as TextColors
 import qualified Lamdu.Data.Definition as Definition
-import qualified Lamdu.I18N.CodeUI as CodeUI
-import qualified Lamdu.I18N.Language as Language
-import qualified Lamdu.I18N.Texts as Texts
+import qualified Lamdu.I18N.CodeUI as Texts
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
 
-builtinFDConfig :: Language.HasLanguage env => env -> FocusDelegator.Config
+builtinFDConfig ::
+    (Has (Texts.CodeUI Text) env, Has (MomentuTexts.Texts Text) env) =>
+    env -> FocusDelegator.Config
 builtinFDConfig env = FocusDelegator.Config
     { FocusDelegator.focusChildKeys = [MetaKey noMods MetaKey.Key'Enter]
-    , FocusDelegator.focusChildDoc = doc CodeUI.changeImportedName
+    , FocusDelegator.focusChildDoc = doc Texts.changeImportedName
     , FocusDelegator.focusParentKeys = [MetaKey noMods MetaKey.Key'Escape]
-    , FocusDelegator.focusParentDoc = doc CodeUI.doneChangingImportedName
+    , FocusDelegator.focusParentDoc = doc Texts.doneChangingImportedName
     }
     where
-        doc lens = E.toDoc env [has . Texts.edit, has . lens]
+        doc lens = E.toDoc env [has . MomentuTexts.edit, has . lens]
 
 builtinFFIPath :: Widget.Id -> Widget.Id
 builtinFFIPath = flip Widget.joinId ["FFIPath"]
@@ -50,7 +52,7 @@ builtinFFIName = flip Widget.joinId ["FFIName"]
 
 makeNamePartEditor ::
     ( Applicative f, MonadReader env m, GuiState.HasCursor env
-    , TextEdit.HasStyle env, Language.HasLanguage env
+    , TextEdit.HasStyle env, Has (Texts.CodeUI Text) env, TextEdit.HasTexts env
     ) =>
     MDraw.Color -> Text -> (Text -> f ()) -> Widget.Id ->
     m (TextWidget f)
@@ -72,8 +74,8 @@ makeNamePartEditor color namePartStr setter myId =
 
 make ::
     ( MonadReader env f, Monad o, Has Theme env, GuiState.HasCursor env
-    , TextEdit.HasStyle env, Element.HasAnimIdPrefix env
-    , Language.HasLanguage env
+    , TextEdit.HasStyle env, Element.HasAnimIdPrefix env, TextEdit.HasTexts env
+    , Has (Texts.CodeUI Text) env, Glue.HasTexts env
     ) =>
     Sugar.DefinitionBuiltin name o -> Widget.Id ->
     f (TextWidget o)
