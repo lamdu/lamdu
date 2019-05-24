ad6af485f4d1e3221cca9520f900746c9dd13999
Yair Chuchem <yairchu@gmail.com>
2019-05-24 23:06:58 +0300

    missing operator in char classification

--- src/Lamdu/CharClassification.hs
+++ src/Lamdu/CharClassification.hs
@@ -6,7 +6,7 @@ module Lamdu.CharClassification
 import Lamdu.Prelude
 
 operator :: String
-operator = "+-*/^=><&|%$:.!;?@~≥≤≠⋲"
+operator = "+-*/\\^=><&|%$:.!;?@~≥≤≠⋲"
 
 bracket :: String
 bracket = "()[]{}"
