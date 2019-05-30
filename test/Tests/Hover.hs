d3d02f727907d375677032defd24f23596eea92f
Yair Chuchem <yairchu@gmail.com>
2019-05-30 15:20:10 +0300

    add failing test for #453
    https://trello.com/c/3SAmsIoF/453-when-tag-choice-popup-appears-above-rather-than-below-due-to-lack-of-space-cannot-navigate-to-tag-itself-to-select-as-new-tag

--- test/Tests/Hover.hs
+++ test/Tests/Hover.hs
@@ -1,10 +1,14 @@
 module Tests.Hover where
 
+import qualified Control.Lens as Lens
 import           Data.Vector.Vector2 (Vector2(..))
 import qualified GUI.Momentu.Element as Element
+import qualified GUI.Momentu.EventMap as E
+import qualified GUI.Momentu.Main.Events as Events
 import qualified GUI.Momentu.Hover as H
+import qualified GUI.Momentu.ModKey as ModKey
 import qualified GUI.Momentu.Rect as R
-import           GUI.Momentu.State (Update)
+import           GUI.Momentu.State (Update, VirtualCursor(..))
 import qualified GUI.Momentu.Widget as W
 import           Test.Momentu.Env (env)
 
@@ -18,10 +22,13 @@ focusedSquare = W.setFocused square
 
 testHover :: Vector2 Double -> Vector2 Double -> W.Widget (Identity Update) -> IO ()
 testHover topLeft bottomRight widget =
-    (padded ^?! W.wState . W._StateFocused) (W.Surrounding 0 0 0 0)
-    ^. W.fFocalAreas
-    & traverse_ checkFocal
+    do
+        traverse_ checkFocal focalAreas
+        when (hoverIsAbove && not (hasKeyEvent ModKey.Key'Down))
+            (assertString "Navigation down from hover not working!")
     where
+        rendered = (padded ^?! W.wState . W._StateFocused) (W.Surrounding 0 0 0 0)
+        focalAreas = rendered ^. W.fFocalAreas
         checkFocal a
             | rectWithin a paddedRect = pure ()
             | otherwise =
@@ -31,6 +38,20 @@ testHover topLeft bottomRight widget =
         rectWithin inner outer =
             R.isWithin (inner ^. R.topLeft) outer &&
             R.isWithin (inner ^. R.bottomRight) outer
+        hoverIsAbove =
+            focalAreas ^? Lens.ix 0 . R.top >= focalAreas ^? Lens.ix 1 . R.bottom
+        eventMap =
+            (rendered ^. W.fEventMap)
+            (W.EventContext (VirtualCursor (R.Rect 0 1)) "")
+        lookupEvent e = E.lookup (Identity Nothing) e eventMap ^. Lens._Wrapped
+        hasKeyEvent = Lens.has Lens._Just . lookupEvent . E.EventKey . mkKeyEvent
+        mkKeyEvent k =
+            Events.KeyEvent
+            { Events.keKey = k
+            , Events.keScanCode = 0 -- dummy
+            , Events.keState = ModKey.KeyState'Pressed
+            , Events.keModKeys = mempty
+            }
 
 test :: Test
 test =
@@ -43,4 +64,7 @@ test =
             H.hoverInPlaceOf
             (H.hoverBesideOptions env (H.hover env focusedSquare) anchor)
             anchor
-        anchor = H.anchor env square
+        anchor =
+            square
+            & W.takesFocus (const (pure "blah"))
+            & H.anchor env
