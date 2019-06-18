8e6380ff7a2dfc085c7301a14c5ba8ec7c35a6dc
Yair Chuchem <yairchu@gmail.com>
2019-06-18 18:38:34 +0300

    oops fix nix build
    nix-prefetch-github gives out wrong hash when submodules are involved

--- nix/bindings-freetype-gl.nix
+++ nix/bindings-freetype-gl.nix
@@ -7,7 +7,7 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "bindings-freetype-gl";
-    sha256 = "0d4jxq3ini6c274lx4ys4djwj92qjra9c166l0wg45j2lgpm6gww";
+    sha256 = "1h7j89wlgxv5wjkd1bjw0ygwa43nb7zba9g4lmnmqaax5vvwvw5c";
     rev = "6efdced2d0867988a0a008145ae2c9a7bf69bb94";
     fetchSubmodules = true;
   };
