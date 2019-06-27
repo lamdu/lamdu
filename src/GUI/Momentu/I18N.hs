2f77ccf72683d6488dddf994062e6e4c40e9191b
Yair Chuchem <yairchu@gmail.com>
2019-06-27 12:54:38 +0300

    instance ElemIds GUI.Momentu.I18N

--- src/GUI/Momentu/I18N.hs
+++ src/GUI/Momentu/I18N.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, DerivingVia #-}
 
 module GUI.Momentu.I18N where
 
 import qualified Control.Lens as Lens
 import qualified Data.Aeson.TH.Extended as JsonTH
+import           GUI.Momentu.Animation.Id (ElemIds)
 
 import           Lamdu.Prelude
 
@@ -17,6 +18,8 @@ data Texts a = Texts
     , _choose :: a
     , _forward :: a
     , _backward :: a
-    } deriving Eq
+    } deriving stock (Eq, Generic, Generic1, Functor, Foldable, Traversable)
+    deriving Applicative via (Generically1 Texts)
+    deriving anyclass ElemIds
 Lens.makeLenses ''Texts
 JsonTH.derivePrefixed "_" ''Texts
