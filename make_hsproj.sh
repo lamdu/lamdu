4c67e558955a6480d1b8eb69861ed6c352df79c6
Yair Chuchem <yairchu@gmail.com>
2019-08-14 13:28:29 +0300

    Revert "add script to create not-yet-functioning Haskell For Mac project"
    This reverts commit 209499a37021bff3771d4fc7138650332cd1375a.

--- make_hsproj.sh
+++
@@ -1,7 +0,0 @@
-find Lamdu -type dir -exec mkdir -p lamdu.hsproj/{} ";"
-find Lamdu -name "*.hs" -exec ln -f {} lamdu.hsproj/{} ";"
-
-cd bottlelib
-find * -type dir -exec mkdir -p ../lamdu.hsproj/{} ";"
-find * -name "*.hs" -exec ln -f {} ../lamdu.hsproj/{} ";"
-cd ..
