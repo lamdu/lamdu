7598b2feb4873b285d1ca76167ffcb360639878e
Eyal Lotem <eyal.lotem@gmail.com>
2019-09-28 19:38:36 +0300

    update nix, now builds lamdu

--- default.nix
+++ default.nix
@@ -2,9 +2,9 @@ let config = {
     packageOverrides = pkgs: rec {
         haskell = pkgs.haskell // {
             packages = pkgs.haskell.packages // {
-                ghc862 = pkgs.haskell.packages.ghc862.override {
+                ghc865 = pkgs.haskell.packages.ghc865.override {
                     overrides = self: super: rec {
-                        hsc2hs = haskell.lib.dontCheck super.hsc2hs;
+                        hsc2hs = pkgs.haskell.lib.unmarkBroken (haskell.lib.dontCheck super.hsc2hs);
                         freetype2 = self.callPackage ./nix/freetype2.nix {};
                         bindings-freetype-gl = self.callPackage ./nix/bindings-freetype-gl.nix {};
                         freetype-gl = self.callPackage ./nix/FreetypeGL.nix {};
@@ -12,21 +12,22 @@ let config = {
                         hypertypes = self.callPackage ./nix/hypertypes.nix {};
                         lamdu-calculus = self.callPackage ./nix/lamdu-calculus.nix {};
                         nodejs-exec = self.callPackage ./nix/nodejs-exec.nix {};
+                        lens = haskell.lib.dontCheck (self.callPackage ./nix/lens.nix {});
                         testing-feat = self.callHackage "testing-feat" "1.1.0.0" {};
-                        aeson-diff = pkgs.haskell.lib.doJailbreak (self.callHackage "aeson-diff" "1.1.0.5" {});
-                        language-ecmascript = pkgs.haskell.lib.doJailbreak (self.callHackage "language-ecmascript" "0.19" {});
-                        ekg-core = pkgs.haskell.lib.doJailbreak (self.callHackage "ekg-core" "0.1.1.4" {});
-                        th-abstraction = pkgs.haskell.lib.doJailbreak (self.callHackage "th-abstraction" "0.3.1.0" {});
+                        # aeson-diff = pkgs.haskell.lib.doJailbreak (self.callHackage "aeson-diff" "1.1.0.5" {});
+                        # language-ecmascript = pkgs.haskell.lib.doJailbreak (self.callHackage "language-ecmascript" "0.19" {});
+                        # ekg-core = pkgs.haskell.lib.doJailbreak (self.callHackage "ekg-core" "0.1.1.4" {});
+                        # th-abstraction = pkgs.haskell.lib.doJailbreak (self.callHackage "th-abstraction" "0.3.1.0" {});
                     };
                 };
             };
         };
     };
 };
-in with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/4b24ab13d65.tar.gz") {
+in with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/034bf58d343.tar.gz") {
     inherit config;
 };
 
 {
-lamdu = pkgs.haskell.packages.ghc862.callPackage ./nix/lamdu.nix {};
+lamdu = pkgs.haskell.packages.ghc865.callPackage ./nix/lamdu.nix {};
 }
--- nix/nodejs-exec.nix
+++ nix/nodejs-exec.nix
@@ -1,16 +1,16 @@
-{ mkDerivation, base, fetchFromGitHub, stdenv, nodejs-6_x, which }:
+{ mkDerivation, base, fetchFromGitHub, stdenv, nodejs, which }:
 mkDerivation {
   pname = "nodejs-exec";
   version = "0.2.0.0";
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "nodejs-exec";
-    sha256 = "1r3y7k06sg1nwc1fvbr5ps4q8ymg6iaq4gfmkvjaavjvazw5dqff";
-    rev = "44d5b7b58801481638520f52d23fb3c95c2428f0";
+    sha256 = "0whgdd7r9wy4mijq5b2wlv5q3aq5ckjc10ljsajbf3kcs6k6ghqb";
+    rev = "1cbc71544fa39d8eeb2f477701715a8a3a6e8a49";
   };
   enableSeparateDataOutput = true;
   libraryHaskellDepends = [ base ];
-  extraLibraries = [ nodejs-6_x which ];
+  extraLibraries = [ nodejs ];
   homepage = "https://github.com/lamdu/nodejs-exec";
   description = "Package providing a nodejs installation";
   license = stdenv.lib.licenses.bsd3;
