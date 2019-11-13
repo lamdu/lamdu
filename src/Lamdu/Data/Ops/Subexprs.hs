e8e63cb86c73721671f449bee25bdd6f7f954d10
Yair Chuchem <yairchu@gmail.com>
2019-11-13 17:07:39 +0200

    avoid using subExprPayloads lens, towards heterogenous value types

--- src/Lamdu/Data/Ops/Subexprs.hs
+++ src/Lamdu/Data/Ops/Subexprs.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, GADTs #-}
 module Lamdu.Data.Ops.Subexprs
     ( onMatchingSubexprs
     , toHole
@@ -8,8 +8,8 @@ module Lamdu.Data.Ops.Subexprs
 
 
 import qualified Control.Lens as Lens
-import           Hyper (Tree, Pure, _Pure)
-import qualified Lamdu.Calc.Lens as ExprLens
+import           Hyper
+import           Hyper.Recurse (unwrap)
 import           Lamdu.Calc.Term (Val)
 import qualified Lamdu.Calc.Term as V
 import qualified Lamdu.Data.Ops as DataOps
@@ -21,10 +21,18 @@ import           Lamdu.Prelude
 type T = Transaction
 
 onMatchingSubexprs ::
-    Applicative m => (a -> m ()) -> Lens.Fold (Tree Pure V.Term) b -> Val a -> m ()
-onMatchingSubexprs action predicate =
-    Lens.itraverseOf_ (ExprLens.subExprPayloads . Lens.ifiltered (\i _ -> Lens.has predicate i))
-    (const action)
+    Applicative m =>
+    (a -> m ()) ->
+    Lens.Fold (Tree Pure V.Term) b -> Val a ->
+    m ()
+onMatchingSubexprs action predicate x =
+    ( if Lens.has predicate (unwrap (const (^. hVal)) x)
+        then action (x ^. hAnn . Lens._Wrapped)
+        else pure ()
+    ) *>
+    htraverse_
+    ( \(HWitness V.W_Term_Term) -> onMatchingSubexprs action predicate
+    ) (x ^. hVal)
 
 toHole :: Monad m => ValP m -> T m ()
 toHole = void . DataOps.setToHole
