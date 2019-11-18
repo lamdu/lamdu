451a806dcfc4196ddcdb3f03276c211bdd6c86e3
Yair Chuchem <yairchu@gmail.com>
2019-11-18 16:49:04 +0200

    Lamdu.Sugar.Convert.Input.preparePayloads: changes towards richer source ast

--- src/Lamdu/Sugar/Convert/Input.hs
+++ src/Lamdu/Sugar/Convert/Input.hs
@@ -1,5 +1,5 @@
 -- | Preprocess of input to sugar
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, TypeApplications #-}
 module Lamdu.Sugar.Convert.Input
     ( Payload(..)
         , varRefsOfLambda, entityId, inferredType, inferResult, stored, evalResults, userData, localsInScope
@@ -11,7 +11,7 @@ module Lamdu.Sugar.Convert.Input
 import qualified Control.Lens as Lens
 import           Data.CurAndPrev (CurAndPrev(..))
 import qualified Data.Map as Map
-import           Hyper (Tree, Pure, Ann(..), htraverse1)
+import           Hyper
 import           Hyper.Unify.Binding (UVar)
 import           Lamdu.Calc.Term (Val)
 import qualified Lamdu.Calc.Term as V
@@ -47,29 +47,44 @@ Lens.makeLenses ''Payload
 emptyEvalResults :: EvalResultsForExpr
 emptyEvalResults = EvalResultsForExpr Map.empty Map.empty
 
-preparePayloads :: Val (EntityId, [EntityId] -> pl) -> Val pl
-preparePayloads =
-    snd . go
-    where
-        go :: Val (EntityId, [EntityId] -> pl) -> (Map V.Var [EntityId], Val pl)
-        go (Ann (Const (x, mkPayload)) body) =
-            ( childrenVars
-              & case body of
-                V.BLeaf (V.LVar var) -> Lens.at var <>~ Just [x]
-                V.BLam (V.Lam var _) -> Lens.at var .~ Nothing
-                _ -> id
-            , b & htraverse1 %~ (^. Lens._Wrapped . _2)
-              & Ann
-                ( case body of
-                  V.BLam (V.Lam var _) -> childrenVars ^. Lens.ix var
-                  _ -> []
-                  & mkPayload
-                  & Const
-                )
+data PreparePayloadsRes pl t = PreparePayloadsRes
+    { ppVarMap :: Map V.Var [EntityId]
+    , ppRes :: Ann (Const pl) t
+    }
+
+class PreparePayloads t where
+    preparePayloadsH ::
+        Annotated (EntityId, [EntityId] -> pl) t ->
+        Tree (PreparePayloadsRes pl) t
+
+instance PreparePayloads V.Term where
+    preparePayloadsH (Ann (Const (x, mkPayload)) body) =
+        PreparePayloadsRes
+        { ppVarMap =
+            childrenVars
+            & case body of
+            V.BLeaf (V.LVar var) -> Lens.at var <>~ Just [x]
+            V.BLam (V.Lam var _) -> Lens.at var .~ Nothing
+            _ -> id
+        , ppRes =
+            hmap (const ppRes) b
+            & Ann
+            ( case body of
+                V.BLam (V.Lam var _) -> childrenVars ^. Lens.ix var
+                _ -> []
+                & mkPayload
+                & Const
             )
-            where
-                childrenVars = Map.unionsWith (++) (b ^.. htraverse1 . Lens._Wrapped . _1)
-                b = body & htraverse1 %~ Lens.Const . go
+        }
+        where
+            childrenVars = Map.unionsWith (++) (hfoldMap (const ((:[]) . ppVarMap)) b)
+            b = hmap (Proxy @PreparePayloads #> preparePayloadsH) body
+
+preparePayloads ::
+    PreparePayloads t =>
+    Annotated (EntityId, [EntityId] -> pl) t ->
+    Annotated pl t
+preparePayloads = ppRes . preparePayloadsH
 
 initLocalsInScope :: [V.Var] -> Val (Payload m a) -> Val (Payload m a)
 initLocalsInScope locals (Ann (Const pl) body) =
