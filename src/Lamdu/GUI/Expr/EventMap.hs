lamdu/lamdu

commit 8cb842a4a2396d4770464143a648e21454bce74a

Fix failing test: only jump to open fragment id when opening a fragment

src/Lamdu/GUI/Expr/EventMap.hs
@@ -217,13 +217,16 @@ transformEventMap =
             <&> (detach <&>)
     in  case exprInfoActions exprInfo ^. Sugar.detach of
         Sugar.DetachAction detach ->
-            addOperatorSetHoleState options & maybe detach pure
-        Sugar.FragmentAlready holeId -> pure holeId
-        Sugar.FragmentExprAlready holeId -> pure holeId
-        <&> WidgetIds.fromEntityId
-        <&> WidgetIds.fragmentHoleId
-        <&> HoleWidgetIds.makeFrom <&> HoleWidgetIds.hidOpen
+            addOperatorSetHoleState options & maybe detachAndOpen widgetId
+            <&> HoleWidgetIds.makeFrom <&> HoleWidgetIds.hidOpen
+            where
+                detachAndOpen =
+                    detach <&> WidgetIds.fromEntityId <&> WidgetIds.fragmentHoleId
+        Sugar.FragmentAlready holeId -> widgetId holeId
+        Sugar.FragmentExprAlready holeId -> widgetId holeId
         & action
+    where
+        widgetId = pure . WidgetIds.fromEntityId
 
 detachEventMap ::
     ( MonadReader env m, Has Config env

commit 9ca1a68b0959503a6ba960ec1346496f1acc4bf2

fix for operator application on fragment
8cb842a4a23 (yesterday) broke typing the "%" in "0 .to 1000 .filter num -> num %"

src/Lamdu/GUI/Expr/EventMap.hs
@@ -218,14 +218,12 @@ transformEventMap =
     in  case exprInfoActions exprInfo ^. Sugar.detach of
         Sugar.DetachAction detach ->
             addOperatorSetHoleState options & maybe detachAndOpen widgetId
-            <&> HoleWidgetIds.makeFrom <&> HoleWidgetIds.hidOpen
             where
                 detachAndOpen =
                     detach <&> WidgetIds.fromEntityId <&> WidgetIds.fragmentHoleId
-        Sugar.FragmentAlready holeId -> widgetId holeId
-        Sugar.FragmentExprAlready holeId ->
-            widgetId holeId
-            <&> WidgetIds.fragmentHoleId <&> HoleWidgetIds.makeFrom <&> HoleWidgetIds.hidOpen
+        Sugar.FragmentAlready holeId -> widgetId holeId <&> WidgetIds.fragmentHoleId
+        Sugar.FragmentExprAlready holeId -> widgetId holeId <&> WidgetIds.fragmentHoleId
+        <&> HoleWidgetIds.makeFrom <&> HoleWidgetIds.hidOpen
         & action
     where
         widgetId = pure . WidgetIds.fromEntityId