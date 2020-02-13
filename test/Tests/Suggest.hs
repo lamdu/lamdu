1d604ece1eb6422c0c6b47cbdfe93d1b912888cb
Yair Chuchem <yairchu@gmail.com>
2020-02-13 12:51:18 +0200

    add failing test for #494
    https://trello.com/c/AKfdwwDK/494-nullary-injects-results-are-cumbersome

--- test/Tests/Suggest.hs
+++ test/Tests/Suggest.hs
@@ -4,17 +4,23 @@
 module Tests.Suggest where
 
 import qualified Control.Lens as Lens
+import           Control.Monad.Reader (local)
 import           Control.Monad.State (runStateT)
+import qualified Data.Map as Map
 import           Hyper
 import           Hyper.Infer
+import           Hyper.Recurse
 import           Hyper.Type.AST.FuncType
 import           Hyper.Type.AST.Row
+import           Hyper.Type.AST.Scheme
+import           Hyper.Type.AST.Nominal
 import           Hyper.Unify.Binding
 import           Hyper.Unify.New (newTerm, newUnbound)
+import           Lamdu.Calc.Definition (Deps(..))
 import           Lamdu.Calc.Infer
 import qualified Lamdu.Calc.Term as V
 import qualified Lamdu.Calc.Type as T
-import           Lamdu.Sugar.Convert.Hole.Suggest (termTransforms)
+import           Lamdu.Sugar.Convert.Hole.Suggest (termTransforms, termTransformsWithModify)
 import           Revision.Deltum.Transaction (Transaction)
 import           Test.HUnit (assertBool)
 
@@ -50,8 +56,62 @@ testApplyForms =
                 FuncType paramsRecord <$> newUnbound
                     <&> T.TFun
                     >>= newTerm :: PureInfer (V.Scope # UVar) (UVar # T.Type)
+
+-- Tests that nullary inject in nominals are suggested
+-- see https://trello.com/c/AKfdwwDK/494-nullary-injects-results-are-cumbersome
+testBool :: Test
+testBool =
+    case res of
+    [s] ->
+        Lens.has
+        ( hVal . V._BToNom . tnVal . hVal . V._BInject . V.injectVal
+        . hVal . V._BLeaf . V._LRecEmpty
+        ) s
+        & assertBool "didn't complete nullary inject in nominal"
+    _ -> fail "termTransformsWithModify didn't result with 1 option"
+    & testCase "bool"
+    where
+        res =
+            termTransformsWithModify V.emptyScope id id expr
+            & (`runStateT` state)
+            <&> fst
+        inferExample :: PureInfer (V.Scope # UVar) (Ann (InferResult UVar) # V.Term)
+        inferExample =
+            V.BToNomP "Bool" (V.BInjectP "True" (V.BLeafP V.LHole)) ^. hPlain
+            & wrap (\_ x -> Ann (Const ()) x)
+            & infer
+            <&> hflipped %~ hmap (const (^. _2))
+        (expr, state) =
+            loadDeps deps >>= (`local` inferExample)
+            & runPureInfer V.emptyScope (InferState emptyPureInferState varGen)
+            & either (error . show) id
+
+deps :: Deps
+deps =
+    Deps
+    { _depsNominals = Map.fromList [boolTypePair]
+    , _depsGlobalTypes = mempty
+    }
+
+boolTypePair :: (T.NominalId, Pure # NominalDecl T.Type)
+boolTypePair =
+    ( "Bool"
+    , _Pure # NominalDecl
+        { _nParams = T.Types mempty mempty
+        , _nScheme =
+            _Pure # T.REmpty
+            & RowExtend "True" emptyRec & T.RExtend & Pure
+            & RowExtend "False" emptyRec & T.RExtend & Pure
+            & T.TVariant & Pure
+            & Scheme (T.Types mempty mempty)
+        }
+    )
+    where
+        emptyRec = Pure T.REmpty & T.TRecord & Pure
+
 test :: Test
 test =
     testGroup "suggest-tests"
     [ testApplyForms
+    , testBool
     ]
