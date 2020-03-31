6ae71b0fae6b1231540610abd072385342b30b49
Yair Chuchem <yairchu@gmail.com>
2020-03-31 16:41:54 +0300

    Fix invalid hole completions suggested in some cases #497
    https://trello.com/c/OAeXOCMi/497-invalid-nullary-inject-suggested

--- src/Lamdu/Sugar/Convert/Hole/Suggest.hs
+++ src/Lamdu/Sugar/Convert/Hole/Suggest.hs
@@ -182,10 +182,20 @@ termTransformsWithModify ::
     Ann a # V.Term ->
     StateT InferState [] (Ann a # V.Term)
 termTransformsWithModify _ _ _ v@(Ann _ V.BLam {}) = pure v -- Avoid creating a surprise redex
-termTransformsWithModify _ _ _ v@(Ann pl0 (V.BInject (V.Inject tag (Ann pl1 (V.BLeaf V.LHole))))) =
-    -- Variant:<hole> ==> Variant.
-    pure (Ann pl0 (V.BInject (V.Inject tag (Ann pl1 (V.BLeaf V.LRecEmpty)))))
-    <|> pure v
+termTransformsWithModify _ _ getInferred v@(Ann pl0 (V.BInject (V.Inject tag (Ann pl1 (V.BLeaf V.LHole))))) =
+    getInferred pl1 ^. inferResult & lookupBody & liftInfer ()
+    >>=
+    \case
+    Just (T.TRecord r) ->
+        lookupBody r & liftInfer ()
+        >>=
+        \case
+        Just T.REmpty ->
+            -- Variant:<hole> ==> Variant.
+            pure (Ann pl0 (V.BInject (V.Inject tag (Ann pl1 (V.BLeaf V.LRecEmpty)))))
+            <|> pure v
+        _ -> pure v
+    _ -> pure v
 termTransformsWithModify srcScope mkPl getInferred src =
     getInferred (src ^. hAnn) ^. inferResult & lookupBody & liftInfer ()
     >>=
