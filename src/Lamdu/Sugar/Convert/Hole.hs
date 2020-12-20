lamdu/lamdu

commit 5faf4caa4c45dd692ed8504d9dce70d33dc43067

Simplify hole search term generation
Not based on sugaring a base term,
but rather on parallel generation of hole result search terms.

This is a step in cleaning up the hole result generation code.

src/Lamdu/Sugar/Convert/Hole.hs
@@ -5,7 +5,7 @@ module Lamdu.Sugar.Convert.Hole
       -- Used by Convert.Fragment:
     , Preconversion, ResultGen
     , ResultProcessor(..)
-    , mkOptions, detachValIfNeeded, sugar, loadNewDeps
+    , mkOptions, detachValIfNeeded, mkHoleSearchTerms, loadNewDeps
     , mkResult
     , mkOption, addWithoutDups
     , assertSuccessfulInfer
@@ -18,7 +18,6 @@ import           Control.Monad.ListT (ListT(..))
 import           Control.Monad.Once (OnceT, _OnceT, MonadOnce(..))
 import           Control.Monad.State (State, StateT(..), mapStateT, evalState, state)
 import qualified Control.Monad.State as State
-import           Control.Monad.Transaction (transaction)
 import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.Binary as Binary
 import           Data.Bits (xor)
@@ -43,6 +42,7 @@ import           Hyper.Type.Prune (Prune(..))
 import           Hyper.Unify (Unify(..), BindingDict(..), unify)
 import           Hyper.Unify.Binding (UVar)
 import           Hyper.Unify.Term (UTerm(..), UTermBody(..))
+import qualified Lamdu.Builtins.Anchors as Builtins
 import qualified Lamdu.Cache as Cache
 import           Lamdu.Calc.Definition (Deps(..), depsGlobalTypes, depsNominals)
 import           Lamdu.Calc.Infer (InferState, runPureInfer, PureInfer)
@@ -116,6 +116,34 @@ holeResultProcessor =
     , rpPreConversion = id
     }
 
+mkHoleSearchTerms :: Monad m => ConvertM.Context m -> Input.Payload m a # V.Term -> Val () -> T m [HoleTerm InternalName]
+mkHoleSearchTerms ctx pl x =
+    case x ^. hVal of
+    V.BLeaf (V.LVar v)
+        | v `notElem` pl ^. Input.localsInScope -> taggedName v <&> (:[]) . HoleGetDef
+        | v `elem` recordParams -> pure [HoleParamsRecord]
+        | otherwise -> ofName v
+    V.BLeaf (V.LFromNom n) -> ofName n <&> (<> [HoleIf | n == Builtins.boolTid])
+    V.BLeaf V.LRecEmpty -> pure [HoleRecord]
+    V.BLeaf V.LAbsurd -> pure [HoleEmptyCase]
+    V.BLeaf V.LHole -> pure []
+    V.BLeaf V.LLiteral{} -> pure [] -- Literals not created from hole results
+    V.BToNom (V.ToNom n b) -> ofName n <> mkHoleSearchTerms ctx pl b
+    V.BInject i -> pure [HoleInject (nameWithoutContext (i ^. V.injectTag))]
+    V.BApp a -> mkHoleSearchTerms ctx pl (a ^. V.appFunc)
+    V.BLam{} -> pure [HoleLambda]
+    V.BRecExtend{} -> pure [HoleRecord]
+    V.BCase{} -> pure [HoleCase]
+    V.BGetField (V.GetField e f)
+        | Lens.anyOf (hVal . V._BLeaf . V._LVar) (`elem` recordParams) e ->
+            pure [HoleName (nameWithoutContext f)]
+        | otherwise -> pure [HoleGetField (nameWithoutContext f)]
+    where
+        ofName v = taggedName v <&> (:[]) . HoleName
+        recordParams =
+            ctx ^.. ConvertM.scScopeInfo . ConvertM.siTagParamInfos . traverse . ConvertM._TagFieldParam
+            <&> ConvertM.tpiFromParameters
+
 mkOption ::
     (Monad m, Typeable m) =>
     ConvertM.Context m -> ResultProcessor m ->
@@ -123,7 +151,7 @@ mkOption ::
     OnceT (T m) (HoleOption EvalPrep InternalName (OnceT (T m)) (T m))
 mkOption sugarContext resultProcessor holePl x =
     HoleOption entityId
-    <$> once (sugar sugarContext holePl x)
+    <$> once (lift (mkHoleSearchTerms sugarContext holePl x))
     ?? mkResults resultProcessor sugarContext holePl x
     where
         entityId =
@@ -132,7 +160,6 @@ mkOption sugarContext resultProcessor holePl x =
             & show
             & Random.randFunc
             & EntityId.EntityId
-
 
 mkHoleSuggesteds ::
     (Monad m, Typeable m) =>
@@ -281,37 +308,6 @@ loadNewDeps currentDeps scope x =
         newDepVars = newDeps depsGlobalTypes (ExprLens.valGlobals scopeVars)
         newNoms = newDeps depsNominals ExprLens.valNominals
 
--- Unstored and without eval results.
--- Used for hole's base exprs, to perform sugaring and get names from sugared exprs.
--- TODO: We shouldn't need to perform sugaring for base exprs, and this should be removed.
-prepareUnstoredPayloads ::
-    Ann (InferResult (Pure :*: UVar) :*: Const (EntityId, a)) # V.Term ->
-    Ann (Input.Payload m a) # V.Term
-prepareUnstoredPayloads v =
-    v & hflipped %~ hmap (const mk) & Input.preparePayloads
-    where
-        mk (inferPl :*: Const (eId, x)) =
-            Input.PreparePayloadInput
-            { Input.ppEntityId = eId
-            , Input.ppMakePl =
-                \varRefs ->
-                Input.Payload
-                { Input._varRefsOfLambda = varRefs
-                , Input._userData = x
-                , Input._localsInScope = []
-                , Input._inferRes = inferPl
-                , Input._inferScope = V.emptyScope
-                , Input._entityId = eId
-                , Input._stored =
-                    HRef
-                    (_F # IRef.unsafeFromUUID fakeStored)
-                    (error "stored output of base expr used!")
-                }
-            }
-            where
-                -- TODO: Which code reads this?
-                EntityId.EntityId fakeStored = eId
-
 assertSuccessfulInfer ::
     HasCallStack =>
     Either (Pure # T.TypeError) a -> a
@@ -334,38 +330,6 @@ loadInfer sugarContext scope v =
     where
         memoInfer = Cache.infer (sugarContext ^. ConvertM.scCacheFunctions)
         sugarDeps = sugarContext ^. ConvertM.scFrozenDeps . Property.pVal
-
-sugar ::
-    (Monad m, Monoid a) =>
-    ConvertM.Context m -> Input.Payload m dummy # V.Term -> Val a ->
-    OnceT (T m) (Expr Binder EvalPrep InternalName (OnceT (T m)) (T m) a)
-sugar sugarContext holePl v =
-    do
-        (val, inferCtx) <-
-            loadInfer sugarContext scope v
-            <&> snd
-            <&> assertSuccessfulInfer
-            <&>
-            ( \((term, topLevelScope), inferCtx) ->
-                ( inferUVarsApplyBindings term
-                    & runPureInfer () inferCtx
-                    & assertSuccessfulInfer
-                    & fst
-                    & hflipped %~ hmap (const mkPayload)
-                    & EntityId.randomizeExprAndParams
-                        (Random.genFromHashable (holePl ^. Input.entityId))
-                    & prepareUnstoredPayloads
-                    & Input.initScopes topLevelScope (holePl ^. Input.localsInScope)
-                , inferCtx
-                )
-            ) & transaction & lift
-        convertBinder val
-            <&> convertPayloads
-            & ConvertM.run (sugarContext & ConvertM.scInferContext .~ inferCtx)
-    where
-        scope = holePl ^. Input.inferScope
-        mkPayload (Const x :*: inferPl) =
-            _HFunc # \(Const entityId) -> inferPl :*: Const (entityId, x)
 
 getLocalScopeGetVars :: ConvertM.Context m -> V.Var -> [HPlain V.Term]
 getLocalScopeGetVars sugarContext par

src/Lamdu/GUI/Expr/HoleEdit/ResultGroups.hs
@@ -181,11 +181,11 @@ mkGroup ::
     ) =>
     env -> Sugar.HoleOption (Sugar.Annotation (Sugar.EvaluationScopes Name i) Name) Name i o -> i (Group i o)
 mkGroup env option =
-    option ^. Sugar.hoSugaredBaseExpr
+    option ^. Sugar.hoSearchTerms
     <&>
-    \sugaredBaseExpr ->
+    \searchTerms ->
     Group
-    { _groupSearchTerms = sugaredBaseExpr ^. hVal & ValTerms.binder env
+    { _groupSearchTerms = searchTerms >>= ValTerms.holeSearchTerm env
     , _groupResults = option ^. Sugar.hoResults
     , _groupId = option ^. Sugar.hoEntityId & WidgetIds.fromEntityId
     }

src/Lamdu/GUI/Expr/HoleEdit/ValTerms.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE TypeFamilies #-}
 module Lamdu.GUI.Expr.HoleEdit.ValTerms
-    ( expr
-    , binder
+    ( holeSearchTerm
     , allowedSearchTermCommon
     , allowedFragmentSearchTerm
     , getSearchStringRemainder
@@ -14,13 +13,11 @@ import qualified Data.Char as Char
 import qualified Data.Text as Text
 import qualified GUI.Momentu.Widgets.Menu.Search as SearchMenu
 import           Hyper
-import qualified Lamdu.Builtins.Anchors as Builtins
 import qualified Lamdu.CharClassification as Chars
 import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
 import           Lamdu.Name (Name(..), Collision(..))
 import qualified Lamdu.Name as Name
-import qualified Lamdu.Sugar.Lens as SugarLens
 import           Lamdu.Sugar.Types
 
 import           Lamdu.Prelude
@@ -41,87 +38,25 @@ ofName (Name.NameTag x) =
     where
         Name.TagText displayName textCollision = x ^. Name.tnDisplayText
 
-expr ::
-    ( Has (Texts.Code Text) env
-    , Has (Texts.CodeUI Text) env
-    ) =>
-    env -> Annotated a # Term v Name i o -> [Text]
-expr env = ofBody env . (^. hVal)
-
-ofBody ::
-    ( Has (Texts.Code Text) env
-    , Has (Texts.CodeUI Text) env
-    ) =>
-    env -> Term v Name i o # Annotated a -> [Text]
-ofBody env =
-    \case
-    BodyLam {} ->
-        [ env ^. has . Texts.lambda
-        , "\\", "Λ", "λ", "->", "→"
-        ]
-    BodySimpleApply x ->
-        env ^. has . Texts.apply : x ^. htraverse1 . Lens.to (expr env)
-    BodyLabeledApply x ->
-        env ^. has . Texts.apply
-        : ofName (x ^. aFunc . hVal . Lens._Wrapped . bvNameRef . nrName)
-        ++ (x ^.. aAnnotatedArgs . Lens.folded . aaTag . tagName >>= ofName)
-    BodyRecord {} ->
-        -- We don't allow completing a record by typing one of its
-        -- field names/vals
-        ["{}", "()", "[]"]
-    BodyGetField gf ->
-        ofName (gf ^. gfTag . tagRefTag . tagName) <&> ("." <>)
-    BodyCase cas ->
-        [ env ^. has . Texts.case_
-        , env ^. has . Texts.of_
-        ] ++
-        case cas of
-            Case LambdaCase (Composite [] [] ClosedComposite{} _) ->
-                [env ^. has . Texts.absurd]
-            _ -> []
-    BodyIfElse {} -> [env ^. has . Texts.if_, ":"]
-    -- An inject "base expr" can have various things in its val filled
-    -- in, so the result group based on it may have both nullary
-    -- inject (".") and value inject (":"). Thus, an inject must match
-    -- both.
-    -- So these terms are used to filter the whole group, and then
-    -- isExactMatch (see below) is used to filter each entry.
-    BodyInject (Inject tag _) ->
-        (<>) <$> ofName (tag ^. tagRefTag . tagName) <*> [":", "."]
-    BodyLiteral {} -> []
-    BodyGetVar GetParamsRecord {} -> [env ^. has . Texts.paramsRecordOpener]
-    BodyGetVar (GetParam x) -> ofName (x ^. pNameRef . nrName)
-    BodyGetVar (GetBinder x) ->
-        ofName (x ^. bvNameRef . nrName)
-        & maybePrefixDot x
-    BodyToNom (Nominal tid b) ->
-        ofName (tid ^. tidName)
-        ++ b ^. SugarLens.binderResultExpr . Lens.asIndex . Lens.to (ofBody env)
-    BodyFromNom tid ->
-        ofName (tid ^. tidName) <>
-        -- The hole's "extra" apply-form results will be an
-        -- IfElse, but we give val terms only to the base expr
-        -- which looks like this:
-        [ env ^. has . Texts.if_
-        | tid ^. tidTId == Builtins.boolTid
-        ]
-    BodyHole {} -> []
-    BodyFragment {} -> []
-    BodyPlaceHolder {} -> []
+holeSearchTerm ::
+    (Has (Texts.Code Text) env, Has (Texts.CodeUI Text) env) =>
+    env -> HoleTerm Name -> [Text]
+holeSearchTerm _ (HoleGetDef x) =
+    ofName x >>= maybePrefixDot
     where
-        maybePrefixDot bv =
-            case bv ^. bvForm of
-            GetDefinition _
-                | bv ^. bvNameRef . nrName & Name.isOperator & not -> (>>= \x -> [x, "." <> x])
-            _ -> id
-
-binder ::
-    ( Has (Texts.Code Text) env
-    , Has (Texts.CodeUI Text) env
-    ) =>
-    env -> Binder v Name i o # Annotated a -> [Text]
-binder env BinderLet{} = [env ^. has . Texts.let_]
-binder env (BinderTerm x) = ofBody env x
+        maybePrefixDot n
+            | Name.isOperator x = [n]
+            | otherwise = [n, "." <> n]
+holeSearchTerm _ (HoleName x) = ofName x
+holeSearchTerm _ (HoleGetField x) = ofName x <&> ("." <>)
+holeSearchTerm _ (HoleInject x) = (<>) <$> ofName x <*> [":", "."]
+holeSearchTerm e HoleLet = [e ^. has . Texts.let_]
+holeSearchTerm e HoleLambda = [e ^. has . Texts.lambda, "\\", "Λ", "λ", "->", "→"]
+holeSearchTerm e HoleIf = [e ^. has . Texts.if_, ":"]
+holeSearchTerm e HoleCase = [e ^. has . Texts.case_, e ^. has . Texts.of_]
+holeSearchTerm e HoleEmptyCase = [e ^. has . Texts.absurd]
+holeSearchTerm _ HoleRecord = ["{}", "()", "[]"]
+holeSearchTerm e HoleParamsRecord = [e ^. has . Texts.paramsRecordOpener]
 
 type Suffix = Char
 

src/Lamdu/Sugar.hs
@@ -73,9 +73,8 @@ makeHoleOptionAnn ::
     Annotations.Mode ->
     Sugar.HoleOption (ShowAnnotation, EvalPrep) AddNames.InternalName (OnceT m) m ->
     Sugar.HoleOption (Sugar.Annotation EvalPrep AddNames.InternalName) AddNames.InternalName (OnceT m) m
-makeHoleOptionAnn annMode (Sugar.HoleOption i e r) =
-    Sugar.HoleOption i
-    (e >>= onNode)
+makeHoleOptionAnn annMode (Sugar.HoleOption i t r) =
+    Sugar.HoleOption i t
     (r <&> _2 %~ (>>= Sugar.holeResultConverted onNode))
     where
         onNode = SugarLens.annotations (<&> Lens.mapped %~ makeHoleOptionAnn annMode) (makeAnnotation annMode)

src/Lamdu/Sugar/Convert/Fragment.hs
@@ -346,7 +346,7 @@ mkOptionFromFragment sugarContext exprPl x =
         x & hflipped %~ hmap (\_ _ -> Const ())
         & show & Random.randFunc
         & EntityId.EntityId
-    , _hoSugaredBaseExpr = Hole.sugar sugarContext exprPl baseExpr
+    , _hoSearchTerms = Hole.mkHoleSearchTerms sugarContext exprPl baseExpr & lift -- TODO: Once?
     , _hoResults =
         do
             newDeps <- Hole.loadNewDeps (depsProp ^. Property.pVal) (exprPl ^. Input.inferScope) x

src/Lamdu/Sugar/Eval.hs
@@ -158,8 +158,8 @@ instance AddEval Term where
         BodyFragment (Fragment f h t o) ->
             Fragment (addToNode r f) h t (o <&> Lens.mapped %~ addToHoleOption) & BodyFragment
         where
-            addToHoleOption (HoleOption hi e s) =
-                HoleOption hi (e <&> addToNode r)
+            addToHoleOption (HoleOption hi t s) =
+                HoleOption hi t
                 (s <&> _2 . Lens.mapped . holeResultConverted %~ addToNode r)
 
 addToPayload ::

src/Lamdu/Sugar/Lens.hs
@@ -122,19 +122,16 @@ binderResultExpr f (Ann (Const pl) x) =
 
 holeOptionTransformExprs ::
     Monad i =>
-    (Expr Binder v0 n0 i o () -> i (Expr Binder v1 n1 i o ())) ->
-    HoleOption v0 n0 i o ->
-    HoleOption v1 n1 i o
-holeOptionTransformExprs onExpr option =
-    option
-    { _hoSugaredBaseExpr = option ^. hoSugaredBaseExpr >>= onExpr
-    , _hoResults = option ^. hoResults <&> _2 %~ (>>= holeResultConverted onExpr)
-    }
+    (Expr Binder v0 n i o () -> i (Expr Binder v1 n i o ())) ->
+    HoleOption v0 n i o ->
+    HoleOption v1 n i o
+holeOptionTransformExprs onExpr =
+    hoResults . Lens.mapped . _2 %~ (>>= holeResultConverted onExpr)
 
 holeTransformExprs ::
     Monad i =>
-    (Expr Binder v0 n0 i o () -> i (Expr Binder v1 n1 i o ())) ->
-    Hole v0 n0 i o -> Hole v1 n1 i o
+    (Expr Binder v0 n i o () -> i (Expr Binder v1 n i o ())) ->
+    Hole v0 n i o -> Hole v1 n i o
 holeTransformExprs onExpr =
     holeOptions . Lens.mapped . Lens.mapped %~ holeOptionTransformExprs onExpr
 
@@ -164,8 +161,8 @@ holeOptionAnnotations ::
     Lens.Setter (HoleOption v0 n i o) (HoleOption v1 n i o) v0 v1
 holeOptionAnnotations =
     Lens.setting $
-    \f (HoleOption i b r) ->
-    HoleOption i (b <&> onNode f)
+    \f (HoleOption i t r) ->
+    HoleOption i t
     (r <&> _2 . Lens.mapped . holeResultConverted %~ onNode f)
     where
         onNode f = annotations (<&> Lens.mapped . holeOptionAnnotations %~ f) %~ f

src/Lamdu/Sugar/Names/Walk.hs
@@ -14,7 +14,6 @@ import           Hyper.Class.Morph (morphTraverse)
 import           Hyper.Type.AST.App (MorphWitness(..))
 import           Hyper.Type.AST.FuncType (FuncType(..))
 import qualified Lamdu.Calc.Type as T
-import qualified Lamdu.Sugar.Lens as SugarLens
 import           Lamdu.Sugar.Names.CPS (CPS(..), liftCPS)
 import qualified Lamdu.Sugar.Types as Sugar
 import           Lamdu.Sugar.Types hiding (Tag(..))
@@ -306,27 +305,39 @@ toLabeledApply
 type SugarElem t n m (o :: * -> *) = t (Annotation (EvaluationScopes n (IM m)) n) n (IM m) o
 type WalkElem t m o = SugarElem t (OldName m) m o -> m (SugarElem t (NewName m) m o)
 
+toHoleOption ::
+    MonadNaming m =>
+    (m (ExprW Binder (NewName m) m o ()) -> IM m (ExprW Binder (NewName m) m o ())) ->
+    (m (NewName m) -> IM m (NewName m)) ->
+    SugarElem HoleOption (OldName m) m o -> SugarElem HoleOption (NewName m) m o
+toHoleOption run0 run1 option =
+    option
+    { _hoSearchTerms =
+        -- Hack: Just using TaggedVar as NameType because disambiguations aren't important in hole results
+        option ^. hoSearchTerms >>= traverse . traverse %%~ run1 . opGetName Nothing TaggedVar
+    , _hoResults = option ^. hoResults <&> _2 %~ (>>= holeResultConverted (run0 . toNode toBinder))
+    }
+
 toHole :: MonadNaming m => WalkElem Hole m o
 toHole hole =
-    opRun
+    (,) <$> opRun <*> opRun
     <&>
-    \run ->
-    SugarLens.holeTransformExprs (run . toNode toBinder) hole
+    \(r0, r1) ->
+    hole & Sugar.holeOptions . Lens.mapped . Lens.mapped %~ toHoleOption r0 r1
 
 toFragment :: MonadNaming m => WalkBody Fragment m o a
 toFragment Fragment{_fExpr, _fHeal, _fTypeMismatch, _fOptions} =
     do
         newTypeMismatch <- Lens._Just toType _fTypeMismatch
-        run <- opRun
         newExpr <- toExpression _fExpr
+        r0 <- opRun
+        r1 <- opRun
         pure Fragment
             { _fExpr = newExpr
             , _fTypeMismatch = newTypeMismatch
             , _fOptions =
                  _fOptions
-                 <&> Lens.mapped %~
-                     SugarLens.holeOptionTransformExprs
-                     (run . toNode toBinder)
+                 <&> Lens.mapped %~ toHoleOption r0 r1
             , _fHeal
             }
 

commit e5c2df827bec986e9b52c1cd25ef9b49b7536654

Oops fix typing let broken in 5faf4caa4

src/Lamdu/Sugar/Convert/Hole.hs
@@ -125,6 +125,7 @@ mkHoleSearchTerms ctx pl x =
     V.BLeaf V.LLiteral{} -> pure [] -- Literals not created from hole results
     V.BToNom (V.ToNom n b) -> ofName n <> mkHoleSearchTerms ctx pl b
     V.BInject i -> pure [HoleInject (nameWithoutContext (i ^. V.injectTag))]
+    V.BApp (V.App (Ann _ V.BLam{}) _) -> pure [HoleLet]
     V.BApp a -> mkHoleSearchTerms ctx pl (a ^. V.appFunc)
     V.BLam{} -> pure [HoleLambda]
     V.BRecExtend{} -> pure [HoleRecord]