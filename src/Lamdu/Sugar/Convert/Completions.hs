lamdu/lamdu

commit 2129b222f083639a9e5a06cbf0e0ddf871c128d8

Use cache rather than canonicalization for ids in hole results
* Removes a lot of code, which is nice :)
* The new behaviour necessitated uses of "once", which as a side-product may make things more responsive!
* Still have a hacky "randomizeNewVars" which will be removed after fragments redesign

src/Lamdu/Sugar/Convert/Completions.hs
@@ -29,105 +29,116 @@ lookupBody x = semiPruneLookup x <&> (^? _2 . _UTerm . uBody)
 -- | Suggest values that fit a type, may "split" once, to suggest many
 -- injects for a sum type. These are offerred in holes (not fragments).
 suggestForType ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
-    UVarOf m # T.Type -> m [TypedTerm m]
-suggestForType t =
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
+    UVarOf m # T.Type ->
+    m [f (TypedTerm m)]
+suggestForType mkVar t =
     -- TODO: DSL for matching/deref'ing UVar structure
     lookupBody t
     >>= \case
-    Just (T.TVariant r) -> forVariant r
-    typ -> suggestForTypeUTermWithoutSplit typ <&> (^.. Lens._Just)
-    <&> Lens.mapped %~ Ann (inferResult # t)
+    Just (T.TVariant r) -> forVariant mkVar r
+    typ -> suggestForTypeUTermWithoutSplit mkVar typ <&> (^.. Lens._Just)
+    <&> Lens.mapped . Lens.mapped %~ Ann (inferResult # t)
 
 forVariant ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
-    UVarOf m # T.Row -> m [V.Term # Ann (InferResult (UVarOf m))]
-forVariant r =
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
+    UVarOf m # T.Row ->
+    m [f (V.Term # Ann (InferResult (UVarOf m)))]
+forVariant mkVar r =
     lookupBody r >>=
     \case
     Just (T.RExtend (RowExtend tag typ rest)) ->
         (:)
-        <$> (suggestForTypeObvious typ <&> V.Inject tag <&> V.BInject)
-        <*> forVariant rest
+        <$> (suggestForTypeObvious mkVar typ <&> Lens.mapped %~ V.BInject . V.Inject tag)
+        <*> forVariant mkVar rest
     _ -> pure []
 
 suggestForTypeObvious ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
-    UVarOf m # T.Type -> m (TypedTerm m)
-suggestForTypeObvious t =
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
+    UVarOf m # T.Type ->
+    m (f (TypedTerm m))
+suggestForTypeObvious mkVar t =
     lookupBody t
-    >>= suggestForTypeUTermObvious
-    <&> fromMaybe (V.BLeaf V.LHole)
-    <&> Ann (inferResult # t)
+    >>= suggestForTypeUTermObvious mkVar
+    <&> fromMaybe (V.BLeaf V.LHole & pure)
+    <&> Lens.mapped %~ Ann (inferResult # t)
 
 suggestForTypeUTermWithoutSplit ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
-    Maybe (T.Type # UVarOf m) -> m (Maybe (V.Term # Ann (InferResult (UVarOf m))))
-suggestForTypeUTermWithoutSplit (Just (T.TRecord r)) = forRecord r
-suggestForTypeUTermWithoutSplit t = suggestForTypeUTermObvious t
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
+    Maybe (T.Type # UVarOf m) ->
+    m (Maybe (f (V.Term # Ann (InferResult (UVarOf m)))))
+suggestForTypeUTermWithoutSplit mkVar (Just (T.TRecord r)) = forRecord mkVar r
+suggestForTypeUTermWithoutSplit mkVar t = suggestForTypeUTermObvious mkVar t
 
 suggestForTypeUTermObvious ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
-    Maybe (T.Type # UVarOf m) -> m (Maybe (V.Term # Ann (InferResult (UVarOf m))))
-suggestForTypeUTermObvious (Just (T.TFun (FuncType param result))) =
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
+    Maybe (T.Type # UVarOf m) ->
+    m (Maybe (f (V.Term # Ann (InferResult (UVarOf m)))))
+suggestForTypeUTermObvious mkVar (Just (T.TFun (FuncType param result))) =
     lookupBody param >>=
     \case
-    Just (T.TVariant row) -> suggestCaseWith row result
-    _ -> suggestLam result
+    Just (T.TVariant row) -> suggestCaseWith mkVar row result
+    _ -> suggestLam mkVar result
     <&> Just
-suggestForTypeUTermObvious (Just (T.TRecord r)) =
-    lookupBody r <&> forRecordUTermObvious
-suggestForTypeUTermObvious _ = pure Nothing
+suggestForTypeUTermObvious _ (Just (T.TRecord r)) =
+    lookupBody r <&> forRecordUTermObvious <&> Lens._Just %~ pure
+suggestForTypeUTermObvious _ _ = pure Nothing
 
 suggestLam ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
     UVarOf m # T.Type ->
-    m (V.Term # Ann (InferResult (UVarOf m)))
-suggestLam result =
-    V.TypedLam "var"
-    <$> (newUnbound <&> (inferResult #) <&> (`Ann` (_HCompose # Pruned)))
-    <*> suggestForTypeObvious result
-    <&> V.BLam
+    m (f (V.Term # Ann (InferResult (UVarOf m))))
+suggestLam mkVar result =
+    do
+        t <- newUnbound <&> (inferResult #) <&> (`Ann` (_HCompose # Pruned))
+        body <- suggestForTypeObvious mkVar result
+        V.TypedLam <$> mkVar <*> pure t <*> body
+            <&> V.BLam & pure
 
 forRecord ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
-    UVarOf m # T.Row -> m (Maybe (V.Term # Ann (InferResult (UVarOf m))))
-forRecord r =
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
+    UVarOf m # T.Row ->
+    m (Maybe (f (V.Term # Ann (InferResult (UVarOf m)))))
+forRecord mkVar r =
     lookupBody r >>=
     \case
     Just (T.RExtend (RowExtend tag typ rest)) ->
-        RowExtend tag
-        <$> suggestForTypeObvious typ
-        <*> ( Ann
-                <$> (newTerm (T.TRecord rest) <&> (inferResult #))
-                <*> (forRecord rest <&> fromMaybe (V.BLeaf V.LHole))
-            )
-        <&> V.BRecExtend
+        do
+            field <- suggestForTypeObvious mkVar typ
+            restType <- newTerm (T.TRecord rest) <&> (inferResult #)
+            restBody <- forRecord mkVar rest <&> fromMaybe (pure (V.BLeaf V.LHole))
+            RowExtend tag <$> field <*> (restBody <&> Ann restType) <&> V.BRecExtend & pure
         <&> Just
-    t -> forRecordUTermObvious t & pure
+    t -> forRecordUTermObvious t <&> pure & pure
 
 forRecordUTermObvious :: Maybe (T.Row # h0) -> Maybe (V.Term # h1)
 forRecordUTermObvious (Just T.REmpty) = V.BLeaf V.LRecEmpty & Just
 forRecordUTermObvious _ = Nothing
 
 suggestCaseWith ::
-    (UnifyGen m T.Type, UnifyGen m T.Row) =>
-    UVarOf m # T.Row -> UVarOf m # T.Type ->
-    m (V.Term # Ann (InferResult (UVarOf m)))
-suggestCaseWith variantType resultType =
+    (Applicative f, UnifyGen m T.Type, UnifyGen m T.Row) =>
+    f V.Var ->
+    UVarOf m # T.Row ->
+    UVarOf m # T.Type ->
+    m (f (V.Term # Ann (InferResult (UVarOf m))))
+suggestCaseWith mkVar variantType resultType =
     lookupBody variantType >>=
     \case
-    Just T.REmpty -> V.BLeaf V.LAbsurd & pure
+    Just T.REmpty -> V.BLeaf V.LAbsurd & pure & pure
     Just (T.RExtend (RowExtend tag fieldType rest)) ->
-        RowExtend tag
-        <$> (Ann
-                <$> (mkCaseType fieldType <&> (inferResult #))
-                <*> suggestLam resultType
-            )
-        <*> (Ann
-                <$> (T.TVariant rest & newTerm >>= mkCaseType <&> (inferResult #))
-                <*> suggestCaseWith rest resultType)
-        <&> V.BCase
+        do
+            handlerType <- mkCaseType fieldType
+            field <- suggestLam mkVar resultType
+            restType <- newTerm (T.TVariant rest) >>= mkCaseType
+            restBody <- suggestCaseWith mkVar rest resultType
+            RowExtend tag <$> (field <&> Ann handlerType) <*> (restBody <&> Ann restType) <&> V.BRecExtend & pure
         where
-            mkCaseType which = FuncType which resultType & T.TFun & newTerm
-    _ -> suggestLam resultType
+            mkCaseType which = FuncType which resultType & T.TFun & newTerm <&> (inferResult #)
+    _ -> suggestLam mkVar resultType

Lamdu.cabal
@@ -324,7 +324,6 @@ Library
                   , Lamdu.Eval.Manager
                   , Lamdu.Eval.Results
                   , Lamdu.Eval.Results.Process
-                  , Lamdu.Expr.GenIds
                   , Lamdu.Expr.IRef
                   , Lamdu.Expr.Load
                   , Lamdu.Expr.UniqueId
@@ -472,7 +471,6 @@ Library
                   , Revision.Deltum.Rev.ViewBranchInternal
                   , Revision.Deltum.Transaction
                   , System.Process.Utils
-                  , System.Random.Extended
                   , System.Remote.Monitoring.Shim
                   , System.TimeIt.Pure
 

src/Control/Monad/Once.hs
@@ -5,12 +5,15 @@ module Control.Monad.Once
     , OnceT(..), _OnceT
     , OnceState
     , Typeable
+    , onceList
     ) where
 
 import qualified Control.Lens as Lens
+import           Control.Monad.ListT (ListT(..))
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.State
 import           Data.Dynamic (Dynamic, toDyn, fromDynamic)
+import qualified Data.List.Class as ListClass
 import           Data.Typeable (Typeable, typeRep)
 import           Data.IORef
 import qualified Data.Sequence as Sequence
@@ -63,3 +66,14 @@ instance Monad m => MonadOnce (OnceT m) where
                     x <- a
                     x <$ OnceT (Lens.ix r .= toDyn (Just x))
             Nothing -> error "Once used incorrectly: wrong type for key!"
+
+onceList ::
+    (MonadOnce m, Monad m, Typeable m, Typeable a) =>
+    ListT m a -> m (ListT m a)
+onceList (ListT a) =
+    once
+    ( a >>=
+        \case
+        ListClass.Nil -> pure ListClass.Nil
+        ListClass.Cons x xs -> onceList xs <&> ListClass.Cons x
+    ) <&> ListT

src/Lamdu/Data/Ops.hs
@@ -15,13 +15,16 @@ module Lamdu.Data.Ops
     ) where
 
 import qualified Control.Lens as Lens
+import qualified Data.ByteString.Extended as BS
 import           Data.Property (MkProperty', Property(..))
 import qualified Data.Property as Property
 import qualified Data.Set as Set
+import qualified Data.UUID as UUID
 import qualified GUI.Momentu.Direction as Dir
 import           Hyper (_HCompose)
 import           Hyper.Type.AST.Row (RowExtend(..))
 import           Hyper.Type.Prune (Prune(..))
+import           Lamdu.Calc.Identifier (Identifier(..))
 import qualified Lamdu.Calc.Term as V
 import qualified Lamdu.Calc.Type as T
 import qualified Lamdu.CharClassification as Chars
@@ -29,7 +32,6 @@ import qualified Lamdu.Data.Anchors as Anchors
 import           Lamdu.Data.Definition (Definition(..))
 import           Lamdu.Data.Meta (SpecialArgs(..), PresentationMode)
 import           Lamdu.Data.Tag (Symbol(..), TextsInLang(..), tagOrder, tagTexts, tagSymbol, getTagName, name)
-import qualified Lamdu.Expr.GenIds as GenIds
 import           Lamdu.Expr.IRef (DefI, HRef, ValI)
 import qualified Lamdu.Expr.IRef as ExprIRef
 import           Lamdu.I18N.LangId (LangId)
@@ -106,7 +108,7 @@ data CompositeExtendResult m = CompositeExtendResult
     }
 
 genNewTag :: Monad m => T m T.Tag
-genNewTag = GenIds.transaction GenIds.randomTag
+genNewTag = Transaction.newKey <&> T.Tag . Identifier . BS.strictify . UUID.toByteString
 
 recExtend :: Monad m => T.Tag -> ValI m -> T m (CompositeExtendResult m)
 recExtend tag valI =

src/Lamdu/Expr/GenIds.hs
@@ -1,117 +1,0 @@
--- TODO: Split/rename to more generic (non-sugar) modules
-{-# LANGUAGE GADTs #-}
-
-module Lamdu.Expr.GenIds
-    ( randomizeParamIdsG
-    , randomTag
-
-    , transaction
-        -- TODO: To its own module?
-    , onNgMakeName
-    , NameGen(..), randomNameGen
-    ) where
-
-import qualified Control.Lens as Lens
-import           Control.Monad (replicateM)
-import           Control.Monad.Trans.Reader (ReaderT(..))
-import qualified Control.Monad.Trans.Reader as Reader
-import           Control.Monad.Trans.State (evalState, state, runState)
-import qualified Data.ByteString as BS
-import qualified Data.Map as Map
-import           Hyper
-import           Lamdu.Calc.Identifier (Identifier(..))
-import qualified Lamdu.Calc.Term as V
-import qualified Lamdu.Calc.Type as T
-import           Revision.Deltum.Transaction (Transaction)
-import qualified Revision.Deltum.Transaction as Transaction
-import           System.Random (RandomGen, random)
-import qualified System.Random.Extended as Random
-
-import           Lamdu.Prelude
-
-type T = Transaction
-
-transaction :: Monad m => (Random.StdGen -> (a, Random.StdGen)) -> T m a
-transaction f = Transaction.newKey <&> fst . f . Random.genFromHashable
-
-randomIdentifier :: RandomGen g => g -> (Identifier, g)
-randomIdentifier =
-    runState $ Identifier . BS.pack <$> replicateM idLength (state random)
-    where
-        idLength = 16
-
-randomVar :: RandomGen g => g -> (V.Var, g)
-randomVar g = randomIdentifier g & _1 %~ V.Var
-
-randomTag :: RandomGen g => g -> (T.Tag, g)
-randomTag g = randomIdentifier g & _1 %~ T.Tag
-
-data NameGen par pl = NameGen
-    { ngSplit :: (NameGen par pl, NameGen par pl)
-    , ngMakeName :: par -> pl -> (par, NameGen par pl)
-    }
-
-onNgMakeName ::
-    (NameGen par b ->
-     (par -> a -> (par, NameGen par b)) ->
-     par -> b -> (par, NameGen par b)) ->
-    NameGen par a -> NameGen par b
-onNgMakeName onMakeName =
-    go
-    where
-        go nameGen =
-            result
-            where
-                result =
-                    nameGen
-                    { ngMakeName =
-                        ngMakeName nameGen
-                        <&> Lens.mapped . _2 %~ go
-                        & onMakeName result
-                    , ngSplit =
-                        ngSplit nameGen
-                        & Lens.both %~ go
-                    }
-
-randomNameGen :: RandomGen g => g -> NameGen V.Var dummy
-randomNameGen g = NameGen
-    { ngSplit = Random.split g & Lens.both %~ randomNameGen
-    , ngMakeName = const . const $ randomVar g & _2 %~ randomNameGen
-    }
-
-randomizeParamIdsG ::
-    (a # V.Term -> n) ->
-    NameGen V.Var n -> Map V.Var V.Var ->
-    Ann a # V.Term -> Ann a # V.Term
-randomizeParamIdsG preNG gen initMap =
-    (`evalState` gen) . (`runReaderT` initMap) . go
-    where
-        go (Ann s v) =
-            case v of
-            V.BLam (V.TypedLam oldParamId typ body) ->
-                do
-                    newParamId <- lift . state $ makeName oldParamId s
-                    go body
-                        & Reader.local (Map.insert oldParamId newParamId)
-                        <&> V.TypedLam newParamId typ
-                        <&> V.BLam
-            V.BLeaf (V.LVar par) ->
-                Reader.ask <&> Map.lookup par <&> fromMaybe par
-                <&> V.LVar <&> V.BLeaf
-            V.BLeaf{}      -> def
-            V.BApp{}       -> def
-            V.BGetField{}  -> def
-            V.BRecExtend{} -> def
-            V.BCase{}      -> def
-            V.BInject{}    -> def
-            V.BToNom{}     -> def
-            <&> Ann s
-            where
-                def =
-                    htraverse
-                    ( \case
-                        HWitness V.W_Term_Term -> go
-                        HWitness V.W_Term_HCompose_Prune_Type -> pure
-                    ) v
-        makeName oldParamId s nameGen =
-            ngMakeName nameGen oldParamId $ preNG s

src/Lamdu/Sugar/Convert/Fragment.hs
@@ -11,7 +11,7 @@ module Lamdu.Sugar.Convert.Fragment
 import qualified Control.Lens as Lens
 import           Control.Monad.Except (MonadError(..))
 import           Control.Monad.ListT (ListT)
-import           Control.Monad.Once (OnceT, Typeable)
+import           Control.Monad.Once (OnceT, Typeable, MonadOnce(..), onceList)
 import           Control.Monad.State (State, runState, StateT(..), mapStateT)
 import qualified Control.Monad.State as State
 import           Control.Monad.Trans.Maybe (MaybeT(..))
@@ -44,7 +44,7 @@ import qualified Lamdu.Sugar.Internal.EntityId as EntityId
 import           Lamdu.Sugar.Types
 import           Revision.Deltum.Hyper (Write(..))
 import           Revision.Deltum.Transaction (Transaction)
-import qualified System.Random.Extended as Random
+import qualified Revision.Deltum.Transaction as Transaction
 
 import           Lamdu.Prelude
 
@@ -71,10 +71,18 @@ mkOptions ::
 mkOptions posInfo sugarContext argI argS exprPl =
     Hole.mkOptions posInfo (fragmentResultProcessor topEntityId argI) exprPl
     <&> (fragmentOptions <>)
-    <&> Lens.mapped %~ Hole.addWithoutDups mkSuggested
-    <&> Lens.mapped . Lens.mapped %~ snd
+    <&>
+    ( \mkOpts ->
+        Hole.addWithoutDups
+        <$> mkOpts
+        <*> ( runStateT (mkAppliedHoleSuggesteds sugarContext argI exprPl) (sugarContext ^. ConvertM.scInferContext)
+                & ListClass.toList
+                <&> Lens.mapped %~ fst
+            )
+        <&> Lens.mapped %~ snd
+    )
+    >>= ConvertM.convertOnce
     where
-        mkSuggested = mkAppliedHoleSuggesteds sugarContext argI exprPl
         fragmentOptions =
             [ App hole hole & V.BApp & Ann (Const ()) | Lens.nullOf (hVal . _BodyLam) argS ]
             & traverse
@@ -90,33 +98,34 @@ mkAppliedHoleSuggesteds ::
     ConvertM.Context m ->
     Ann (Input.Payload m a) # V.Term ->
     Input.Payload m a # V.Term ->
-    [(V.Val (), HoleOption EvalPrep InternalName (OnceT (T m)) (T m))]
+    StateT InferState (ListT (OnceT (T m)))
+        (V.Val (), HoleOption EvalPrep InternalName (OnceT (T m)) (T m))
 mkAppliedHoleSuggesteds sugarContext argI exprPl =
-    runStateT
-    ( Suggest.termTransforms (exprPl ^. Input.inferScope) (WriteNew :*:) (^. _2)
-        ( argI & hflipped %~
-            hmap
-            ( Proxy @(Recursively (InferOfConstraint HFunctor)) #*#
-                \w ->
-                withDict (recursively (p0 w)) $
-                withDict (inferOfConstraint (Proxy @HFunctor) w)
-                onPl
-            )
-        )
-    ) (sugarContext ^. ConvertM.scInferContext)
-    <&> onSuggestion
+    do
+        (sugg, newInferCtx) <-
+            runStateT
+            ( Suggest.termTransforms ((lift . lift . lift) Hole.genLamVar) (exprPl ^. Input.inferScope) (WriteNew :*:) (^. _2)
+                ( argI & hflipped %~
+                    hmap
+                    ( Proxy @(Recursively (InferOfConstraint HFunctor)) #*#
+                        \w ->
+                        withDict (recursively (p0 w)) $
+                        withDict (inferOfConstraint (Proxy @HFunctor) w)
+                        onPl
+                    )
+                )
+            ) (sugarContext ^. ConvertM.scInferContext)
+        mkOptionFromFragment
+            (sugarContext & ConvertM.scInferContext .~ newInferCtx)
+            exprPl sugg
+            & lift & lift
+            <&> (,) (sugg & hflipped %~ hmap (\_ _ -> Const ()))
     where
         p0 :: proxy h -> Proxy (InferOfConstraint HFunctor h)
         p0 _ = Proxy
         onPl pl =
             ExistingRef (pl ^. Input.stored . iref) :*:
             (pl ^. Input.inferRes & hflipped %~ hmap (const (^. _2)))
-        onSuggestion (sugg, newInferCtx) =
-            ( sugg & hflipped %~ hmap (\_ _ -> Const ())
-            , mkOptionFromFragment
-                (sugarContext & ConvertM.scInferContext .~ newInferCtx)
-                exprPl sugg
-            )
 
 checkTypeMatch :: Monad m => UVar # T.Type -> UVar # T.Type -> ConvertM m Bool
 checkTypeMatch x y =
@@ -339,32 +348,29 @@ mkOptionFromFragment ::
     ConvertM.Context m ->
     Input.Payload m a # V.Term ->
     Ann (Write m :*: InferResult UVar) # V.Term ->
-    HoleOption EvalPrep InternalName (OnceT (T m)) (T m)
+    OnceT (T m) (HoleOption EvalPrep InternalName (OnceT (T m)) (T m))
 mkOptionFromFragment sugarContext exprPl x =
     HoleOption
-    { _hoEntityId =
-        x & hflipped %~ hmap (\_ _ -> Const ())
-        & show & Random.randFunc
-        & EntityId.EntityId
-    , _hoSearchTerms = Hole.mkHoleSearchTerms sugarContext exprPl baseExpr & lift -- TODO: Once?
-    , _hoResults =
-        do
-            newDeps <- Hole.loadNewDeps (depsProp ^. Property.pVal) (exprPl ^. Input.inferScope) x
-            let newSugarContext =
-                    sugarContext
-                    & ConvertM.scInferContext .~ inferCtx
-                    & ConvertM.scFrozenDeps . Property.pVal .~ newDeps
-            let updateDeps = (depsProp ^. Property.pSet) newDeps
-            pure
-                ( resultScore resolved
-                , Hole.mkResult (replaceFragment topEntityId 0)
-                    updateDeps exprPl result
-                    & ConvertM.run newSugarContext & join
-                )
-            & lift
-            <&> pure & ListClass.joinL
-    }
+    <$> (lift Transaction.newKey <&> EntityId.EntityId)
+    <*> once (lift (Hole.mkHoleSearchTerms sugarContext exprPl baseExpr))
+    <*> onceList res
     where
+        res =
+            do
+                newDeps <- Hole.loadNewDeps (depsProp ^. Property.pVal) (exprPl ^. Input.inferScope) x
+                let newSugarContext =
+                        sugarContext
+                        & ConvertM.scInferContext .~ inferCtx
+                        & ConvertM.scFrozenDeps . Property.pVal .~ newDeps
+                let updateDeps = (depsProp ^. Property.pSet) newDeps
+                pure
+                    ( resultScore resolved
+                    , Hole.mkResult (replaceFragment topEntityId 0)
+                        updateDeps exprPl result
+                        & ConvertM.run newSugarContext & join
+                    )
+                & lift
+                <&> pure & ListClass.joinL
         depsProp = sugarContext ^. ConvertM.scFrozenDeps
         (result, inferCtx) =
             runState

commit ab6200d8656ebb2a97a915f004cc30beaa956fc9

Oops fix suggestions of pattern matches broken in 2129b222
Was doing too-much type-driven completions I guess..

src/Lamdu/Sugar/Convert/Completions.hs
@@ -138,7 +138,7 @@ suggestCaseWith mkVar variantType resultType =
             field <- suggestLam mkVar resultType
             restType <- newTerm (T.TVariant rest) >>= mkCaseType
             restBody <- suggestCaseWith mkVar rest resultType
-            RowExtend tag <$> (field <&> Ann handlerType) <*> (restBody <&> Ann restType) <&> V.BRecExtend & pure
+            RowExtend tag <$> (field <&> Ann handlerType) <*> (restBody <&> Ann restType) <&> V.BCase & pure
         where
             mkCaseType which = FuncType which resultType & T.TFun & newTerm <&> (inferResult #)
     _ -> suggestLam mkVar resultType