225dccd6856cda9abe270706908fbf18897cb676
Yair Chuchem <yairchu@gmail.com>
2021-02-25 12:07:11 +0200

    Cleanup: inline one-liner

--- src/Lamdu/GUI/Expr/InjectEdit.hs
+++ src/Lamdu/GUI/Expr/InjectEdit.hs
@@ -1,22 +1,14 @@
-{-# LANGUAGE RankNTypes #-}
 module Lamdu.GUI.Expr.InjectEdit
     ( make
     ) where
 
-import           Control.Lens.Extended (OneOf)
-import           GUI.Momentu.Align (WithTextPos)
-import qualified GUI.Momentu.Direction as Dir
-import qualified GUI.Momentu.Element as Element
 import           GUI.Momentu.Glue ((/|/))
 import           GUI.Momentu.Responsive (Responsive)
 import qualified GUI.Momentu.Responsive as Responsive
 import qualified GUI.Momentu.Responsive.Expression as ResponsiveExpr
-import           GUI.Momentu.View (View)
 import qualified GUI.Momentu.Widgets.Grid as Grid
 import qualified GUI.Momentu.Widgets.Menu.Search as SearchMenu
 import qualified GUI.Momentu.Widgets.TextEdit as TextEdit
-import qualified GUI.Momentu.Widgets.TextView as TextView
-import           Lamdu.Config.Theme (Theme)
 import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
 import           Lamdu.GUI.Monad (GuiM)
 import           Lamdu.GUI.Styled (text, grammar)
@@ -31,13 +23,6 @@ import           Lamdu.Name (Name)
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
-
-injectIndicator ::
-    ( MonadReader env f, Has TextView.Style env, Has Theme env
-    , Element.HasAnimIdPrefix env, Has (Texts.Code Text) env
-    , Has Dir.Layout env
-    ) => OneOf Texts.Code -> f (WithTextPos View)
-injectIndicator l = grammar (text ["injectIndicator"] l)
 
 make ::
     ( Monad i, Monad o
@@ -54,6 +39,6 @@ make ::
     GuiM env i o (Responsive o)
 make (Ann (Const pl) (Const tag)) =
     maybe (pure id) (ResponsiveExpr.addParens ??) (ExprGui.mParensId pl)
-    <*> injectIndicator Texts.injectSymbol /|/ TagEdit.makeVariantTag tag
+    <*> grammar (text ["injectIndicator"] Texts.injectSymbol) /|/ TagEdit.makeVariantTag tag
     <&> Responsive.fromWithTextPos
     & stdWrap pl
