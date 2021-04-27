d8b7cdcbc6aa2038b95f724aaf00acd1e9b5f0e5
Yair Chuchem <yairchu@gmail.com>
2021-04-27 17:38:49 +0300

    Update hypertypes
    Has instance derivation fixes

--- nix/hypertypes.nix
+++ nix/hypertypes.nix
@@ -9,8 +9,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "hypertypes";
-    sha256 = "1z5q84lwj2xmvc3mbsdgy45583dfk0hmv61q3wdsqznm83pygz8d";
-    rev = "e63d3e66dcb9b384a61734f141bc3e6924fcf873";
+    sha256 = "1liy6fc0bh8952vbm2adfgvy59m6blgasqifhfzn9zw93fc0lyc1";
+    rev = "ac5c350d2add51094561d524ccae3e05a2cba80a";
   };
   libraryHaskellDepends = [
     base base-compat binary constraints containers deepseq generic-data lattices lens
