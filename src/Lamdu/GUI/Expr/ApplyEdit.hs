lamdu/lamdu

commit de3e83f26ecdb24f76c069960b2de9386c370182

Holes/fragments rewrite!
Much cleaner UI for fragments, and also much cleaner code for fragments and holes.

Notable additional changes in rewrite:
* Holes don't "open" unless you type something. This may change back later
* Doesn't display results as fragments when types don't match (but this looks cleaner)

src/Lamdu/GUI/Expr/ApplyEdit.hs
@@ -1,8 +1,9 @@
 module Lamdu.GUI.Expr.ApplyEdit
-    ( makeSimple, makePostfix, makeLabeled, makePostfixFunc
+    ( makeSimple, makePostfix, makeLabeled, makePostfixFunc, makeOperatorRow
     ) where
 
 import qualified Control.Lens as Lens
+import qualified GUI.Momentu as M
 import           GUI.Momentu ((/|/))
 import qualified GUI.Momentu.EventMap as E
 import qualified GUI.Momentu.I18N as MomentuTexts
@@ -62,15 +63,8 @@ makeLabeled (Ann (Const pl) apply) =
                         (E.toDoc env [has . MomentuTexts.edit, has . Texts.swapOperatorArgs]) s)
                 (ResponsiveExpr.boxSpacedMDisamb ?? ExprGui.mParensId pl)
                     <*> sequenceA
-                    [ GuiM.makeSubexpression l
-                        <&> swapAction Config.swapWithRightKeys
-                    , (Options.boxSpaced ?? Options.disambiguationNone)
-                        <*> sequenceA
-                        [ makeFunc GetVarEdit.Operator func
-                        , GuiM.makeSubexpression r
-                            <&> swapAction Config.swapWithLeftKeys
-                        ]
-                        >>= wrap
+                    [ GuiM.makeSubexpression l <&> swapAction Config.swapWithRightKeys
+                    , makeOperatorRow (swapAction Config.swapWithLeftKeys) func r >>= wrap
                     ]
     )
     where
@@ -78,6 +72,19 @@ makeLabeled (Ann (Const pl) apply) =
             (maybeAddAnnotationPl pl <&> (Widget.widget %~)) <*>
             addArgs apply x
         func = apply ^. Sugar.aFunc
+
+makeOperatorRow ::
+    _ =>
+    (Responsive o -> Responsive o) ->
+    (Annotated (ExprGui.Payload i o) # Const (Sugar.BinderVarRef Name o)) ->
+    ExprGui.Expr Sugar.Term i o ->
+    GuiM env i o (Responsive o)
+makeOperatorRow onR func r =
+    (Options.boxSpaced ?? Options.disambiguationNone)
+    <*> sequenceA
+    [ makeFunc GetVarEdit.Operator func
+    , GuiM.makeSubexpression r <&> onR
+    ]
 
 makeArgRow :: _ => ExprGui.Body Sugar.AnnotatedArg i o -> GuiM env i o (TaggedItem o)
 makeArgRow arg =
@@ -131,12 +138,17 @@ makePostfix (Ann (Const pl) (Sugar.PostfixApply arg func)) =
     , makePostfixFunc func
     ] & stdWrapParentExpr pl
 
+makePostfixFuncBody ::
+    _ => Widget.Id -> ExprGui.Body Sugar.PostfixFunc i o -> GuiM env i o (Responsive o)
+makePostfixFuncBody i (Sugar.PfCase x) = CaseEdit.make i x
+makePostfixFuncBody _ (Sugar.PfFromNom x) = NominalEdit.makeFromNom x
+makePostfixFuncBody _ (Sugar.PfGetField x) = GetFieldEdit.make x
+
 makePostfixFunc :: _ => ExprGui.Expr Sugar.PostfixFunc i o -> GuiM env i o (Responsive o)
 makePostfixFunc (Ann (Const pl) x) =
     (ResponsiveExpr.boxSpacedMDisamb ?? ExprGui.mParensId pl) <*>
-    ( case x of
-        Sugar.PfCase c -> Ann (Const pl) c & CaseEdit.make
-        Sugar.PfFromNom n -> Ann (Const pl) (Const n) & NominalEdit.makeFromNom
-        Sugar.PfGetField f -> GetFieldEdit.make pl f
-        <&> (:[])
-    ) & stdWrap pl
+    (makePostfixFuncBody (WidgetIds.fromExprPayload pl) x <&> (:[]))
+    & stdWrapParentExpr pl
+    -- Without adjusting anim-ids there is clash in fragment results such as
+    -- ".Maybe .case"
+    & local (M.animIdPrefix .~ Widget.toAnimId (WidgetIds.fromExprPayload pl))

src/Lamdu/GUI/Expr/CaseEdit.hs
@@ -25,14 +25,12 @@ import           Lamdu.Config.Theme.TextColors (TextColors)
 import qualified Lamdu.Config.Theme.TextColors as TextColors
 import qualified Lamdu.GUI.Expr.GetVarEdit as GetVarEdit
 import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
-import qualified Lamdu.GUI.Annotation as Annotation
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Monad as GuiM
 import           Lamdu.GUI.Styled (label, grammar)
 import qualified Lamdu.GUI.Styled as Styled
 import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
-import qualified Lamdu.GUI.Wrap as Wrap
 import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
 import           Lamdu.Name (Name(..))
@@ -51,8 +49,8 @@ doc env lens =
 addAltId :: Widget.Id -> Widget.Id
 addAltId = (`Widget.joinId` ["add alt"])
 
-make :: _ => ExprGui.Expr Sugar.Composite i o -> GuiM env i o (Responsive o)
-make (Ann (Const pl) (Sugar.Composite alts punned caseTail addAlt)) =
+make :: _ => Widget.Id -> ExprGui.Body Sugar.Composite i o -> GuiM env i o (Responsive o)
+make myId (Sugar.Composite alts punned caseTail addAlt) =
     do
         env <- Lens.view id
         altsGui <-
@@ -66,13 +64,10 @@ make (Ann (Const pl) (Sugar.Composite alts punned caseTail addAlt)) =
                 & E.keysEventMapMovesCursor (env ^. has . Config.caseAddAltKeys)
                     (doc env Texts.addAlt)
         header <- grammar (Label.make ".") M./|/ makeCaseLabel
-        (Annotation.maybeAddAnnotationPl pl <&> (Widget.widget %~)) <*>
-            ( Styled.addValFrame <*>
-                (Options.boxSpaced ?? Options.disambiguationNone ?? [header, altsGui]))
+        Styled.addValFrame <*>
+            (Options.boxSpaced ?? Options.disambiguationNone ?? [header, altsGui])
             <&> Widget.weakerEvents addAltEventMap
-    & Wrap.stdWrapParentExpr pl
     where
-        myId = WidgetIds.fromExprPayload pl
         headerId = Widget.joinId myId ["header"]
         altsId = Widget.joinId myId ["alts"]
         makeCaseLabel =

Lamdu.cabal
@@ -281,6 +281,7 @@ Library
                   , Lamdu.GUI.Expr.LambdaEdit
                   , Lamdu.GUI.Expr.LiteralEdit
                   , Lamdu.GUI.Expr.NominalEdit
+                  , Lamdu.GUI.Expr.OptionEdit
                   , Lamdu.GUI.Expr.RecordEdit
                   , Lamdu.GUI.Expr.TagEdit
                   , Lamdu.GUI.Annotation
@@ -355,6 +356,7 @@ Library
                   , Lamdu.Sugar.Convert.Load
                   , Lamdu.Sugar.Convert.Monad
                   , Lamdu.Sugar.Convert.Nominal
+                  , Lamdu.Sugar.Convert.Option
                   , Lamdu.Sugar.Convert.PostProcess
                   , Lamdu.Sugar.Convert.Record
                   , Lamdu.Sugar.Convert.TId

nix/momentu.nix
@@ -11,8 +11,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "momentu";
-    sha256 = "11klfzwb2nr9gbky9jvwbglqms238g1x08hia5vl1x72k1822c73";
-    rev = "136f28f027be13b657ca0c12ba74522590297aa8";
+    sha256 = "15wvz6yzzkn7yhnqin493pggxrzjsz23wsnkq49kz3rfcrpmjgxk";
+    rev = "2df16cf5f3be3552a28c0a42f9c82e8b58a0a93e";
   };
   libraryHaskellDepends = [
     GLFW-b HUnit OpenGL QuickCheck aeson base base-compat binary bytestring

src/Lamdu/GUI/Expr.hs
@@ -48,7 +48,7 @@ makeEditor (Ann (Const pl) body) =
     Sugar.BodyNullaryInject x -> editor pl x InjectEdit.makeNullary
     Sugar.BodyLeaf         l ->
         case l of
-        Sugar.LeafHole          -> HoleEdit.make pl
+        Sugar.LeafHole        x -> editor pl (Const x) HoleEdit.make
         Sugar.LeafLiteral     x -> editor pl (Const x) LiteralEdit.make
         Sugar.LeafInject      x -> editor pl (Const x) InjectEdit.make
         Sugar.LeafGetVar      x -> editor pl (Const x) GetVarEdit.make

src/Lamdu/GUI/Expr/EventMap.hs
@@ -5,10 +5,11 @@ module Lamdu.GUI.Expr.EventMap
     , addLetEventMap
     , makeLiteralNumberEventMap
     , makeLiteralTextEventMap
-    , makeLiteralEventMap
+    , allowedSearchTerm
     ) where
 
 import qualified Control.Lens as Lens
+import qualified Data.Char as Char
 import qualified Data.Text as Text
 import qualified GUI.Momentu.Direction as Dir
 import           GUI.Momentu.EventMap (EventMap)
@@ -165,6 +166,7 @@ transformSearchTerm =
         transform c =
             do
                 guard (c `notElem` Chars.operator)
+                guard (allowedSearchTerm (Text.singleton c))
                 pure (Text.singleton c)
         searchStrRemainder = eventCtx ^. Widget.ePrevTextRemainder
         acceptOp = (>= exprInfoMinOpPrec exprInfo) . precedence
@@ -260,3 +262,27 @@ makeLiteralTextEventMap =
 
 makeLiteralEventMap :: _ => m ((Sugar.Literal Identity -> o Sugar.EntityId) -> EventMap (o GuiState.Update))
 makeLiteralEventMap = (<>) <$> makeLiteralTextEventMap <*> makeLiteralNumberEventMap ""
+
+allowedSearchTerm :: Text -> Bool
+allowedSearchTerm searchTerm =
+    any (searchTerm &)
+    [ Text.all (`elem` Chars.operator)
+    , isNameOrPrefixed
+    , (`elem` ["\\", "{", "}"])
+    ]
+
+isNameOrPrefixed :: Text -> Bool
+isNameOrPrefixed t =
+    case Text.uncons t of
+    Nothing -> True
+    Just (x, xs) | x `elem` prefixes -> isAlphaNumericName xs
+    Just _ -> isAlphaNumericName t
+    where
+        prefixes :: String
+        prefixes = ".'"
+
+isAlphaNumericName :: Text -> Bool
+isAlphaNumericName suffix =
+    case Text.uncons suffix of
+    Nothing -> True
+    Just (x, xs) -> Char.isAlpha x && Text.all Char.isAlphaNum xs

src/Lamdu/GUI/Expr/FragmentEdit.hs
@@ -9,23 +9,35 @@ import qualified GUI.Momentu.Direction as Dir
 import qualified GUI.Momentu.Element as Element
 import qualified GUI.Momentu.EventMap as E
 import qualified GUI.Momentu.I18N as MomentuTexts
+import qualified GUI.Momentu.MetaKey as MetaKey
 import           GUI.Momentu.Responsive (Responsive(..))
 import qualified GUI.Momentu.Responsive as Responsive
 import qualified GUI.Momentu.Responsive.Expression as ResponsiveExpr
 import qualified GUI.Momentu.State as GuiState
 import qualified GUI.Momentu.Widget as Widget
 import qualified GUI.Momentu.Widgets.Label as Label
+import qualified GUI.Momentu.Widgets.Menu as Menu
+import qualified GUI.Momentu.Widgets.Menu.Search as SearchMenu
 import qualified GUI.Momentu.Widgets.Spacer as Spacer
 import qualified Lamdu.Config as Config
 import qualified Lamdu.Config.Theme as Theme
 import qualified Lamdu.Config.Theme.ValAnnotation as ValAnnotation
+import qualified Lamdu.I18N.Code as Texts
 import           Lamdu.GUI.Annotation (addInferredType, shrinkValAnnotationsIfNeeded)
+import qualified Lamdu.GUI.Expr.ApplyEdit as ApplyEdit
+import qualified Lamdu.GUI.Expr.EventMap as ExprEventMap
+import qualified Lamdu.GUI.Expr.GetVarEdit as GetVarEdit
+import qualified Lamdu.GUI.Expr.InjectEdit as InjectEdit
+import qualified Lamdu.GUI.Expr.NominalEdit as NominalEdit
+import           Lamdu.GUI.Expr.OptionEdit
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Monad as GuiM
+import           Lamdu.GUI.Styled (label, grammar)
 import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
-import           Lamdu.GUI.Wrap (stdWrapParentExpr)
+import           Lamdu.GUI.Wrap (stdWrapParentExpr, stdWrap)
 import qualified Lamdu.I18N.CodeUI as Texts
+import           Lamdu.Name (Name)
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
@@ -42,7 +54,14 @@ make (Ann (Const pl) fragment) =
 
         fragmentExprGui <- fragment ^. Sugar.fExpr & GuiM.makeSubexpression
 
-        qmarkView <- Label.make "?"
+        searchMenu <-
+            SearchMenu.make
+            (SearchMenu.searchTermEdit menuId (pure . ExprEventMap.allowedSearchTerm))
+            (makeResults (fragment ^. Sugar.fOptions)) M.empty menuId
+            ?? Menu.AnyPlace
+            & local (has . SearchMenu.emptyStrings . Lens.mapped .~ "?")
+            -- Space goes to next hole in target (not necessarily visible)
+            & local (has . Config.menu . Menu.keysPickOptionAndGotoNext <>~ [MetaKey.MetaKey MetaKey.noMods MetaKey.Key'Space])
 
         let healKeys = env ^. has . Config.healKeys
         let healChars =
@@ -66,12 +85,13 @@ make (Ann (Const pl) fragment) =
             & Element.locallyAugmented ("inner type"::Text)
         hbox
             [ fragmentExprGui
-            , qmarkView & M.tValue %~ Widget.fromView & Responsive.fromWithTextPos
+            , Responsive.fromWithTextPos searchMenu
             ]
             & Widget.widget %~ addInnerType
             & pure & stdWrapParentExpr pl
             <&> Widget.weakerEvents (healEventMap healKeys healChars env)
     where
+        menuId = WidgetIds.fromExprPayload pl & WidgetIds.fragmentHoleId
         lineBelow color animId spacing ann =
             ann
             & Element.setLayeredImage . Element.layers . Lens.ix 0 %~ (<> line)
@@ -87,3 +107,43 @@ make (Ann (Const pl) fragment) =
             where
                 action = fragment ^. Sugar.fHeal <&> WidgetIds.fromEntityId
                 doc = fragmentDoc env (has . Texts.heal)
+
+makeResults ::
+    _ =>
+    i (Sugar.Query Text -> i [Sugar.Option Sugar.FragOpt Name i o]) ->
+    SearchMenu.ResultsContext ->
+    GuiM env i o (Menu.OptionList (Menu.Option (GuiM env i o) o))
+makeResults opts ctx
+    | ctx ^. SearchMenu.rSearchTerm == "" = pure Menu.TooMany
+    | otherwise =
+        do
+            c <- Lens.view (has . Config.completion . Config.completionResultCount)
+            GuiM.im opts <*>
+                makeQuery ctx
+                >>= GuiM.im
+                <&> take c
+                <&> Lens.mapped %~ makeResult makeFragOpt ctx
+                <&> Menu.FullList
+
+makeFragOpt :: _ => ExprGui.Expr Sugar.FragOpt i o -> GuiM env i o (Responsive o)
+makeFragOpt (Ann (Const a) b) =
+    case b of
+    Sugar.FragPostfix x ->
+        (ResponsiveExpr.boxSpacedMDisamb ?? Nothing)
+        <*> traverse ApplyEdit.makePostfixFunc x
+    Sugar.FragInject x -> InjectEdit.make (Ann (Const a) (Const x))
+    Sugar.FragGetVar x -> GetVarEdit.make (Ann (Const a) (Const x))
+    Sugar.FragOp x -> makeFragOperator x
+    Sugar.FragToNom x -> NominalEdit.label x <&> Responsive.fromTextView & stdWrap a
+    Sugar.FragIf t ->
+        (grammar (label Texts.if_) M./|/ grammar (Label.make ":")) M./|/
+        Spacer.stdHSpace M./|/ GuiM.makeSubexpression t
+    Sugar.FragLam -> grammar (label Texts.lam) <&> Responsive.fromTextView & stdWrap a
+    -- Reproduction of behaviour from Lamdu.GUI.Expr.make,
+    -- otherwise fragment editors would have clashing anim ids
+    & local (M.animIdPrefix .~ Widget.toAnimId myId)
+    where
+        myId = WidgetIds.fromExprPayload a
+
+makeFragOperator :: _ => ExprGui.Body Sugar.FragOperator i o -> GuiM env i o (Responsive o)
+makeFragOperator (Sugar.FragOperator f arg) = ApplyEdit.makeOperatorRow id f arg

src/Lamdu/GUI/Expr/GetFieldEdit.hs
@@ -9,15 +9,10 @@ import qualified GUI.Momentu.Widgets.Label as Label
 import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Styled as Styled
-import qualified Lamdu.GUI.Types as ExprGui
-import           Lamdu.GUI.Wrap (stdWrapParentExpr)
 import           Lamdu.Name (Name)
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
 
-make :: _ => ExprGui.Payload i o -> Sugar.TagRef Name i o -> GuiM env i o (Responsive o)
-make pl tag =
-    Styled.grammar (Label.make ".") /|/ TagEdit.makeRecordTag tag
-    <&> Responsive.fromWithTextPos
-    & stdWrapParentExpr pl
+make :: _ => Sugar.TagRef Name i o -> GuiM env i o (Responsive o)
+make tag = Styled.grammar (Label.make ".") /|/ TagEdit.makeRecordTag tag <&> Responsive.fromWithTextPos

commit bbeda2215c531f8517af340d60450204ced3351d

Oops "," should add field on case when the whole expr is selected
Seems like it was broken in de3e83f26ecdb24f76

src/Lamdu/GUI/Expr/ApplyEdit.hs
@@ -138,17 +138,14 @@ makePostfix (Ann (Const pl) (Sugar.PostfixApply arg func)) =
     , makePostfixFunc func
     ] & stdWrapParentExpr pl
 
-makePostfixFuncBody ::
-    _ => Widget.Id -> ExprGui.Body Sugar.PostfixFunc i o -> GuiM env i o (Responsive o)
-makePostfixFuncBody i (Sugar.PfCase x) = CaseEdit.make i x
-makePostfixFuncBody _ (Sugar.PfFromNom x) = NominalEdit.makeFromNom x
-makePostfixFuncBody _ (Sugar.PfGetField x) = GetFieldEdit.make x
-
 makePostfixFunc :: _ => ExprGui.Expr Sugar.PostfixFunc i o -> GuiM env i o (Responsive o)
-makePostfixFunc (Ann (Const pl) x) =
+makePostfixFunc (Ann (Const pl) b) =
     (ResponsiveExpr.boxSpacedMDisamb ?? ExprGui.mParensId pl) <*>
-    (makePostfixFuncBody (WidgetIds.fromExprPayload pl) x <&> (:[]))
-    & stdWrapParentExpr pl
+    ( case b of
+        Sugar.PfCase x -> CaseEdit.make (Ann (Const pl) x)
+        Sugar.PfFromNom x -> NominalEdit.makeFromNom x & stdWrapParentExpr pl
+        Sugar.PfGetField x -> GetFieldEdit.make x & stdWrapParentExpr pl
+        <&> (:[]))
     -- Without adjusting anim-ids there is clash in fragment results such as
     -- ".Maybe .case"
     & local (M.animIdPrefix .~ Widget.toAnimId (WidgetIds.fromExprPayload pl))

src/Lamdu/GUI/Expr/CaseEdit.hs
@@ -31,6 +31,7 @@ import           Lamdu.GUI.Styled (label, grammar)
 import qualified Lamdu.GUI.Styled as Styled
 import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
+import           Lamdu.GUI.Wrap (stdWrapParentExpr)
 import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
 import           Lamdu.Name (Name(..))
@@ -49,8 +50,8 @@ doc env lens =
 addAltId :: Widget.Id -> Widget.Id
 addAltId = (`Widget.joinId` ["add alt"])
 
-make :: _ => Widget.Id -> ExprGui.Body Sugar.Composite i o -> GuiM env i o (Responsive o)
-make myId (Sugar.Composite alts punned caseTail addAlt) =
+make :: _ => ExprGui.Expr Sugar.Composite i o -> GuiM env i o (Responsive o)
+make (Ann (Const pl) (Sugar.Composite alts punned caseTail addAlt)) =
     do
         env <- Lens.view id
         altsGui <-
@@ -66,8 +67,10 @@ make myId (Sugar.Composite alts punned caseTail addAlt) =
         header <- grammar (Label.make ".") M./|/ makeCaseLabel
         Styled.addValFrame <*>
             (Options.boxSpaced ?? Options.disambiguationNone ?? [header, altsGui])
+            & stdWrapParentExpr pl
             <&> Widget.weakerEvents addAltEventMap
     where
+        myId = WidgetIds.fromExprPayload pl
         headerId = Widget.joinId myId ["header"]
         altsId = Widget.joinId myId ["alts"]
         makeCaseLabel =