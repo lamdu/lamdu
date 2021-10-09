573c346551879ce8025074f7e0330c7ba6b8ecec
Eyal Lotem <eyal.lotem@gmail.com>
2021-10-09 23:23:56 +0300

    GotoDefinition: Explicit type constraints

--- src/Lamdu/GUI/CodeEdit/GotoDefinition.hs
+++ src/Lamdu/GUI/CodeEdit/GotoDefinition.hs
@@ -8,9 +8,12 @@ import qualified Data.ByteString.Char8 as BS8
 import           Data.MRUMemo (memo)
 import qualified Data.Text as Text
 import qualified GUI.Momentu as M
+import qualified GUI.Momentu.Direction as Dir
 import qualified GUI.Momentu.Widget as Widget
 import qualified GUI.Momentu.Widgets.Menu as Menu
 import qualified GUI.Momentu.Widgets.Menu.Search as SearchMenu
+import qualified GUI.Momentu.Widgets.TextView as TextView
+import           Lamdu.Config.Theme (Theme)
 import qualified Lamdu.Config.Theme as Theme
 import qualified Lamdu.Config.Theme.TextColors as TextColors
 import           Lamdu.Fuzzy (Fuzzy)
@@ -18,7 +21,8 @@ import qualified Lamdu.Fuzzy as Fuzzy
 import qualified Lamdu.GUI.Expr.GetVarEdit as GetVarEdit
 import qualified Lamdu.GUI.StatusBar.Common as StatusBar
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
-import qualified Lamdu.I18N.Navigation as Navigation
+import qualified Lamdu.I18N.Name as Texts
+import qualified Lamdu.I18N.Navigation as Texts
 import           Lamdu.Name (Name)
 import qualified Lamdu.Name as Name
 import qualified Lamdu.Sugar.Types as Sugar
@@ -46,13 +50,18 @@ nameToText name =
         collisionText Name.UnknownCollision = "?"
 
 makeOptions ::
-    _ => Sugar.Globals Name m o -> SearchMenu.ResultsContext -> m (Menu.OptionList (Menu.Option m o))
+    ( MonadReader env m, Has (Texts.Navigation Text) env, Has (Texts.Name Text) env
+    , M.HasCursor env, M.HasAnimIdPrefix env, Has Theme env, Has TextView.Style env
+    , Has Dir.Layout env
+    , Applicative o
+    ) =>
+    Sugar.Globals Name m o -> SearchMenu.ResultsContext -> m (Menu.OptionList (Menu.Option m o))
 makeOptions globals (SearchMenu.ResultsContext searchTerm prefix)
     | Text.null searchTerm =
         pure Menu.OptionList { Menu._olIsTruncated = False, Menu._olOptions = [] }
     | otherwise =
         do
-            goto <- Lens.view (has . Navigation.goto)
+            goto <- Lens.view (has . Texts.goto)
             let toRenderedOption nameRef widget =
                     Menu.RenderedOption
                     { Menu._rWidget = widget
@@ -95,7 +104,7 @@ makeOptions globals (SearchMenu.ResultsContext searchTerm prefix)
 make :: _ => Sugar.Globals Name m o -> m (StatusBar.StatusWidget o)
 make globals =
     do
-        goto <- Lens.view (has . Navigation.goto)
+        goto <- Lens.view (has . Texts.goto)
         SearchMenu.make (SearchMenu.searchTermEdit myId (pure . allowSearchTerm))
             (makeOptions globals) M.empty myId ?? Menu.Below
             & local (has . Theme.searchTerm %~ onTermStyle goto)
