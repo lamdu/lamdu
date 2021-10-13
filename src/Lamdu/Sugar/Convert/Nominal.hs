7f632a4fc2d86354168a3804afff5732406024fd
Eyal Lotem <eyal.lotem@gmail.com>
2021-10-13 16:10:00 +0300

    Sugar converts nominal params (missing opaque params)

--- src/Lamdu/Sugar/Convert/Nominal.hs
+++ src/Lamdu/Sugar/Convert/Nominal.hs
@@ -1,14 +1,23 @@
+{-# LANGUAGE TypeApplications, ScopedTypeVariables #-}
+
 module Lamdu.Sugar.Convert.Nominal
     ( convertToNom, convertFromNom, pane
     ) where
 
+import qualified Control.Lens as Lens
 import           Control.Monad.Once (OnceT)
 import           Control.Monad.Reader (ReaderT(..))
+import           Control.Monad.Reader.Instances ()
 import           Control.Monad.Trans.Except.Extended (runMatcherT, justToLeft)
-import           Hyper (_ANode)
+import qualified Data.Set as Set
+import           Hyper
 import           Hyper.Syntax.Nominal (ToNom(..))
 import qualified Hyper.Syntax.Nominal as Nominal
+import qualified Hyper.Syntax.Scheme as HyperScheme
+import           Hyper.Unify.QuantifiedVar (QVar)
+import           Lamdu.Calc.Identifier (Identifier(..))
 import qualified Lamdu.Calc.Term as V
+import qualified Lamdu.Calc.Type as T
 import           Lamdu.Data.Anchors (HasCodeAnchors)
 import qualified Lamdu.Data.Anchors as Anchors
 import qualified Lamdu.Expr.Load as ExprLoad
@@ -53,6 +62,42 @@ convertFromNom ::
 convertFromNom tid pl =
     ConvertTId.convert tid <&> PfFromNom <&> BodyPostfixFunc >>= addActions (Const ()) pl
 
+class NominalParamKind h where
+    qvarIdentifier :: Proxy h -> Lens.AnIso' (QVar h) Identifier
+    kindOf :: Proxy h -> ParamKind
+
+instance NominalParamKind T.Row where
+    qvarIdentifier _ = T._Var
+    kindOf _ = RowParam
+instance NominalParamKind T.Type where
+    qvarIdentifier _ = T._Var
+    kindOf _ = TypeParam
+
+convertNominalParams ::
+    forall m env p.
+    (Monad m, HasCodeAnchors env m, NominalParamKind p) =>
+    EntityId -> Set T.Tag -> HyperScheme.QVars # p ->
+    ReaderT env (OnceT (T m)) [NominalParam InternalName (OnceT (T m)) (T m)]
+convertNominalParams entityId tagList =
+    traverse (convParam (kindOf (Proxy @p))) . (^.. qvars)
+    where
+        replaceTag = todo "replace tag for nominal param"
+        convParam kind t =
+            ConvertTag.ref t Nothing (tagList & Lens.contains t .~ False)
+            (EntityId.ofTag entityId)
+            replaceTag
+            >>= lift
+            <&> \tagRef -> NominalParam
+            { _pName = tagRef
+            , _pKind = kind
+            }
+
+qvars ::
+    forall t.
+    NominalParamKind t =>
+    Lens.Fold (HyperScheme.QVars # t) T.Tag
+qvars = HyperScheme._QVars . Lens.ifolded . Lens.asIndex . Lens.cloneIso (qvarIdentifier (Proxy @t)) . Lens.to T.Tag
+
 pane ::
     (Monad m, HasCodeAnchors env m) =>
     env -> NominalId -> OnceT (T m) (PaneBody v InternalName (OnceT (T m)) (T m) a)
@@ -61,14 +106,19 @@ pane env nomId =
         nom <- ExprLoad.nominal nomId & lift
         tag <- ConvertTag.taggedEntityWith (env ^. Anchors.codeAnchors) Nothing nomId & join
         let entityId = EntityId.ofNominalPane nomId
-        body <- case nom of
-            Nothing -> pure Nothing
-            Just (Pure (Nominal.NominalDecl _params scheme)) ->
-                ConvertType.convertScheme (EntityId.currentTypeOf entityId) (Pure scheme)
-                & (`runReaderT` env) <&> Just & lift
+        (body, params) <-
+            case nom of
+            Nothing -> pure (Nothing, [])
+            Just (Pure (Nominal.NominalDecl params scheme)) ->
+                (,)
+                <$> (ConvertType.convertScheme (EntityId.currentTypeOf entityId) (Pure scheme) <&> Just)
+                <*> hfoldMap (Proxy @NominalParamKind #> convertNominalParams entityId tagList) params
+                & (`runReaderT` env)
+                where
+                    tagList = hfoldMap (Proxy @NominalParamKind #> (^.. qvars)) params & Set.fromList
         PaneNominal NominalPane
             { _npName = tag
-            , _npParams = [] -- TODO
+            , _npParams = params
             , _npEntityId = entityId
             , _npBody = body
             , _npNominalId = nomId
