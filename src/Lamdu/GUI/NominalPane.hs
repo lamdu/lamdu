2c15a42902b3dbe2540f67dd0c5d19c95a86a696
Yair Chuchem <yairchu@gmail.com>
2021-12-07 11:50:01 +0200

    Nominal pane: Use colon for separation

--- src/Lamdu/GUI/NominalPane.hs
+++ src/Lamdu/GUI/NominalPane.hs
@@ -8,13 +8,13 @@ import qualified GUI.Momentu.Responsive as Responsive
 import qualified GUI.Momentu.Responsive.Options as ResponsiveOptions
 import qualified GUI.Momentu.State as GuiState
 import qualified GUI.Momentu.Widget as Widget
+import qualified GUI.Momentu.Widgets.Label as Label
 import qualified Lamdu.Config.Theme.TextColors as TextColors
 import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Styled as Styled
 import qualified Lamdu.GUI.TypeView as TypeView
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
-import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
 import           Lamdu.Name (Name)
 import qualified Lamdu.Sugar.Types as Sugar
@@ -31,9 +31,9 @@ make nom =
         paramEdits <-
             nom ^. Sugar.npParams & traverse (TagEdit.makeRecordTag . (^. Sugar.pName))
             <&> map Responsive.fromWithTextPos
-        equals <- Styled.grammar (Styled.label Texts.assign) <&> Responsive.fromTextView
+        sep <- Styled.grammar (Label.make ":") <&> Responsive.fromTextView
         bodyEdit <- makeNominalPaneBody (nom ^. Sugar.npBody)
-        hbox [hbox ((nameEdit : paramEdits) <> [equals]), bodyEdit]
+        hbox [hbox ((nameEdit : paramEdits) <> [sep]), bodyEdit]
             & pure
         & local (M.animIdPrefix .~ Widget.toAnimId myId)
         & GuiState.assignCursor myId nameEditId
