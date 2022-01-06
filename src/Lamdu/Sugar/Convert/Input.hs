lamdu/lamdu

commit 14abf8c14695cf077e1fe55aac29803b94683a15

Simplify pre-processing of source ASTs
* Calculate both scopes and usages of variables in the same pass.
* The fact that varRefsOfLambda is initialized in a separate pass is not encoded in the types,
  but the previous commit at least adds tests to verify it.

src/Lamdu/Sugar/Convert/Input.hs
@@ -6,12 +6,11 @@ module Lamdu.Sugar.Convert.Input
         , varRefsOfLambda, entityId, inferRes, stored
         , userData, localsInScope, inferScope
         , inferredType, inferredTypeUVar
-    , PreparePayloadInput(..), preparePayloads
     , SugarInput(..)
     ) where
 
+import           Control.Lens.Extended ((~~>))
 import qualified Control.Lens as Lens
-import qualified Data.Map as Map
 import           Hyper
 import           Hyper.Infer (InferResult, inferResult)
 import           Hyper.Type.AST.FuncType (funcIn)
@@ -46,82 +45,36 @@ inferredType = inferRes . inferResult . _1
 inferredTypeUVar :: Lens' (Payload m a # V.Term) (UVar # Type)
 inferredTypeUVar = inferRes . inferResult . _2
 
-data PreparePayloadInput (pl :: HyperType) t = PreparePayloadInput
-    { ppEntityId :: EntityId -- used to build a map from Var => EntityId of all getVars
-    , ppMakePl :: [EntityId] -> pl t
-    }
-
-data PreparePayloadsRes (pl :: HyperType) t = PreparePayloadsRes
-    { ppVarMap :: Map V.Var [EntityId]
-    , ppRes :: Ann pl t
-    }
-
 class SugarInput t where
-    preparePayloadsH ::
-        Ann (PreparePayloadInput pl) # t ->
-        PreparePayloadsRes pl # t
-    initScopes ::
+    initialize ::
         V.Scope # UVar -> [(V.Var, Pure # Type)] ->
         Ann (Payload m a) # t ->
-        Ann (Payload m a) # t
-    initScopes _ _ = id
+        (Map V.Var [EntityId], Ann (Payload m a) # t)
+    initialize _ _ = (,) mempty
 
 instance SugarInput V.Term where
-    preparePayloadsH (Ann (PreparePayloadInput eId mkPayload) body) =
-        PreparePayloadsRes
-        { ppVarMap =
-            childrenVars
-            & case body of
-            V.BLeaf (V.LVar var) -> Lens.at var <>~ Just [eId]
-            V.BLam (V.TypedLam var _ _) -> Lens.at var .~ Nothing
-            _ -> id
-        , ppRes =
-            hmap (const ppRes) b
-            & Ann
-            ( case body of
-                V.BLam (V.TypedLam var _ _) -> childrenVars ^. Lens.ix var
-                _ -> []
-                & mkPayload
-            )
-        }
-        where
-            childrenVars = Map.unionsWith (++) (hfoldMap (const ((:[]) . ppVarMap)) b)
-            b = hmap (Proxy @SugarInput #> preparePayloadsH) body
-    initScopes iScope locals (Ann pl body) =
+    initialize iScope locals (Ann pl body) =
         case body of
         V.BLam (V.TypedLam var t b) ->
-            initScopes innerScope ((var, varType) : locals) b
-            & V.TypedLam var t
-            & V.BLam
+            ( usesOfVar
+            , V.TypedLam var t r & V.BLam & Ann (resPl & varRefsOfLambda .~ usesOfVar ^. Lens.ix var)
+            )
             where
+                (usesOfVar, r) = initialize innerScope ((var, varType) : locals) b
                 varType = pl ^?! inferredType . _Pure . T._TFun . funcIn
                 innerScope =
                     iScope
                     & V.scopeVarTypes . Lens.at var ?~
                         _HFlip # GMono (t ^. hAnn . inferRes . inferResult . _2)
-        x -> hmap (Proxy @SugarInput #> initScopes iScope locals) x
-        & Ann (pl & localsInScope <>~ locals & inferScope .~ iScope)
+        V.BLeaf (V.LVar v) ->
+            ( v ~~> [pl ^. entityId]
+            , V.LVar v & V.BLeaf & Ann resPl
+            )
+        x ->
+            htraverse (Proxy @SugarInput #> initialize iScope locals) x
+            <&> Ann resPl
+        where
+            resPl = pl & localsInScope <>~ locals & inferScope .~ iScope
 
-instance SugarInput (HCompose Prune Type) where
-    preparePayloadsH (Ann (PreparePayloadInput _ mkPayload) body) =
-        PreparePayloadsRes
-        { ppVarMap = mempty
-        , ppRes =
-            hmap (Proxy @SugarInput #> ppRes . preparePayloadsH) body
-            & Ann (mkPayload [])
-        }
-
-instance SugarInput (HCompose Prune Row) where
-    preparePayloadsH (Ann (PreparePayloadInput _ mkPayload) body) =
-        PreparePayloadsRes
-        { ppVarMap = mempty
-        , ppRes =
-            hmap (Proxy @SugarInput #> ppRes . preparePayloadsH) body
-            & Ann (mkPayload [])
-        }
-
-preparePayloads ::
-    SugarInput t =>
-    Ann (PreparePayloadInput pl) # t ->
-    Ann pl # t
-preparePayloads = ppRes . preparePayloadsH
+instance SugarInput (HCompose Prune Type)
+instance SugarInput (HCompose Prune Row)

src/Lamdu/Sugar/Convert/Hole.hs
@@ -404,24 +404,18 @@ writeResult inferCtx holeStored inferredVal =
             & assertSuccessfulInfer
             & fst
             & hflipped %~ hmap (const toPayload)
-            & Input.preparePayloads
             & pure
     where
         toPayload ((stored :*: Const a) :*: inferRes) =
             -- TODO: Evaluate hole results instead of Map.empty's?
-            Input.PreparePayloadInput
-            { Input.ppEntityId = eId
-            , Input.ppMakePl =
-                \varRefs ->
-                Input.Payload
-                { Input._varRefsOfLambda = varRefs
-                , Input._userData = a
-                , Input._inferRes = inferRes
-                , Input._inferScope = V.emptyScope -- TODO: HACK
-                , Input._stored = stored
-                , Input._entityId = eId
-                , Input._localsInScope = []
-                }
+            Input.Payload
+            { Input._varRefsOfLambda = [] -- TODO
+            , Input._userData = a
+            , Input._inferRes = inferRes
+            , Input._inferScope = V.emptyScope -- TODO: HACK
+            , Input._stored = stored
+            , Input._entityId = eId
+            , Input._localsInScope = []
             }
             where
                 eId = stored ^. ExprIRef.iref . _F & IRef.uuid & EntityId.EntityId
@@ -500,13 +494,14 @@ mkResult preConversion updateDeps holePl x =
                 (holePl ^. Input.stored) x
             <&> preConversion
             & lift
-            <&> Input.initScopes
+            <&> Input.initialize
                     (holePl ^. Input.inferScope)
                         -- TODO: this is kind of wrong
                         -- The scope for a proper term should be from after loading its infer deps
                         -- But that's only necessary for suggesting hole results?
                         -- And we are in a hole result here
                     (holePl ^. Input.localsInScope)
+            <&> (^. _2)
             <&> convertBinder
             <&> fmap convertPayloads
             >>= ConvertM.run sugarContext

src/Lamdu/Sugar/Convert/Load.hs
@@ -61,23 +61,17 @@ preparePayloads ::
 preparePayloads topLevelScope inferredVal =
     inferredVal
     & hflipped %~ hmap (const f)
-    & Input.preparePayloads
-    & Input.initScopes topLevelScope []
+    & Input.initialize topLevelScope [] & snd
     where
         f (valIProp :*: inferRes) =
-            Input.PreparePayloadInput
-            { Input.ppEntityId = eId
-            , Input.ppMakePl =
-                \varRefs ->
-                Input.Payload
-                { Input._varRefsOfLambda = varRefs
-                , Input._entityId = eId
-                , Input._localsInScope = []
-                , Input._stored = valIProp
-                , Input._inferRes = inferRes
-                , Input._inferScope = V.emptyScope -- UGLY: This is initialized by initScopes
-                , Input._userData = ()
-                }
+            Input.Payload
+            { Input._varRefsOfLambda = [] -- TODO
+            , Input._entityId = eId
+            , Input._localsInScope = []
+            , Input._stored = valIProp
+            , Input._inferRes = inferRes
+            , Input._inferScope = V.emptyScope -- UGLY: This is initialized by initScopes
+            , Input._userData = ()
             }
             where
                 eId = valIProp ^. ExprIRef.iref . _F & IRef.uuid & EntityId.EntityId

commit 2080d83f75d4eabac63a6ce5286958228a5a556b

Fix bug introduced in 14abf8c14695cf077e1fe55aac29803b94683a15

src/Lamdu/Sugar/Convert/Input.hs
@@ -11,6 +11,7 @@ module Lamdu.Sugar.Convert.Input
 
 import           Control.Lens.Extended ((~~>))
 import qualified Control.Lens as Lens
+import           Data.MMap (MMap)
 import           Hyper
 import           Hyper.Infer (InferResult, inferResult)
 import           Hyper.Syntax (funcIn)
@@ -49,7 +50,7 @@ class SugarInput t where
     prep ::
         V.Scope # UVar -> [(V.Var, Pure # Type)] ->
         Ann (Payload m a) # t ->
-        (Map V.Var [EntityId], Ann (Payload m a) # t)
+        (MMap V.Var [EntityId], Ann (Payload m a) # t)
     prep _ _ = (,) mempty
 
 preprocess ::