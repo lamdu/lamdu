0c755f9cb27d3a7581aea3bc87531c6540677635
Yair Chuchem <yairchu@gmail.com>
2022-01-16 23:41:37 +0200

    Extract LHS code from LambdaEdit to ParamsEdit towards re-use for light-let

--- src/Lamdu/GUI/Expr/LambdaEdit.hs
+++ src/Lamdu/GUI/Expr/LambdaEdit.hs
@@ -2,116 +2,24 @@ module Lamdu.GUI.Expr.LambdaEdit
     ( make
     ) where
 
-import qualified Control.Lens as Lens
 import qualified GUI.Momentu as M
-import qualified GUI.Momentu.EventMap as E
-import qualified GUI.Momentu.Glue as Glue
 import qualified GUI.Momentu.I18N as MomentuTexts
-import           GUI.Momentu.ModKey (noMods)
-import qualified GUI.Momentu.ModKey as ModKey
 import           GUI.Momentu.Responsive (Responsive)
-import qualified GUI.Momentu.Responsive as Responsive
 import qualified GUI.Momentu.Responsive.Expression as ResponsiveExpr
 import qualified GUI.Momentu.Responsive.Options as Options
-import qualified GUI.Momentu.State as GuiState
 import qualified GUI.Momentu.Widget as Widget
-import qualified GUI.Momentu.Widgets.TextView as TextView
-import qualified Lamdu.Config as Config
 import qualified Lamdu.GUI.Expr.AssignmentEdit as AssignmentEdit
 import           Lamdu.GUI.Expr.EventMap (closeParenEvent)
-import qualified Lamdu.GUI.LightLambda as LightLambda
+import qualified Lamdu.GUI.Expr.ParamsEdit as ParamsEdit
 import           Lamdu.GUI.Monad (GuiM)
-import           Lamdu.GUI.Styled (label, grammar)
 import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import           Lamdu.GUI.Wrap (stdWrapParentExpr)
-import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
 import qualified Lamdu.I18N.Navigation as Texts
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
-import qualified Lamdu.Sugar.Lens as SugarLens
-
-addScopeEdit :: _ => m (Maybe (M.Widget o) -> Responsive o -> Responsive o)
-addScopeEdit =
-    Glue.mkGlue ?? Glue.Vertical
-    <&> (\(|---|) mScopeEdit ->
-                (|---| maybe M.empty (M.WithTextPos 0) mScopeEdit))
-
-mkLhsEdits :: _ => m (Maybe (Responsive o) -> Maybe (M.Widget o) -> [Responsive o])
-mkLhsEdits =
-    addScopeEdit <&> \add mParamsEdit mScopeEdit ->
-    mParamsEdit ^.. Lens._Just <&> add mScopeEdit
-
-mkExpanded :: _ => f (Maybe (Responsive o) -> Maybe (M.Widget o) -> [Responsive o])
-mkExpanded =
-    do
-        lam <- grammar (label Texts.lam) <&> Responsive.fromTextView
-        lhsEdits <- mkLhsEdits
-        arrow <- grammar (label Texts.arrow) <&> Responsive.fromTextView
-        pure (\mParamsEdit mScopeEdit -> lam : lhsEdits mParamsEdit mScopeEdit <> [arrow])
-
-lamId :: Widget.Id -> Widget.Id
-lamId = (`Widget.joinId` ["lam"])
-
-mkShrunk :: _ => [Sugar.EntityId] -> Widget.Id -> f (Maybe (M.Widget o) -> [Responsive o])
-mkShrunk paramIds myId =
-    do
-        env <- Lens.view id
-        let expandEventMap =
-                paramIds ^? Lens.traverse
-                & foldMap
-                  (E.keysEventMapMovesCursor
-                      (env ^. has . Config.jumpToDefinitionKeys)
-                      ( E.toDoc env
-                          [ has . MomentuTexts.view
-                          , has . Texts.expandLambdaParams
-                          ]
-                      ) . pure . WidgetIds.fromEntityId)
-        theme <- Lens.view has
-        lamLabel <-
-            (Widget.makeFocusableView ?? lamId myId <&> (M.tValue %~))
-            <*> grammar (label Texts.lam)
-            <&> Responsive.fromWithTextPos
-            & local (TextView.underline ?~ LightLambda.underline theme)
-        addScopeEd <- addScopeEdit
-        pure $ \mScopeEdit ->
-            [ addScopeEd mScopeEdit lamLabel
-              & M.weakerEvents expandEventMap
-            ]
-
-mkLightLambda ::
-    _ =>
-    Sugar.Params v a i o -> Widget.Id ->
-    f (Maybe (Responsive o) -> Maybe (M.Widget o) -> [Responsive o])
-mkLightLambda params myId =
-    do
-        isSelected <-
-            paramIds <&> WidgetIds.fromEntityId
-            & traverse (GuiState.isSubCursor ??)
-            <&> or
-        let shrinkKeys = [noMods ModKey.Key'Escape]
-        env <- Lens.view id
-        let shrinkEventMap =
-                E.keysEventMapMovesCursor shrinkKeys
-                (E.toDoc env
-                    [ has . MomentuTexts.view
-                    , has . Texts.shrinkLambdaParams
-                    ]) (pure (lamId myId))
-        if isSelected
-            then
-                 mkExpanded
-                 <&> Lens.mapped . Lens.mapped . Lens.mapped %~
-                     M.weakerEvents shrinkEventMap
-            else mkShrunk paramIds myId
-                 <&> \mk _mParamsEdit mScopeEdit -> mk mScopeEdit
-    where
-        paramIds =
-            case params of
-            Sugar.NullParam{} -> []
-            Sugar.VarParam (_, p) -> [p ^. Sugar.vpiTag . Sugar.oTag . Sugar.tagRefTag . Sugar.tagInstance]
-            Sugar.RecordParams ps -> ps ^.. SugarLens.taggedListItems . Sugar.tiTag . Sugar.tagRefTag . Sugar.tagInstance
 
 make :: _ => ExprGui.Expr Sugar.Lambda i o -> GuiM env i o (Responsive o)
 make (Ann (Const pl) lam) =
@@ -120,11 +28,7 @@ make (Ann (Const pl) lam) =
             AssignmentEdit.makeFunctionParts (lam ^. Sugar.lamApplyLimit)
             (Ann (Const pl) func) (WidgetIds.fromEntityId bodyId)
         rhsJumperEquals <- AssignmentEdit.makeJumpToRhs rhsId
-        paramsAndLabelEdits <-
-            case (lam ^. Sugar.lamMode, params) of
-            (_, Sugar.NullParam{}) -> mkLhsEdits ?? mParamsEdit ?? mScopeEdit
-            (Sugar.LightLambda, _) -> mkLightLambda params myId ?? mParamsEdit ?? mScopeEdit
-            _ -> mkExpanded ?? mParamsEdit ?? mScopeEdit <&> Lens.ix 0 %~ M.weakerEvents lhsEventMap
+        paramsAndLabelEdits <- ParamsEdit.makeLhs (lam ^. Sugar.lamMode) params mParamsEdit mScopeEdit lhsEventMap myId
         navigateOut <-
             closeParenEvent
             [has . MomentuTexts.navigation, has . Texts.lambda, has . Texts.leaveSubexpression]
--- src/Lamdu/GUI/Expr/ParamsEdit.hs
+++ src/Lamdu/GUI/Expr/ParamsEdit.hs
@@ -2,6 +2,7 @@ module Lamdu.GUI.Expr.ParamsEdit
     ( make
     , ScopeCursor(..), scopeCursor
     , IsScopeNavFocused(..)
+    , makeLhs
     ) where
 
 import           Control.Applicative ((<|>))
@@ -10,16 +11,21 @@ import           Data.CurAndPrev (CurAndPrev, current)
 import qualified GUI.Momentu as M
 import           GUI.Momentu.EventMap (EventMap)
 import qualified GUI.Momentu.EventMap as E
+import qualified GUI.Momentu.Glue as Glue
 import qualified GUI.Momentu.I18N as MomentuTexts
 import           GUI.Momentu.ModKey (noMods)
+import qualified GUI.Momentu.ModKey as ModKey
 import           GUI.Momentu.Responsive (Responsive)
+import qualified GUI.Momentu.Responsive as Responsive
 import qualified GUI.Momentu.Responsive.Options as Options
 import qualified GUI.Momentu.State as GuiState
 import qualified GUI.Momentu.Widget as Widget
 import qualified GUI.Momentu.Widgets.Menu as Menu
+import qualified GUI.Momentu.Widgets.TextView as TextView
 import qualified Lamdu.Config as Config
 import qualified Lamdu.GUI.Annotation as Annotation
 import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
+import qualified Lamdu.GUI.LightLambda as LightLambda
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Monad as GuiM
 import qualified Lamdu.GUI.ParamEdit as ParamEdit
@@ -30,6 +36,7 @@ import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
 import           Lamdu.Name (Name(..))
+import qualified Lamdu.Sugar.Lens as SugarLens
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
@@ -61,6 +68,94 @@ scopeCursor mChosenScope scopes =
             , sMPrevParamScope = Nothing
             , sMNextParamScope = scopes ^? Lens.ix 1
             }
+
+addScopeEdit :: _ => m (Maybe (M.Widget o) -> Responsive o -> Responsive o)
+addScopeEdit =
+    Glue.mkGlue ?? Glue.Vertical
+    <&> (\(|---|) mScopeEdit -> (|---| maybe M.empty (M.WithTextPos 0) mScopeEdit))
+
+mkLhsEdits :: _ => m (Maybe (Responsive o) -> Maybe (M.Widget o) -> [Responsive o])
+mkLhsEdits =
+    addScopeEdit <&> \add mParamsEdit mScopeEdit ->
+    mParamsEdit ^.. Lens._Just <&> add mScopeEdit
+
+mkExpanded :: _ => f (Maybe (Responsive o) -> Maybe (M.Widget o) -> [Responsive o])
+mkExpanded =
+    do
+        lam <- grammar (label Texts.lam) <&> Responsive.fromTextView
+        lhsEdits <- mkLhsEdits
+        arrow <- grammar (label Texts.arrow) <&> Responsive.fromTextView
+        pure (\mParamsEdit mScopeEdit -> lam : lhsEdits mParamsEdit mScopeEdit <> [arrow])
+
+mkShrunk :: _ => [Sugar.EntityId] -> Widget.Id -> f (Maybe (M.Widget o) -> [Responsive o])
+mkShrunk paramIds myId =
+    do
+        env <- Lens.view id
+        let expandEventMap =
+                paramIds ^? Lens.traverse
+                & foldMap
+                  (E.keysEventMapMovesCursor
+                      (env ^. has . Config.jumpToDefinitionKeys)
+                      ( E.toDoc env
+                          [ has . MomentuTexts.view
+                          , has . Texts.expandLambdaParams
+                          ]
+                      ) . pure . WidgetIds.fromEntityId)
+        theme <- Lens.view has
+        lamLabel <-
+            (Widget.makeFocusableView ?? lamId myId <&> (M.tValue %~))
+            <*> grammar (label Texts.lam)
+            <&> Responsive.fromWithTextPos
+            & local (TextView.underline ?~ LightLambda.underline theme)
+        addScopeEd <- addScopeEdit
+        pure $ \mScopeEdit ->
+            [ addScopeEd mScopeEdit lamLabel
+              & M.weakerEvents expandEventMap
+            ]
+
+lamId :: Widget.Id -> Widget.Id
+lamId = (`Widget.joinId` ["lam"])
+
+mkLightLambda ::
+    _ =>
+    Sugar.Params v a i o -> Widget.Id ->
+    f (Maybe (Responsive o) -> Maybe (M.Widget o) -> [Responsive o])
+mkLightLambda params myId =
+    do
+        isSelected <-
+            paramIds <&> WidgetIds.fromEntityId
+            & traverse (GuiState.isSubCursor ??)
+            <&> or
+        let shrinkKeys = [noMods ModKey.Key'Escape]
+        env <- Lens.view id
+        let shrinkEventMap =
+                E.keysEventMapMovesCursor shrinkKeys
+                (E.toDoc env
+                    [ has . MomentuTexts.view
+                    , has . Texts.shrinkLambdaParams
+                    ]) (pure (lamId myId))
+        if isSelected
+            then
+                 mkExpanded
+                 <&> Lens.mapped . Lens.mapped . Lens.mapped %~
+                     M.weakerEvents shrinkEventMap
+            else mkShrunk paramIds myId
+                 <&> \mk _mParamsEdit mScopeEdit -> mk mScopeEdit
+    where
+        paramIds =
+            case params of
+            Sugar.NullParam{} -> []
+            Sugar.VarParam (_, p) -> [p ^. Sugar.vpiTag . Sugar.oTag . Sugar.tagRefTag . Sugar.tagInstance]
+            Sugar.RecordParams ps -> ps ^.. SugarLens.taggedListItems . Sugar.tiTag . Sugar.tagRefTag . Sugar.tagInstance
+
+makeLhs ::
+    _ =>
+    Sugar.BinderMode -> Sugar.Params v a i o ->
+    Maybe (Responsive o) -> Maybe (Widget.Widget o) -> EventMap (o GuiState.Update) -> Widget.Id ->
+    m [Responsive o]
+makeLhs _ Sugar.NullParam{} mParamsEdit mScopeEdit _ _ = mkLhsEdits ?? mParamsEdit ?? mScopeEdit
+makeLhs Sugar.LightLambda params mParamsEdit mScopeEdit _ myId = mkLightLambda params myId ?? mParamsEdit ?? mScopeEdit
+makeLhs _ _ mParamsEdit mScopeEdit lhsEventMap _ = mkExpanded ?? mParamsEdit ?? mScopeEdit <&> Lens.ix 0 %~ M.weakerEvents lhsEventMap
 
 make ::
     _ =>
