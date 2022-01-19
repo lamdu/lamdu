lamdu/lamdu

commit 10528c0c747fd6f9a32a65d938142aa2f896c7b6

Syntax: Remove distinction (color highlighting etc) between let variables and parameters

src/Lamdu/Sugar/Names/Walk.hs
@@ -77,12 +77,9 @@ instance Walk m (Property o ParamKind) (Property o ParamKind) where walk = pure
 instance Walk m s t => Walk m (s, x) (t, x) where
     walk = _1 walk
 
-instance (a ~ OldName m, b ~ NewName m) => Walk m (ParamRef a o) (ParamRef b o) where
-    walk p = (pNameRef . nrName) (opGetName Nothing (binderAmbiguity (p ^. pBinderMode)) TaggedVar) p
-
 binderVarType :: VarForm name m -> NameType
-binderVarType GetLocal = TaggedVar
 binderVarType (GetDefinition _) = GlobalDef
+binderVarType _ = TaggedVar
 
 instance
     (a ~ OldName m, b ~ NewName m, Walk m fa fb) =>
@@ -132,7 +129,6 @@ toBinderVarRef mDisambig (VarRef nameRef form var inline) =
     ?? inline
 
 instance (a ~ OldName m, b ~ NewName m) => Walk m (GetVar a o) (GetVar b o) where
-    walk (GetParam x) = walk x <&> GetParam
     walk (GetVar x) = toBinderVarRef Nothing x <&> GetVar
     walk (GetParamsRecord x) =
         traverse (opGetName Nothing MayBeAmbiguous Tag) x <&> GetParamsRecord
@@ -380,10 +376,6 @@ instance ToBody PostfixApply where
     toBody (PostfixApply a f) =
         PostfixApply <$> toExpression a <*> toExpression f
 
-binderAmbiguity :: BinderMode -> IsUnambiguous
-binderAmbiguity LightLambda = Unambiguous
-binderAmbiguity _ = MayBeAmbiguous
-
 instance
     (a ~ OldName m, b ~ NewName m, i ~ IM m, Walk m pa pb) =>
     Walk m (NullaryInject a i o # Annotated pa) (NullaryInject b i o # Annotated pb) where
@@ -415,10 +407,13 @@ instance ToBody Term where
         BodyLabeledApply x -> x & toBody <&> BodyLabeledApply
         BodyPostfixFunc  x -> x & toBody <&> BodyPostfixFunc
         BodyToNom        x -> x & toBody <&> BodyToNom
-        BodyLam          x -> x & lamFunc (toFunction (binderAmbiguity (x ^. lamMode))) <&> BodyLam
         BodyFragment     x -> x & toBody <&> BodyFragment
         BodyNullaryInject x -> walk x <&> BodyNullaryInject
         BodyLeaf         x -> walk x <&> BodyLeaf
+        BodyLam x ->
+            x
+            & lamFunc (toFunction (if x ^. lamLightweight then Unambiguous else MayBeAmbiguous))
+            <&> BodyLam
 
 funcSignature :: LabeledApply v name i o a -> FunctionSignature
 funcSignature apply =

src/Lamdu/Config/Theme/TextColors.hs
@@ -27,8 +27,7 @@ data TextColors = TextColors
     -- Names:
     , _collisionSuffixTextColor :: Draw.Color
     , _definitionColor :: Draw.Color
-    , _parameterColor :: Draw.Color
-    , _letColor :: Draw.Color
+    , _variableColor :: Draw.Color
     , _recordTagColor :: Draw.Color
     , _caseTagColor :: Draw.Color
     , _argTagColor :: Draw.Color

src/Lamdu/GUI/Expr/BinderEdit.hs
@@ -65,7 +65,7 @@ makeLetEdit item =
         grammar (label Texts.let_)
             M./|/ Spacer.stdHSpace
             M./|/ (
-                TagEdit.makeBinderTagEdit TextColors.letColor (item ^. Sugar.lName) >>=
+                TagEdit.makeBinderTagEdit TextColors.variableColor (item ^. Sugar.lName) >>=
                 AssignmentEdit.make Nothing
                 (WidgetIds.fromEntityId (item ^. Sugar.lName . Sugar.oTag . Sugar.tagRefTag . Sugar.tagInstance))
                 binder

src/Lamdu/GUI/Expr/GetVarEdit.hs
@@ -69,7 +69,7 @@ makeParamsRecord myId paramsRecordVar =
                     let paramId = ["params", SBS8.pack (show (i :: Int))]
                     in
                     Widget.joinId myId paramId
-                    & makeSimpleView TextColors.parameterColor fieldName
+                    & makeSimpleView TextColors.variableColor fieldName
                     <&> Responsive.fromWithTextPos
                     & local (M.animIdPrefix %~ (<> paramId))
                 )
@@ -212,34 +212,25 @@ processDefinitionWidget (Sugar.DefTypeChanged info) myId mkLayout =
 
 makeGetBinder ::
     _ => Role -> Sugar.VarRef Name o -> Widget.Id -> GuiM env i o (M.TextWidget o)
-makeGetBinder role binderVar myId =
+makeGetBinder role var myId =
     do
         env <- Lens.view id
         let (color, processDef) =
-                case binderVar ^. Sugar.vForm of
-                Sugar.GetLocal -> (TextColors.letColor, id)
+                case var ^. Sugar.vForm of
                 Sugar.GetDefinition defForm ->
                     ( TextColors.definitionColor
                     , processDefinitionWidget defForm myId
                     )
-        makeNameRef role color myId (binderVar ^. Sugar.vNameRef)
+                _ -> (TextColors.variableColor, id)
+        mUnderline <-
+            case var ^. Sugar.vForm of
+            Sugar.GetLightParam -> Lens.view has <&> LightLambda.underline <&> (TextView.underline ?~)
+            _ -> pure id
+        makeNameRef role color myId (var ^. Sugar.vNameRef)
+            & local mUnderline
             <&> Align.tValue %~ Widget.weakerEvents
-                (makeInlineEventMap env (binderVar ^. Sugar.vInline))
+                (makeInlineEventMap env (var ^. Sugar.vInline))
             & processDef
-
-makeGetParam :: _ => Sugar.ParamRef Name o -> Widget.Id -> GuiM env i o (M.TextWidget o)
-makeGetParam param myId =
-    do
-        underline <- Lens.view has <&> LightLambda.underline
-        let mk = makeNameRef Normal TextColors.parameterColor myId (param ^. Sugar.pNameRef)
-        case param ^. Sugar.pBinderMode of
-            Sugar.LightLambda ->
-                mk
-                & local (TextView.underline ?~ underline)
-                & Styled.nameAtBinder name
-            Sugar.NormalBinder -> mk
-    where
-        name = param ^. Sugar.pNameRef . Sugar.nrName
 
 make ::
     _ =>
@@ -251,8 +242,6 @@ make (Ann (Const pl) (Const getVar)) =
         makeGetBinder Normal binderVar myId <&> Responsive.fromWithTextPos
     Sugar.GetParamsRecord paramsRecordVar ->
         makeParamsRecord myId paramsRecordVar
-    Sugar.GetParam param ->
-        makeGetParam param myId <&> Responsive.fromWithTextPos
     & stdWrap pl
     where
         myId = WidgetIds.fromExprPayload pl

src/Lamdu/GUI/Expr/LambdaEdit.hs
@@ -28,7 +28,7 @@ make (Ann (Const pl) lam) =
             AssignmentEdit.makeFunctionParts (lam ^. Sugar.lamApplyLimit)
             (Ann (Const pl) func) (WidgetIds.fromEntityId bodyId)
         rhsJumperEquals <- AssignmentEdit.makeJumpToRhs rhsId
-        paramsAndLabelEdits <- ParamsEdit.makeLhs (lam ^. Sugar.lamMode) params mParamsEdit mScopeEdit lhsEventMap myId
+        paramsAndLabelEdits <- ParamsEdit.makeLhs (lam ^. Sugar.lamLightweight) params mParamsEdit mScopeEdit lhsEventMap myId
         navigateOut <-
             closeParenEvent
             [has . MomentuTexts.navigation, has . Texts.lambda, has . Texts.leaveSubexpression]

src/Lamdu/GUI/Expr/ParamsEdit.hs
@@ -150,11 +150,11 @@ mkLightLambda params myId =
 
 makeLhs ::
     _ =>
-    Sugar.BinderMode -> Sugar.Params v a i o ->
+    Bool -> Sugar.Params v a i o ->
     Maybe (Responsive o) -> Maybe (Widget.Widget o) -> EventMap (o GuiState.Update) -> Widget.Id ->
     m [Responsive o]
 makeLhs _ Sugar.NullParam{} mParamsEdit mScopeEdit _ _ = mkLhsEdits ?? mParamsEdit ?? mScopeEdit
-makeLhs Sugar.LightLambda params mParamsEdit mScopeEdit _ myId = mkLightLambda params myId ?? mParamsEdit ?? mScopeEdit
+makeLhs True params mParamsEdit mScopeEdit _ myId = mkLightLambda params myId ?? mParamsEdit ?? mScopeEdit
 makeLhs _ _ mParamsEdit mScopeEdit lhsEventMap _ = mkExpanded ?? mParamsEdit ?? mScopeEdit <&> Lens.ix 0 %~ M.weakerEvents lhsEventMap
 
 make ::

src/Lamdu/GUI/Expr/TagEdit.hs
@@ -381,7 +381,7 @@ makeLHSTag onPickNext color mSetToAnon tag =
 
 makeParamTag :: _ => Maybe (o EntityId) -> Sugar.TagRef Name i o -> GuiM env i o (M.TextWidget o)
 makeParamTag =
-    makeLHSTag onPickNext TextColors.parameterColor
+    makeLHSTag onPickNext TextColors.variableColor
     where
         onPickNext pos = WidgetIds.fromEntityId pos & addItemId & Just
 

src/Lamdu/GUI/ParamEdit.hs
@@ -76,7 +76,7 @@ mkAddParam addParam myId =
         addParam & im
         >>= TagEdit.makeTagHoleEdit mkParamPickResult addId
         & local (has . Menu.configKeysPickOptionAndGotoNext <>~ [noMods M.Key'Space])
-        & Styled.withColor TextColors.parameterColor
+        & Styled.withColor TextColors.variableColor
         <&> Responsive.fromWithTextPos
         <&> (:[])
     where

commit 6d8598e43ec2146a44ce2e35bb07ab799e8413a1

Oops fix light param disambiguation broken in 10528c0c747f

src/Lamdu/Sugar/Names/Walk.hs
@@ -122,11 +122,14 @@ toBinderVarRef ::
 toBinderVarRef mDisambig (GetVar nameRef form var inline) =
     GetVar
     <$> ( nrName %%~
-          opGetName mDisambig MayBeAmbiguous (binderVarType form)
+          opGetName mDisambig amb (binderVarType form)
         ) nameRef
     <*> (_GetDefinition . _DefTypeChanged %%~ walk) form
     ?? var
     ?? inline
+    where
+        amb | Lens.has _GetLightParam form = Unambiguous
+            | otherwise = MayBeAmbiguous
 
 instance (a ~ OldName m, b ~ NewName m) => Walk m (GetVar a o) (GetVar b o) where
     walk = toBinderVarRef Nothing