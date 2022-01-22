lamdu/lamdu

commit 759bb4de14193e9474a8e99ee403dd75689b8d88

Syntax: ParamsRecord available only in hole results

src/Lamdu/Sugar/OrderTags.hs
@@ -183,6 +183,7 @@ instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.Term v n
         & pure
 
 instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.FragOpt v name i o)
+instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.HoleOpt v name i o)
 
 orderNode ::
     (MonadTransaction m i, Order i f) =>

src/Lamdu/GUI/Expr.hs
@@ -51,7 +51,7 @@ makeEditor (Ann (Const pl) body) =
         Sugar.LeafHole        x -> editor pl (Const x) HoleEdit.make
         Sugar.LeafLiteral     x -> editor pl (Const x) LiteralEdit.make
         Sugar.LeafInject      x -> editor pl (Const x) InjectEdit.make
-        Sugar.LeafGetVar      x -> editor pl (Const x) GetVarEdit.make
+        Sugar.LeafGetVar      x -> editor pl (Const x) (GetVarEdit.make GetVarEdit.Normal)
     & local
         (Element.animIdPrefix .~ Widget.toAnimId (WidgetIds.fromExprPayload pl))
 

src/Lamdu/GUI/Expr/ApplyEdit.hs
@@ -30,7 +30,7 @@ import qualified Lamdu.GUI.Monad as GuiM
 import qualified Lamdu.GUI.Styled as Styled
 import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
-import           Lamdu.GUI.Wrap (stdWrap, stdWrapParentExpr)
+import           Lamdu.GUI.Wrap (stdWrapParentExpr)
 import qualified Lamdu.GUI.Wrap as Wrap
 import qualified Lamdu.I18N.CodeUI as Texts
 import qualified Lamdu.I18N.Navigation as Texts
@@ -39,25 +39,12 @@ import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
 
-makeFunc ::
-    _ =>
-    GetVarEdit.Role ->
-    Annotated (ExprGui.Payload i o) # Const (Sugar.VarRef Name o) ->
-    GuiM env i o (Responsive o)
-makeFunc role func =
-    GetVarEdit.makeGetBinder role (func ^. hVal . Lens._Wrapped) myId
-    <&> Responsive.fromWithTextPos
-    & stdWrap pl
-    where
-        pl = func ^. annotation
-        myId = WidgetIds.fromExprPayload pl
-
 makeLabeled :: _ => ExprGui.Expr Sugar.LabeledApply i o -> GuiM env i o (Responsive o)
 makeLabeled (Ann (Const pl) apply) =
     ExprEventMap.add ExprEventMap.defaultOptions pl <*>
     ( Wrap.parentDelegator myId <*>
         case apply ^. Sugar.aMOpArgs of
-        Nothing -> makeFunc GetVarEdit.Normal func >>= wrap
+        Nothing -> GetVarEdit.make GetVarEdit.Normal func >>= wrap
         Just (Sugar.OperatorArgs l r s) ->
             do
                 env <- Lens.view id
@@ -94,13 +81,13 @@ makeLabeled (Ann (Const pl) apply) =
 makeOperatorRow ::
     _ =>
     (Responsive o -> Responsive o) ->
-    (Annotated (ExprGui.Payload i o) # Const (Sugar.VarRef Name o)) ->
+    (Annotated (ExprGui.Payload i o) # Const (Sugar.GetVar Name o)) ->
     ExprGui.Expr Sugar.Term i o ->
     GuiM env i o (Responsive o)
 makeOperatorRow onR func r =
     (Options.boxSpaced ?? Options.disambiguationNone)
     <*> sequenceA
-    [ makeFunc GetVarEdit.Operator func
+    [ GetVarEdit.make GetVarEdit.Operator func
     , GuiM.makeSubexpression r <&> onR
     ]
 

src/Lamdu/GUI/Expr/FragmentEdit.hs
@@ -28,6 +28,7 @@ import           Lamdu.GUI.Annotation (addInferredType, shrinkValAnnotationsIfNe
 import qualified Lamdu.GUI.Expr.ApplyEdit as ApplyEdit
 import qualified Lamdu.GUI.Expr.EventMap as ExprEventMap
 import qualified Lamdu.GUI.Expr.GetVarEdit as GetVarEdit
+import qualified Lamdu.GUI.Expr.HoleOptEdit as HoleOptEdit
 import qualified Lamdu.GUI.Expr.InjectEdit as InjectEdit
 import qualified Lamdu.GUI.Expr.NominalEdit as NominalEdit
 import           Lamdu.GUI.Expr.OptionEdit
@@ -150,13 +151,13 @@ makeFragOpt (Ann (Const a) b) =
         (ResponsiveExpr.boxSpacedMDisamb ?? Nothing)
         <*> traverse ApplyEdit.makePostfixFunc x
     Sugar.FragInject x -> InjectEdit.make (Ann (Const a) (Const x))
-    Sugar.FragApplyFunc x -> GetVarEdit.make (Ann (Const a) (Const x)) /|/ grammar (Label.make " _")
+    Sugar.FragApplyFunc x -> GetVarEdit.make GetVarEdit.Normal (Ann (Const a) (Const x)) /|/ grammar (Label.make " _")
     Sugar.FragOp x -> makeFragOperator x
     Sugar.FragToNom x -> NominalEdit.makeTIdView x & fromView
     Sugar.FragIf t ->
         (grammar (label Texts.if_) /|/ grammar (Label.make ":")) /|/
         Spacer.stdHSpace /|/ GuiM.makeSubexpression t
-    Sugar.FragArgument x -> GuiM.makeSubexpression (Ann (Const a) x)
+    Sugar.FragArgument x -> HoleOptEdit.make (Ann (Const a) x)
     Sugar.FragLam -> grammar (label Texts.lam) & fromView
     Sugar.FragDefer -> grammar (label Texts.defer) & fromView
     Sugar.FragWrapInRec x ->

src/Lamdu/GUI/Expr/GetVarEdit.hs
@@ -1,11 +1,10 @@
 module Lamdu.GUI.Expr.GetVarEdit
-    ( make, makeGetBinder, makeSimpleView
+    ( make, makeSimpleView
     , makePunnedVars
     , Role(..)
     ) where
 
 import qualified Control.Lens as Lens
-import qualified Data.ByteString.Char8 as SBS8
 import qualified GUI.Momentu as M
 import qualified GUI.Momentu.Align as Align
 import           GUI.Momentu.EventMap (EventMap)
@@ -53,31 +52,6 @@ makeSimpleView color name myId =
     (Widget.makeFocusableView ?? myId <&> (Align.tValue %~))
     <*> NameView.make name
     & Styled.withColor (Lens.cloneLens color)
-
-makeParamsRecord :: _ => Widget.Id -> Sugar.ParamsRecordVarRef Name -> m (Responsive f)
-makeParamsRecord myId paramsRecordVar =
-    do
-        respondToCursor <- Widget.respondToCursorPrefix ?? myId
-        (Options.box ?? Options.disambiguationNone)
-            <*> sequence
-            [ grammar (label Texts.paramsRecordOpener) <&> Responsive.fromTextView
-            , (Options.boxSpaced ?? Options.disambiguationNone)
-              <*>
-              ( fieldNames
-                & Lens.itraverse
-                (\i fieldName ->
-                    let paramId = ["params", SBS8.pack (show (i :: Int))]
-                    in
-                    Widget.joinId myId paramId
-                    & makeSimpleView TextColors.variableColor fieldName
-                    <&> Responsive.fromWithTextPos
-                    & local (M.animIdPrefix %~ (<> paramId))
-                )
-              )
-            , grammar (label Texts.recordCloser) <&> Responsive.fromTextView
-            ] <&> respondToCursor
-    where
-        Sugar.ParamsRecordVarRef fieldNames = paramsRecordVar
 
 data Role = Normal | Operator deriving Eq
 
@@ -210,9 +184,12 @@ processDefinitionWidget (Sugar.DefTypeChanged info) myId mkLayout =
     where
         hiddenId = myId `Widget.joinId` ["hidden"]
 
-makeGetBinder ::
-    _ => Role -> Sugar.VarRef Name o -> Widget.Id -> GuiM env i o (M.TextWidget o)
-makeGetBinder role var myId =
+make ::
+    _ =>
+    Role ->
+    Annotated (ExprGui.Payload i o) # Const (Sugar.GetVar Name o) ->
+    GuiM env i o (Responsive o)
+make role (Ann (Const pl) (Const var)) =
     do
         env <- Lens.view id
         let (color, processDef) =
@@ -231,18 +208,7 @@ makeGetBinder role var myId =
             <&> Align.tValue %~ Widget.weakerEvents
                 (makeInlineEventMap env (var ^. Sugar.vInline))
             & processDef
-
-make ::
-    _ =>
-    Annotated (ExprGui.Payload i o) # Const (Sugar.GetVar Name o) ->
-    GuiM env i o (Responsive o)
-make (Ann (Const pl) (Const getVar)) =
-    case getVar of
-    Sugar.GetVar binderVar ->
-        makeGetBinder Normal binderVar myId <&> Responsive.fromWithTextPos
-    Sugar.GetParamsRecord paramsRecordVar ->
-        makeParamsRecord myId paramsRecordVar
-    & stdWrap pl
+    <&> Responsive.fromWithTextPos & stdWrap pl
     where
         myId = WidgetIds.fromExprPayload pl
 
@@ -251,7 +217,7 @@ makePunnedVar ::
     Sugar.PunnedVar Name o # Annotated (ExprGui.Payload i o) ->
     GuiM env i o (Responsive o)
 makePunnedVar (Sugar.PunnedVar var tagId) =
-    make var
+    make Normal var
     & GuiState.assignCursor
         (WidgetIds.fromEntityId tagId)
         (WidgetIds.fromExprPayload (var ^. annotation))

src/Lamdu/GUI/Expr/HoleEdit.hs
@@ -18,6 +18,7 @@ import qualified Lamdu.Config as Config
 import qualified Lamdu.I18N.Navigation as Texts
 import           Lamdu.GUI.Annotation (maybeAddAnnotationPl)
 import qualified Lamdu.GUI.Expr.EventMap as ExprEventMap
+import qualified Lamdu.GUI.Expr.HoleOptEdit as HoleOptEdit
 import           Lamdu.GUI.Expr.OptionEdit
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Monad as GuiM
@@ -77,7 +78,7 @@ makeResults (Ann (Const pl) (Const hole)) ctx =
             >>= GuiM.im
             <&> take c
             <&> Lens.mapped %~
-                makeResult GuiM.makeBinder ctx .
+                makeResult HoleOptEdit.make ctx .
                 -- Initialize the operator precedence of results.
                 -- Without this results accept too many operators (and a test fails).
                 (Sugar.optionExpr . annotation . Sugar.plParenInfo . Sugar.piMinOpPrec .~

src/Lamdu/GUI/Expr/HoleOptEdit.hs
@@ -1,0 +1,52 @@
+module Lamdu.GUI.Expr.HoleOptEdit
+    ( make
+    ) where
+
+import qualified Control.Lens as Lens
+import qualified Data.ByteString.Char8 as SBS8
+import           Hyper
+import qualified GUI.Momentu as M
+import           GUI.Momentu.Responsive (Responsive)
+import qualified GUI.Momentu.Responsive as Responsive
+import qualified GUI.Momentu.Responsive.Options as Options
+import qualified GUI.Momentu.Widget as Widget
+import qualified Lamdu.Config.Theme.TextColors as TextColors
+import           Lamdu.GUI.Styled (grammar, label)
+import qualified Lamdu.I18N.Code as Texts
+import qualified Lamdu.GUI.Expr.GetVarEdit as GetVarEdit
+import           Lamdu.GUI.Monad (GuiM)
+import qualified Lamdu.GUI.Monad as GuiM
+import qualified Lamdu.GUI.Types as ExprGui
+import qualified Lamdu.GUI.WidgetIds as WidgetIds
+import qualified Lamdu.Sugar.Types as Sugar
+
+import           Lamdu.Prelude
+
+make ::
+    _ => ExprGui.Expr Sugar.HoleOpt i o -> GuiM env i o (Responsive o)
+make (Ann (Const pl) b) =
+    case b of
+    Sugar.HoleBinder x -> GuiM.makeBinder (Ann (Const pl) x)
+    Sugar.HoleVarsRecord fieldNames ->
+        do
+            respondToCursor <- Widget.respondToCursorPrefix ?? myId
+            (Options.box ?? Options.disambiguationNone)
+                <*> sequence
+                [ grammar (label Texts.recordOpener) <&> Responsive.fromTextView
+                , (Options.boxSpaced ?? Options.disambiguationNone)
+                <*>
+                ( fieldNames
+                    & Lens.itraverse
+                    (\i fieldName ->
+                        let paramId = ["params", SBS8.pack (show (i :: Int))]
+                        in
+                        Widget.joinId myId paramId
+                        & GetVarEdit.makeSimpleView TextColors.variableColor fieldName
+                        <&> Responsive.fromWithTextPos
+                        & local (M.animIdPrefix %~ (<> paramId))
+                    )
+                )
+                , grammar (label Texts.recordCloser) <&> Responsive.fromTextView
+                ] <&> respondToCursor
+    where
+        myId = WidgetIds.fromExprPayload pl

src/Lamdu/I18N/Code.hs
@@ -24,8 +24,6 @@ data Code a = Code
     , _elseIf :: a -- shorthand for else-if, "elif" a la Python
     , -- Inject
       _injectSymbol :: a
-    , -- Getvar
-      _paramsRecordOpener :: a
     , -- Lambda:
       _defer :: a
     , _lam :: a

commit 5f9915616553f7272d5154093df225dc95645c79

Oops fix tag ordering bug in hole results introduced in 759bb4de1419

src/Lamdu/Sugar/OrderTags.hs
@@ -182,8 +182,13 @@ instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.Term v n
         & Sugar.BodyLeaf
         & pure
 
-instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.FragOpt v name i o)
-instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.HoleOpt v name i o)
+instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.FragOpt v name i o) where
+    order (Sugar.FragArgument a) = order a <&> Sugar.FragArgument
+    order x = pure x
+
+instance (MonadTransaction m o, MonadTransaction m i) => Order i (Sugar.HoleOpt v name i o) where
+    order (Sugar.HoleBinder x) = order x <&> Sugar.HoleBinder
+    order (Sugar.HoleVarsRecord f) = Sugar.HoleVarsRecord f & pure -- TODO: Sort fields!
 
 orderNode ::
     (MonadTransaction m i, Order i f) =>