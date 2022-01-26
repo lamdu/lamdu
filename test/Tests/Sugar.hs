lamdu/lamdu

commit cec66e24de5c49ee8ba81c43e590327a0408ea77

Destructure for let-items, parameters syntax changed to avoid ambiguity!

nix/lamdu-calculus.nix
@@ -9,8 +9,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "lamdu-calculus";
-    sha256 = "0zgw9i9hff8jdvv71sdi99x1vkiacjxgwg6iayc3shbjbp6a8i9r";
-    rev = "4876da8f515670cefc18e58aa00a0839bf08ff9a";
+    sha256 = "13axrx3vfs9a0pxqv23c9khh5nra14jskkk4bncylm9zlgib3nqv";
+    rev = "75da23ca591204a1531c0b4539fef963bee1a5d7";
   };
   libraryHaskellDepends = [
     base base-compat base16-bytestring binary bytestring constraints containers

src/Lamdu/GUI/DefinitionEdit.hs
@@ -33,8 +33,9 @@ makeExprDefinition ::
     M.WidgetId ->
     GuiM env i o (Responsive o)
 makeExprDefinition def bodyExpr myId =
-    TagEdit.makeBinderTagEdit TextColors.definitionColor (def ^. Sugar.drName) >>=
-    AssignmentEdit.make (bodyExpr ^. Sugar.dePresentationMode) nameEditId (bodyExpr ^. Sugar.deContent)
+    TagEdit.makeBinderTagEdit TextColors.definitionColor (def ^. Sugar.drName)
+    <&> Responsive.fromWithTextPos
+    >>= AssignmentEdit.make (bodyExpr ^. Sugar.dePresentationMode) nameEditId (bodyExpr ^. Sugar.deContent)
     & GuiState.assignCursor myId nameEditId
     where
         nameEditId =

src/Lamdu/GUI/Expr/AssignmentEdit.hs
@@ -270,7 +270,7 @@ make ::
     Maybe (i (Property o Meta.PresentationMode)) ->
     Widget.Id ->
     ExprGui.Expr Sugar.Assignment i o ->
-    M.TextWidget o ->
+    Responsive o ->
     GuiM env i o (Responsive o)
 make pMode delParamDest assignment nameEdit =
     makeParts Sugar.UnlimitedFuncApply assignment delParamDest
@@ -286,9 +286,8 @@ make pMode delParamDest assignment nameEdit =
                     (PresentationModeEdit.make presentationChoiceId (x ^. Sugar.fParams))
         (|---|) <- Glue.mkGlue ?? Glue.Vertical
         let defNameEdit =
-                (nameEdit & M.tValue %~ Widget.weakerEvents (rhsJumperEquals <> lhsEventMap))
+                Widget.weakerEvents (rhsJumperEquals <> lhsEventMap) nameEdit
                 |---| fromMaybe M.empty mPresentationEdit
-                & Responsive.fromWithTextPos
         mParamEdit <-
             case mParamsEdit of
             Nothing -> pure Nothing

src/Lamdu/GUI/Expr/BinderEdit.hs
@@ -13,10 +13,9 @@ import qualified GUI.Momentu.Widget as Widget
 import qualified GUI.Momentu.Widgets.Spacer as Spacer
 import qualified Lamdu.Config as Config
 import qualified Lamdu.Config.Theme as Theme
-import qualified Lamdu.Config.Theme.TextColors as TextColors
 import qualified Lamdu.GUI.Expr.AssignmentEdit as AssignmentEdit
 import qualified Lamdu.GUI.Expr.EventMap as ExprEventMap
-import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
+import qualified Lamdu.GUI.Expr.ParamsEdit as ParamsEdit
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Monad as GuiM
 import           Lamdu.GUI.Styled (grammar, label)
@@ -31,8 +30,8 @@ import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
 
-makeLetEdit :: _ => ExprGui.Body Sugar.Let i o -> GuiM env i o (Responsive o)
-makeLetEdit item =
+makeLetEdit :: _ => ExprGui.Body Sugar.Let i o -> Widget.Id -> GuiM env i o (Responsive o)
+makeLetEdit item myId =
     do
         env <- Lens.view id
         let eventMap =
@@ -45,30 +44,11 @@ makeLetEdit item =
                         ])
                     . fmap ExprEventMap.extractCursor
                 ) (item ^? Sugar.lValue . annotation . Sugar.plActions . Sugar.extract)
-                <>
-                E.keysEventMapMovesCursor (Config.delKeys env)
-                (E.toDoc env
-                    [ has . MomentuTexts.edit
-                    , has . CodeUI.letClause
-                    , has . MomentuTexts.delete
-                    ])
-                (bodyId <$ item ^. Sugar.lDelete)
-                <>
-                foldMap
-                ( E.keysEventMapMovesCursor (env ^. has . Config.inlineKeys)
-                    (E.toDoc env
-                        [ has . MomentuTexts.navigation
-                        , has . Texts.jumpToFirstUse
-                        ])
-                    . pure . WidgetIds.fromEntityId
-                ) (item ^? Sugar.lUsages . Lens.ix 0)
+        (_, paramsEdit) <- ParamsEdit.make (pure Nothing) ParamsEdit.ScopeNavNotFocused myId myId bodyId (item ^. Sugar.lNames)
         grammar (label Texts.let_)
             M./|/ Spacer.stdHSpace
             M./|/ (
-                TagEdit.makeBinderTagEdit TextColors.variableColor (item ^. Sugar.lName) >>=
-                AssignmentEdit.make Nothing
-                (WidgetIds.fromEntityId (item ^. Sugar.lName . Sugar.oTag . Sugar.tagRefTag . Sugar.tagInstance))
-                binder
+                AssignmentEdit.make Nothing myId binder paramsEdit
                 <&> M.weakerEvents eventMap
                 <&> M.padAround (env ^. has . Theme.letItemPadding))
     where
@@ -103,7 +83,7 @@ makeBody (Ann (Const pl) (Sugar.BinderLet l)) =
         Responsive.vboxSpaced
             <*>
             sequence
-            [ makeLetEdit l <&> M.weakerEvents moveToInnerEventMap
+            [ makeLetEdit l myId <&> M.weakerEvents moveToInnerEventMap
             , make body
             ]
         & stdWrapParentExpr pl

src/Lamdu/GUI/Expr/ParamsEdit.hs
@@ -30,7 +30,6 @@ import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Monad as GuiM
 import qualified Lamdu.GUI.ParamEdit as ParamEdit
 import           Lamdu.GUI.Styled (grammar, label)
-import qualified Lamdu.GUI.Styled as Styled
 import qualified Lamdu.GUI.TaggedList as TaggedList
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import qualified Lamdu.I18N.Code as Texts
@@ -167,10 +166,7 @@ make ::
 make mScopeCursor isScopeNavFocused delVarBackwardsId myId bodyId params =
     makeBody annotationMode delVarBackwardsId myId bodyId params
     & GuiM.withLocalMScopeId (mScopeCursor <&> Lens.traversed %~ (^. Sugar.bParamScopeId) . sBinderScope)
-    >>= _2 mAddFrame
     where
-        mAddFrame [x] = pure x
-        mAddFrame xs = Styled.addValFrame <*> (Options.boxSpaced ?? Options.disambiguationNone ?? xs)
         mCurCursor =
             do
                 ScopeNavIsFocused == isScopeNavFocused & guard
@@ -186,7 +182,7 @@ makeBody ::
     Annotation.EvalAnnotationOptions ->
     Widget.Id -> Widget.Id -> Widget.Id ->
     Sugar.Params (Sugar.Annotation (Sugar.EvaluationScopes Name i) Name) Name i o ->
-    GuiM env i o (EventMap (o GuiState.Update), [Responsive o])
+    GuiM env i o (EventMap (o GuiState.Update), Responsive o)
 makeBody annotationOpts delVarBackwardsId lhsId rhsId params =
     case params of
     Sugar.ParamsRecord items -> ParamEdit.makeParams annotationOpts delVarBackwardsId rhsId items
@@ -200,9 +196,8 @@ makeBody annotationOpts delVarBackwardsId lhsId rhsId params =
             )
         <*> ( (Widget.makeFocusableView ?? nullParamId <&> (M.tValue %~))
                 <*> grammar (label Texts.defer)
-                >>= ParamEdit.addAnnotation annotationOpts (p ^. Sugar.vParam) nullParamId
+                >>= ParamEdit.addAnnotationAndEvents annotationOpts (p ^. Sugar.vParam) nullParamId
             )
-        <&> (:[])
         <&> (,) mempty
         where
             nullParamId = Widget.joinId lhsId ["param"]
@@ -212,10 +207,11 @@ makeBody annotationOpts delVarBackwardsId lhsId rhsId params =
             let eventMap =
                     TaggedList.delEventMap (has . Texts.parameter) (p ^. Sugar.vDelete) delVarBackwardsId rhsId env <>
                     ParamEdit.eventMapAddNextParamOrPickTag widgetId (p ^. Sugar.vAddNext) env
-            mconcat
+            (Options.boxSpaced ?? Options.disambiguationNone) <*>
+                mconcat
                 [ foldMap (`ParamEdit.mkAddParam` lhsId) (p ^? Sugar.vAddPrev . Sugar._AddNext)
                 , TagEdit.makeParamTag (Just (tag ^. Sugar.oPickAnon)) (tag ^. Sugar.oTag)
-                    >>= ParamEdit.addAnnotation annotationOpts (p ^. Sugar.vParam) widgetId
+                    >>= ParamEdit.addAnnotationAndEvents annotationOpts (p ^. Sugar.vParam) widgetId
                     <&> M.weakerEvents eventMap <&> (:[])
                 , foldMap (`ParamEdit.mkAddParam` widgetId) (p ^? Sugar.vAddNext . Sugar._AddNext)
                 ]

src/Lamdu/GUI/ParamEdit.hs
@@ -1,5 +1,5 @@
 module Lamdu.GUI.ParamEdit
-    ( makeParams, addAnnotation, eventMapAddNextParamOrPickTag, mkAddParam
+    ( makeParams, addAnnotationAndEvents, eventMapAddNextParamOrPickTag, mkAddParam
     , mkParamPickResult
     ) where
 
@@ -13,6 +13,7 @@ import qualified GUI.Momentu.I18N as MomentuTexts
 import           GUI.Momentu.ModKey (noMods)
 import           GUI.Momentu.Responsive (Responsive)
 import qualified GUI.Momentu.Responsive as Responsive
+import qualified GUI.Momentu.Responsive.Options as Options
 import qualified GUI.Momentu.State as GuiState
 import qualified GUI.Momentu.Widget as Widget
 import qualified GUI.Momentu.Widgets.Menu as Menu
@@ -25,7 +26,9 @@ import           Lamdu.GUI.Monad (GuiM, im)
 import qualified Lamdu.GUI.Styled as Styled
 import qualified Lamdu.GUI.TaggedList as TaggedList
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
+import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
+import qualified Lamdu.I18N.Navigation as Texts
 import           Lamdu.Name (Name)
 import qualified Lamdu.Sugar.Types as Sugar
 
@@ -50,21 +53,32 @@ mkParamPickResult tagInstance =
         WidgetIds.fromEntityId tagInstance & TagEdit.addItemId & Just
     }
 
-addAnnotation ::
+addAnnotationAndEvents ::
     _ =>
     Annotation.EvalAnnotationOptions ->
     Sugar.FuncParam (Sugar.Annotation (Sugar.EvaluationScopes Name i) Name) ->
     Widget.Id -> TextWidget o ->
     GuiM env i o (Responsive o)
-addAnnotation annotationOpts param myId widget =
+addAnnotationAndEvents annotationOpts param myId widget =
     do
         postProcessAnnotation <-
             GuiState.isSubCursor ?? myId
             <&> Annotation.postProcessAnnotationFromSelected
+        env <- Lens.view id
+        let eventMap =
+                foldMap
+                ( E.keysEventMapMovesCursor (env ^. has . Config.inlineKeys)
+                    (E.toDoc env
+                        [ has . MomentuTexts.navigation
+                        , has . Texts.jumpToFirstUse
+                        ])
+                    . pure . WidgetIds.fromEntityId
+                ) (param ^? Sugar.fpUsages . traverse)
         Annotation.maybeAddAnnotationWith annotationOpts postProcessAnnotation
             (param ^. Sugar.fpAnnotation)
             <&> (Widget.widget %~)
             ?? Responsive.fromWithTextPos widget
+            <&> M.weakerEvents eventMap
     & local (M.animIdPrefix .~ Widget.toAnimId myId)
 
 mkAddParam :: _ => i (Sugar.TagChoice Name a) -> Widget.Id -> GuiM env i a [Responsive a]
@@ -90,7 +104,7 @@ makeParam ::
 makeParam annotationOpts item =
     (<>)
     <$> (TagEdit.makeParamTag Nothing (item ^. TaggedList.iTag)
-            >>= addAnnotation annotationOpts (item ^. TaggedList.iValue) myId
+            >>= addAnnotationAndEvents annotationOpts (item ^. TaggedList.iValue) myId
             <&> M.weakerEvents (item ^. TaggedList.iEventMap) <&> (:[]))
     <*> mkAddParam (item ^. TaggedList.iAddAfter) myId
     & local (M.animIdPrefix .~ Widget.toAnimId myId)
@@ -102,7 +116,7 @@ makeParams ::
     Annotation.EvalAnnotationOptions ->
     Widget.Id -> Widget.Id ->
     Sugar.TaggedList Name i o (Sugar.FuncParam (Sugar.Annotation (Sugar.EvaluationScopes Name i) Name)) ->
-    GuiM env i o (EventMap (o GuiState.Update), [Responsive o])
+    GuiM env i o (EventMap (o GuiState.Update), Responsive o)
 makeParams annotationOpts prevId nextId items =
     do
         o <- Lens.view has <&> \d -> Lens.cloneLens . dirKey d Horizontal
@@ -113,7 +127,15 @@ makeParams annotationOpts prevId nextId items =
             , TaggedList._kOrderAfter = has . Config.orderDirKeys . o Forward
             }
         (addFirstEventMap, itemsR) <- TaggedList.make (has . Texts.parameter) keys prevId nextId items
-        (<>)
-            <$> mkAddParam (items ^. Sugar.tlAddFirst) prevId
-            <*> (traverse (makeParam annotationOpts) itemsR <&> concat)
+        (Options.box ?? Options.disambiguationNone) <*>
+            sequenceA
+            [ label (Styled.label Texts.recordOpener)
+            , (Options.boxSpaced ?? Options.disambiguationNone) <*>
+                ((<>)
+                <$> mkAddParam (items ^. Sugar.tlAddFirst) prevId
+                <*> (traverse (makeParam annotationOpts) itemsR <&> concat))
+            , label (Styled.label Texts.recordCloser)
+            ]
             <&> (,) addFirstEventMap
+    where
+        label x = Styled.grammar x <&> Responsive.fromTextView

src/Lamdu/Sugar/Annotations.hs
@@ -78,12 +78,16 @@ instance MarkBodyAnnotations v Binder where
     markBodyAnnotations = bBody markBodyAnnotations
 
 instance MarkBodyAnnotations v BinderBody where
-    markBodyAnnotations (BinderTerm body) =
-        markBodyAnnotations body & _2 %~ BinderTerm
-    markBodyAnnotations (BinderLet let_) =
+    markBodyAnnotations (BinderTerm body) = markBodyAnnotations body & _2 %~ BinderTerm
+    markBodyAnnotations (BinderLet let_) = markBodyAnnotations let_ & _2 %~ BinderLet
+
+instance  MarkBodyAnnotations v Let where
+    markBodyAnnotations l =
         ( neverShowAnnotations
-        , morphMap (Proxy @MarkAnnotations #?> markNodeAnnotations) let_
-            & BinderLet
+        , l { _lValue = l ^. lValue & markNodeAnnotations
+            , _lNames = l ^. lNames & annotations @v %~ (,) showAnnotationWhenVerbose
+            , _lBody = l ^. lBody & markNodeAnnotations
+            }
         )
 
 instance MarkBodyAnnotations v Assignment where

src/Lamdu/Sugar/Convert/Apply.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, TypeApplications #-}
 
 module Lamdu.Sugar.Convert.Apply
     ( convert
@@ -21,6 +21,8 @@ import qualified Lamdu.Calc.Term as V
 import qualified Lamdu.Calc.Type as T
 import qualified Lamdu.Expr.IRef as ExprIRef
 import qualified Lamdu.Sugar.Config as Config
+import           Lamdu.Sugar.Convert.Binder.Inline (inlineVar)
+import           Lamdu.Sugar.Convert.Binder.Float (makeFloatLetToOuterScope)
 import           Lamdu.Sugar.Convert.Expression.Actions (addActions)
 import           Lamdu.Sugar.Convert.Fragment (convertAppliedHole)
 import           Lamdu.Sugar.Convert.GetField (convertGetFieldParam)
@@ -48,9 +50,31 @@ convert app@(V.App funcI argI) exprPl =
         convertGetFieldParam app >>= Lens._Just addAct & MaybeT & justToLeft
         appS <-
             do
-                argS <- ConvertM.convertSubexpression argI & lift
+                mFloat <-
+                    case funcI ^. hVal of
+                    V.BLam lam ->
+                        makeFloatLetToOuterScope (exprPl ^. Input.stored . ExprIRef.setIref)
+                        (lam & hmap (Proxy @(Recursively HFunctor) #> hflipped %~ hmap (const (^. Input.stored))))
+                        argI & lift
+                        <&> Just
+                    _ -> pure Nothing
+                argS <-
+                    ConvertM.convertSubexpression argI & lift <&>
+                    case mFloat of
+                    Nothing -> id
+                    Just float -> annotation . pActions . extract .~ float
                 convertAppliedHole funcI exprPl argS & justToLeft
-                funcS <- ConvertM.convertSubexpression funcI & lift
+                let scopeUpdates =
+                        ( case inlineVar expr of
+                            Nothing -> id
+                            Just (v, mkInline) -> ConvertM.siLetItems . Lens.at v ?~ mkInline
+                        ) .
+                        case mFloat of
+                        Nothing -> id
+                        Just{} -> (ConvertM.siExtractPos ?~ pos) . (ConvertM.siFloatPos ?~ pos)
+                funcS <-
+                    ConvertM.convertSubexpression funcI & lift
+                    & local (ConvertM.scScopeInfo %~ scopeUpdates)
                 protectedSetToVal <- lift ConvertM.typeProtectedSetToVal
                 let dst = argI ^. hAnn . Input.stored . ExprIRef.iref
                 let fixDel d
@@ -64,7 +88,13 @@ convert app@(V.App funcI argI) exprPl =
         convertLabeled appS exprPl >>= lift . addAct & justToLeft
         convertPrefix appS exprPl >>= addAct & lift
     where
-        addAct = addActions (Ann exprPl (V.BApp app))
+        addAct = addActions expr
+        expr = Ann exprPl (V.BApp app)
+        pos =
+            ConvertM.OuterScopeInfo
+            { ConvertM._osiPos = exprPl ^. Input.stored
+            , ConvertM._osiScope = exprPl ^. Input.inferScope
+            }
 
 defParamsMatchArgs ::
     V.Var ->

commit 1b0ff3a0201f230b1bce7521b13a2080e79d0737

Add failing test for regression in cec66e24de5c

test/Tests/Sugar.hs
@@ -44,6 +44,7 @@ test =
     , testFloatToRepl
     , floatLetWithGlobalRef
     , lightConst
+    , addParamToLet
     , testHoleTypeShown
     , testUnnamed
     , testPunnedIso
@@ -147,6 +148,14 @@ testExtract =
         action =
             replBody . _BodyLam . lamFunc . fBody . annotation . plActions .
             extract
+
+addParamToLet :: Test
+addParamToLet =
+    testSugarActions "let-used-twice.json" [lift . (^?! action)]
+    & testCase "add-param-to-let"
+    where
+        action =
+            replLet . lValue . hVal . _BodyPlain . apAddFirstParam
 
 testCannotInline :: Test
 testCannotInline =