lamdu/lamdu

commit b10c78a1feff71405403f7764b5e4ec86508453b

Sugaring result is cached
Towards also caching hole/fragments completion,
which will both speed them up and help clean up their code

src/Lamdu/Editor.hs
@@ -9,9 +9,12 @@ import           Control.Concurrent.MVar
 import           Control.DeepSeq (deepseq)
 import qualified Control.Exception as E
 import qualified Control.Lens as Lens
+import           Control.Monad.Once (OnceT, _OnceT, OnceState, MonadOnce(..))
+import           Control.Monad.State (StateT(..), mapStateT)
 import           Control.Monad.Trans.FastWriter (evalWriterT)
 import qualified Data.Aeson.Config as AesonConfig
 import           Data.CurAndPrev (current)
+import           Data.IORef (IORef, newIORef, readIORef, writeIORef)
 import           Data.Property (Property(..), MkProperty', mkProperty)
 import qualified Data.Property as Property
 import           GHC.Stack (SrcLoc(..))
@@ -55,9 +58,11 @@ import           Lamdu.Settings (Settings(..))
 import qualified Lamdu.Settings as Settings
 import qualified Lamdu.Style.Make as MakeStyle
 import           Lamdu.Sugar (sugarWorkArea)
+import qualified Lamdu.VersionControl as VersionControl
 import           Revision.Deltum.IRef (IRef)
 import           Revision.Deltum.Transaction (Transaction)
 import qualified Revision.Deltum.Transaction as Transaction
+import           Revision.Deltum.Rev.Version (Version)
 import qualified System.Environment as Env
 import           System.IO (hPutStrLn, hFlush, stderr)
 import qualified System.Metrics as Metrics
@@ -191,9 +196,10 @@ runMainLoop ::
     M.Window -> MainLoop Handlers -> Sampler ->
     EvalManager.Evaluator -> Transaction.Store DbM ->
     MkProperty' IO Settings -> Cache -> Cache.Functions -> Debug.Monitors ->
+    IORef (Maybe EditorCache) ->
     IO ()
 runMainLoop ekg stateStorage subpixel win mainLoop configSampler
-    evaluator db mkSettingsProp cache cachedFunctions monitors
+    evaluator db mkSettingsProp cache cachedFunctions monitors cacheRef
     =
     do
         getFonts <- EditorFonts.makeGetFonts configSampler subpixel
@@ -208,7 +214,7 @@ runMainLoop ekg stateStorage subpixel win mainLoop configSampler
                     env <-
                         mkEnv cachedFunctions monitors fonts evaluator
                         configSampler mainEnv settingsProp
-                    makeRootWidget env monitors db evaluator sample
+                    makeRootWidget env monitors db evaluator sample cacheRef
         reportPerfCounters <- traverse makeReportPerfCounters ekg
         MainLoop.run mainLoop win MainLoop.Handlers
             { makeWidget = makeWidget
@@ -222,7 +228,7 @@ makeMainGui ::
     [TitledSelection Folder.Theme] -> [TitledSelection Folder.Language] ->
     (forall a. T DbLayout.DbM a -> IO a) ->
     Env -> GUIMain.Model DbLayout.ViewM ->
-    T DbLayout.DbM (Widget IO)
+    OnceT (T DbLayout.DbM) (Widget IO)
 makeMainGui themeNames langNames dbToIO env mkWorkArea =
     GUIMain.make themeNames langNames (env ^. Env.settings) env mkWorkArea
     <&> Widget.updates %~
@@ -264,12 +270,53 @@ titledThemeSelection (Selection lang) sel =
     , _selection = sel
     }
 
+data EditorCache = EditorCache
+    { ecState :: OnceState
+    , ecMkWorkArea :: GUIMain.Model DbLayout.ViewM
+    , ecVer :: Version DbM
+    }
+
+initCache :: Transaction.Store DbM -> Env -> IORef (Maybe EditorCache) -> IO EditorCache
+initCache db env cacheRef =
+    do
+        curVer <- DbLayout.runDbTransaction db VersionControl.getVersion
+        readIORef cacheRef
+            >>=
+            \case
+            Just cache | curVer == ecVer cache -> pure cache
+            _ ->
+                do
+                    (x, s) <-
+                        once (sugarWorkArea (Tag.getTagName env) env DbLayout.codeAnchors) ^. _OnceT
+                        & (`runStateT` mempty)
+                        & VersionControl.runAction
+                        & DbLayout.runDbTransaction db
+                    let r = EditorCache
+                            { ecState = s
+                            , ecMkWorkArea = x
+                            , ecVer = curVer
+                            }
+                    writeIORef cacheRef (Just r)
+                    pure r
+
+withCache ::
+    Transaction.Store DbM -> Env -> IORef (Maybe EditorCache) ->
+    (EditorCache -> OnceT IO a) ->
+    IO a
+withCache db env cacheRef action =
+    do
+        cache <- initCache db env cacheRef
+        (r, s) <- runStateT (action cache ^. _OnceT) (ecState cache)
+        writeIORef cacheRef (Just cache { ecState = s })
+        pure r
+
 makeRootWidget ::
     HasCallStack =>
     Env -> Debug.Monitors ->
     Transaction.Store DbM -> EvalManager.Evaluator -> ConfigSampler.Sample ->
+    IORef (Maybe EditorCache) ->
     IO (Widget IO)
-makeRootWidget env perfMonitors db evaluator sample =
+makeRootWidget env perfMonitors db evaluator sample cacheRef =
     do
         themeNames <-
             Folder.getSelections (Proxy @Theme)
@@ -278,9 +325,11 @@ makeRootWidget env perfMonitors db evaluator sample =
             Folder.getSelections (Proxy @Language)
             >>= traverse titledLangSelection
         let bgColor = env ^. Env.theme . Theme.backgroundColor
-        sugarWorkArea (Tag.getTagName env) env DbLayout.codeAnchors
-            & makeMainGui themeNames langNames dbToIO env
-            & dbToIO
+        withCache db env cacheRef
+            (\cache ->
+                makeMainGui themeNames langNames dbToIO env (ecMkWorkArea cache)
+                & _OnceT %~ mapStateT dbToIO
+            )
             <&> M.backgroundColor backgroundId bgColor
             <&> measureLayout
     where
@@ -303,7 +352,9 @@ makeRootWidget env perfMonitors db evaluator sample =
                 Debug.Evaluator report = monitors ^. Debug.layout . Debug.mPure
                 f x = report ((x ^. Widget.fFocalAreas) `deepseq` x)
 
-run :: HasCallStack => Opts.EditorOpts -> Transaction.Store DbM -> IO ()
+run ::
+    HasCallStack =>
+    Opts.EditorOpts -> Transaction.Store DbM -> IO ()
 run opts rawDb =
     do
         mainLoop <- MainLoop.mainLoopWidget
@@ -321,6 +372,7 @@ run opts rawDb =
         let Debug.EvaluatorM reportDb = monitors ^. Debug.database . Debug.mAction
         let db = Transaction.onStoreM reportDb rawDb
         let stateStorage = stateStorageInIRef db DbLayout.guiState
+        cacheRef <- newIORef Nothing
         withMVarProtection db $
             \dbMVar ->
             M.withGLFW $
@@ -334,7 +386,8 @@ run opts rawDb =
                 mkSettingsProp <-
                     EditorSettings.newProp initialSettings configSampler evaluator
                 runMainLoop ekg stateStorage subpixel win mainLoop
-                    configSampler evaluator db mkSettingsProp cache cachedFunctions monitors
+                    configSampler evaluator db mkSettingsProp cache
+                    cachedFunctions monitors cacheRef
     where
         subpixel
             | opts ^. Opts.eoSubpixelEnabled = Font.LCDSubPixelEnabled

src/Control/Monad/Transaction.hs
@@ -7,6 +7,7 @@ module Control.Monad.Transaction
     , getP, setP, readIRef, writeIRef
     ) where
 
+import           Control.Monad.Once (OnceT)
 import           Control.Monad.Trans.Class (lift)
 import           Control.Monad.Trans.Except (ExceptT)
 import           Control.Monad.Trans.Maybe (MaybeT)
@@ -32,6 +33,7 @@ instance MonadTransaction n m => MonadTransaction n (MaybeT    m) where transact
 instance MonadTransaction n m => MonadTransaction n (StateT  s m) where transaction = lift . transaction
 instance MonadTransaction n m => MonadTransaction n (ReaderT r m) where transaction = lift . transaction
 instance MonadTransaction n m => MonadTransaction n (ExceptT e m) where transaction = lift . transaction
+instance MonadTransaction n m => MonadTransaction n (OnceT m)     where transaction = lift . transaction
 
 getP :: MonadTransaction n m => Property.MkProperty' (T n) a -> m a
 getP = transaction . Property.getP

src/Lamdu/GUI/CodeEdit.hs
@@ -11,6 +11,8 @@ module Lamdu.GUI.CodeEdit
 
 import qualified Control.Lens as Lens
 import           Control.Lens.Extended ((==>))
+import           Control.Monad.Once (OnceT)
+import           Control.Monad.Trans.Reader (ReaderT)
 import           Control.Monad.Transaction (MonadTransaction(..))
 import           Data.CurAndPrev (CurAndPrev(..))
 import qualified Data.Property as Property
@@ -86,18 +88,18 @@ data ExportActions m = ExportActions
 type EvalResults = CurAndPrev EvalResults.EvalResults
 
 type Model m =
-    T m
+    OnceT (T m)
     ( EvalResults ->
-        T m
-        ( Sugar.WorkArea (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m)
-            (Sugar.Payload (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m)
+        OnceT (T m)
+        ( Sugar.WorkArea (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m)
+            (Sugar.Payload (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m)
             , (Sugar.ParenInfo, [Sugar.EntityId])
             )
         )
     )
 
 make ::
-    ( MonadTransaction m n, MonadReader env n, Has Config env
+    ( Has Config env
     , Has Theme env, GuiState.HasState env
     , Spacer.HasStdSpacing env
     , Has EvalResults env
@@ -107,9 +109,10 @@ make ::
     , Has SearchMenu.TermStyle env
     , Element.HasAnimIdPrefix env
     , Language.HasLanguage env
+    , Monad m
     ) =>
     Anchors.CodeAnchors m -> Anchors.GuiAnchors (T m) (T m) -> Widget.R -> Model m ->
-    n (StatusBar.StatusWidget (IOTrans m), Widget (IOTrans m))
+    ReaderT env (OnceT (T m)) (StatusBar.StatusWidget (IOTrans m), Widget (IOTrans m))
 make cp gp width mkWorkArea =
     do
         theExportActions <- Lens.view has
@@ -117,9 +120,9 @@ make cp gp width mkWorkArea =
         workArea <-
             mkWorkArea >>= (env ^. has &)
             <&> Lens.mapped . Lens.mapped %~ uncurry ExprGui.Payload
-            & transaction
+            & lift
         gotoDefinition <-
-            GotoDefinition.make (transaction (workArea ^. Sugar.waGlobals))
+            GotoDefinition.make (workArea ^. Sugar.waGlobals & lift)
             <&> StatusBar.hoist IOTrans.liftTrans
         assocTagName <- DataOps.assocTagName
         do
@@ -139,8 +142,9 @@ make cp gp width mkWorkArea =
             Responsive.vboxSpaced
                 ?? (replGui : panesEdits ++ [newDefinitionButton])
                 <&> Widget.widget . Widget.eventMapMaker . Lens.mapped %~ (<> eventMap)
-            & GuiM.run assocTagName ExpressionEdit.make BinderEdit.make gp env
-            & transaction
+            & GuiM.run assocTagName ExpressionEdit.make BinderEdit.make
+                (Anchors.onGui (Property.mkProperty %~ lift) gp) env
+            & lift
             <&> render
             <&> (^. Align.tValue)
             <&> (,) gotoDefinition
@@ -205,9 +209,9 @@ makePaneBodyEdit pane =
 makePaneEdit ::
     (Monad m, Language.HasLanguage env) =>
     ExportActions m ->
-    Sugar.Pane (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m)
-        (Sugar.Payload (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m), ExprGui.Payload) ->
-    GuiM env (T m) (T m) (Responsive (IOTrans m))
+    Sugar.Pane (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m)
+        (Sugar.Payload (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m), ExprGui.Payload) ->
+    GuiM env (OnceT (T m)) (T m) (Responsive (IOTrans m))
 makePaneEdit theExportActions pane =
     do
         env <- Lens.view id
@@ -237,7 +241,7 @@ makePaneEdit theExportActions pane =
             <&> Widget.weakerEvents paneEventMap
 
 makeNewDefinition ::
-    Monad m => Anchors.CodeAnchors m -> GuiM env (T m) (T m) (T m Widget.Id)
+    Monad m => Anchors.CodeAnchors m -> GuiM env (OnceT (T m)) (T m) (T m Widget.Id)
 makeNewDefinition cp =
     GuiM.mkPrejumpPosSaver <&>
     \savePrecursor ->
@@ -264,7 +268,7 @@ newDefinitionDoc =
 
 makeNewDefinitionButton ::
     (Monad m, Language.HasLanguage env) =>
-    Anchors.CodeAnchors m -> GuiM env (T m) (T m) (Widget (T m))
+    Anchors.CodeAnchors m -> GuiM env (OnceT (T m)) (T m) (Widget (T m))
 makeNewDefinitionButton cp =
     do
         newDefId <- Element.subAnimId ?? ["New definition"] <&> Widget.Id
@@ -283,7 +287,7 @@ jumpBack gp =
 panesEventMap ::
     (Monad m, Language.HasLanguage env) =>
     ExportActions m -> Anchors.CodeAnchors m -> Anchors.GuiAnchors (T m) (T m) ->
-    Sugar.VarInfo -> GuiM env (T m) (T m) (EventMap (IOTrans m GuiState.Update))
+    Sugar.VarInfo -> GuiM env (OnceT (T m)) (T m) (EventMap (IOTrans m GuiState.Update))
 panesEventMap theExportActions cp gp replVarInfo =
     do
         env <- Lens.view id

src/Lamdu/GUI/Main.hs
@@ -9,8 +9,11 @@ module Lamdu.GUI.Main
     ) where
 
 import qualified Control.Lens as Lens
+import           Control.Monad.Once (OnceT, _OnceT)
 import           Control.Monad.Reader (ReaderT(..))
 import qualified Control.Monad.Reader as Reader
+import           Control.Monad.Trans.State (mapStateT)
+import           Control.Monad.Transaction (MonadTransaction(..))
 import           Data.Property (Property)
 import qualified GUI.Momentu.Align as Align
 import           GUI.Momentu.Draw (Sprite)
@@ -88,16 +91,16 @@ make ::
     Ctx env =>
     [TitledSelection Folder.Theme] -> [TitledSelection Folder.Language] ->
     Property IO Settings -> env -> CodeEdit.Model ViewM ->
-    T DbM (Widget (IOTrans DbM))
+    OnceT (T DbM) (Widget (IOTrans DbM))
 make themeNames langNames settingsProp env mkWorkArea =
     do
         vcActions <-
-            VersionControl.makeActions <&> VCActions.hoist IOTrans.liftTrans & lift
+            VersionControl.makeActions <&> VCActions.hoist IOTrans.liftTrans & transaction
         state <- Lens.view has
         let viewToDb x = x & IOTrans.trans %~ VersionControl.runEvent state
         (gotoDefinition, codeEdit) <-
             CodeEdit.make DbLayout.codeAnchors DbLayout.guiAnchors (fullSize ^. _1) mkWorkArea
-            & Reader.mapReaderT VersionControl.runAction
+            & Reader.mapReaderT (_OnceT %~ mapStateT VersionControl.runAction)
             <&> _1 %~ StatusBar.hoist viewToDb
             <&> _2 . Widget.updates %~ viewToDb
         statusBar <-

src/Lamdu/GUI/ReplEdit.hs
@@ -6,6 +6,7 @@ module Lamdu.GUI.ReplEdit
 
 import qualified Control.Lens as Lens
 import           Control.Lens.Extended (OneOf)
+import           Control.Monad.Once (OnceT)
 import qualified Control.Monad.Reader as Reader
 import           Data.CurAndPrev (CurPrevTag(..), curPrevTag, fallbackToPrev)
 import           GUI.Momentu.Align (Aligned(..), value, TextWidget)
@@ -242,9 +243,9 @@ make ::
     , Has (Texts.Navigation Text) env
     ) =>
     ExportRepl m ->
-    Sugar.Repl (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m)
-    (Sugar.Payload (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m), ExprGui.Payload) ->
-    GuiM env (T m) (T m) (Responsive (IOTrans m))
+    Sugar.Repl (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m)
+    (Sugar.Payload (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m), ExprGui.Payload) ->
+    GuiM env (OnceT (T m)) (T m) (Responsive (IOTrans m))
 make expRepl (Sugar.Repl replExpr varInfo replResult) =
     do
         env <- Lens.view id

src/Lamdu/Sugar.hs
@@ -6,6 +6,7 @@ module Lamdu.Sugar
     ) where
 
 import qualified Control.Lens as Lens
+import           Control.Monad.Once (OnceT)
 import           Data.CurAndPrev (CurAndPrev(..))
 import qualified Lamdu.Annotations as Annotations
 import qualified Lamdu.Cache as Cache
@@ -39,20 +40,20 @@ sugarWorkArea ::
     , Monad m
     ) =>
     (Tag -> (IsOperator, TextsInLang)) -> env -> Anchors.CodeAnchors m ->
-    T m
+    OnceT (T m)
     ( CurAndPrev EvalResults ->
-        T m (Sugar.WorkArea (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m)
-            (Sugar.Payload (Sugar.EvaluationScopes Name (T m)) Name (T m) (T m),
+        OnceT (T m) (Sugar.WorkArea (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m)
+            (Sugar.Payload (Sugar.EvaluationScopes Name (OnceT (T m))) Name (OnceT (T m)) (T m),
                 (Sugar.ParenInfo, [Sugar.EntityId])))
     )
 sugarWorkArea getTagName env cp =
     SugarConvert.loadWorkArea env cp
     <&>
     \workArea eval ->
-    addEvaluationResults cp eval workArea
+    addEvaluationResults cp eval workArea & lift
     >>= report .
         AddNames.addToWorkArea env
-        (fmap getTagName . ExprIRef.readTagData)
+        (fmap getTagName . (lift . ExprIRef.readTagData))
     <&> AddParens.addToWorkArea
     <&> Lens.mapped %~ \(parenInfo, pl) -> pl <&> (,) parenInfo
     where

src/Lamdu/Sugar/Convert.hs
@@ -4,6 +4,7 @@ module Lamdu.Sugar.Convert
 
 import           Control.Applicative ((<|>))
 import qualified Control.Lens as Lens
+import           Control.Monad.Once (OnceT)
 import           Control.Monad.Transaction (MonadTransaction)
 import           Data.CurAndPrev (CurAndPrev(..))
 import           Data.List.Extended (insertAt, removeAt)
@@ -94,13 +95,14 @@ convertInferDefExpr ::
     ) =>
     env -> Anchors.CodeAnchors m ->
     Pure # T.Scheme -> Definition.Expr (Ann (HRef m) # V.Term) -> DefI m ->
-    T m
-    (DefinitionBody EvalPrep InternalName (T m) (T m)
-        (Payload EvalPrep InternalName (T m) (T m), [EntityId]))
+    OnceT (T m)
+    (DefinitionBody EvalPrep InternalName (OnceT (T m)) (T m)
+        (Payload EvalPrep InternalName (OnceT (T m)) (T m), [EntityId]))
 convertInferDefExpr env cp defType defExpr defI =
     do
         outdatedDefinitions <-
             OutdatedDefs.scan entityId defExpr setDefExpr postProcess
+            & lift
             <&> Lens.mapped . defTypeUseCurrent %~ (<* postProcess)
         let context =
                 Context
@@ -151,12 +153,12 @@ convertDefBody ::
     ) =>
     env -> Anchors.CodeAnchors m ->
     Definition.Definition (Ann (HRef m) # V.Term) (DefI m) ->
-    T m
-    (DefinitionBody EvalPrep InternalName (T m) (T m)
-        (Payload EvalPrep InternalName (T m) (T m), [EntityId]))
+    OnceT (T m)
+    (DefinitionBody EvalPrep InternalName (OnceT (T m)) (T m)
+        (Payload EvalPrep InternalName (OnceT (T m)) (T m), [EntityId]))
 convertDefBody env cp (Definition.Definition bod defType defI) =
     case bod of
-    Definition.BodyBuiltin builtin -> convertDefIBuiltin defType builtin defI
+    Definition.BodyBuiltin builtin -> convertDefIBuiltin defType builtin defI & lift
     Definition.BodyExpr defExpr ->
         convertInferDefExpr env cp defType defExpr defI
 
@@ -176,18 +178,18 @@ convertRepl ::
     , Has Config env, Has Cache.Functions env, Has Annotations.Mode env
     ) =>
     env -> Anchors.CodeAnchors m ->
-    T m
-    (Repl EvalPrep InternalName (T m) (T m)
-        (Payload EvalPrep InternalName (T m) (T m), [EntityId]))
+    OnceT (T m)
+    (Repl EvalPrep InternalName (OnceT (T m)) (T m)
+        (Payload EvalPrep InternalName (OnceT (T m)) (T m), [EntityId]))
 convertRepl env cp =
     do
-        defExpr <- ExprLoad.defExpr prop
-        entityId <- Property.getP prop <&> (^. Definition.expr) <&> EntityId.ofValI
+        defExpr <- ExprLoad.defExpr prop & lift
+        entityId <- Property.getP prop & lift <&> (^. Definition.expr) <&> EntityId.ofValI
         let Load.InferOut valInferred newInferContext =
                 Load.inferDefExpr cachedInfer (env ^. has) defExpr
                 & assertInferSuccess
         outdatedDefinitions <-
-            OutdatedDefs.scan entityId defExpr (Property.setP prop) postProcess
+            OutdatedDefs.scan entityId defExpr (Property.setP prop) postProcess & lift
         let context =
                 Context
                 { _scInferContext = newInferContext
@@ -232,11 +234,11 @@ convertPaneBody ::
     , Has Config env, Has Cache.Functions env, Has Annotations.Mode env
     ) =>
     env -> Anchors.CodeAnchors m -> Anchors.Pane m ->
-    T m
-    (PaneBody EvalPrep InternalName (T m) (T m)
-        (Payload EvalPrep InternalName (T m) (T m), [EntityId]))
+    OnceT (T m)
+    (PaneBody EvalPrep InternalName (OnceT (T m)) (T m)
+        (Payload EvalPrep InternalName (OnceT (T m)) (T m), [EntityId]))
 convertPaneBody _ _ (Anchors.PaneTag tagId) =
-    ExprIRef.readTagData tagId <&>
+    ExprIRef.readTagData tagId & lift <&>
     \tagData ->
     PaneTag TagPane
     { _tpTag =
@@ -255,10 +257,10 @@ convertPaneBody _ _ (Anchors.PaneTag tagId) =
 convertPaneBody env cp (Anchors.PaneDefinition defI) =
     do
         bodyS <-
-            ExprLoad.def defI <&> Definition.defPayload .~ defI
+            ExprLoad.def defI & lift <&> Definition.defPayload .~ defI
             >>= convertDefBody env cp
         tag <- Anchors.tags cp & ConvertTag.taggedEntityWith cp defVar
-        defState <- Anchors.assocDefinitionState defI ^. Property.mkProperty
+        defState <- Anchors.assocDefinitionState defI ^. Property.mkProperty & lift
         OrderTags.orderDef Definition
             { _drEntityId = EntityId.ofIRef defI
             , _drName = tag
@@ -281,9 +283,9 @@ convertPane ::
     env -> Anchors.CodeAnchors m -> EntityId ->
     Property (T m) [Anchors.Pane dummy] ->
     Int -> Anchors.Pane m ->
-    T m
-    (Pane EvalPrep InternalName (T m) (T m)
-        (Payload EvalPrep InternalName (T m) (T m), [EntityId]))
+    OnceT (T m)
+    (Pane EvalPrep InternalName (OnceT (T m)) (T m)
+        (Payload EvalPrep InternalName (OnceT (T m)) (T m), [EntityId]))
 convertPane env cp replEntityId (Property panes setPanes) i pane =
     convertPaneBody env cp pane
     <&> \body -> Pane
@@ -320,12 +322,12 @@ loadPanes ::
     , Has Config env, Has Cache.Functions env, Has Annotations.Mode env
     ) =>
     env -> Anchors.CodeAnchors m -> EntityId ->
-    T m
-    [Pane EvalPrep InternalName (T m) (T m)
-        (Payload EvalPrep InternalName (T m) (T m), [EntityId])]
+    OnceT (T m)
+    [Pane EvalPrep InternalName (OnceT (T m)) (T m)
+        (Payload EvalPrep InternalName (OnceT (T m)) (T m), [EntityId])]
 loadPanes env cp replEntityId =
     do
-        prop <- Anchors.panes cp ^. Property.mkProperty
+        prop <- Anchors.panes cp ^. Property.mkProperty & lift
         Property.value prop
             & Lens.itraversed %%@~ convertPane env cp replEntityId prop
 
@@ -335,9 +337,9 @@ loadWorkArea ::
     , Has Config env, Has Cache.Functions env, Has Annotations.Mode env
     ) =>
     env -> Anchors.CodeAnchors m ->
-    T m
-    (WorkArea EvalPrep InternalName (T m) (T m)
-        (Payload EvalPrep InternalName (T m) (T m), [EntityId]))
+    OnceT (T m)
+    (WorkArea EvalPrep InternalName (OnceT (T m)) (T m)
+        (Payload EvalPrep InternalName (OnceT (T m)) (T m), [EntityId]))
 loadWorkArea env cp =
     do
         repl <- convertRepl env cp
@@ -348,6 +350,6 @@ loadWorkArea env cp =
             { _waRepl = repl
             , _waPanes = panes
             , _waGlobals =
-                Anchors.globals cp & Property.getP <&> Set.toList
+                Anchors.globals cp & Property.getP & lift <&> Set.toList
                 >>= traverse (ConvertGetVar.globalNameRef cp)
             }

src/Lamdu/Sugar/Convert/Binder.hs
@@ -5,6 +5,7 @@ module Lamdu.Sugar.Convert.Binder
     ) where
 
 import qualified Control.Lens as Lens
+import           Control.Monad.Once (OnceT)
 import qualified Data.Map as Map
 import           Data.Monoid (Any(..))
 import           Data.Property (MkProperty')
@@ -65,7 +66,7 @@ convertLet ::
     (Monad m, Monoid a) =>
     Input.Payload m a # V.Term ->
     Redex # Input.Payload m a ->
-    ConvertM m (Annotated (ConvertPayload m a) # Binder EvalPrep InternalName (T m) (T m))
+    ConvertM m (Annotated (ConvertPayload m a) # Binder EvalPrep InternalName (OnceT (T m)) (T m))
 convertLet pl redex =
     do
         float <- makeFloatLetToOuterScope (pl ^. Input.stored . ExprIRef.setIref) redex
@@ -137,7 +138,7 @@ convertLet pl redex =
 convertBinder ::
     (Monad m, Monoid a) =>
     Ann (Input.Payload m a) # V.Term ->
-    ConvertM m (Annotated (ConvertPayload m a) # Binder EvalPrep InternalName (T m) (T m))
+    ConvertM m (Annotated (ConvertPayload m a) # Binder EvalPrep InternalName (OnceT (T m)) (T m))
 convertBinder expr@(Ann pl body) =
     Lens.view (ConvertM.scConfig . Config.sugarsEnabled . Config.letExpression) >>=
     \case
@@ -178,7 +179,7 @@ makeFunction ::
     (Monad m, Monoid a) =>
     MkProperty' (T m) (Maybe BinderParamScopeId) ->
     ConventionalParams m -> Ann (Input.Payload m a) # V.Term ->
-    ConvertM m (Function EvalPrep InternalName (T m) (T m) # Annotated (ConvertPayload m a))
+    ConvertM m (Function EvalPrep InternalName (OnceT (T m)) (T m) # Annotated (ConvertPayload m a))
 makeFunction chosenScopeProp params funcBody =
     convertBinder funcBody
     <&> mkRes
@@ -189,7 +190,7 @@ makeFunction chosenScopeProp params funcBody =
             { _fParams =
                 -- TODO: avoid partiality here
                 params ^?! cpParams . Lens._Just
-            , _fChosenScopeProp = chosenScopeProp ^. Property.mkProperty
+            , _fChosenScopeProp = chosenScopeProp ^. Property.mkProperty & lift
             , _fBody = assignmentBody
             , _fBodyScopes = mempty
             , _fAddFirstParam = params ^. cpAddFirstParam
@@ -208,7 +209,7 @@ makeAssignment ::
     BinderKind m -> V.Var -> Ann (Input.Payload m a) # V.Term ->
     ConvertM m
     ( Maybe (MkProperty' (T m) PresentationMode)
-    , Annotated (ConvertPayload m a) # Assignment EvalPrep InternalName (T m) (T m)
+    , Annotated (ConvertPayload m a) # Assignment EvalPrep InternalName (OnceT (T m)) (T m)
     )
 makeAssignment chosenScopeProp binderKind defVar (Ann pl (V.BLam lam)) =
     do
@@ -378,7 +379,7 @@ convertAssignment ::
     Ann (Input.Payload m a) # V.Term ->
     ConvertM m
     ( Maybe (MkProperty' (T m) PresentationMode)
-    , Annotated (ConvertPayload m a) # Assignment EvalPrep InternalName (T m) (T m)
+    , Annotated (ConvertPayload m a) # Assignment EvalPrep InternalName (OnceT (T m)) (T m)
     )
 convertAssignment binderKind defVar expr =
     Lens.view (ConvertM.scConfig . Config.sugarsEnabled . Config.assignmentParameters)
@@ -400,7 +401,7 @@ convertDefinitionBinder ::
     DefI m -> Ann (Input.Payload m a) # V.Term ->
     ConvertM m
     ( Maybe (MkProperty' (T m) PresentationMode)
-    , Annotated (ConvertPayload m a) # Assignment EvalPrep InternalName (T m) (T m)
+    , Annotated (ConvertPayload m a) # Assignment EvalPrep InternalName (OnceT (T m)) (T m)
     )
 convertDefinitionBinder defI =
     convertAssignment (BinderKindDef defI) (ExprIRef.globalId defI)

commit 6b31adccf872e644e723785cdc34e1708c899645

Resugar after config change, was broken since b10c78a1feff

src/Lamdu/Editor.hs
@@ -393,12 +393,16 @@ run opts rawDb =
         initialSettings <- EditorSettings.read
         let initialTheme = initialSettings ^. Settings.sSelectedTheme
         let initialLanguage = initialSettings ^. Settings.sSelectedLanguage
-        configSampler <- ConfigSampler.new (const refresh) initialTheme initialLanguage
+        cacheRef <- newIORef Nothing
+        let invalidate _ =
+                do
+                    writeIORef cacheRef Nothing
+                    refresh
+        configSampler <- ConfigSampler.new invalidate initialTheme initialLanguage
         (cache, cachedFunctions) <- Cache.make
         let Debug.EvaluatorM reportDb = monitors ^. Debug.database . Debug.mAction
         let db = Transaction.onStoreM reportDb rawDb
         let stateStorage = stateStorageInIRef db DbLayout.guiState
-        cacheRef <- newIORef Nothing
         withMVarProtection db $
             \dbMVar ->
             M.withGLFW $