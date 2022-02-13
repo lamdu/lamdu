lamdu/lamdu

commit f95ac716726f976880d4d3de52ca26e93227d7e5

Fix annotations caching bug since b10c78a1feff714
When switching annotation mode, annotations didn't properly switch -
for example when switching from values to types, types didn't show,
and would only update upon an edit which would refresh the cache.

Note that parts of this diff counter the changes in the previous commit.

src/Lamdu/Sugar/OrderTags.hs
@@ -18,13 +18,13 @@ import           Lamdu.Prelude
 type OrderT m x = x -> m x
 
 class Order v name i o t where
-    order :: OrderT i (t # Annotated (Sugar.Payload (Sugar.Annotation v name) name i o, a))
+    order :: OrderT i (t # Annotated (Sugar.Payload v name i o, a))
 
     default order ::
         ( MonadTransaction m i, HTraversable t
         , HNodesConstraint t (Order v name i o)
         ) =>
-        OrderT i (t # Annotated (Sugar.Payload (Sugar.Annotation v name) name i o, a))
+        OrderT i (t # Annotated (Sugar.Payload v name i o, a))
     order = htraverse (Proxy @(Order v name i o) #> orderNode)
 
 orderByTag :: MonadTransaction m i => (a -> Sugar.Tag name) -> OrderT i [a]
@@ -58,7 +58,7 @@ orderType = hVal orderTBody
 
 orderRecord ::
     MonadTransaction m i =>
-    OrderT i (Sugar.Body Sugar.Composite (Sugar.Annotation v name) name i o a)
+    OrderT i (Sugar.Body Sugar.Composite v name i o a)
 orderRecord (Sugar.Composite items punned tail_ addItem) =
     Sugar.Composite
     <$> (orderByTag (^. Sugar.ciTag . Sugar.tagRefTag) items
@@ -67,23 +67,23 @@ orderRecord (Sugar.Composite items punned tail_ addItem) =
     <*> (Sugar._OpenComposite . _2) orderNode tail_
     <*> pure addItem
 
-instance MonadTransaction m i => Order v name i o (Sugar.LabeledApply (Sugar.Annotation v name) name i o) where
+instance MonadTransaction m i => Order v name i o (Sugar.LabeledApply v name i o) where
     order (Sugar.LabeledApply func specialArgs annotated punned) =
         Sugar.LabeledApply func specialArgs
         <$> orderByTag (^. Sugar.aaTag) annotated
         <*> pure punned
         >>= htraverse (Proxy @(Order v name i o) #> orderNode)
 
-orderCase :: MonadTransaction m i => OrderT i (Sugar.Body Sugar.Case (Sugar.Annotation v name) name i o a)
+orderCase :: MonadTransaction m i => OrderT i (Sugar.Body Sugar.Case v name i o a)
 orderCase = Sugar.cBody orderRecord
 
-instance MonadTransaction m i => Order v name i o (Sugar.Lambda (Sugar.Annotation v name) name i o)
+instance MonadTransaction m i => Order v name i o (Sugar.Lambda v name i o)
 instance MonadTransaction m i => Order v name i o (Const a)
-instance MonadTransaction m i => Order v name i o (Sugar.Else (Sugar.Annotation v name) name i o)
-instance MonadTransaction m i => Order v name i o (Sugar.IfElse (Sugar.Annotation v name) name i o)
-instance MonadTransaction m i => Order v name i o (Sugar.Let (Sugar.Annotation v name) name i o)
+instance MonadTransaction m i => Order v name i o (Sugar.Else v name i o)
+instance MonadTransaction m i => Order v name i o (Sugar.IfElse v name i o)
+instance MonadTransaction m i => Order v name i o (Sugar.Let v name i o)
 
-instance MonadTransaction m i => Order v name i o (Sugar.Function (Sugar.Annotation v name) name i o) where
+instance MonadTransaction m i => Order v name i o (Sugar.Function v name i o) where
     order x =
         x
         & (Sugar.fParams . Sugar._Params) orderParams
@@ -91,23 +91,20 @@ instance MonadTransaction m i => Order v name i o (Sugar.Function (Sugar.Annotat
 
 orderParams ::
     MonadTransaction m i =>
-    OrderT i [(Sugar.FuncParam (Sugar.Annotation v name) name, Sugar.ParamInfo name i o)]
-orderParams xs =
-    xs
-    & (Lens.traversed . _1 . Sugar.fpAnnotation . Sugar._AnnotationType) orderType
-    >>= orderByTag (^. _2 . Sugar.piTag . Sugar.tagRefTag)
+    OrderT i [(Sugar.FuncParam v name, Sugar.ParamInfo name i o)]
+orderParams = orderByTag (^. _2 . Sugar.piTag . Sugar.tagRefTag)
 
 -- Special case assignment and binder to invoke the special cases in expr
 
-instance MonadTransaction m i => Order v name i o (Sugar.Assignment (Sugar.Annotation v name) name i o) where
+instance MonadTransaction m i => Order v name i o (Sugar.Assignment v name i o) where
     order (Sugar.BodyPlain x) = Sugar.apBody order x <&> Sugar.BodyPlain
     order (Sugar.BodyFunction x) = order x <&> Sugar.BodyFunction
 
-instance MonadTransaction m i => Order v name i o (Sugar.Binder (Sugar.Annotation v name) name i o) where
+instance MonadTransaction m i => Order v name i o (Sugar.Binder v name i o) where
     order (Sugar.BinderTerm x) = order x <&> Sugar.BinderTerm
     order (Sugar.BinderLet x) = order x <&> Sugar.BinderLet
 
-instance MonadTransaction m i => Order v name i o (Sugar.Term (Sugar.Annotation v name) name i o) where
+instance MonadTransaction m i => Order v name i o (Sugar.Term v name i o) where
     order (Sugar.BodyLam l) = order l <&> Sugar.BodyLam
     order (Sugar.BodyRecord r) = orderRecord r <&> Sugar.BodyRecord
     order (Sugar.BodyLabeledApply a) = order a <&> Sugar.BodyLabeledApply
@@ -130,15 +127,12 @@ instance MonadTransaction m i => Order v name i o (Sugar.Term (Sugar.Annotation 
 
 orderNode ::
     (MonadTransaction m i, Order v name i o f) =>
-    OrderT i (Annotated (Sugar.Payload (Sugar.Annotation v name) name i o, a) # f)
-orderNode (Ann (Const a) x) =
-    Ann
-    <$> ((_1 . Sugar.plAnnotation . Sugar._AnnotationType) orderType a <&> Const)
-    <*> order x
+    OrderT i (Annotated (Sugar.Payload v name i o, a) # f)
+orderNode = hVal order
 
 orderDef ::
     MonadTransaction m i =>
-    OrderT i (Sugar.Definition (Sugar.Annotation v name) name i o (Sugar.Payload (Sugar.Annotation v name) name i o, a))
+    OrderT i (Sugar.Definition v name i o (Sugar.Payload v name i o, a))
 orderDef def =
     def
     & (SugarLens.defSchemes . Sugar.schemeType) orderType

src/Lamdu/Sugar.hs
@@ -7,7 +7,9 @@ module Lamdu.Sugar
 
 import qualified Control.Lens as Lens
 import           Control.Monad.Once (OnceT)
+import           Control.Monad.Transaction (MonadTransaction)
 import           Data.CurAndPrev (CurAndPrev(..))
+import           Data.Dynamic (Typeable)
 import qualified Lamdu.Annotations as Annotations
 import qualified Lamdu.Cache as Cache
 import qualified Lamdu.Data.Anchors as Anchors
@@ -18,9 +20,13 @@ import qualified Lamdu.Expr.IRef as ExprIRef
 import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.Name as Texts
 import           Lamdu.Name (Name)
+import           Lamdu.Sugar.Annotations
 import qualified Lamdu.Sugar.Config as SugarConfig
 import qualified Lamdu.Sugar.Convert as SugarConvert
+import           Lamdu.Sugar.Convert.Expression.Actions (makeTypeAnnotation)
 import           Lamdu.Sugar.Eval (addEvaluationResults)
+import           Lamdu.Sugar.Internal (EvalPrep, eEvalId, eType)
+import qualified Lamdu.Sugar.Lens as SugarLens
 import qualified Lamdu.Sugar.Names.Add as AddNames
 import qualified Lamdu.Sugar.Parens as AddParens
 import qualified Lamdu.Sugar.Types as Sugar
@@ -30,15 +36,61 @@ import           Lamdu.Prelude
 
 type T = Transaction
 
+markAnnotations ::
+    Functor i =>
+    SugarConfig.Config ->
+    Sugar.WorkArea v n i o (Sugar.Payload v n i o, a) ->
+    Sugar.WorkArea (ShowAnnotation, v) n i o (Sugar.Payload (ShowAnnotation, v) n i o, a)
+markAnnotations config workArea
+    | config ^. SugarConfig.showAllAnnotations =
+        workArea
+        & SugarLens.workAreaAnnotations
+            (Lens.mapped . Lens.mapped . SugarLens.holeOptionAnnotations %~ (,) alwaysShowAnnotations)
+            %~ (,) alwaysShowAnnotations
+    | otherwise =
+        workArea
+        { Sugar._waPanes = workArea ^. Sugar.waPanes <&> SugarLens.paneBinder %~ markNodeAnnotations
+        , Sugar._waRepl = workArea ^. Sugar.waRepl & Sugar.replExpr %~ markNodeAnnotations
+        }
+
+typeAnnotationFromEvalRes :: MonadTransaction n f => EvalPrep -> f (Sugar.Annotation v AddNames.InternalName)
+typeAnnotationFromEvalRes x =
+    makeTypeAnnotation (x ^. eEvalId) (x ^. eType) <&> Sugar.AnnotationType
+
+makeAnnotation ::
+    MonadTransaction n m =>
+    Annotations.Mode ->
+    (ShowAnnotation, EvalPrep) ->
+    m (Sugar.Annotation EvalPrep AddNames.InternalName)
+makeAnnotation annMode (showAnn, x) =
+    case annMode of
+    _ | showAnn ^. showTypeAlways -> typeAnnotationFromEvalRes x
+    Annotations.Types | showAnn ^. showInTypeMode -> typeAnnotationFromEvalRes x
+    Annotations.Evaluation | showAnn ^. showInEvalMode -> Sugar.AnnotationVal x & pure
+    _ -> pure Sugar.AnnotationNone
+
+makeHoleOptionAnn ::
+    (Monad m, Typeable m, MonadTransaction n m) =>
+    Annotations.Mode ->
+    Sugar.HoleOption (ShowAnnotation, EvalPrep) AddNames.InternalName (OnceT m) m ->
+    Sugar.HoleOption (Sugar.Annotation EvalPrep AddNames.InternalName) AddNames.InternalName (OnceT m) m
+makeHoleOptionAnn annMode (Sugar.HoleOption i e r) =
+    Sugar.HoleOption i
+    (e >>= onNode)
+    (r <&> _2 %~ (>>= Sugar.holeResultConverted onNode))
+    where
+        onNode = SugarLens.annotations (<&> Lens.mapped %~ makeHoleOptionAnn annMode) (makeAnnotation annMode)
+
 sugarWorkArea ::
     ( HasCallStack
     , Has Debug.Monitors env0
     , Has SugarConfig.Config env0
-    , Has Cache.Functions env0, Has Annotations.Mode env0
+    , Has Cache.Functions env0
+    , Has Annotations.Mode env1
     , Has (Texts.Name Text) env1
     , Has (Texts.Code Text) env1
     , Has (CurAndPrev EvalResults) env1
-    , Monad m
+    , Monad m, Typeable m
     ) =>
     env0 -> Anchors.CodeAnchors m ->
     OnceT (T m)
@@ -51,7 +103,9 @@ sugarWorkArea env0 cp =
     SugarConvert.loadWorkArea env0 cp
     <&>
     \workArea getTagName env1 ->
-    addEvaluationResults cp (env1 ^. has) workArea & lift
+    markAnnotations (env0 ^. has) workArea
+    & SugarLens.workAreaAnnotations (fmap (map (makeHoleOptionAnn (env1 ^. has)))) (makeAnnotation (env1 ^. has))
+    >>= lift . addEvaluationResults cp (env1 ^. has)
     >>= report .
         AddNames.addToWorkArea env1
         (fmap getTagName . (lift . ExprIRef.readTagData))

src/Lamdu/Sugar/Annotations.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE TemplateHaskell, TypeApplications, DataKinds, TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables #-}
 
 module Lamdu.Sugar.Annotations
     ( ShowAnnotation(..), showTypeAlways, showInTypeMode, showInEvalMode
-    , MarkAnnotations
-    , markNodeAnnotations
+    , MarkAnnotations(..)
     , neverShowAnnotations, alwaysShowAnnotations
     ) where
 
 import qualified Control.Lens as Lens
 import           Hyper
+import           Hyper.Class.Morph
 import qualified Lamdu.Builtins.Anchors as Builtins
 import qualified Lamdu.Sugar.Lens as SugarLens
 import           Lamdu.Sugar.Types
@@ -53,119 +54,108 @@ dontShowType =
     , _showInEvalMode = True
     }
 
-markNodeAnnotations ::
-    MarkAnnotations t =>
-    Annotated a # t ->
-    Annotated (ShowAnnotation, a) # t
-markNodeAnnotations (Ann (Const pl) x) =
-    Ann (Const (showAnn, pl)) newBody
-    where
-        (showAnn, newBody) = markAnnotations x
+class MarkBodyAnnotations v n i o e where
+    markBodyAnnotations ::
+        Body e v n i o a -> (ShowAnnotation, Body e (ShowAnnotation, v) n i o a)
 
-class MarkAnnotations t where
-    markAnnotations :: t # Annotated a -> (ShowAnnotation, t # Annotated (ShowAnnotation, a))
+class MarkAnnotations v n i o t0 t1 where
+    markNodeAnnotations ::
+        Annotated (Payload v n i o, a) # t0 ->
+        Annotated (Payload (ShowAnnotation, v) n i o, a) # t1
 
-instance MarkAnnotations (Binder v name i o) where
-    markAnnotations (BinderTerm body) =
+instance MarkAnnotations v n i o (Const a) (Const a) where
+    markNodeAnnotations (Ann a (Const b)) = Ann (a & Lens._Wrapped . _1 . plAnnotation %~ (,) neverShowAnnotations) (Const b)
+
+instance MarkBodyAnnotations v n i o e => MarkAnnotations v n i o (e v n i o) (e (ShowAnnotation, v) n i o) where
+    markNodeAnnotations (Ann (Const pl) x) =
+        Ann (Const (pl & _1 . plAnnotation %~ (,) showAnn)) newBody
+        where
+            (showAnn, newBody) = markBodyAnnotations x
+
+instance Functor i => MarkBodyAnnotations v n i o Binder where
+    markBodyAnnotations (BinderTerm body) =
         markBodyAnnotations body & _2 %~ BinderTerm
-    markAnnotations (BinderLet let_) =
+    markBodyAnnotations (BinderLet let_) =
         ( neverShowAnnotations
-        , hmap (Proxy @MarkAnnotations #> markNodeAnnotations) let_
+        , morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) let_
             & BinderLet
         )
 
-instance MarkAnnotations (Assignment v name i o) where
-    markAnnotations (BodyPlain (AssignPlain a b)) =
-        markAnnotations b
+instance Functor i => MarkBodyAnnotations v n i o Assignment where
+    markBodyAnnotations (BodyPlain (AssignPlain a b)) =
+        markBodyAnnotations b
         & _2 %~ BodyPlain . AssignPlain a
-    markAnnotations (BodyFunction f) = markAnnotations f & _2 %~ BodyFunction
+    markBodyAnnotations (BodyFunction f) = markBodyAnnotations f & _2 %~ BodyFunction
 
-instance MarkAnnotations (Else v name i o) where
-    markAnnotations (SimpleElse body) =
-        markBodyAnnotations body & _2 %~ SimpleElse
-    markAnnotations (ElseIf elseIf) =
+instance Functor i => MarkBodyAnnotations v n i o Else where
+    markBodyAnnotations (SimpleElse body) = markBodyAnnotations body & _2 %~ SimpleElse . markCaseHandler
+    markBodyAnnotations (ElseIf x) =
         ( neverShowAnnotations
-        , hmap (Proxy @MarkAnnotations #> markNodeAnnotations) elseIf
-            & ElseIf
+        , morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) x & ElseIf
         )
 
-instance MarkAnnotations (Function v n i o) where
-    markAnnotations f =
+instance Functor i => MarkBodyAnnotations v n i o Function where
+    markBodyAnnotations func =
         ( neverShowAnnotations
-        , f & fBody %~ markNodeAnnotations
+        , func
+            { _fBody = func ^. fBody & markNodeAnnotations
+            , _fParams = func ^. fParams & SugarLens.binderParamsAnnotations %~ (,) showAnnotationWhenVerbose
+            }
         )
 
-instance MarkAnnotations (Const a) where
-    markAnnotations (Const x) = (neverShowAnnotations, Const x)
+instance Functor i => MarkBodyAnnotations v n i o IfElse where
+    markBodyAnnotations (IfElse i t e) =
+        ( showAnnotationWhenVerbose
+        , IfElse (markNodeAnnotations i) (markNodeAnnotations t & hVal %~ markCaseHandler) (markNodeAnnotations e)
+        )
 
-instance MarkAnnotations (Term v name i o) where
-    markAnnotations = markBodyAnnotations
-
-markCaseHandler :: Term v n i o # Annotated (ShowAnnotation, a) -> Term v n i o # Annotated (ShowAnnotation, a)
-markCaseHandler =
-    _BodyLam . lamFunc . fBody .
-    SugarLens.binderResultExpr . Lens.ifiltered (const . Lens.nullOf SugarLens.bodyUnfinished) . _1
-    .~ neverShowAnnotations
-
-markBodyAnnotations ::
-    Term v name i o # Annotated a ->
-    ( ShowAnnotation
-    , Term v name i o # Annotated (ShowAnnotation, a)
-    )
-markBodyAnnotations oldBody =
-    case newBody of
-    BodyPlaceHolder -> set neverShowAnnotations
-    BodyLiteral LiteralNum {} -> set neverShowAnnotations
-    BodyLiteral LiteralText {} -> set neverShowAnnotations
-    BodyLiteral LiteralBytes {} -> set dontShowEval
-    BodyRecord _ -> set neverShowAnnotations
-    BodyLam _ -> set neverShowAnnotations
-    BodyGetVar (GetParam ParamRef { _pBinderMode = LightLambda }) ->
-        set showAnnotationWhenVerbose
-    BodyGetVar (GetParam ParamRef { _pBinderMode = NormalBinder }) ->
-        set neverShowAnnotations
-    BodyGetVar (GetBinder BinderVarRef { _bvForm = GetDefinition _ }) ->
-        set showAnnotationWhenVerbose
-    BodyGetVar (GetBinder BinderVarRef { _bvForm = GetLet }) ->
-        set neverShowAnnotations
-    BodyFromNom _ -> set dontShowEval
-    BodyToNom (Nominal tid binder) ->
+instance Functor i => MarkBodyAnnotations v n i o Term where
+    markBodyAnnotations BodyPlaceHolder = (neverShowAnnotations, BodyPlaceHolder)
+    markBodyAnnotations (BodyLiteral x@LiteralBytes{}) = (dontShowEval, BodyLiteral x)
+    markBodyAnnotations (BodyLiteral x) = (neverShowAnnotations, BodyLiteral x)
+    markBodyAnnotations (BodyRecord x) =
+        (neverShowAnnotations, morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) x & BodyRecord)
+    markBodyAnnotations (BodyLam x) = lamFunc markBodyAnnotations x & _2 %~ BodyLam
+    markBodyAnnotations (BodyGetVar x) =
+        ( case x of
+            GetParamsRecord{} -> showAnnotationWhenVerbose
+            GetParam ParamRef{ _pBinderMode = LightLambda } -> showAnnotationWhenVerbose
+            GetBinder BinderVarRef { _bvForm = GetDefinition{} } -> showAnnotationWhenVerbose
+            _ -> neverShowAnnotations
+        , BodyGetVar x
+        )
+    markBodyAnnotations (BodyFromNom x) = (neverShowAnnotations, BodyFromNom x)
+    markBodyAnnotations (BodyToNom (Nominal tid binder)) =
         ( showAnnotationWhenVerbose
             & showInEvalMode .~
                 ( tid ^. tidTId == Builtins.textTid
-                    || binder ^. SugarLens.binderResultExpr . _1 . showInEvalMode
+                    || newBinder ^. SugarLens.binderResultExpr . _1 . plAnnotation . _1 . showInEvalMode
                 )
-        , hmap
-            ( Proxy @SugarLens.SugarExpr #>
-                Lens.filtered (not . SugarLens.isUnfinished . (^. hVal)) . annotation . _1 .~ dontShowEval
-            ) newBody
+        , newBinder
+            & Lens.filtered (not . SugarLens.isUnfinished . (^. hVal)) .
+                annotation . _1 . plAnnotation . _1 .~ dontShowEval
+            & Nominal tid & BodyToNom
         )
-    BodyInject _ -> set dontShowEval
-    BodyGetVar (GetParamsRecord _) -> set showAnnotationWhenVerbose
-    BodyGetField _ -> set showAnnotationWhenVerbose
-    BodySimpleApply app ->
+        where
+            newBinder = markNodeAnnotations binder
+    markBodyAnnotations (BodyInject x) =
+        (dontShowEval, morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) x & BodyInject)
+    markBodyAnnotations (BodyGetField x) =
+        (showAnnotationWhenVerbose, morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) x & BodyGetField)
+    markBodyAnnotations (BodySimpleApply x) =
         ( showAnnotationWhenVerbose
-        , app
+        , morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) x
             & appFunc . nonHoleAnn .~ neverShowAnnotations
             & BodySimpleApply
         )
-    BodyLabeledApply _ -> set showAnnotationWhenVerbose
-    BodyIfElse i ->
-       ( showAnnotationWhenVerbose
-       , onIfElse i & BodyIfElse
-       )
-    BodyHole hole -> (alwaysShowAnnotations, BodyHole hole)
-    BodyFragment fragment ->
-        ( alwaysShowAnnotations
-        , fragment
-            & (if Lens.has Lens._Just (fragment ^. fTypeMismatch)
-                  then fExpr . nonHoleAnn .~ dontShowType
-                  else id)
-            & BodyFragment
-        )
-    BodyCase cas ->
+    markBodyAnnotations (BodyLabeledApply x) =
         ( showAnnotationWhenVerbose
-        , cas
+        , morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) x & BodyLabeledApply
+        )
+    markBodyAnnotations (BodyIfElse x) = markBodyAnnotations x & _2 %~ BodyIfElse
+    markBodyAnnotations (BodyCase x) =
+        ( showAnnotationWhenVerbose
+        , morphMap (Proxy @(MarkAnnotations v n i o) #?> markNodeAnnotations) x
             -- cKind contains the scrutinee which is not always
             -- visible (for case alts that aren't lambdas), so
             -- maybe we do want to show the annotation
@@ -173,16 +163,38 @@ markBodyAnnotations oldBody =
             & cBody . cItems . Lens.mapped . ciExpr . hVal %~ markCaseHandler
             & BodyCase
         )
-    where
-        set x = (x, newBody)
-        newBody =
-            hmap (Proxy @MarkAnnotations #> markNodeAnnotations) oldBody
-        nonHoleAnn =
-            Lens.filtered (Lens.nullOf (hVal . SugarLens.bodyUnfinished)) .
-            annotation . _1
-        onElse (SimpleElse x) = markCaseHandler x & SimpleElse
-        onElse (ElseIf elseIf) = onIfElse elseIf & ElseIf
-        onIfElse x =
-            x
-            & iThen . hVal %~ markCaseHandler
-            & iElse . hVal %~ onElse
+    markBodyAnnotations (BodyHole x) =
+        ( alwaysShowAnnotations
+        , x & holeOptions . Lens.mapped . Lens.mapped %~ markHoleOption
+            & BodyHole
+        )
+    markBodyAnnotations (BodyFragment (Fragment e h t o)) =
+        ( alwaysShowAnnotations
+        , Fragment
+            ( markNodeAnnotations e
+                & if Lens.has Lens._Just t
+                    then nonHoleAnn .~ dontShowType
+                    else id
+            ) h t (o <&> Lens.mapped %~ markHoleOption)
+            & BodyFragment
+        )
+
+nonHoleAnn ::
+    Lens.Traversal' (Annotated (Payload (ShowAnnotation, v0) n i o, a) # Term v1 n i o) ShowAnnotation
+nonHoleAnn =
+    Lens.filtered (Lens.nullOf (hVal . SugarLens.bodyUnfinished)) .
+    annotation . _1 . plAnnotation . _1
+
+markCaseHandler ::
+    Term v n i o # Annotated (Payload (ShowAnnotation, v0) n i o, a) ->
+    Term v n i o # Annotated (Payload (ShowAnnotation, v0) n i o, a)
+markCaseHandler =
+    _BodyLam . lamFunc . fBody .
+    SugarLens.binderResultExpr . Lens.ifiltered (const . Lens.nullOf SugarLens.bodyUnfinished) .
+    _1 . plAnnotation . _1
+    .~ neverShowAnnotations
+
+markHoleOption :: Functor i => HoleOption v n i o -> HoleOption (ShowAnnotation, v) n i o
+markHoleOption (HoleOption i b r) =
+    HoleOption i (b <&> markNodeAnnotations)
+    (r <&> _2 . Lens.mapped . holeResultConverted %~ markNodeAnnotations)

commit e620b59b4b0b434cf445cf30e11777da34d920b0

Fix tag ordering in type annotations broken since f95ac716726f9

src/Lamdu/Sugar/Convert/Annotation.hs
@@ -10,6 +10,7 @@ import           Lamdu.Sugar.Annotations
 import           Lamdu.Sugar.Internal
 import           Lamdu.Sugar.Internal.EntityId (EntityId)
 import qualified Lamdu.Sugar.Internal.EntityId as EntityId
+import           Lamdu.Sugar.OrderTags (orderType)
 import           Lamdu.Sugar.Convert.Type (convertType)
 import qualified Lamdu.Sugar.Types as Sugar
 
@@ -35,4 +36,4 @@ typeAnnotationFromEvalRes x =
 makeTypeAnnotation ::
     MonadTransaction n m =>
     EntityId -> Pure # T.Type -> m (Annotated EntityId # Sugar.Type InternalName Unit)
-makeTypeAnnotation = convertType . EntityId.ofTypeOf
+makeTypeAnnotation e t = convertType (EntityId.ofTypeOf e) t >>= orderType

src/Lamdu/Sugar/OrderTags.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables, UndecidableInstances #-}
 
 module Lamdu.Sugar.OrderTags
-    ( orderWorkArea
+    ( orderWorkArea, orderType
     ) where
 
 import qualified Control.Lens as Lens