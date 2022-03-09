lamdu/lamdu

commit 46e8daceea063c6a77ac74fe0b626b384173967d

Make sure tag suffixes don't clash with other tag names that end with numbers

src/Lamdu/Sugar/Names/Add.hs
@@ -12,8 +12,10 @@ module Lamdu.Sugar.Names.Add
 import           Control.Lens (ALens)
 import qualified Control.Lens as Lens
 import           Control.Lens.Extended ((~~>))
+import           Control.Monad (zipWithM)
 import           Control.Monad.Reader (ReaderT(..), Reader, runReader)
 import qualified Control.Monad.Reader as Reader
+import           Control.Monad.State (evalState)
 import qualified Control.Monad.Writer as Writer
 import           Control.Monad.Trans.FastWriter (Writer, runWriter, MonadWriter)
 import qualified Data.Char as Char
@@ -269,17 +271,32 @@ isReserved env name =
             & Set.fromList
 
 toSuffixMap ::
+    Map T.Tag TagText ->
     MMap T.Tag (MMap Walk.NameType (OrderedSet UUID)) ->
     MMap T.Tag Clash.Info ->
     Map TaggedVarId Int
-toSuffixMap contexts top =
-    (collisions & Lens.imapped %@~ eachTag) ^. Lens.folded
+toSuffixMap tagTexts contexts top =
+    evalState (Lens.itraverse eachTag collisions) nonCollisionTexts ^. Lens.folded
     where
-        eachTag tag ctx = ctx ^.. Lens.folded & Lens.imap (item tag) & Map.fromList
-        item tag idx uuid = (TaggedVarId uuid tag, idx)
+        eachTag tag ctx =
+            do
+                texts <- Lens.use id
+                let indices = filter (\i -> not (texts ^. Lens.contains (addSuf i))) [0 ..]
+                zipWithM item indices (ctx ^.. Lens.folded)
+            <&> Map.fromList
+            where
+                addSuf :: Int -> Text
+                addSuf i = txt <> Text.pack (show i)
+                txt = tagTexts ^?! Lens.ix tag . ttText
+                item idx uuid =
+                    (TaggedVarId uuid tag, idx) <$ (Lens.contains (addSuf idx) .= True)
         collisions =
             MMap.filter Clash.isClash top
             & Lens.imapped %@~ \tag _ -> toContexts tag
+        nonCollisionTexts =
+            MMap.filter (not . Clash.isClash) top ^.. Lens.ifolded . Lens.asIndex
+            & foldMap (\t -> tagTexts ^.. Lens.ix t . ttText)
+            & Set.fromList
         toContexts k =
             fromMaybe (error "No Contexts for clashing tag??") (contexts ^. Lens.at k)
             ^. traverse
@@ -303,7 +320,7 @@ initialP2Env env P1Out{_p1Globals, _p1Locals, _p1Contexts, _p1TypeVars, _p1Texts
         & Map.fromList
     , _p2TagTexts = tagTexts
     , _p2Texts = _p1Texts ^. traverse . Tag.name . Lens.to Set.singleton
-    , _p2TagSuffixes = toSuffixMap _p1Contexts top
+    , _p2TagSuffixes = toSuffixMap tagTexts _p1Contexts top
     , _p2TagsAbove = uncolliders _p1Globals
         -- all globals are "above" everything, and locals add up as
         -- we descend

commit 3be98d8dbe762ce7c624d89519f320d2b26936d9

Add failing test for regression in 46e8daceea0

test/Tests/Names.hs
@@ -7,6 +7,7 @@ module Tests.Names where
 import           Control.Monad.Trans.FastWriter (Writer, runWriter)
 import           Control.Monad.Unit (Unit(..))
 import           Control.Monad.Writer (MonadWriter(..))
+import           Lamdu.Data.Anchors (anonTag)
 import qualified Lamdu.I18N.Name as Texts
 import           Lamdu.Name (Name)
 import qualified Lamdu.Name as Name
@@ -37,6 +38,7 @@ test =
     testGroup "Disambiguation"
     [ testCase "disambiguation(#396)" workArea396
     , testCase "globals collide" workAreaGlobals
+    , testCase "anonymous globals" anonGlobals
     ]
 
 nameTexts :: Texts.Name Text
@@ -125,8 +127,27 @@ workAreaGlobals =
                 [ "Unexpected/bad collision for name", show text
                 , show textCollision, show tagCollision
                 ] & assertString
-        trivialBinder =
-            Sugar.Hole mempty mempty & Sugar.LeafHole & Sugar.BodyLeaf & Sugar.BinderTerm
-            & Sugar.Binder Unit & Sugar.AssignPlain Unit
-            & Sugar.BodyPlain
-            & Ann (Const Stub.payload)
+
+trivialBinder ::
+    Annotated (Sugar.Payload (Sugar.Annotation v InternalName) Unit) #
+    Sugar.Assignment
+        (Sugar.Annotation (Sugar.EvaluationScopes InternalName Identity) InternalName)
+        InternalName Identity Unit
+trivialBinder =
+    Sugar.Hole mempty mempty & Sugar.LeafHole & Sugar.BodyLeaf & Sugar.BinderTerm
+    & Sugar.Binder Unit & Sugar.AssignPlain Unit
+    & Sugar.BodyPlain
+    & Ann (Const Stub.payload)
+
+anonGlobals :: IO ()
+anonGlobals =
+    Sugar.WorkArea
+    { Sugar._waRepl = Stub.repl Stub.hole
+    , Sugar._waPanes =
+        -- 2 defs sharing the same tag with different Vars/UUIDs,
+        -- should collide with ordinary suffixes
+        [ Stub.def Stub.numType "def1" anonTag trivialBinder & Stub.pane
+        , Stub.def Stub.numType "def2" anonTag trivialBinder & Stub.pane
+        ]
+    , Sugar._waGlobals = Sugar.Globals (pure []) (pure []) (pure [])
+    } & testWorkArea (\x -> length (show x) `seq` pure ())