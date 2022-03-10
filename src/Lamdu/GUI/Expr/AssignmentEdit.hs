lamdu/lamdu

commit 5e9396de371a1739dc80d57b48044793f76eadad

Extracting part out of AssignmentEdit.makeParts towards splitting it

src/Lamdu/GUI/Expr/AssignmentEdit.hs
@@ -57,7 +57,6 @@ data Parts i o = Parts
     { pAddFirstEventMap :: EventMap (o M.Update)
     , pMParamsEdit :: Maybe (Responsive o)
     , pMScopesEdit :: Maybe (M.Widget o)
-    , pBodyEdit :: Responsive o
     , pScopeEventMap :: EventMap (o M.Update)
     , pMLamPayload :: Maybe (ExprGui.Payload i o)
     , pRhsId :: Widget.Id
@@ -209,8 +208,7 @@ makeFunctionParts funcApplyLimit (Ann (Const pl) func) delVarBackwardsId =
             (lhsEventMap, paramsEdit) <-
                 ParamsEdit.make False mScopeCursor isScopeNavFocused delVarBackwardsId myId
                 bodyId (func ^. Sugar.fParams)
-            rhs <- GuiM.makeBinder (func ^. Sugar.fBody)
-            Parts lhsEventMap (Just paramsEdit) mScopeNavEdit rhs scopeEventMap (Just pl) bodyId & pure
+            Parts lhsEventMap (Just paramsEdit) mScopeNavEdit scopeEventMap (Just pl) bodyId & pure
             & case mScopeNavEdit of
               Nothing -> GuiState.assignCursorPrefix scopesNavId (const destId)
               Just _ -> id
@@ -234,13 +232,12 @@ makePlainParts ::
     ExprGui.Expr Sugar.AssignPlain i o -> GuiM env i o (Parts i o)
 makePlainParts (Ann (Const pl) assignPlain) =
     do
-        rhs <- assignPlain ^. Sugar.apBody & Ann (Const pl) & GuiM.makeBinder
         env <- Lens.view id
         let addParam =
                 E.keysEventMapMovesCursor (env ^. has . Config.addNextParamKeys)
                 (E.toDoc env [has . MomentuTexts.edit, has . Texts.parameter, has . Texts.add])
                 (assignPlain ^. Sugar.apAddFirstParam <&> enterParam)
-        Parts addParam Nothing Nothing rhs mempty Nothing myId & pure
+        Parts addParam Nothing Nothing mempty Nothing myId & pure
     where
         myId = WidgetIds.fromExprPayload pl
         enterParam = WidgetIds.tagHoleId . WidgetIds.fromEntityId
@@ -276,8 +273,13 @@ make ::
     GuiM env i o (Responsive o)
 make pMode delParamDest assignment nameEdit =
     makeParts Sugar.UnlimitedFuncApply assignment delParamDest
-    >>= \(Parts lhsEventMap mParamsEdit mScopeEdit bodyEdit eventMap mLamPl rhsId) ->
+    >>= \(Parts lhsEventMap mParamsEdit mScopeEdit eventMap mLamPl rhsId) ->
     do
+        bodyEdit <-
+            case assignment ^. hVal of
+            Sugar.BodyFunction func -> func ^. Sugar.fBody
+            Sugar.BodyPlain x -> x ^. Sugar.apBody & Ann (Const (assignment ^. annotation))
+            & GuiM.makeBinder
         rhsJumperEquals <- makeJumpToRhs rhsId
         mPresentationEdit <-
             case assignmentBody of

src/Lamdu/GUI/Expr/LambdaEdit.hs
@@ -12,6 +12,7 @@ import qualified Lamdu.GUI.Expr.AssignmentEdit as AssignmentEdit
 import           Lamdu.GUI.Expr.EventMap (closeParenEvent)
 import qualified Lamdu.GUI.Expr.ParamsEdit as ParamsEdit
 import           Lamdu.GUI.Monad (GuiM)
+import qualified Lamdu.GUI.Monad as GuiM
 import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import           Lamdu.GUI.Wrap (stdWrapParentExpr)
@@ -24,9 +25,10 @@ import           Lamdu.Prelude
 make :: _ => ExprGui.Expr Sugar.Lambda i o -> GuiM env i o (Responsive o)
 make (Ann (Const pl) lam) =
     do
-        AssignmentEdit.Parts lhsEventMap mParamsEdit mScopeEdit bodyEdit scopeEventMap _wrap rhsId <-
+        AssignmentEdit.Parts lhsEventMap mParamsEdit mScopeEdit scopeEventMap _wrap rhsId <-
             AssignmentEdit.makeFunctionParts (lam ^. Sugar.lamApplyLimit)
             (Ann (Const pl) func) (WidgetIds.fromEntityId bodyId)
+        bodyEdit <- func ^. Sugar.fBody & GuiM.makeBinder
         rhsJumperEquals <- AssignmentEdit.makeJumpToRhs rhsId
         paramsAndLabelEdits <- ParamsEdit.makeLhs (lam ^. Sugar.lamLightweight) params mParamsEdit mScopeEdit lhsEventMap myId
         navigateOut <-

commit 3414d89fd3476fe4a538c281ad4485aeff40aa88

Oops fix display of evaluation results in inner scopes broken in 5e9396de371a
Moved a piece of code that was under Reader.local so its state has changed.

src/Lamdu/GUI/Expr/AssignmentEdit.hs
@@ -58,6 +58,7 @@ data Parts o = Parts
     , pMParamsEdit :: Maybe (Responsive o)
     , pMScopesEdit :: Maybe (M.Widget o)
     , pScopeEventMap :: EventMap (o M.Update)
+    , pMScopeId :: Maybe (CurAndPrev (Maybe Sugar.ScopeId))
     }
 
 readFunctionChosenScope ::
@@ -206,11 +207,10 @@ makeFunctionParts funcApplyLimit (Ann (Const pl) func) delVarBackwardsId =
             (lhsEventMap, paramsEdit) <-
                 ParamsEdit.make False mScopeCursor isScopeNavFocused delVarBackwardsId myId
                 bodyId (func ^. Sugar.fParams)
-            Parts lhsEventMap (Just paramsEdit) mScopeNavEdit scopeEventMap & pure
+            Parts lhsEventMap (Just paramsEdit) mScopeNavEdit scopeEventMap (Just binderScopeId) & pure
             & case mScopeNavEdit of
               Nothing -> GuiState.assignCursorPrefix scopesNavId (const destId)
               Just _ -> id
-            & GuiM.withLocalMScopeId binderScopeId
     where
         myId = WidgetIds.fromExprPayload pl
         destId =
@@ -235,7 +235,7 @@ makePlainParts assignPlain =
                 E.keysEventMapMovesCursor (env ^. has . Config.addNextParamKeys)
                 (E.toDoc env [has . MomentuTexts.edit, has . Texts.parameter, has . Texts.add])
                 (assignPlain ^. Sugar.apAddFirstParam <&> enterParam)
-        Parts addParam Nothing Nothing mempty & pure
+        Parts addParam Nothing Nothing mempty Nothing & pure
     where
         enterParam = WidgetIds.tagHoleId . WidgetIds.fromEntityId
 
@@ -270,9 +270,9 @@ make ::
     GuiM env i o (Responsive o)
 make pMode delParamDest assignment nameEdit =
     makeParts Sugar.UnlimitedFuncApply assignment delParamDest
-    >>= \(Parts lhsEventMap mParamsEdit mScopeEdit eventMap) ->
+    >>= \(Parts lhsEventMap mParamsEdit mScopeEdit eventMap mScopeId) ->
     do
-        bodyEdit <- GuiM.makeBinder body
+        bodyEdit <- GuiM.makeBinder body & maybe id GuiM.withLocalMScopeId mScopeId
         rhsJumperEquals <- body ^. annotation & WidgetIds.fromExprPayload & makeJumpToRhs
         mPresentationEdit <-
             case assignmentBody of

src/Lamdu/GUI/Expr/LambdaEdit.hs
@@ -25,10 +25,13 @@ import           Lamdu.Prelude
 make :: _ => ExprGui.Expr Sugar.Lambda i o -> GuiM env i o (Responsive o)
 make (Ann (Const pl) lam) =
     do
-        AssignmentEdit.Parts lhsEventMap mParamsEdit mScopeEdit scopeEventMap <-
+        AssignmentEdit.Parts lhsEventMap mParamsEdit mScopeEdit scopeEventMap mScopeId <-
             AssignmentEdit.makeFunctionParts (lam ^. Sugar.lamApplyLimit)
             (Ann (Const pl) func) (WidgetIds.fromEntityId bodyId)
-        bodyEdit <- func ^. Sugar.fBody & GuiM.makeBinder
+        bodyEdit <-
+            func ^. Sugar.fBody
+            & GuiM.makeBinder
+            & maybe id GuiM.withLocalMScopeId mScopeId
         rhsJumperEquals <- func ^. Sugar.fBody . annotation & WidgetIds.fromExprPayload & AssignmentEdit.makeJumpToRhs
         paramsAndLabelEdits <- ParamsEdit.makeLhs (lam ^. Sugar.lamLightweight) params mParamsEdit mScopeEdit lhsEventMap myId
         navigateOut <-