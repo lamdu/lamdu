lamdu/lamdu

commit e9e54b0e37cb121fe4bd761ba555b841011fd60b

Multi-repl support!
* With small feature regression: No option to execute repl actions yet from the indicator (works with key binding on definition)
  * Feature will return soon. It was just already a big change and a big step forward,
    so keeping this change no larger seemd appropriate.
* With behavior change: Prev results don't immediately get cleared when repl finishes evaluation
  * They disappear after time duration like they do when it is still running

src/Lamdu/GUI/CodeEdit.hs
@@ -4,7 +4,7 @@ module Lamdu.GUI.CodeEdit
     ( make
     , Model
     , EvalResults
-    , ReplEdit.ExportRepl(..), ExportActions(..)
+    , ExportActions(..)
 
     , -- exported for tests
       makePaneBodyEdit
@@ -46,7 +46,6 @@ import qualified Lamdu.GUI.IOTrans as IOTrans
 import qualified Lamdu.GUI.NominalPane as NominalPane
 import           Lamdu.GUI.Monad (GuiM)
 import qualified Lamdu.GUI.Monad as GuiM
-import qualified Lamdu.GUI.ReplEdit as ReplEdit
 import qualified Lamdu.GUI.StatusBar.Common as StatusBar
 import qualified Lamdu.GUI.Styled as Styled
 import qualified Lamdu.GUI.TagPane as TagPaneEdit
@@ -57,7 +56,6 @@ import qualified Lamdu.I18N.Collaboration as Texts
 import qualified Lamdu.I18N.Definitions as Texts
 import qualified Lamdu.I18N.Navigation as Texts
 import           Lamdu.Name (Name)
-import qualified Lamdu.Sugar.Lens as SugarLens
 import qualified Lamdu.Sugar.Types as Sugar
 import           Revision.Deltum.Transaction (Transaction)
 
@@ -67,8 +65,9 @@ type T = Transaction
 
 data ExportActions m = ExportActions
     { exportAll :: IOTrans m ()
-    , exportReplActions :: ReplEdit.ExportRepl m
     , exportDef :: V.Var -> IOTrans m ()
+    , exportDefToJS :: V.Var -> IOTrans m ()
+    , executeDef :: V.Var -> IO ()
     , exportTag :: T.Tag -> IOTrans m ()
     , exportNominal :: T.NominalId -> IOTrans m ()
     , importAll :: FilePath -> IOTrans m ()
@@ -99,25 +98,20 @@ make cp gp width mkWorkArea =
             <&> StatusBar.hoist IOTrans.liftTrans
         assocTagName <- DataOps.assocTagName
         do
-            replGui <-
-                ReplEdit.make (exportReplActions theExportActions)
-                (workArea ^. Sugar.waRepl)
+            newDefId <- Element.subAnimId ?? ["New definition"] <&> Widget.Id
             let dsts =
-                    workArea ^. Sugar.waRepl . Sugar.replExpr . SugarLens.binderResultExpr . Sugar.plEntityId :
-                    workArea ^.. Sugar.waPanes . traverse . Sugar.paneEntityId
-                    <&> WidgetIds.fromEntityId
+                    newDefId :
+                    (workArea ^.. Sugar.waPanes . traverse . Sugar.paneEntityId <&> WidgetIds.fromEntityId)
             panesEdits <-
                 workArea ^. Sugar.waPanes
                 & zipWithM (makePaneEdit theExportActions) dsts
             newDefinitionButton <-
-                makeNewDefinitionButton cp
+                makeNewDefinitionButton cp newDefId
                 <&> Widget.updates %~ IOTrans.liftTrans
                 <&> Responsive.fromWidget
-            eventMap <-
-                panesEventMap theExportActions cp gp
-                (workArea ^. Sugar.waRepl . Sugar.replVarInfo)
+            eventMap <- panesEventMap theExportActions cp gp
             Responsive.vboxSpaced
-                ?? (replGui : panesEdits ++ [newDefinitionButton])
+                ?? panesEdits <> [newDefinitionButton]
                 <&> Widget.widget . Widget.eventMapMaker . Lens.mapped %~ (<> eventMap)
             & GuiM.run assocTagName ExpressionEdit.make BinderEdit.make
                 (Anchors.onGui (Property.mkProperty %~ lift) gp) env
@@ -141,6 +135,15 @@ exportPaneEventMap env theExportActions paneBody =
     case paneBody of
     Sugar.PaneDefinition def ->
         exportEventMap exportDef (def ^. Sugar.drDefI) Texts.exportDefToJSON
+        <> execEventMap
+        where
+            execEventMap =
+                case def ^? Sugar.drBody . Sugar._DefinitionBodyExpression . Sugar.deVarInfo of
+                Just ( Sugar.VarNominal (Sugar.TId _ tid _)) | tid == Builtins.mutTid ->
+                    E.keysEventMap (env ^. has . Config.export . Config.executeKeys)
+                    (E.toDoc (env ^. has) [Texts.execRepl])
+                    (IOTrans.liftIO (executeDef theExportActions (def ^. Sugar.drDefI)))
+                _ -> mempty
     Sugar.PaneTag tag ->
         exportEventMap exportTag (tag ^. Sugar.tpTag) Texts.exportTagToJSON
     Sugar.PaneNominal nom ->
@@ -264,10 +267,9 @@ newDefinitionDoc =
     Lens.view id
     <&> (`E.toDoc` [has . MomentuTexts.edit, has . Texts.new])
 
-makeNewDefinitionButton :: _ => Anchors.CodeAnchors m -> GuiM env (OnceT (T m)) (T m) (Widget (T m))
-makeNewDefinitionButton cp =
+makeNewDefinitionButton :: _ => Anchors.CodeAnchors m -> Widget.Id -> GuiM env (OnceT (T m)) (T m) (Widget (T m))
+makeNewDefinitionButton cp newDefId =
     do
-        newDefId <- Element.subAnimId ?? ["New definition"] <&> Widget.Id
         newDefDoc <- newDefinitionDoc
         makeNewDefinition cp
             >>= Styled.actionable newDefId Texts.newDefinitionButton newDefDoc
@@ -283,8 +285,8 @@ jumpBack gp =
 panesEventMap ::
     _ =>
     ExportActions m -> Anchors.CodeAnchors m -> Anchors.GuiAnchors (T m) (T m) ->
-    Sugar.VarInfo -> GuiM env (OnceT (T m)) (T m) (EventMap (IOTrans m GuiState.Update))
-panesEventMap theExportActions cp gp replVarInfo =
+    GuiM env (OnceT (T m)) (T m) (EventMap (IOTrans m GuiState.Update))
+panesEventMap theExportActions cp gp =
     do
         env <- Lens.view id
         let exportConfig = env ^. has . Config.export
@@ -311,13 +313,6 @@ panesEventMap theExportActions cp gp replVarInfo =
             , importAll (exportConfig ^. Config.exportPath)
               & E.keysEventMap (exportConfig ^. Config.importKeys)
                 (collaborationDoc [Texts.collaboration, Texts.importReplFromJSON])
-            , case replVarInfo of
-                Sugar.VarNominal (Sugar.TId _ tid _) | tid == Builtins.mutTid ->
-                    E.keysEventMap (exportConfig ^. Config.executeKeys)
-                    (E.toDoc (env ^. has) [Texts.execRepl])
-                    (IOTrans.liftIO executeRepl)
-                _ -> mempty
             ] & pure
     where
-        executeRepl = exportReplActions theExportActions & ReplEdit.executeIOProcess
         ExportActions{importAll,exportAll} = theExportActions

test/Test/Lamdu/Gui.hs
@@ -31,7 +31,6 @@ import qualified Lamdu.GUI.Expr as ExpressionEdit
 import qualified Lamdu.GUI.Expr.BinderEdit as BinderEdit
 import qualified Lamdu.GUI.Monad as GuiM
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
-import qualified Lamdu.Sugar.Lens as SugarLens
 import qualified Lamdu.Sugar.Types as Sugar
 
 import           Test.Lamdu.Prelude
@@ -57,19 +56,20 @@ makeGui ::
     String -> Env -> WorkArea -> OnceT (T ViewM) (Responsive (T ViewM))
 makeGui afterDoc env workArea =
     do
-        let repl = workArea ^. Sugar.waRepl . Sugar.replExpr
-        let replExprId = repl ^. SugarLens.binderResultExpr & WidgetIds.fromExprPayload
+        let defId =
+                workArea ^?!
+                Sugar.waPanes . traverse . Sugar.paneBody .
+                Sugar._PaneDefinition . Sugar.drBody . Sugar._DefinitionBodyExpression .
+                Sugar.deContent . annotation
+                & WidgetIds.fromExprPayload
         let assocTagName = DataOps.assocTagName env
         gui <-
-            do
-                replGui <-
-                    GuiM.makeBinder repl
-                    & GuiState.assignCursor WidgetIds.replId replExprId
-                paneGuis <-
-                    workArea ^..
-                    Sugar.waPanes . traverse
-                    & traverse CodeEdit.makePaneBodyEdit
-                Responsive.vbox ?? (replGui : paneGuis)
+            Responsive.vbox <*>
+            ( workArea ^..
+                Sugar.waPanes . traverse
+                & traverse CodeEdit.makePaneBodyEdit
+                & GuiState.assignCursor WidgetIds.defaultCursor defId
+            )
             & GuiM.run assocTagName ExpressionEdit.make BinderEdit.make
                 (Anchors.onGui (Property.mkProperty %~ lift) DbLayout.guiAnchors)
                 env

src/Lamdu/Sugar/Convert/Definition.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE TypeApplications, ScopedTypeVariables #-}
 
 module Lamdu.Sugar.Convert.Definition
-    ( pane, repl
+    ( pane
     ) where
 
 import qualified Control.Lens as Lens
 import           Control.Monad.Once (OnceT, Typeable)
 import           Control.Monad.Reader (ReaderT(..))
 import           Control.Monad.Transaction (MonadTransaction)
-import           Data.CurAndPrev (CurAndPrev(..))
 import qualified Data.Map as Map
 import           Data.Property (Property(Property))
 import qualified Data.Property as Property
@@ -27,13 +26,10 @@ import           Lamdu.Expr.IRef (DefI, HRef)
 import qualified Lamdu.Expr.IRef as ExprIRef
 import qualified Lamdu.Expr.Load as ExprLoad
 import           Lamdu.Sugar.Config (Config)
-import           Lamdu.Sugar.Convert.Binder (convertBinder)
-import           Lamdu.Sugar.Convert.Binder.Params (mkVarInfo)
 import qualified Lamdu.Sugar.Convert.DefExpr as ConvertDefExpr
 import qualified Lamdu.Sugar.Convert.DefExpr.OutdatedDefs as OutdatedDefs
 import qualified Lamdu.Sugar.Convert.Expression as ConvertExpr
 import qualified Lamdu.Sugar.Convert.Input as Input
-import           Lamdu.Sugar.Convert.LightLam (addLightLambdas)
 import qualified Lamdu.Sugar.Convert.Load as Load
 import           Lamdu.Sugar.Convert.Monad (Context(..), ScopeInfo(..), RecursiveRef(..))
 import qualified Lamdu.Sugar.Convert.Monad as ConvertM
@@ -165,54 +161,6 @@ convertDefBody env (Definition.Definition bod defType defI) =
         & lift
     Definition.BodyExpr defExpr ->
         convertInferDefExpr env defType defExpr defI
-
-repl ::
-    ( HasCallStack, Monad m, Typeable m
-    , Has Debug.Monitors env
-    , Has Config env, Has Cache.Functions env
-    , Anchors.HasCodeAnchors env m
-    ) =>
-    env ->
-    OnceT (T m)
-    (Repl EvalPrep InternalName (OnceT (T m)) (T m) ([EntityId], ConvertPayload m))
-repl env =
-    do
-        defExpr <- ExprLoad.defExpr prop & lift
-        entityId <- Property.getP prop & lift <&> (^. Definition.expr) <&> EntityId.ofValI
-        let Load.InferOut valInferred newInferContext =
-                Load.inferDefExpr cachedInfer (env ^. has) defExpr
-                & assertInferSuccess
-        outdatedDefinitions <-
-            OutdatedDefs.scan entityId defExpr (Property.setP prop) postProcess
-            & (`runReaderT` env)
-            & lift
-        let context =
-                Context
-                { _scInferContext = newInferContext
-                , _scConfig = env ^. has
-                , _scCodeAnchors = env ^. Anchors.codeAnchors
-                , _scScopeInfo = emptyScopeInfo Nothing
-                , _scDebugMonitors = env ^. has
-                , _scCacheFunctions = env ^. has
-                , _scTopLevelExpr = valInferred
-                , _scPostProcessRoot = postProcess
-                , _scOutdatedDefinitions = outdatedDefinitions
-                , _scFrozenDeps =
-                    Property (defExpr ^. Definition.exprFrozenDeps) setFrozenDeps
-                , scConvertSubexpression = ConvertExpr.convert
-                }
-        Repl
-            <$> ConvertM.run context
-                (addLightLambdas <*> convertBinder ConvertM.BinderPos valInferred <&> collectHiddenEntityIds valInferred)
-            <*> runReaderT (mkVarInfo (valInferred ^. hAnn . Input.inferredType)) env
-            ?? CurAndPrev Nothing Nothing
-    where
-        cachedInfer = Cache.infer (env ^. has)
-        postProcess = PostProcess.expr cachedInfer (env ^. has) prop
-        prop = Anchors.repl (env ^. Anchors.codeAnchors)
-        setFrozenDeps deps =
-            prop ^. Property.mkProperty
-            >>= (`Property.pureModify` (Definition.exprFrozenDeps .~ deps))
 
 collectHiddenEntityIds ::
     forall h m.

test/Tests/Gui.hs
@@ -56,10 +56,23 @@ test =
     , testChooseTagAndAddNext
     ]
 
-replExpr ::
-    Lens.Traversal' (Sugar.WorkArea v name i o a)
-    (Sugar.Term v name i o # Annotated a)
-replExpr = Sugar.waRepl . Sugar.replExpr . hVal . Sugar.bBody . Sugar._BinderTerm
+defExprs :: Lens.Traversal' (Sugar.WorkArea v name i o a) (Annotated a # Sugar.Assignment v name i o)
+defExprs =
+    Sugar.waPanes . traverse . Sugar.paneBody . Sugar._PaneDefinition .
+    Sugar.drBody . Sugar._DefinitionBodyExpression . Sugar.deContent
+
+defExprPlainBodies :: Lens.Traversal' (Sugar.WorkArea v name i o a) (Sugar.BinderBody v name i o # Annotated a)
+defExprPlainBodies = defExprs . hVal . Sugar._BodyPlain . Sugar.apBody . Sugar.bBody
+
+repl :: Lens.Traversal' (Sugar.WorkArea v name i o a) (Annotated a # Sugar.Term v name i o)
+repl =
+    defExprPlainBodies . Sugar._BinderTerm .
+    -- When migrating to multirepl, repl exprs were placed in fragments
+    -- (to avoid need to infer their type for the simple migration)
+    Sugar._BodyFragment . Sugar.fExpr
+
+replExpr :: Lens.Traversal' (Sugar.WorkArea v name i o a) (Sugar.Term v name i o # Annotated a)
+replExpr = repl . hVal
 
 convertAndMakeGui :: HasCallStack => String -> Env -> OnceT (T ViewM) (Responsive (T ViewM))
 convertAndMakeGui afterDoc env = convertWorkArea afterDoc env >>= makeGui afterDoc env
@@ -137,9 +150,7 @@ testFragmentSize =
     \baseEnv ->
     testProgram "simple-fragment.json" $
     do
-        frag <-
-            fromWorkArea baseEnv
-            (Sugar.waRepl . Sugar.replExpr . annotation)
+        frag <- fromWorkArea baseEnv (repl . annotation)
         let env0 =
                 baseEnv
                 & cursor .~ WidgetIds.fromExprPayload frag
@@ -168,8 +179,8 @@ testOpPrec =
         workArea' <- convertWorkArea "" baseEnv
         unless (workAreaEq workArea workArea') (error "bad operator precedence")
 
-letBody :: Lens.Traversal' (Ann a # Sugar.Binder v n i o) (Ann a # Sugar.Binder v n i o)
-letBody = hVal . Sugar.bBody . Sugar._BinderLet . Sugar.lBody
+letBody :: Lens.Traversal' (Sugar.Binder v n i o # h) (h # Sugar.Binder v n i o)
+letBody = Sugar.bBody . Sugar._BinderLet . Sugar.lBody
 
 binderRec :: Lens.Traversal' (Ann a # Sugar.Binder v n i o) (Sugar.Composite v n i o # Ann a)
 binderRec = hVal . Sugar.bBody . Sugar._BinderTerm . Sugar._BodyRecord
@@ -182,9 +193,8 @@ testPunnedRecordAddField =
     do
         punnedId <-
             fromWorkArea baseEnv
-            ( Sugar.waRepl . Sugar.replExpr . letBody . letBody . binderRec
-            . Sugar.cPunnedItems . traverse . Sugar.pvVar
-            . annotation . Sugar.plEntityId
+            ( defExprs . hVal . Sugar._BodyPlain . Sugar.apBody . letBody . hVal . letBody
+            . binderRec . Sugar.cPunnedItems . traverse . Sugar.pvVar . annotation . Sugar.plEntityId
             )
         baseEnv & cursor .~ WidgetIds.tagHoleId (WidgetIds.fromEntityId punnedId)
             & applyEvent dummyVirt (simpleKeyEvent (noMods GLFW.Key'Comma))
@@ -199,7 +209,7 @@ testRecordPunAndAdd =
     do
         holeId <-
             fromWorkArea baseEnv
-            ( Sugar.waRepl . Sugar.replExpr . letBody . binderRec
+            ( defExprs . hVal . Sugar._BodyPlain . Sugar.apBody . letBody . binderRec
             . Sugar.cList . SugarLens.taggedListItems . Sugar.tiValue
             . annotation . Sugar.plEntityId
             )
@@ -218,7 +228,7 @@ testChooseTagAndAddNext =
     do
         tagId <-
             fromWorkArea baseEnv
-            ( Sugar.waRepl . Sugar.replExpr . binderRec
+            ( defExprs . hVal . Sugar._BodyPlain . Sugar.apBody . Sugar.bBody . Sugar._BinderTerm . Sugar._BodyRecord
             . Sugar.cList . SugarLens.taggedListItems . Sugar.tiTag . Sugar.tagRefTag . Sugar.tagInstance
             )
         baseEnv & cursor .~ WidgetIds.tagHoleId (WidgetIds.fromEntityId tagId)
@@ -250,7 +260,7 @@ testPunCursor =
     & testProgram "rec-with-let.json"
     >>= assertEqual "Item should be punned" (Just 1)
     where
-        waRec = Sugar.waRepl . Sugar.replExpr . letBody . binderRec
+        waRec = defExprPlainBodies . Sugar._BinderLet . Sugar.lBody . binderRec
 
 workAreaEq ::
     Sugar.WorkArea v Name (OnceT (T m)) (T m) (Sugar.Payload v (T m)) ->

commit 70a67cac1dd95dcdec05c23925578bd455d05642

Can execute repl's action by clicking on it's indicator again
(was regression in e9e54b0e37cb)

src/Lamdu/GUI/CodeEdit.hs
@@ -14,9 +14,12 @@ import qualified Control.Lens as Lens
 import           Control.Monad (zipWithM)
 import           Control.Monad.Once (OnceT)
 import           Control.Monad.Trans.Reader (ReaderT)
+import           Control.Monad.Trans.FastWriter (runWriterT)
 import           Control.Monad.Transaction (MonadTransaction(..))
 import           Data.CurAndPrev (CurAndPrev(..))
+import           Data.Functor.Compose (Compose(..))
 import qualified Data.Property as Property
+import           Data.Tuple (swap)
 import qualified GUI.Momentu as M
 import qualified GUI.Momentu.Align as Align
 import qualified GUI.Momentu.Element as Element
@@ -39,6 +42,7 @@ import           Lamdu.Data.Tag (Tag, IsOperator, TextsInLang, getTagName)
 import qualified Lamdu.Eval.Results as EvalResults
 import qualified Lamdu.GUI.CodeEdit.GotoDefinition as GotoDefinition
 import qualified Lamdu.GUI.Definition as DefinitionEdit
+import           Lamdu.GUI.Definition (DefRes, _DefRes)
 import qualified Lamdu.GUI.Expr as ExpressionEdit
 import qualified Lamdu.GUI.Expr.BinderEdit as BinderEdit
 import           Lamdu.GUI.IOTrans (IOTrans(..))
@@ -173,12 +177,12 @@ deleteAndClosePaneEventMap prevId pane defState =
         , has . MomentuTexts.delete
         ])
 
-makePaneBodyEdit :: _ => ExprGui.Top Sugar.Pane i o -> GuiM env i o (Responsive o)
+makePaneBodyEdit :: _ => ExprGui.Top Sugar.Pane i o -> GuiM env i o (Responsive (DefRes o))
 makePaneBodyEdit pane =
     case pane ^. Sugar.paneBody of
-    Sugar.PaneTag tag -> TagPaneEdit.make tag myId <&> Responsive.fromWidget
+    Sugar.PaneTag tag -> TagPaneEdit.make tag myId <&> Responsive.fromWidget <&> Widget.updates %~ lift
     Sugar.PaneDefinition def -> DefinitionEdit.make def myId
-    Sugar.PaneNominal nom -> NominalPane.make nom
+    Sugar.PaneNominal nom -> NominalPane.make nom <&> Widget.updates %~ lift
     where
         myId = pane ^. Sugar.paneEntityId & WidgetIds.fromEntityId
 
@@ -235,10 +239,15 @@ makePaneEdit theExportActions prevId pane =
                 , exportPaneEventMap env theExportActions (pane ^. Sugar.paneBody)
                 ] & mconcat
             paneConfig = env ^. has . Config.pane
-        bodyGui <- makePaneBodyEdit pane
+        bodyGui <-
+            makePaneBodyEdit pane <&> Widget.updates %~
+            IOTrans . Compose . pure . Compose .
+            fmap ((_1 %~ (`when` exec) . (^. Lens._Wrapped)) . swap) .
+            runWriterT . (^. _DefRes)
         case pane ^. Sugar.paneDefinitionState . Property.pVal of
             Sugar.LiveDefinition ->
                 deleteAndClosePaneEventMap prevId pane (pane ^. Sugar.paneDefinitionState)
+                <&> Lens.mapped %~ IOTrans.liftTrans
                 <&> (`Widget.weakerEvents` bodyGui)
             Sugar.DeletedDefinition ->
                 do
@@ -246,6 +255,7 @@ makePaneEdit theExportActions prevId pane =
                         WidgetIds.fromEntityId (pane ^. Sugar.paneEntityId)
                         <$ (pane ^. Sugar.paneDefinitionState . Property.pSet) Sugar.LiveDefinition
                         & undeleteButton <&> Responsive.fromWithTextPos
+                        <&> Widget.updates %~ IOTrans.liftTrans
                     style <- Styled.deletedDef
                     Responsive.vbox ??
                         [ buttonGui
@@ -253,8 +263,11 @@ makePaneEdit theExportActions prevId pane =
                             & Responsive.alignedWidget . M.tValue .> Widget.wFocused %@~ wholeFocused
                             & style
                         ]
-            <&> Widget.updates %~ IOTrans.liftTrans
             <&> Widget.weakerEvents paneEventMap
+    where
+        exec =
+            traverse_ (executeDef theExportActions)
+            (pane ^? Sugar.paneBody . Sugar._PaneDefinition . Sugar.drDefI)
 
 makeNewDefinition ::
     Monad m => Anchors.CodeAnchors m -> GuiM env (OnceT (T m)) (T m) (T m Widget.Id)

src/Lamdu/GUI/Definition.hs
@@ -2,10 +2,12 @@
 
 module Lamdu.GUI.Definition
     ( make
+    , module Lamdu.GUI.Definition.Result
     ) where
 
 import qualified Control.Lens as Lens
 import           Control.Lens.Extended (OneOf)
+import           Control.Monad.Writer (MonadWriter(..))
 import           Data.CurAndPrev (CurPrevTag(..), fallbackToPrev, curPrevTag)
 import           Hyper (annValue)
 import qualified GUI.Momentu as M
@@ -27,6 +29,7 @@ import qualified Lamdu.Config as Config
 import           Lamdu.Config.Theme (Theme)
 import qualified Lamdu.Config.Theme as Theme
 import qualified Lamdu.Config.Theme.TextColors as TextColors
+import           Lamdu.GUI.Definition.Result
 import qualified Lamdu.GUI.Expr.AssignmentEdit as AssignmentEdit
 import qualified Lamdu.GUI.Expr.BuiltinEdit as BuiltinEdit
 import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
@@ -39,6 +42,7 @@ import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import qualified Lamdu.I18N.Code as Texts
 import qualified Lamdu.I18N.CodeUI as Texts
+import qualified Lamdu.I18N.Definitions as Texts
 import qualified Lamdu.I18N.Navigation as Texts
 import           Lamdu.Name (Name(..))
 import qualified Lamdu.Sugar.Types as Sugar
@@ -48,18 +52,26 @@ import           Lamdu.Prelude
 resultWidget ::
     _ =>
     M.WidgetId -> Sugar.VarInfo -> CurPrevTag -> Sugar.EvalCompletionResult o ->
-    m (M.TextWidget o)
+    m (M.TextWidget (DefRes o))
 resultWidget myId varInfo tag res =
     case res of
-    Sugar.EvalSuccess{} ->
+    Sugar.EvalSuccess ->
         do
             view <- makeIndicator tag Theme.successColor "✔"
+            toDoc <- Lens.view has <&> E.toDoc
             case varInfo of
                 Sugar.VarNominal (Sugar.TId _ tid _) | tid == Builtins.mutTid ->
-                    Widget.makeFocusableView ?? myId <&> (M.tValue %~) ?? view
+                    do
+                        actionKeys <- Lens.view (has . Config.actionKeys)
+                        let executeEventMap =
+                                Lens._Wrapped # True & tell & DefRes
+                                & E.keysEventMap actionKeys (toDoc [Texts.execRepl])
+                        Widget.makeFocusableView ?? myId <&> (M.tValue %~) ?? view
+                            <&> M.tValue %~ Widget.weakerEvents executeEventMap
                 _ -> view & M.tValue %~ Widget.fromView & pure
     Sugar.EvalError err ->
         errorIndicator myId tag err
+        <&> M.tValue . Widget.updates %~ lift
 
 indicatorColor :: _ => CurPrevTag -> Lens.ALens' Theme M.Color -> m M.Color
 indicatorColor Current color = Lens.view (has . Lens.cloneLens color)
@@ -130,7 +142,7 @@ makeExprDefinition ::
     Sugar.OptionalTag Name i o ->
     ExprGui.Top Sugar.DefinitionExpression i o ->
     M.WidgetId ->
-    GuiM env i o (Responsive o)
+    GuiM env i o (Responsive (DefRes o))
 makeExprDefinition defName bodyExpr myId =
     case bodyExpr ^. Sugar.deContent . hVal of
     Sugar.BodyPlain x | Lens.has (Sugar.oTag . Sugar.tagRefJumpTo . Lens._Nothing) defName ->
@@ -143,6 +155,7 @@ makeExprDefinition defName bodyExpr myId =
                         nameEdit <- makeNameEdit <&> Responsive.fromWithTextPos
                         AssignmentEdit.makePlainLhs nameEdit (x ^. Sugar.apAddFirstParam)
                             (WidgetIds.fromExprPayload (bodyExpr ^. Sugar.deContent . annotation))
+                    <&> Lens.mapped . Widget.updates %~ lift
                 else
                     do
                         nameEventMap <- TagEdit.makeChooseEventMap pickNameId
@@ -159,6 +172,7 @@ makeExprDefinition defName bodyExpr myId =
             bodyExpr ^. Sugar.deContent & annValue .~ x ^. Sugar.apBody & GuiM.makeBinder
                 & GuiState.assignCursor myId
                     (WidgetIds.fromExprPayload (bodyExpr ^. Sugar.deContent . annotation))
+                <&> Widget.updates %~ lift
                 >>= AssignmentEdit.layout lhs
         where
             indicatorId = Widget.joinId myId ["result indicator"]
@@ -177,6 +191,7 @@ makeExprDefinition defName bodyExpr myId =
             makeNameEdit <&> (|---| fromMaybe M.empty mPresentationEdit) <&> Responsive.fromWithTextPos
                 >>= AssignmentEdit.make nameEditId (bodyExpr ^. Sugar.deContent)
         & GuiState.assignCursor myId nameEditId
+        <&> Widget.updates %~ lift
     where
         makeNameEdit = TagEdit.makeBinderTagEdit TextColors.definitionColor defName
         nameEditId = defName ^. Sugar.oTag . Sugar.tagRefTag . Sugar.tagInstance & WidgetIds.fromEntityId
@@ -203,7 +218,7 @@ make ::
     _ =>
     ExprGui.Top Sugar.Definition i o ->
     M.WidgetId ->
-    GuiM env i o (Responsive o)
+    GuiM env i o (Responsive (DefRes o))
 make def myId =
     do
         env <- Lens.view id
@@ -213,11 +228,13 @@ make def myId =
                 (E.Doc [env ^. has . Texts.gotoNextOutdated])
                 (def ^. Sugar.drGotoNextOutdated
                     <&> foldMap (GuiState.updateCursor . WidgetIds.fromEntityId))
+                <&> lift
         case def ^. Sugar.drBody of
             Sugar.DefinitionBodyExpression bodyExpr ->
                 makeExprDefinition (def ^. Sugar.drName) bodyExpr myId
             Sugar.DefinitionBodyBuiltin builtin ->
                 makeBuiltinDefinition def builtin myId <&> Responsive.fromWithTextPos
+                <&> Widget.updates %~ lift
             <&> M.weakerEvents nextOutdated
     & local (M.animIdPrefix .~ Widget.toAnimId myId)
 

src/Lamdu/GUI/Definition/Result.hs
@@ -1,0 +1,17 @@
+{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving #-}
+
+module Lamdu.GUI.Definition.Result (DefRes(..), _DefRes) where
+
+import qualified Control.Lens as Lens
+import           Control.Monad.Trans.Class (MonadTrans)
+import           Control.Monad.Trans.FastWriter (WriterT)
+import           Data.Monoid (Any)
+
+import           Lamdu.Prelude
+
+-- The results of definition pane can either execute underlying db transactions (edits),
+-- or output to execute the definition (the writer result).
+newtype DefRes m a = DefRes (WriterT Any m a)
+    deriving newtype (Functor, Applicative, Monad, MonadTrans)
+
+Lens.makePrisms ''DefRes

test/Test/Lamdu/Gui.hs
@@ -1,6 +1,7 @@
 module Test.Lamdu.Gui where
 
 import           Control.Monad.Once (OnceT)
+import           Control.Monad.Trans.FastWriter (runWriterT)
 import           Data.List (group, sort)
 import           GHC.Stack (prettyCallStack, callStack)
 import           GUI.Momentu.Responsive (Responsive)
@@ -27,6 +28,7 @@ import qualified Lamdu.Data.Anchors as Anchors
 import qualified Lamdu.Data.Db.Layout as DbLayout
 import qualified Lamdu.Data.Ops as DataOps
 import qualified Lamdu.GUI.CodeEdit as CodeEdit
+import           Lamdu.GUI.Definition.Result (_DefRes)
 import qualified Lamdu.GUI.Expr as ExpressionEdit
 import qualified Lamdu.GUI.Expr.BinderEdit as BinderEdit
 import qualified Lamdu.GUI.Monad as GuiM
@@ -73,6 +75,8 @@ makeGui afterDoc env workArea =
             & GuiM.run assocTagName ExpressionEdit.make BinderEdit.make
                 (Anchors.onGui (Property.mkProperty %~ lift) DbLayout.guiAnchors)
                 env
+            -- We ignore whether execute button was clicked
+            <&> Widget.updates %~ fmap fst . runWriterT . (^. _DefRes)
         if Lens.has wideFocused gui
             then pure gui
             else error ("Red cursor after " ++ afterDoc ++ ": " ++ show (env ^. cursor))