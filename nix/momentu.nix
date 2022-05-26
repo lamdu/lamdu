54d75bf55dc6e89676d716be0380d0ebc4359519
Eyal Lotem <eyal.lotem@gmail.com>
2022-05-26 13:41:06 +0300

    Bump momentu: Yields error message for failed glfwInit

--- nix/momentu.nix
+++ nix/momentu.nix
@@ -11,8 +11,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "momentu";
-    sha256 = "0s26q6rxglsq1gqaxm6xnrfpb0mn9hvf9gxp7c8zr9wc20aq0x76";
-    rev = "1a215fbafab015578ecc34b8d6e21a572f1168c3";
+    sha256 = "0fcrj8pkkggc1qpb5vnmwqfm42m1dqcng2p6hwvgw4hq23vk6qfy";
+    rev = "6edd1bbc59812a4389dd98a34ae8cd27ee529b44";
   };
   libraryHaskellDepends = [
     GLFW-b HUnit OpenGL QuickCheck aeson base base-compat binary bytestring
