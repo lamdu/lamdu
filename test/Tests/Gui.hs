lamdu/lamdu

commit 70348d19695be306abb77eaef926c93aae5709ce

InjectEdit: focus handled correctly

src/Lamdu/GUI/Expr/InjectEdit.hs
@@ -13,10 +13,11 @@ import qualified GUI.Momentu.Responsive.Expression as ResponsiveExpr
 import qualified GUI.Momentu.State as GuiState
 import qualified GUI.Momentu.Widget as Widget
 import qualified Lamdu.Config as Config
+import qualified Lamdu.Config.Theme.TextColors as TextColors
 import qualified Lamdu.GUI.Expr.RecordEdit as RecordEdit
-import qualified Lamdu.GUI.Expr.TagEdit as TagEdit
 import           Lamdu.GUI.Monad (GuiM)
-import           Lamdu.GUI.Styled (text, grammar)
+import           Lamdu.GUI.Styled (text, grammar, withColor)
+import qualified Lamdu.GUI.TagView as TagView
 import qualified Lamdu.GUI.Types as ExprGui
 import qualified Lamdu.GUI.WidgetIds as WidgetIds
 import qualified Lamdu.GUI.Wrap as Wrap
@@ -28,15 +29,21 @@ import qualified Lamdu.Sugar.Types as Sugar
 
 import           Lamdu.Prelude
 
-injectTag :: _ => Sugar.TagRef Name i o -> GuiM env i o (M.WithTextPos (M.Widget o))
-injectTag tag = grammar (text ["injectIndicator"] Texts.injectSymbol) /|/ TagEdit.makeVariantTag tag
+injectTag :: _ => Sugar.TagRef Name i o -> GuiM env i o (M.WithTextPos M.View)
+injectTag tag =
+    grammar (text ["injectIndicator"] Texts.injectSymbol) /|/
+    withColor TextColors.caseTagColor (TagView.make (tag ^. Sugar.tagRefTag))
 
 make :: _ => Annotated (ExprGui.Payload i o) # Const (Sugar.TagRef Name i o) -> GuiM env i o (Responsive o)
 make (Ann (Const pl) (Const tag)) =
-    maybe (pure id) (ResponsiveExpr.addParens ??) (ExprGui.mParensId pl)
-    <*> injectTag tag
-    <&> Responsive.fromWithTextPos
-    & Wrap.stdWrap pl
+    (Widget.makeFocusableWidget ?? myId <&> (Widget.widget %~)) <*>
+    ( maybe (pure id) (ResponsiveExpr.addParens ??) (ExprGui.mParensId pl)
+        <*> (injectTag tag <&> Lens.mapped %~ Widget.fromView)
+        <&> Responsive.fromWithTextPos
+        & Wrap.stdWrap pl
+    )
+    where
+        myId = pl ^. _1 & WidgetIds.fromExprPayload
 
 data NullaryRecord o
     = HiddenNullaryRecord (E.EventMap (o GuiState.Update)) -- enter it
@@ -77,13 +84,17 @@ makeNullary ::
     _ => Annotated (ExprGui.Payload i o) # Sugar.NullaryInject Name i o -> GuiM env i o (Responsive o)
 makeNullary (Ann (Const pl) (Sugar.NullaryInject tag r)) =
     do
+        makeFocusable <- Widget.makeFocusableView
         nullary <- nullaryRecord r
-        rawInjectEdit <- injectTag tag <&> Responsive.fromWithTextPos
+        rawInjectEdit <-
+            injectTag tag <&> M.tValue %~ makeFocusable myId <&> Responsive.fromWithTextPos
 
         widgets <-
                 case nullary of
                 HiddenNullaryRecord enterNullary ->
-                    pure [M.weakerEvents enterNullary rawInjectEdit]
+                    pure
+                    [ rawInjectEdit & Widget.widget %~ M.weakerEvents enterNullary
+                    ]
                 FocusedNullaryRecord w ->
                     leaveSubexpr myId
                     <&> \leaveEventMap ->
@@ -102,8 +113,6 @@ makeNullary (Ann (Const pl) (Sugar.NullaryInject tag r)) =
             ?? ExprGui.mParensId pl
             ?? widgets
             <&> M.weakerEvents valEventMap
-    & GuiState.assignCursor
-        myId (tag ^. Sugar.tagRefTag . Sugar.tagInstance & WidgetIds.fromEntityId)
     & Wrap.stdWrap pl
     where
         myId = pl ^. _1 & WidgetIds.fromExprPayload

commit 36e1c4c29de4cde7446bc0688fd562038d67c6cb

Add failing test to jumping to tag from inject
Broken since 70348d19695be306abb77e, reverting that fixes this (kind-of *1)
but breaks the test added in previous commit.
[1] It fixes a variant of the test where the cursor is at the tag ref, not the expr.

test/Tests/Gui.hs
@@ -8,6 +8,7 @@ import           Data.Containers.ListUtils (nubOrdOn)
 import qualified Data.Property as Property
 import qualified Data.Text as Text
 import           Data.Vector.Vector2 (Vector2(..))
+import           Hyper.Syntax.App (appFunc)
 import qualified GUI.Momentu.Align as Align
 import qualified GUI.Momentu.Element as Element
 import           GUI.Momentu.EventMap (Event(..))
@@ -58,6 +59,7 @@ test =
     , testTwoDeletedDefs
     , testDelArg
     , testRecordIndentClash
+    , testJumpToInjectTag
     ]
 
 defExprs :: Lens.Traversal' (Sugar.WorkArea v name i o a) (Annotated a # Sugar.Assignment v name i o)
@@ -94,6 +96,22 @@ fromWorkArea ::
 fromWorkArea env path =
     convertWorkArea "" env
     <&> (^?! Lens.cloneTraversal path)
+
+testJumpToInjectTag :: Test
+testJumpToInjectTag =
+    testCase "jump-to-inject-tag" $
+    Env.make >>=
+    \baseEnv ->
+    do
+        injId <-
+            fromWorkArea baseEnv
+            ( defExprs . hVal . Sugar._BodyPlain . Sugar.apBody . Sugar.bBody . Sugar._BinderTerm
+            . Sugar._BodySimpleApply . appFunc . hAnn . Lens._Wrapped . Sugar.plEntityId
+            )
+        baseEnv & cursor .~ WidgetIds.fromEntityId injId
+            & applyEvent dummyVirt (simpleKeyEvent (noMods GLFW.Key'Enter))
+            & void
+    & testProgram "inject.json"
 
 testTwoDeletedDefs :: Test
 testTwoDeletedDefs =