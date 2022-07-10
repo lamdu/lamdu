lamdu/lamdu

commit 97ebc008ab8789311e4f93bb824a9f2e674efc86

IfEdit: More robust mechanism to determine when to use one-liner layout

src/Lamdu/GUI/Expr/ParamsEdit.hs
@@ -76,7 +76,14 @@ scopeCursor mChosenScope scopes =
 addScopeEdit :: _ => m (Maybe (M.Widget o) -> Responsive o -> Responsive o)
 addScopeEdit =
     Glue.mkGlue ?? Glue.Vertical
-    <&> (\(|---|) mScopeEdit -> (|---| maybe M.empty (M.WithTextPos 0) mScopeEdit))
+    <&>
+    (\(|---|) ->
+        \case
+        Nothing -> id
+        Just scopeEdit ->
+            (Responsive.rWide . Responsive.lForm .~ Responsive.WideOneLiner) .
+            (|---|) (M.WithTextPos 0 scopeEdit)
+    )
 
 mkLhsEdits :: _ => m (Responsive o -> Maybe (M.Widget o) -> [Responsive o])
 mkLhsEdits =

nix/momentu.nix
@@ -11,8 +11,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "momentu";
-    sha256 = "1iwf4zlvj4azcv20fx101b3mghlmawp6ck0ykhy5xik4fwqf73ns";
-    rev = "75211f85b3cc992c2c1e83c8a243f684ec6d98c2";
+    sha256 = "19f87vqdbv1s1kr30yv58sca9x45zd7qq4yx2174b6nya2hvxhp7";
+    rev = "4ea4e09c8be8be6b6d0a5d22a4e49753d3a1e642";
   };
   libraryHaskellDepends = [
     GLFW-b HUnit OpenGL QuickCheck aeson base base-compat binary bytestring

src/Lamdu/GUI/Expr/IfElseEdit.hs
@@ -145,8 +145,9 @@ make (Ann (Const pl) ifElse) =
         res <- renderRows (ExprGui.mParensId pl) ?? rows
         case rows of
             [if_, else_]
-                | Lens.has (Sugar.iThen . hVal . noLet) ifElse
-                && Lens.has (Sugar.iElse . hVal . Sugar._SimpleElse . noLet) ifElse ->
+                | (if_ ^.. traverse <> else_ ^.. traverse)
+                    ^. traverse . Responsive.rWide . Responsive.lForm
+                    == Responsive.WideOneLiner ->
                 do
                     frame <- addValFrame
                     hbox <-
@@ -161,6 +162,3 @@ make (Ann (Const pl) ifElse) =
     & stdWrapParentExpr pl
     where
         animId = WidgetIds.fromExprPayload pl & Widget.toAnimId
-        noLet =
-            Sugar._BodyLeaf . Lens.united <>
-            Sugar._BodyLam . Sugar.lamFunc . Sugar.fBody . hVal . Sugar.bBody . Sugar._BinderTerm . Lens.united

stack.yaml
@@ -16,7 +16,7 @@ extra-deps:
 - github: lamdu/lamdu-calculus
   commit: 6fe1f9d89d584eb173c10f851a819865824fb5ee
 - github: lamdu/momentu
-  commit: 75211f85b3cc992c2c1e83c8a243f684ec6d98c2
+  commit: 4ea4e09c8be8be6b6d0a5d22a4e49753d3a1e642
 - github: lamdu/hypertypes
   commit: 9a28e3863b0e2525f38ce7b6efabbc056d3145b2
 - github: lamdu/graphics-drawingcombinators

commit 35d9e45158cac3c3d4c7a3229ec1f209a086f8d3

Oops fix layout regression for scope edits in 97ebc008ab878

src/Lamdu/GUI/Expr/ParamsEdit.hs
@@ -82,7 +82,7 @@ addScopeEdit =
         Nothing -> id
         Just scopeEdit ->
             (Responsive.rWide . Responsive.lForm .~ Responsive.WideOneLiner) .
-            (|---|) (M.WithTextPos 0 scopeEdit)
+            ((|---|) ?? M.WithTextPos 0 scopeEdit)
     )
 
 mkLhsEdits :: _ => m (Responsive o -> Maybe (M.Widget o) -> [Responsive o])