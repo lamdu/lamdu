b55f7db9d459ebbe3a54a7671ff3578981e422bc
Yair Chuchem <yairchu@gmail.com>
2023-02-18 14:27:00 +0200

    Fix Lamdu build on macOS ARM native
    Note that I broke the nix build because I wasted an hour on trying to get nix or just nix-hash to install here but couldn't..
    Even tried running nix via docker, failed nix-env -i nix-prefetch-github in there as well..
    Next person to try maintain nix build please maintain it.

--- nix/rocksdb-haskell.nix
+++ nix/rocksdb-haskell.nix
@@ -7,8 +7,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "lamdu";
     repo = "rocksdb-haskell";
-    sha256 = "0gl4c32yr4qws25b9pxp9pcx3iy7x1b6g6cf6jd1ajs5rb4nfsr7";
-    rev = "b0016acefbb38b1fbe148163f226f4cae15c03c7";
+    sha256 = "TODO";
+    rev = "5bac865400c67e7bab0c219ec7327284cb87e348";
   };
   postPatch = ''
     rm Setup.hs
