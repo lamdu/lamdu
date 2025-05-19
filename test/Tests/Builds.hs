3e62db2ffa29b26c87fdf7d0f074d066ff940c24
Yair Chuchem <yairchu@gmail.com>
2025-05-19 22:13:59 +0300

    Fix tests for nix build consistency
    (following previous commit changing the nix build)
    
    Now that the nix build isn't duplicating the stack configuration,
    we only need to check the GHC version setting in its new position.

--- test/Tests/Builds.hs
+++ test/Tests/Builds.hs
@@ -15,127 +15,17 @@
 module Tests.Builds (test) where
 
 import qualified Control.Lens as Lens
-import qualified Data.Aeson.Lens as LensAeson
-import           Data.List (isInfixOf, isPrefixOf)
 import           Data.List.Split (splitOn)
-import qualified Data.Set as Set
-import qualified Data.Text as Text
-import qualified Data.Yaml as Yaml
-import           System.Directory (listDirectory)
 
 import           Test.Lamdu.Prelude
 
--- TODO: Consider using Cabal & nix format parsers
--- instead of quick & dirty string manipulations
-
 test :: TestTree
-test = testGroup "Builds" [stackDepsTest, cabalDepsTest, nixGhcVersionTest, ciGhcVersionTest]
-
-stackDepsTest :: TestTree
-stackDepsTest =
-    do
-        stackYaml <-
-            Yaml.decodeFileEither "stack.yaml"
-            >>= either (fail . Yaml.prettyPrintParseException) pure
-            :: IO Yaml.Value
-        let deps =
-                stackYaml ^..
-                LensAeson.key "extra-deps" . LensAeson.values . Lens.filteredBy (LensAeson.key "github")
-        traverse_ verifyStackDep deps
-        let expectedNixFiles =
-                (deps ^.. traverse .
-                    Lens.failing (LensAeson.key "subdirs" . LensAeson._Array . traverse) (LensAeson.key "github") .
-                    LensAeson._String . Lens.to Text.unpack
-                    <&> packageNameFromGitUrl
-                    <&> (<> ".nix")
-                ) <> extraNixFiles
-        nixFiles <- listDirectory "nix"
-        assertSetEquals "Nix files" (Set.fromList expectedNixFiles) (Set.fromList nixFiles)
-        & testCase "verify-stack"
-    where
-        extraNixFiles =
-            [ "lamdu.nix" -- Top level nix file (implicit in stack)
-            ]
-
-verifyStackDep :: Yaml.Value -> IO ()
-verifyStackDep dep =
-    do
-        github <- getKey "github"
-        commit <- getKey "commit"
-        let packageName =
-                dep ^? LensAeson.key "subdirs" . LensAeson._Array . traverse . LensAeson._String
-                & maybe (packageNameFromGitUrl github) Text.unpack
-        do
-            nixFile <- readFile ("nix/" <> packageName <> ".nix")
-            let nixCommit = splitOn "rev = \"" nixFile !! 1 & takeWhile (/= '"')
-            when (nixCommit /= commit)
-                (assertString ("Nix revision for dependency " <> packageName <> " mismatches stack"))
-    where
-        getKey key =
-            dep ^? LensAeson.key key . LensAeson._String
-            & maybe (fail ("stack.yaml dependency with no " <> show key <> "?")) (pure . Text.unpack)
-
-packageNameFromGitUrl :: String -> String
-packageNameFromGitUrl = last . splitOn "/"
-
-cabalDepsTest :: TestTree
-cabalDepsTest =
-    do
-        cabalDeps <- readFile "Lamdu.cabal" <&> parseCabalDeps
-        nixFile <- readFile "nix/lamdu.nix"
-        let nixFindDeps name =
-                nixFile
-                & splitOn (name <> " = [")
-                & (^?! Lens.ix 1)
-                & splitOn "];"
-                & (^?! Lens.ix 0)
-                & words
-                & Set.fromList
-        let nixHaskellDeps =
-                nixFindDeps "libraryHaskellDepends"
-                <> nixFindDeps "executableHaskellDepends"
-                <> nixFindDeps "testHaskellDepends"
-        let nixVars =
-                tail nixFile
-                & takeWhile (/= '}')
-                & words
-                <&> takeWhile (/= ',')
-                & filter (/= "")
-                & Set.fromList
-        checkMissing cabalDeps nixHaskellDeps "Missing nix deps"
-        checkMissing nixHaskellDeps nixVars "Undeclared nix vars"
-        checkMissing nixHaskellDeps cabalDeps "Unneccesary nix deps"
-    & testCase "verify-cabal"
-
-checkMissing :: Set String -> Set String -> String -> IO ()
-checkMissing whole part msg
-    | Set.null diff = pure ()
-    | otherwise = assertString (msg <> ": " <> show diff)
-    where
-        diff = Set.difference whole part
-
-parseCabalDeps :: String -> Set String
-parseCabalDeps cabal =
-    scanl processLine (False, mempty) (lines cabal) ^. traverse . _2
-    where
-        processLine (isDeps, _) line
-            | buildDepsPrefix `isPrefixOf` line =
-                (True, parseDeps (drop (length buildDepsPrefix) line))
-            | ":" `isInfixOf` line || " if " `isInfixOf` line = (False, mempty)
-            | isDeps = (True, parseDeps line)
-            | otherwise = (False, mempty)
-        parseDeps line =
-            splitOn "," line
-            <&> dropWhile (== ' ') <&> takeWhile (/= ' ')
-            & filter (`notElem` ["", "Lamdu"])
-            & Set.fromList
-        -- Specifically check for one indentation level, to avoid conditional "build-depends: ekg".
-        buildDepsPrefix = "  build-depends:"
+test = testGroup "Builds" [nixGhcVersionTest, ciGhcVersionTest]
 
 nixGhcVersionTest :: TestTree
 nixGhcVersionTest =
     do
-        nixFile <- readFile "default.nix"
+        nixFile <- readFile "flake.nix"
         let nixGhcVer = read (take 2 (splitOn "ghc" nixFile ^?! Lens.ix 1))
         assertEqual "ghc version" ghcVer nixGhcVer
     & testCase "nix-ghc-version"
